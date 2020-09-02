a0f227d6c445a7a44a9b206d62b2e40182505b77
Habib Kadiri <kadirit@hotmail.com>
2020-09-02 21:23:48 +0200

    added server-side rendering of material-ui css

--- src/app/pages/_app.js
+++ src/app/pages/_app.js
@@ -1,20 +1,29 @@
 // import App from 'next/app'
-import { Fragment } from 'react';
+import {Fragment} from 'react';
 import 'semantic/dist/semantic.min.css';
 import 'styles.css';
 import FirebaseContext from 'data/firebase/FirebaseContext';
 import Firebase from 'data/firebase/Firebase';
 import * as Sentry from '@sentry/browser';
-import { ThemeProvider } from '@material-ui/core/styles';
+import {ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 
 
 import Head from 'next/head';
 import {theme} from "../materialUI";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
 
     Sentry.init({dsn: "https://6852108b71ce4fbab24839792f82fa90@sentry.io/4261031"});
+
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
 
     return (
         <Fragment>
@@ -22,6 +31,8 @@ function MyApp({ Component, pageProps }) {
                 <title>CareerFairy | Watch live streams. Get hired.</title>
             </Head>
             <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline/>
                 <FirebaseContext.Provider value={new Firebase()}>
                     <Component {...pageProps} />
                 </FirebaseContext.Provider>
@@ -29,7 +40,7 @@ function MyApp({ Component, pageProps }) {
         </Fragment>
     );
 }
-  
+
 // Only uncomment this method if you have blocking data requirements for
 // every single page in your application. This disables the ability to
 // perform automatic static optimization, causing every page in your app to
@@ -41,5 +52,5 @@ function MyApp({ Component, pageProps }) {
 //
 //   return { ...appProps }
 // }
-  
+
 export default MyApp
