38d314a4b1b8c0ed8694b8cbd0b08428b85a8c04
Habib Kadiri <kadirit@hotmail.com>
2020-09-11 16:35:12 +0200

    added animated nav for admin page

--- src/app/components/views/group/create/DisplayCategoryElement.js
+++ src/app/components/views/group/create/DisplayCategoryElement.js
@@ -1,67 +1,60 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import {withFirebase} from 'data/firebase';
-import {makeStyles} from "@material-ui/core/styles";
-import {Box, Chip} from "@material-ui/core";
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { withFirebase } from "data/firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Chip } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-
-    whiteBox: {
-        backgroundColor: "white",
-        borderRadius: "5px",
-        padding: "20px",
-        margin: "10px 0",
-        textAlign: "left",
-        display: "flex"
-    },
-    label: {
-        fontSize: "0.8em",
-        fontWeight: "700",
-        color: "rgb(160,160,160)",
-        margin: "0 0 5px 0",
-    },
-    title: {
-        fontSize: "1.2em",
-        fontWeight: "700",
-        color: "rgb(80,80,80)",
-    },
-    chip: {
-        margin: theme.spacing(0.5),
-        border: "1px solid black"
-    }
+  whiteBox: {
+    backgroundColor: "white",
+    borderRadius: "5px",
+    padding: "20px",
+    margin: "10px 0",
+    textAlign: "left",
+    display: "flex",
+  },
+  label: {
+    fontSize: "0.8em",
+    fontWeight: "700",
+    color: "rgb(160,160,160)",
+    margin: "0 0 5px 0",
+  },
+  title: {
+    fontSize: "1.2em",
+    fontWeight: "700",
+    color: "rgb(80,80,80)",
+  },
+  chip: {
+    margin: theme.spacing(0.5),
+  },
 }));
 
-const DisplayCategoryElement = ({category}) => {
+const DisplayCategoryElement = ({ category }) => {
+  const classes = useStyles();
 
-    const classes = useStyles()
-
-    const optionElements = category.options.map((option, index) => {
-        return (
-            <Chip key={option.id || index}
-                  label={option.name}
-                  className={classes.chip}
-                // color="primary"
-                  variant="outlined"
-            />
-        );
-    });
-
-
+  const optionElements = category.options.map((option, index) => {
     return (
-        <Paper className={classes.whiteBox}>
-            <Box flex="0.3">
-                <div className={classes.label}>Category Name</div>
-                <div className='white-box-title'>
-                    {category.name}
-                </div>
-            </Box>
-            <Box flex="0.7">
-                <div className={classes.label}>Category Options</div>
-                {optionElements}
-            </Box>
-        </Paper>
+      <Chip
+        key={option.id || index}
+        label={option.name}
+        className={classes.chip}
+        color="primary"
+      />
     );
+  });
 
-}
+  return (
+    <Paper className={classes.whiteBox}>
+      <Box flex="0.3">
+        <div className={classes.label}>Category Name</div>
+        <div className="white-box-title">{category.name}</div>
+      </Box>
+      <Box flex="0.7">
+        <div className={classes.label}>Category Options</div>
+        {optionElements}
+      </Box>
+    </Paper>
+  );
+};
 
 export default withFirebase(DisplayCategoryElement);
--- src/app/pages/group/[groupId]/admin.js
+++ src/app/pages/group/[groupId]/admin.js
@@ -1,119 +1,120 @@
-import React, {useEffect, useState} from 'react'
-import {Container, Button, Image, Menu} from 'semantic-ui-react';
-import {useRouter} from 'next/router';
-import Header from '../../../components/views/header/Header';
-import Head from 'next/head';
-import Footer from '../../../components/views/footer/Footer';
+import React, { useEffect, useState } from "react";
+import { Container, Button, Image, Menu } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import Header from "../../../components/views/header/Header";
+import Head from "next/head";
+import Footer from "../../../components/views/footer/Footer";
 import AdminHeader from "../../../components/views/group/admin/AdminHeader";
 import GroupNav from "../../../components/views/group/admin/GroupNav";
-import {withFirebase} from "../../../data/firebase";
-
+import { withFirebase } from "../../../data/firebase";
 
 const JoinGroup = (props) => {
+  const router = useRouter();
+  const groupId = router.query.groupId;
 
+  const [user, setUser] = useState(null);
 
-    const router = useRouter();
-    const groupId = router.query.groupId;
-    console.log("groupId", groupId)
+  const [userData, setUserData] = useState(null);
 
-    const [user, setUser] = useState(null);
+  const [group, setGroup] = useState({});
 
-    const [userData, setUserData] = useState(null);
+  const [menuItem, setMenuItem] = useState("settings");
 
+  useEffect(() => {
+    props.firebase.auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        router.replace("/login");
+      }
+    });
+  }, []);
 
-    const [group, setGroup] = useState({});
-
-
-    const [menuItem, setMenuItem] = useState("settings")
-
-    useEffect(() => {
-        props.firebase.auth.onAuthStateChanged(user => {
-            if (user) {
-                setUser(user);
-            } else {
-                router.replace('/login');
-            }
-        })
-    }, []);
-
-    useEffect(() => {
-        if (groupId) {
-            const unsubscribe = props.firebase.listenToCareerCenterById(groupId, querySnapshot => {
-                let careerCenter = querySnapshot.data();
-                careerCenter.id = querySnapshot.id;
-                setGroup(careerCenter);
-            })
-            return () => unsubscribe()
+  useEffect(() => {
+    if (groupId) {
+      const unsubscribe = props.firebase.listenToCareerCenterById(
+        groupId,
+        (querySnapshot) => {
+          let careerCenter = querySnapshot.data();
+          careerCenter.id = querySnapshot.id;
+          setGroup(careerCenter);
         }
-    }, [groupId]);
+      );
+      return () => unsubscribe();
+    }
+  }, [groupId]);
 
+  return (
+    <div className="greyBackground">
+      <Head>
+        <title key="title">CareerFairy | Join Groups</title>
+      </Head>
+      <Header classElement="relative white-background" />
+      <AdminHeader group={group} menuItem={menuItem} />
+      <GroupNav
+        group={group}
+        groupId={groupId}
+        userData={userData}
+        user={user}
+      />
+      <Footer />
+      <style jsx>{`
+        .hidden {
+          display: none;
+        }
 
-    return (
-        <div className='greyBackground'>
-            <Head>
-                <title key="title">CareerFairy | Join Groups</title>
-            </Head>
-            <Header classElement='relative white-background'/>
-            <AdminHeader group={group} menuItem={menuItem}/>
-            <GroupNav group={group} groupId={groupId} userData={userData} user={user}/>
-            <Footer/>
-            <style jsx>{`
-                    .hidden {
-                        display: none;
-                    }
+        .greyBackground {
+          background-color: rgb(250, 250, 250);
+          height: 100%;
+          min-height: 100vh;
+        }
 
-                    .greyBackground {
-                        background-color: rgb(250,250,250);
-                        height: 100%;
-                        min-height: 100vh;
-                    }
+        .white-box {
+          padding: 10px;
+          margin: 10px 0 10px 0;
+          text-align: left;
+        }
 
-                    .white-box {
-                        padding: 10px;
-                        margin: 10px 0 10px 0;
-                        text-align: left;
-                    }
-                    
-                    .title-container {
-                      display: flex;
-                      align-items: center;
-                    }
+        .title-container {
+          display: flex;
+          align-items: center;
+        }
 
-                    .image-container {
-                        position: relative;
-                        width: 100%;
-                        padding-top: 95%;
-                        border-radius: 50%;
-                        border: 5px solid rgb(0, 210, 170);
-                        background-color: white;
-                        margin: 0 auto;
-                        box-shadow: 0 0 5px rgb(200,200,200);
-                    }
+        .image-container {
+          position: relative;
+          width: 100%;
+          padding-top: 95%;
+          border-radius: 50%;
+          border: 5px solid rgb(0, 210, 170);
+          background-color: white;
+          margin: 0 auto;
+          box-shadow: 0 0 5px rgb(200, 200, 200);
+        }
 
-                    .field-error {
-                        margin-top: 10px;
-                        color: red;
-                    }
+        .field-error {
+          margin-top: 10px;
+          color: red;
+        }
 
-                    .join-group-title {
-                        text-align: left;
-                        margin: 0 0 30px 0;
-                        font-weight: 700;
-                        font-size: 1.3em;
-                        color: rgb(80,80,80);
-                    }
-                    
-                    .sublabel {
-                        margin: 40px 0 15px 0;
-                        text-align: center;
-                    }
+        .join-group-title {
+          text-align: left;
+          margin: 0 0 30px 0;
+          font-weight: 700;
+          font-size: 1.3em;
+          color: rgb(80, 80, 80);
+        }
 
-                    #profileContainer {
-                        padding: 30px 0;
-                    }
-                `}</style>
-        </div>
-    );
+        .sublabel {
+          margin: 40px 0 15px 0;
+          text-align: center;
+        }
+
+        #profileContainer {
+          padding: 30px 0;
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default withFirebase(JoinGroup);
