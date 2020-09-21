77cd7f0a54dc9659b868b83f2efa251221012131
Maximilian Voss <maximilian@careerfairy.io>
2020-09-21 14:18:23 +0200

    fix bugs

--- src/app/pages/upcoming-livestream/[livestreamId].js
+++ src/app/pages/upcoming-livestream/[livestreamId].js
@@ -82,7 +82,7 @@ function UpcomingLivestream(props) {
 
     useEffect(() => {
         if (currentLivestream) {
-            props.firebase.getDetailLivestreamCareerCentersc(currentLivestream.universities).then( querySnapshot => {
+            props.firebase.getDetailLivestreamCareerCenters(currentLivestream.universities).then( querySnapshot => {
                 let groupList = [];
                 querySnapshot.forEach(doc => {
                     let group = doc.data();
