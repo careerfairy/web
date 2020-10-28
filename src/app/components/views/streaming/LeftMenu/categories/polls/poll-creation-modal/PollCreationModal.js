d1a14e3824744cf37ceb6327c8747875b617208d
Habib Kadiri <kadirit@hotmail.com>
2020-10-28 13:58:44 +0100

    materialized creat PollCreationModal.js

--- src/app/components/views/streaming/LeftMenu/categories/polls/poll-creation-modal/PollCreationModal.js
+++ src/app/components/views/streaming/LeftMenu/categories/polls/poll-creation-modal/PollCreationModal.js
@@ -1,26 +1,23 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, {useState, useEffect} from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import {withFirebase} from 'context/firebase';
-import {Input, Icon, Modal} from 'semantic-ui-react';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import {
     DialogTitle,
     DialogContent,
     Dialog,
     Typography,
-    ButtonGroup,
     Button,
     Paper,
     Fab,
     Box
 } from '@material-ui/core';
-import {BarChart, CloseRounded} from "@material-ui/icons";
+import {BarChart} from "@material-ui/icons";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import DialogActions from "@material-ui/core/DialogActions";
 import TextField from "@material-ui/core/TextField";
 import Grow from "@material-ui/core/Grow";
 import {makeStyles} from "@material-ui/core/styles";
-import Collapse from "@material-ui/core/Collapse";
 
 const useStyles = makeStyles(theme => ({
     modalPaper: {
@@ -99,7 +96,7 @@ function PollCreationModal({open, handleClose, livestreamId, initialOptions, ini
     const optionElements = options.map((option, index) => {
 
             return (
-                <Collapse in>
+                <Grow in>
                     <TextField value={option} error={(option.trim() === '') && (error)}
                                label={`Option ${index + 1}`}
                                helperText={(option.trim() === '') && (error) && "Please fill or remove"}
@@ -116,85 +113,45 @@ function PollCreationModal({open, handleClose, livestreamId, initialOptions, ini
                                    </Box>
                                } : {}}
                                fullWidth/>
-                </Collapse>
+                </Grow>
             );
         }
         )
     ;
 
     return (
-        <Fragment>
-            <Dialog PaperProps={{className: classes.dialog}} maxWidth="sm" fullWidth open={open} onClose={handleClose}>
-                <DialogTitle disableTypography
-                             style={{display: "flex", justifyContent: "center", alignItems: "flex-end"}} align="center">
-                    <BarChart style={{color: "white"}} fontSize="large"/> <Typography
-                    style={{fontSize: "1.8em", fontWeight: 500, color: "white"}} variant="h3">Create a
-                    Poll</Typography>
-                </DialogTitle>
-                <DialogContent>
-                    <Paper className={classes.modalPaper}>
-                            <TextField
-                                label="Your Question"
-                                value={question}
-                                fullWidth
-                                variant="outlined"
-                                onChange={({currentTarget: {value}}) => setQuestion(value)}
-                                placeholder='Write down your question or poll to your audience'
-                            />
-                        {optionElements}
-                    </Paper>
-                    <Button startIcon={<AddIcon/>} variant="contained" color="secondary"
-                            style={{marginTop: "1rem", border: "2px solid white"}}
-                            children='Add an Option' onClick={increaseNumberOfOptions}
-                            disabled={options.length > 3}/>
-                    <DialogActions>
-                        <Button children='Cancel' style={{border: "2px solid white"}} variant="contained" onClick={handleClose}/>
-                        <Button startIcon={loading && <CircularProgress size={20} color="inherit"/>} disabled={loading}
-                                children={initialPoll ? 'Update Poll' : 'Create Poll'} color="primary"
-                                variant="contained" onClick={savePoll} style={{border: "2px solid white"}}/>
-                    </DialogActions>
-                </DialogContent>
-            </Dialog>
-            <style jsx>{`
-                .modal-title {
-                    font-size: 1.8em;
-                    text-align: center;
-                    font-weight: 500;
-                    color: rgb(80,80,80);
-                    margin: 0 0 20px 0;
-                }
-
-                .modal-content {
-                    width: 60%;
-                    margin: 0 auto;
-                    padding: 0 0 20px 0;
-                }
-
-                .modal-content-question {
-                    margin: 20px 0;
-                }
-
-                .modal-content-options {
-                    margin: 10px 0 20px 0;
-                }
-
-                .modal-content-option {
-                    margin: 5px 0 10px 0;
-                }
-
-                .modal-content .label {
-                    font-weight: 700;
-                    margin-bottom: 3px;
-                    color: rgb(80,80,80);
-                }
-
-                .modal-content .label.teal {
-                    font-size: 1.3em;
-                    color: rgb(0, 210, 170);
-                    margin-bottom: 5px;
-                }
-          `}</style>
-        </Fragment>
+        <Dialog PaperProps={{className: classes.dialog}} maxWidth="sm" fullWidth open={open} onClose={handleClose}>
+            <DialogTitle disableTypography
+                         style={{display: "flex", justifyContent: "center", alignItems: "flex-end"}} align="center">
+                <BarChart style={{color: "white"}} fontSize="large"/> <Typography
+                style={{fontSize: "1.8em", fontWeight: 500, color: "white"}} variant="h3">Create a
+                Poll</Typography>
+            </DialogTitle>
+            <DialogContent style={{padding: "0 24px 8px 24px"}}>
+                <Paper className={classes.modalPaper}>
+                    <TextField
+                        label="Your Question"
+                        value={question}
+                        fullWidth
+                        variant="outlined"
+                        onChange={({currentTarget: {value}}) => setQuestion(value)}
+                        placeholder='Write down your question or poll to your audience'
+                    />
+                    {optionElements}
+                </Paper>
+                <Button startIcon={<AddIcon/>} variant="contained" color="secondary"
+                        style={{marginTop: "1rem", border: "2px solid white"}}
+                        children='Add an Option' onClick={increaseNumberOfOptions}
+                        disabled={options.length > 3}/>
+                <DialogActions>
+                    <Button children='Cancel' style={{border: "2px solid white"}} variant="contained"
+                            onClick={handleClose}/>
+                    <Button startIcon={loading && <CircularProgress size={20} color="inherit"/>} disabled={loading}
+                            children={initialPoll ? 'Update Poll' : 'Create Poll'} color="primary"
+                            variant="contained" onClick={savePoll} style={{border: "2px solid white"}}/>
+                </DialogActions>
+            </DialogContent>
+        </Dialog>
     );
 }
 
