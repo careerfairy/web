92c251a38d6a0d694172b4197e3627127213760a
Habib Kadiri <kadirit@hotmail.com>
2020-11-04 16:40:15 +0100

    removed conditional

--- src/app/components/views/streaming/modal/DemoEndModal.js
+++ src/app/components/views/streaming/modal/DemoEndModal.js
@@ -8,15 +8,28 @@ import DialogActions from "@material-ui/core/DialogActions";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import TutorialContext from "../../../../context/tutorials/TutorialContext";
 import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import Grow from "@material-ui/core/Grow";
 
+
+const pdfLink = "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/company-documents%2FExampleReport.pdf?alt=media&token=be44d8be-d914-4074-9197-77d5ab830719"
 const DemoEndModal = ({firebase, livestreamId, open, handleClose}) => {
 
     const {tutorialSteps, setTutorialSteps} = useContext(TutorialContext);
     const [loading, setLoading] = useState(false)
     const [activePage, setActivePage] = useState(0)
+    const [hasClickedDownload, setHasClickedDownload] = useState(false)
 
     const handleNext = () => {
         setActivePage(prevState => prevState + 1)
+    }
+
+    const handleComplete = () => {
+        handleNext()
+        setTutorialSteps({
+            ...tutorialSteps,
+            showBubbles: true
+        })
     }
 
     const pages = [
@@ -46,26 +59,126 @@ const DemoEndModal = ({firebase, livestreamId, open, handleClose}) => {
             </DialogTitle>
             <DialogContent>
                 <DialogContentText>
-                    During the stream, at any time, your viewer are able
-                    willingly give their details by clicking on the button
-                    Join Talent pool below (this will button will be present on their UI).
+                    Once your stream has ended, you will be able to
+                    to download analytics of the stream, giving you vital information like
+                    like how many student registered, what they study and their universities,
+                    to name a few
                 </DialogContentText>
                 <Box fullWidth m={2}>
                     <Button
+                        download
+                        href={pdfLink}
                         onClick={handleNext}
-                        children={'Join Talent Pool'}
-                        variant="contained"
-                        startIcon={<PeopleAltIcon/>}
+                        children={'Download Analytics'}
+                        startIcon={<CloudDownloadIcon/>}
                         color={"primary"}/>
                 </Box>
             </DialogContent>
+            <DialogActions>
+                <Button
+                    download
+                    disabled={!hasClickedDownload}
+                    variant="contained"
+                    onClick={handleComplete}
+                    children={'Next'}
+                    color={"primary"}/>
+            </DialogActions>
         </>,
-
+        <>
+            <DialogTitle>
+                Congratulations!!
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    You've now completed the tutorial :).
+                    We believe you're now ready to venture into the world of streaming!
+                    Good luck!
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    disabled={!hasClickedDownload}
+                    variant="contained"
+                    onClick={handleClose}
+                    children={'Back to Stream'}
+                    color={"primary"}/>
+            </DialogActions>
+        </>
     ]
 
     return (
         <Dialog open={open}>
-            {pages[page]}
+            <Grow in={activePage === 0}>
+                <DialogTitle>
+                    What makes us different
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        During the stream, at any time, your viewer are able
+                        willingly give their details by clicking on the button
+                        Join Talent pool below (this will button will be present on their UI).
+                    </DialogContentText>
+                    <Box fullWidth m={2}>
+                        <Button
+                            onClick={handleNext}
+                            children={'Join Talent Pool'}
+                            variant="contained"
+                            startIcon={<PeopleAltIcon/>}
+                            color={"primary"}/>
+                    </Box>
+                </DialogContent>
+            </Grow>
+            <Grow in={activePage === 1}>
+                <DialogTitle>
+                    What makes us different
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Once your stream has ended, you will be able to
+                        to download analytics of the stream, giving you vital information like
+                        like how many student registered, what they study and their universities,
+                        to name a few
+                    </DialogContentText>
+                    <Box fullWidth m={2}>
+                        <Button
+                            download
+                            href={pdfLink}
+                            onClick={handleNext}
+                            children={'Download Analytics'}
+                            startIcon={<CloudDownloadIcon/>}
+                            color={"primary"}/>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        download
+                        disabled={!hasClickedDownload}
+                        variant="contained"
+                        onClick={handleComplete}
+                        children={'Next'}
+                        color={"primary"}/>
+                </DialogActions>
+            </Grow>
+            <Grow in={activePage === 2}>
+                <DialogTitle>
+                    Congratulations!!
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        You've now completed the tutorial :).
+                        We believe you're now ready to venture into the world of streaming!
+                        Good luck!
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        disabled={!hasClickedDownload}
+                        variant="contained"
+                        onClick={handleClose}
+                        children={'Back to Stream'}
+                        color={"primary"}/>
+                </DialogActions>
+            </Grow>
         </Dialog>
     );
 };
