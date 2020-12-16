61fa7a0e7e5b72f7c3104da17e5d5f32b0edb6dc
habib <Rutgers94>
2020-12-16 10:23:09 +0100

    added stream cloud functions

--- src/functions/index.js
+++ src/functions/index.js
@@ -26,13 +26,14 @@ dotenv.config();
 
 const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID
 const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_API_KEY
-const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME
-
+const ALGOLIA_GROUP_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME
+const ALGOLIA_STREAM_INDEX_NAME = process.env.ALGOLIA_STREAM_INDEX_NAME
 
 
 // configure algolia
 const algolia = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY);
-const index = algolia.initIndex(ALGOLIA_INDEX_NAME);
+const groupIndex = algolia.initIndex(ALGOLIA_GROUP_INDEX_NAME);
+const streamIndex = algolia.initIndex(ALGOLIA_STREAM_INDEX_NAME);
 
 
 admin.initializeApp({
@@ -87,7 +88,7 @@ exports.addToIndex = functions.firestore.document('careerCenterData/{careerCente
         data.groupId = objectID
         // deletes personal Identifiable data
         delete data.adminEmail
-        return index.saveObject({...data, objectID})
+        return groupIndex.saveObject({...data, objectID})
             .then(() => {
                 functions.logger.log('Groups imported into Algolia');
             })
@@ -97,6 +98,25 @@ exports.addToIndex = functions.firestore.document('careerCenterData/{careerCente
 
     })
 
+exports.addToStreamIndex = functions.firestore.document('livestreams/{livestream}')
+    .onCreate(snapshot => {
+        const data = snapshot.data();
+        if (data.test === false) { // dont add test streams to algolia
+            const objectID = snapshot.id
+            data.numberOfRegistered = data.registeredUsers?.length || 0
+
+            // deletes personal Identifiable data
+            delete data.registeredUsers
+            return streamIndex.saveObject({...data, objectID})
+                .then(() => {
+                    functions.logger.log('Stream imported into Algolia');
+                })
+                .catch(error => {
+                    functions.logger.warn('Error when importing stream into Algolia', error);
+                });
+        }
+    })
+
 exports.updateIndex = functions.firestore.document('careerCenterData/{careerCenter}')
     .onUpdate(change => {
         const newData = change.after.data();
@@ -104,8 +124,7 @@ exports.updateIndex = functions.firestore.document('careerCenterData/{careerCent
         delete newData.adminEmail
 
         const objectID = change.after.id;
-        console.log("-> index", index);
-        return index.saveObject({...newData, objectID})
+        return groupIndex.saveObject({...newData, objectID})
             .then(() => {
                 functions.logger.log('Groups updated into Algolia');
             })
@@ -114,8 +133,32 @@ exports.updateIndex = functions.firestore.document('careerCenterData/{careerCent
             })
     })
 
+exports.updateStreamIndex = functions.firestore.document('livestreams/{livestream}')
+    .onUpdate(change => {
+        const newData = change.after.data();
+        if (newData.test === false) { // dont add test streams to algolia
+
+            newData.numberOfRegistered = newData.registeredUsers?.length || 0
+            // deletes personal Identifiable data
+            delete newData.registeredUsers
+
+            const objectID = change.after.id;
+            return streamIndex.saveObject({...newData, objectID})
+                .then(() => {
+                    functions.logger.log('Stream updated into Algolia');
+                })
+                .catch(error => {
+                    functions.logger.warn('Error when importing stream into Algolia', error);
+                })
+
+        }
+    })
+
 exports.deleteFromIndex = functions.firestore.document('careerCenterData/{careerCenter}')
-    .onDelete(snapshot => index.deleteObject(snapshot.id))
+    .onDelete(snapshot => groupIndex.deleteObject(snapshot.id))
+
+exports.deleteStreamFromIndex = functions.firestore.document('livestreams/{livestream}')
+    .onDelete(snapshot => streamIndex.deleteObject(snapshot.id))
 
 
 // Http functions
