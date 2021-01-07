677bdea7e2257b2916c520bcb4e37503f6915be9
habib <Rutgers94>
2021-01-07 18:34:37 +0100

    - drafted up graph goals

--- src/app/pages/group/[groupId]/admin/analytics.js
+++ src/app/pages/group/[groupId]/admin/analytics.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import GroupDashboardLayout from "../../../../layouts/GroupDashboardLayout";
 import Page from "../../../../components/page";
-import Typography from "@material-ui/core/Typography";
+import AnalyticsOverview from "../../../../components/views/group/admin/analytics";
 
 const AnalyticsPage = ({group, firebase}) => {
 
     return (
         <Page title="CareerFairy | Admin Upcoming Streams">
-        <Typography variant="h4">
-            Analytics
-        </Typography>
+            <AnalyticsOverview
+                group={group}
+                firebase={firebase}
+            />
         </Page>
     );
 };
