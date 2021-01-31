a322a64dc04c7dca1abe063d19b158897c1ef73b
Maximilian Voss <maximilian@careerfairy.io>
2021-01-31 18:53:07 +0100

    method for email sendout

--- src/functions/index.js
+++ src/functions/index.js
@@ -718,6 +718,58 @@ exports.sendReminderEmailToRegistrants = functions.https.onRequest(async (req, r
     })
 });
 
+exports.sendEmailToStudentOfUniversityAndField = functions.https.onRequest(async (req, res) => {
+
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        // Send response to OPTIONS requests
+        res.set('Access-Control-Allow-Methods', 'GET');
+        res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.set('Access-Control-Max-Age', '3600');
+        return res.status(204).send('');
+    }
+
+    const recipientsGroupsAndCategories = req.body.recipientsGroupsAndCategories;
+    console.log(req.body);
+    const groups = Object.keys(recipientsGroupsAndCategories);
+
+    console.log(groups)
+    let recipients = new Set();
+    let snapshot = await admin.firestore().collection("userData").where("groupIds", "array-contains-any", groups).get();
+    console.log(snapshot.size)
+    snapshot.forEach( doc => {
+        let student = doc.data()
+        groups.forEach( group => {
+            let registeredGroup = student.registeredGroups.find( registeredGroup => registeredGroup.groupId === group)
+            if (registeredGroup) {
+                let categoryId = recipientsGroupsAndCategories[group].categoryId;
+                let selectedOptions = recipientsGroupsAndCategories[group].selectedOptions;
+                let registeredCategory = registeredGroup.categories.find( category => category.id === categoryId);
+                if (selectedOptions.includes(registeredCategory.selectedValueId)) {
+                    recipients.add(student.userEmail);
+                }
+            }
+        })
+        // recipients.forEach( recipient => {
+        //     const email = {
+        //         "TemplateId": XXXXXXX,
+        //         "From": 'CareerFairy <noreply@careerfairy.io>',
+        //         "To": recipient,
+        //         "TemplateModel": {}
+        //     };
+        //     client.sendEmailWithTemplate(email).then(response => {
+        //         console.log(`Successfully sent email to ${recipient_email}`);
+        //         res.sendStatus(200);
+        //     }, error => {
+        //         console.error(`Error sending email to ${recipient_email}`, error);
+        //         res.sendStatus(500);
+        //     });
+        // })
+    })
+});
+
 exports.getXirsysNtsToken = functions.https.onRequest(async (req, res) => {
 
     const https = require('https');
