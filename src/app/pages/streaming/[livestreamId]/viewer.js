d4080cff896bd043e2e893350c0765c7566448ee
habib <Rutgers94>
2021-03-25 13:01:35 +0100

    fixed new stream creation bug add no translate meta tag to head of viewer page

--- src/app/pages/streaming/[livestreamId]/viewer.js
+++ src/app/pages/streaming/[livestreamId]/viewer.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ViewerLayout from "../../../layouts/ViewerLayout";
 import ViewerOverview from "../../../components/views/viewer";
+import Head from "next/head";
+
 
 const ViewerPage = () => {
-    return(
-        <ViewerLayout>
-            <ViewerOverview/>
-        </ViewerLayout>
+    return (
+        <React.Fragment>
+            <Head>
+                <title>CareerFairy | Watch live streams. Get hired.</title>
+                <meta name="google" content="notranslate"/>
+            </Head>
+            <ViewerLayout>
+                <ViewerOverview/>
+            </ViewerLayout>
+        </React.Fragment>
     )
 }
 
