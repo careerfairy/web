a0510109e013d577736faa30bcd7c16c6696ce4d
habib <Rutgers94>
2021-03-26 17:33:29 +0100

    replaced filter component with Masonry

--- src/app/components/views/admin/Statistics/FilterComponent/index.js
+++ src/app/components/views/admin/Statistics/FilterComponent/index.js
@@ -1,46 +1,50 @@
-import PropTypes from 'prop-types'
 import React from 'react';
-import {makeStyles} from "@material-ui/core/styles";
-import {Grid} from "@material-ui/core";
+import {makeStyles, useTheme} from "@material-ui/core/styles";
+import {Box, Grid} from "@material-ui/core";
 import GroupAddButton from "./GroupAddButton";
 import {useDispatch, useSelector} from "react-redux";
 import FilterCard from "./FilterCard";
 import * as actions from '../../../../../store/actions'
 import {isLoaded} from "react-redux-firebase";
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+
 const useStyles = makeStyles(theme => ({
     root: {
         width: "100%"
     },
-    item:{
-    }
+    item: {}
 }));
 
 const FilterComponent = () => {
     const dispatch = useDispatch()
     const classes = useStyles()
     const filters = useSelector(state => state.currentFilterGroup.data.filters || [])
+    const theme = useTheme()
 
-    const groupsLoaded = useSelector(({firestore:{data:{careerCenterData}}}) => isLoaded(careerCenterData))
+    const groupsLoaded = useSelector(({firestore: {data: {careerCenterData}}}) => isLoaded(careerCenterData))
     const handleRemoveGroupFromFilters = (targetGroupId) => {
         const newFilters = filters.map(({groupId}) => groupId).filter(groupId => groupId !== targetGroupId)
         dispatch(actions.setFilters(newFilters))
     }
 
     return (
-        <Grid alignContent="center" alignItems="center" container spacing={2} className={classes.root}>
-            {filters.map(filter => (
-                <Grid className={classes.item} xs={12} md={6} lg={4}  key={filter.groupId} item>
-                    <FilterCard  groupsLoaded={groupsLoaded} handleRemoveGroupFromFilters={handleRemoveGroupFromFilters} key={filter.groupId} filter={filter}/>
-                </Grid>
-            ))}
-            <Grid item>
+        <ResponsiveMasonry
+            columnsCountBreakPoints={{350: 1, 800: 2, 1280: 2, 1450: 3}}
+
+        >
+            <Masonry gutter={theme.spacing(2)}>
+                {filters.map(filter => (
+                    <FilterCard groupsLoaded={groupsLoaded} handleRemoveGroupFromFilters={handleRemoveGroupFromFilters}
+                                key={filter.groupId} filter={filter}/>
+                ))}
+                <Box display="flex" justifyContent="center">
                 <GroupAddButton/>
-            </Grid>
-        </Grid>
-    );
+                </Box>
+            </Masonry>
+        </ResponsiveMasonry>
+    )
 };
 
-FilterComponent.propTypes = {
-}
+FilterComponent.propTypes = {}
 export default FilterComponent;
 
