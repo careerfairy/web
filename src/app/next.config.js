ff3329ea254bf1288b33277f08fe0de8830eca85
Maximilian Voss <maximilian@careerfairy.io>
2021-05-12 16:27:22 +0200

    don't log config

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -28,7 +28,6 @@ module.exports = (phase, {defaultConfig}) => {
     //     config.distDir = '../../dist/client'
     // }
     /* config options for all phases except development here */
-    console.log("config", config)
     return withBundleAnalyzer(config)
 }
 
