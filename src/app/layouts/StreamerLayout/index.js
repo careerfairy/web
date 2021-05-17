ac493201e8b3d60b98f751018781e60cffb5f00f
Maximilian Voss <maximilian@careerfairy.io>
2021-05-17 14:20:53 +0200

    show unmute overlay for streamers

--- src/app/layouts/StreamerLayout/index.js
+++ src/app/layouts/StreamerLayout/index.js
@@ -82,6 +82,9 @@ const StreamerLayout = (props) => {
     const [selectedState, setSelectedState] = useState("questions");
     const [sliding, setSliding] = useState(false);
 
+    const [showVideoButton, setShowVideoButton] = useState({paused: false, muted: false});
+    const [play, setPlay] = useState(false);
+    const [unmute, setUnmute] = useState(true);
 
     const handleSetNumberOfViewers = useCallback((number) => setNumberOfViewers(number), [])
     const isMainStreamer = useMemo(() => pathname === "/streaming/[livestreamId]/main-streamer", [pathname])
@@ -175,6 +178,19 @@ const StreamerLayout = (props) => {
         setAudienceDrawerOpen(false)
     }, []);
 
+    const unmuteVideos = useCallback(() => {
+        setShowVideoButton(prevState => {
+            return {paused: prevState.paused, muted: false}
+        });
+        setUnmute(true);
+    }, [])
+
+    const playVideos = useCallback(() => {
+        setShowVideoButton(prevState => {
+            return {paused: false, muted: false}
+        });
+        setPlay(true);
+    }, [])
 
     const handleStateChange = useCallback((state) => {
         if (!showMenu) {
@@ -237,6 +253,12 @@ const StreamerLayout = (props) => {
                             <div className={classes.content}>
                                 {React.cloneElement(children, {
                                     ...props,
+                                    playVideos,
+                                    unmuteVideos,
+                                    showVideoButton,
+                                    setShowVideoButton,
+                                    play,
+                                    unmute,
                                     newNotification,
                                     isMainStreamer,
                                     isStreamer: true,
