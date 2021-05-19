3a82e132a3a5f162437958e4cf7401212ef9407d
Maximilian Voss <maximilian@careerfairy.io>
2021-05-19 22:17:46 +0200

    rearrange streamer order when many streamers

--- src/app/components/views/streaming/video-container/CurrentSpeakerDisplayer.js
+++ src/app/components/views/streaming/video-container/CurrentSpeakerDisplayer.js
@@ -175,17 +175,39 @@ function CurrentSpeakerDisplayer(props) {
         }
     }
 
-    let externalVideoElements = props.streams.filter(stream => !stream.streamId.includes("screen")).map((stream, index) => {
-        const videoClass = getVideoContainerClass(stream.streamId, "external");
-        return (
-            <div key={stream.streamId} className={classes[videoClass]}
-                 style={{padding: 0}}>
-                <RemoteVideoContainer {...props} isPlayMode={props.isPlayMode} muted={props.muted} stream={stream}
-                                      height={getVideoContainerHeight(stream.streamId)}
-                                      small={videoClass.includes("QuarterWidth")} index={index}/>
-            </div>
-        );
-    });
+    let externalVideoElements = []
+
+    if (props.streams.length > 4) {
+        let currentSpeakerStream = props.streams.find( stream => stream.streamId === props.currentSpeaker )
+        let rearrangedVideoStreams = props.streams.filter( stream => stream.streamId !== props.currentSpeaker )
+
+        if (currentSpeakerStream) {
+            rearrangedVideoStreams.unshift(currentSpeakerStream);
+        }
+        externalVideoElements = rearrangedVideoStreams.filter(stream => !stream.streamId.includes("screen")).map((stream, index) => {
+            const videoClass = getVideoContainerClass(stream.streamId, "external");
+            return (
+                <div key={stream.streamId} className={classes[videoClass]}
+                     style={{padding: 0}}>
+                    <RemoteVideoContainer {...props} isPlayMode={props.isPlayMode} muted={props.muted} stream={stream}
+                                          height={getVideoContainerHeight(stream.streamId)}
+                                          small={videoClass.includes("QuarterWidth")} index={index}/>
+                </div>
+            );
+        });
+    } else {
+        externalVideoElements = props.streams.filter(stream => !stream.streamId.includes("screen")).map((stream, index) => {
+            const videoClass = getVideoContainerClass(stream.streamId, "external");
+            return (
+                <div key={stream.streamId} className={classes[videoClass]}
+                     style={{padding: 0}}>
+                    <RemoteVideoContainer {...props} isPlayMode={props.isPlayMode} muted={props.muted} stream={stream}
+                                          height={getVideoContainerHeight(stream.streamId)}
+                                          small={videoClass.includes("QuarterWidth")} index={index}/>
+                </div>
+            );
+        });
+    }
 
     if (!props.isPlayMode) {
         const localVideoClass = getVideoContainerClass(props.localId, "local");
@@ -201,13 +223,6 @@ function CurrentSpeakerDisplayer(props) {
     }
 
     //TO BE TESTED
-    if (externalVideoElements.length > 4) {
-        let currentSpeakerElement = externalVideoElements.find( stream => stream.streamId === props.currentSpeaker )
-        let rearrangedVideoElements = externalVideoElements.filter( stream => stream.streamId !== props.currentSpeaker )
-
-        rearrangedVideoElements.unshift(currentSpeakerElement);
-        externalVideoElements = rearrangedVideoElements
-    }
 
     return (
         <div className={classes.relativeContainer}>
