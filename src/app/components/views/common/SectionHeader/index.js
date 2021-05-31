cbc25d7e58d01f531f2048d92d25a8597572694e
habib <habib@careerfairy.io>
2021-05-31 09:02:41 +0200

    added comment

--- src/app/components/views/common/SectionHeader/index.js
+++ src/app/components/views/common/SectionHeader/index.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles(theme => ({
         display: "inline-block"
     }
 }));
-
+//
 function SectionHeader(props) {
     const classes = useStyles({
         color: props.color,
