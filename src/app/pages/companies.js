7d7fc679d59c79c38dcb4adbebfb60c3a5eadf8b
habib <habib@careerfairy.io>
2021-06-11 13:18:37 +0200

    fixed images styling on upcoming page

--- src/app/pages/companies.js
+++ src/app/pages/companies.js
@@ -1,158 +1,86 @@
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useTheme } from "@material-ui/core/styles";
+import LandingLayout from "../layouts/LandingLayout";
+import BookADemoSection from "../components/views/landing/BookADemoSection";
+import TestimonialsSection from "../components/views/landing/TestimonialsSection";
+import AnalyticsSection from "../components/views/landing/AnalyticsSection";
+import StreamSection from "../components/views/landing/StreamSection";
+import UniversitySection from "../components/views/landing/UniversitySection";
+import BenefitsSection from "../components/views/landing/BenefitsSection";
+import CompaniesSection from "../components/views/landing/CompaniesSection";
+import HeroSection from "../components/views/landing/HeroSection";
+import CalendlyModal from "../components/views/landing/CalendlyModal";
+import ScrollToTop from "../components/views/common/ScrollToTop";
+import { rectangle1 } from "../constants/images";
 
-import Header from "../components/views/header/Header";
-import Footer from "../components/views/footer/Footer";
-import { withFirebase } from "../context/firebase";
-import ElementTagList from "../components/views/common/ElementTagList";
+const CompaniesLandingPage = ({}) => {
+  const {
+    palette: { secondary, common, grey },
+  } = useTheme();
 
-import Loader from "../components/views/loader/Loader";
-import { useRouter } from "next/router";
-import { Button, Container, Grid, Typography } from "@material-ui/core";
-import GeneralLayout from "../layouts/GeneralLayout";
+  const [calendlyModalOpen, setCalendlyModalOpen] = useState(false);
 
-function CompaniesOverview(props) {
-   const router = useRouter();
+  const handleOpenCalendly = () => {
+    setCalendlyModalOpen(true);
+  };
 
-   const [companies, setCompanies] = useState([]);
-   const [loading, setLoading] = useState(false);
+  const handleCloseCalendly = () => setCalendlyModalOpen(false);
 
-   useEffect(() => {
-      setLoading(true);
-      props.firebase.getCompaniesWithProfile().then((querySnapshot) => {
-         var companyList = [];
-         querySnapshot.forEach((doc) => {
-            let company = doc.data();
-            company.id = doc.id;
-            companyList.push(company);
-         });
-         setCompanies(companyList);
-         setLoading(false);
-      });
-   }, []);
+  return (
+    <LandingLayout>
+      <HeroSection
+        title={
+          <b>Boost student & employer interactions with our career live streams</b>
+        }
+        big
+        handleOpenCalendly={handleOpenCalendly}
+      />
+      <CompaniesSection />
+      <BenefitsSection title={"Why CareerFairy?"} />
+      <UniversitySection
+        subtitle="The best talent is evenly distributed, which makes it
+            hard to reach. With CareerFairy live streams,
+            you can reach students at multiple top universities in a
+            single one hour-long event. No travel, no logistics, no days off work."
+        title="Some universities that we work with"
+      />
+      <StreamSection
+        title={
+          <>
+            Showcase your best ambassadors <b>- your employees.</b>
+          </>
+        }
+        subtitle="We believe that your employees are your biggest asset, and their insights provide
+                an authentic look into the opportunities that your company has to offer."
+      />
+      <AnalyticsSection
+        title={
+          <>
+            Boost your <b>employer brand</b>, measure the results with{" "}
+            <b>data</b>
+          </>
+        }
+        backgroundImage={rectangle1}
+        subtitle="Evaluate the success of your events, gather live feedback from your audience and follow up easily with interesting candidates"
+      />
+      <TestimonialsSection
+        title="What They Are Saying"
+        backgroundColor={grey["200"]}
+      />
+      <BookADemoSection
+        backgroundColor={`linear-gradient(-8deg, ${secondary.main} 1%, ${secondary.light} 100%)`}
+        color={common.white}
+        big
+        handleOpenCalendly={handleOpenCalendly}
+        title={"Join the ranks of leading organisations today"}
+      />
+      <CalendlyModal
+        open={calendlyModalOpen}
+        onClose={handleCloseCalendly}
+      />
+      <ScrollToTop />
+    </LandingLayout>
+  );
+};
 
-   function goToCompany(companyId) {
-      router.push(`/company?id=${companyId}`, `/company/${companyId}`);
-   }
-
-   const companyElements = companies.map((company) => {
-      return (
-         <Grid key={company.companyId} item xs={12} sm={4}>
-            <div className="companies-container-element">
-               <div className="companies-container-image">
-                  <img
-                     id="companies-container-image-tag"
-                     src={company.logoUrl}
-                     style={{
-                        margin: 0,
-                        position: "absolute",
-                        top: "50%",
-                        maxHeight: "100%",
-                        maxWidth: "60%",
-                        height: "auto",
-                        width: "auto",
-                        transform: "translateY(-50%)",
-                     }}
-                  />
-               </div>
-               <Typography
-                  id="companies-company-name"
-                  variant="h3"
-                  style={{ marginBottom: 20 }}
-               >
-                  {company.name}
-               </Typography>
-               <p>{company.industry}</p>
-               <p className="hiring-label">hiring from</p>
-               <ElementTagList fields={company.fieldsHiring} />
-               <Button
-                  style={{
-                     position: "absolute",
-                     bottom: "10px",
-                     left: "0",
-                     right: "0",
-                     width: "94%",
-                     margin: "0 auto",
-                  }}
-                  variant="contained"
-                  onClick={() => goToCompany(company.id)}
-                  color="primary"
-               >
-                  Discover {company.name}
-               </Button>
-            </div>
-            <style jsx>{`
-               .companies-container-element {
-                  position: relative;
-                  margin-bottom: 0;
-                  padding: 20px 40px 60px 40px;
-                  height: 100%;
-                  border-radius: 5px;
-                  cursor: pointer;
-                  background-color: white;
-                  transition: all 0.2s ease-in-out;
-                  -webkit-transition: all 0.2s ease-in-out;
-                  box-shadow: 0 0 10px rgb(220, 220, 220);
-               }
-
-               .companies-container-element p {
-                  color: grey;
-               }
-
-               .companies-container-image {
-                  height: 100px;
-                  position: relative;
-                  margin: 20px 0;
-               }
-
-               .hiring-label {
-                  font-size: 1em;
-                  text-transform: uppercase;
-                  color: rgb(150, 150, 150);
-               }
-
-               #discover-company {
-                  position: absolute;
-                  bottom: 10px;
-                  left: 0;
-                  right: 0;
-                  width: 94%;
-                  margin: 0 auto;
-               }
-            `}</style>
-         </Grid>
-      );
-   });
-
-   if (loading === false) {
-      return (
-         <>
-            <GeneralLayout>
-               <div className="companies-container">
-                  <Container>
-                     <Grid container spacing={3}>
-                        {companyElements}
-                     </Grid>
-                  </Container>
-               </div>
-               <style jsx>{`
-                  .companies-background {
-                     background-color: rgb(248, 248, 248);
-                  }
-
-                  .companies-container {
-                     margin: 3% auto;
-                  }
-
-                  #companies-company-name {
-                     margin-top: 5px;
-                  }
-               `}</style>
-            </GeneralLayout>
-         </>
-      );
-   } else {
-      return <Loader />;
-   }
-}
-
-export default withFirebase(CompaniesOverview);
+export default CompaniesLandingPage;
