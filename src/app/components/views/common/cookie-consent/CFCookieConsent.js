252f17779bdbc361a4d1209575a47d1bd0a81b5e
Maximilian Voss <maximilian@careerfairy.io>
2021-06-15 22:01:16 +0200

    update imports

--- src/app/components/views/common/cookie-consent/CFCookieConsent.js
+++ src/app/components/views/common/cookie-consent/CFCookieConsent.js
@@ -51,7 +51,7 @@ const CFCookieConsent = () => {
             style={cookieConsentStyle}
         >
             <Typography className={classes.title}>CareerFairy uses cookies.</Typography>
-            <p>We use cookies to personalize content and analyze our traffic in order to offer you a better user experience and understand where you are joining us from. You cannot disable the necessary cookies that we need to get the website to run, but you can disable the cookies we use to gather statistics. You can find details about this in our <Link href='/privacy'><a className={classes.link}>data protection notice</a></Link></p>.
+            <p>We use cookies to personalize content and analyze our traffic in order to offer you a better user experience and understand where you are joining us from. You cannot disable the necessary cookies that we need to get the website to run, but you can disable the cookies we use to gather statistics. You can find details about this in our <Link href='/privacy'><a className={classes.link}>data protection notice</a></Link>.</p>
         </CookieConsent>
     )
 }
