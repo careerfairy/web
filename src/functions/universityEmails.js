fc391d1005bb1a203b99a582fe8f9dfc17e4d398
Maximilian Voss <maximilian@careerfairy.io>
2021-06-15 11:53:10 +0200

    new emails to retain for uni wien

--- src/functions/universityEmails.js
+++ src/functions/universityEmails.js
@@ -9,38 +9,38 @@ const {setHeaders} = require("./util");
 exports.sendEmailToStudentOfUniversityAndField = functions.https.onRequest(async (req, res) => {
     setHeaders(req, res)
 
-    const recipientsGroupsAndCategories = req.body.recipientsGroupsAndCategories;
+    // const recipientsGroupsAndCategories = req.body.recipientsGroupsAndCategories;
     console.log(req.body);
-    const groups = Object.keys(recipientsGroupsAndCategories);
+    const groups = req.body.groups
 
     console.log(groups)
     let recipients = new Set();
 
-    const emailsToRemove = ["christine.kaiser@hr.ethz.ch", "evelyne.kappel@hr.ethz.ch", "ekappel@gmx.ch,", "franziska.liese@hr.ethz.ch", "anja.pauling@hr.ethz.ch", "lorena.coletti@hr.ethz.ch",  "stephan.burkart@fhnw.ch", "sibylle.graf@fhnw.ch", "csc.business@fhnw.ch",  "wels@zhaw.ch", "spib@zhaw.ch",  "birgit-helga.mueller@unibas.ch", "n.astitouh@unibas.ch",
+    const emailsToRemove = ["christina.habetler@tucareer.com", "michaela.unger@tucareer.com", "susanne.leeb@tucareer.com", "julia.hainitz@tucareer.com", "christine.kaiser@hr.ethz.ch", "evelyne.kappel@hr.ethz.ch", "ekappel@gmx.ch,", "franziska.liese@hr.ethz.ch", "anja.pauling@hr.ethz.ch", "lorena.coletti@hr.ethz.ch",  "stephan.burkart@fhnw.ch", "sibylle.graf@fhnw.ch", "csc.business@fhnw.ch",  "wels@zhaw.ch", "spib@zhaw.ch",  "birgit-helga.mueller@unibas.ch", "n.astitouh@unibas.ch",
         "john.imonopi@lernende.ethz.ch", "michael.grunder@hr.ethz.ch", "daniela.gunz@uzh.ch", "roger.gfroerer@careerservices.uzh.ch", "sabine.demerle@epfl.ch", "cc@epfl.ch", "regina.cardoso@epfl.ch", "philippe.ory@epfl.ch"]
 
     let snapshot = await admin.firestore().collection("userData").where("groupIds", "array-contains-any", groups).get();
     console.log(snapshot.size)
     snapshot.forEach(doc => {
         let student = doc.data()
-        groups.forEach(group => {
-            let registeredGroup = student.registeredGroups.find(registeredGroup => registeredGroup.groupId === group)
-            if (registeredGroup) {
-                let categoryId = recipientsGroupsAndCategories[group].categoryId;
-                let selectedOptions = recipientsGroupsAndCategories[group].selectedOptions;
-                let registeredCategory = registeredGroup.categories.find(category => category.id === categoryId);
-                if (selectedOptions.includes(registeredCategory.selectedValueId) && !emailsToRemove.includes(student.userEmail) && !student.unsubscribed) {
+        // groups.forEach(group => {
+        //     let registeredGroup = student.registeredGroups.find(registeredGroup => registeredGroup.groupId === group)
+        //     if (registeredGroup) {
+        //         let categoryId = recipientsGroupsAndCategories[group].categoryId;
+        //         let selectedOptions = recipientsGroupsAndCategories[group].selectedOptions;
+        //         let registeredCategory = registeredGroup.categories.find(category => category.id === categoryId);
+                 if (!emailsToRemove.includes(student.userEmail) && !student.unsubscribed) {
                     recipients.add(student.userEmail);
-                }
-            }
-        })
+                 }
+        //     }
+        // })
     })
     console.log(recipients.size)
     let testEmails = ["maximilian@careerfairy.io"]
     let templates = []
     recipients.forEach(recipient => {
         const email = {
-            "TemplateId": 23147847,
+            "TemplateId": 22748248,
             "From": 'CareerFairy <noreply@careerfairy.io>',
             "To": recipient,
             "TemplateModel": {
