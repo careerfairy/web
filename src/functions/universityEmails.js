b079b37d44b5ec705330672603a9c5316863d5a2
Maximilian Voss <maximilian@careerfairy.io>
2021-07-19 08:50:56 +0200

    new university admin emails to filter out

--- src/functions/universityEmails.js
+++ src/functions/universityEmails.js
@@ -9,14 +9,14 @@ const {setHeaders} = require("./util");
 exports.sendEmailToStudentOfUniversityAndField = functions.https.onRequest(async (req, res) => {
     setHeaders(req, res)
 
-    const recipientsGroupsAndCategories = req.body.recipientsGroupsAndCategories;
-    console.log(req.body);
-    const groups = Object.keys(recipientsGroupsAndCategories)
+    // const recipientsGroupsAndCategories = req.body.recipientsGroupsAndCategories;
+    // console.log(req.body);
+    const groups = req.body.groups;
 
     console.log(groups)
     let recipients = new Set();
 
-    const emailsToRemove = ["christina.habetler@tucareer.com", "michaela.unger@tucareer.com", "susanne.leeb@tucareer.com", "julia.hainitz@tucareer.com", "christine.kaiser@hr.ethz.ch", "evelyne.kappel@hr.ethz.ch", "ekappel@gmx.ch,", "franziska.liese@hr.ethz.ch", "anja.pauling@hr.ethz.ch", "lorena.coletti@hr.ethz.ch",  "stephan.burkart@fhnw.ch", "sibylle.graf@fhnw.ch", "csc.business@fhnw.ch",  "wels@zhaw.ch", "spib@zhaw.ch",  "birgit-helga.mueller@unibas.ch", "n.astitouh@unibas.ch",
+    const emailsToRemove = ["christina.habetler@tucareer.com", "michaela.unger@tucareer.com", "susanne.leeb@tucareer.com", "julia.hainitz@tucareer.com", "christine.kaiser@hr.ethz.ch", "evelyne.kappel@hr.ethz.ch", "ekappel@gmx.ch,", "franziska.liese@hr.ethz.ch", "anja.pauling@hr.ethz.ch", "lorena.coletti@hr.ethz.ch",  "stephan.burkart@fhnw.ch", "sibylle.graf@fhnw.ch", "csc.business@fhnw.ch",  "wels@zhaw.ch", "spib@zhaw.ch",  "birgit-helga.mueller@unibas.ch", "n.astitouh@unibas.ch", " anja.robert@zhv.rwth-aachen.de", "neslihan.congar@zhv.rwth-aachen.de", "s.potstada@tu-braunschweig.de", "reiner.laue@verwaltung.uni-stuttgart.de", "thao.luu@tu-dresden.de", "cecilia.czambor@tu-dresden.de", "jazzny@gmx.de",
         "john.imonopi@lernende.ethz.ch", "michael.grunder@hr.ethz.ch", "daniela.gunz@uzh.ch", "roger.gfroerer@careerservices.uzh.ch", "sabine.demerle@epfl.ch", "cc@epfl.ch", "regina.cardoso@epfl.ch", "philippe.ory@epfl.ch", "nadine.leemann@unisg.ch", "gerd.winandi-martin@unisg.ch", "maxim.polikarpov@telejob.ch", "sandro.luh@telejob.ch", "konrad.jakubowski@telejob.ch", "reiner.laue@verwaltung.uni-stuttgart.de", "s.potstada@tu-braunschweig.de", "Anja.Robert@zhv.rwth-aachen.de", "Neslihan.Congar@zhv.rwth-aachen.de", "kathrin.neumann@uni-graz.at", "melanie.koeppel@uni-graz.at", "angela.messner-lipp@uni-graz.at"]
 
     let snapshot = await admin.firestore().collection("userData").where("groupIds", "array-contains-any", groups).get();
@@ -24,15 +24,15 @@ exports.sendEmailToStudentOfUniversityAndField = functions.https.onRequest(async
     snapshot.forEach(doc => {
         let student = doc.data()
         groups.forEach(group => {
-            let registeredGroup = student.registeredGroups.find(registeredGroup => registeredGroup.groupId === group)
-            if (registeredGroup) {
-                let categoryId = recipientsGroupsAndCategories[group].categoryId;
-                let selectedOptions = recipientsGroupsAndCategories[group].selectedOptions;
-                let registeredCategory = registeredGroup.categories.find(category => category.id === categoryId);
-                 if (selectedOptions.includes(registeredCategory.selectedValueId) && !emailsToRemove.includes(student.userEmail) && !student.unsubscribed) {
+            // let registeredGroup = student.registeredGroups.find(registeredGroup => registeredGroup.groupId === group)
+            // if (registeredGroup) {
+            //     let categoryId = recipientsGroupsAndCategories[group].categoryId;
+            //     let selectedOptions = recipientsGroupsAndCategories[group].selectedOptions;
+            //     let registeredCategory = registeredGroup.categories.find(category => category.id === categoryId);
+                 if (!emailsToRemove.includes(student.userEmail) && !student.unsubscribed) {
                     recipients.add(student.userEmail);
                  }
-            }
+            // }
         })
     })
     console.log(recipients.size)
@@ -40,7 +40,7 @@ exports.sendEmailToStudentOfUniversityAndField = functions.https.onRequest(async
     let templates = []
     recipients.forEach(recipient => {
         const email = {
-            "TemplateId": 23147847,
+            "TemplateId": 22748248,
             "From": 'CareerFairy <noreply@careerfairy.io>',
             "To": recipient,
             "TemplateModel": {
