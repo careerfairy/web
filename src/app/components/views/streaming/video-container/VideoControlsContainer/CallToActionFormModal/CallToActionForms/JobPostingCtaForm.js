63c4709979815cf6be7e43093a3111df2c4d971b
Maximilian Voss <maximilian@careerfairy.io>
2021-09-15 10:51:05 +0200

    refactor call to action form

---
+++ src/app/components/views/streaming/video-container/VideoControlsContainer/CallToActionFormModal/CallToActionForms/JobPostingCtaForm.js
@@ -0,0 +1,155 @@
+const { memo } = require("react");
+import { Collapse, Grid, TextField } from "@material-ui/core";
+import { DateTimePicker } from "@material-ui/pickers";
+
+const JobPostingCtaForm = memo(
+   ({ formik, maxMessageLength, onEntered, onExited }) => {
+      const now = new Date();
+
+      return (
+         <Grid container spacing={3}>
+            <Grid xs={12} item>
+               <Collapse unmountOnExit in={true}>
+                  <TextField
+                     fullWidth
+                     variant="outlined"
+                     id="jobTitle"
+                     name="jobData.jobTitle"
+                     disabled={formik.isSubmitting}
+                     autoFocus={true}
+                     inputProps={{
+                        maxLength: maxMessageLength,
+                     }}
+                     placeholder="Mechanical Engineer"
+                     label="Job Title*"
+                     value={formik.values.jobData.jobTitle}
+                     onChange={formik.handleChange}
+                     error={
+                        formik.touched.jobData?.jobTitle &&
+                        Boolean(formik.errors.jobData?.jobTitle)
+                     }
+                     helperText={
+                        formik.touched.jobData?.jobTitle &&
+                        formik.errors.jobData?.jobTitle
+                     }
+                  />
+               </Collapse>
+            </Grid>
+            <Grid xs={12} sm={8} item>
+               <Collapse unmountOnExit in={true}>
+                  <TextField
+                     fullWidth
+                     variant="outlined"
+                     id="salary"
+                     name="jobData.salary"
+                     disabled={formik.isSubmitting}
+                     autoFocus={true}
+                     inputProps={{
+                        maxLength: maxMessageLength,
+                     }}
+                     placeholder="CHF - 82'000"
+                     label="Salary"
+                     value={formik.values.jobData.salary}
+                     onChange={formik.handleChange}
+                     error={
+                        formik.touched.jobData?.salary &&
+                        Boolean(formik.errors.jobData?.salary)
+                     }
+                     helperText={
+                        formik.touched.jobData?.salary &&
+                        formik.errors.jobData?.salary
+                     }
+                  />
+               </Collapse>
+            </Grid>
+            <Grid xs={12} sm={4} item>
+               <Collapse unmountOnExit in={true}>
+                  <DateTimePicker
+                     id="applicationDeadline"
+                     clearable
+                     disablePast
+                     label="Application deadline"
+                     value={formik.values.jobData.applicationDeadline}
+                     name="jobData.applicationDeadline"
+                     onChange={(value) => {
+                        const newValue = value ? new Date(value) : null;
+                        formik.setFieldValue(
+                           "jobData.applicationDeadline",
+                           newValue,
+                           true
+                        );
+                     }}
+                     disabled={formik.isSubmitting}
+                     minDate={now}
+                     inputVariant="outlined"
+                     fullWidth
+                     error={
+                        formik.touched.jobData?.applicationDeadline &&
+                        Boolean(formik.errors.jobData?.applicationDeadline)
+                     }
+                     helperText={
+                        formik.touched.jobData?.applicationDeadline &&
+                        formik.errors.jobData?.applicationDeadline
+                     }
+                  />
+               </Collapse>
+            </Grid>
+            <Grid xs={12} item>
+               <Collapse
+                  onEntered={onEntered}
+                  onExited={onExited}
+                  unmountOnExit
+                  in={true}
+               >
+                  <TextField
+                     fullWidth
+                     variant="outlined"
+                     id="message"
+                     name="message"
+                     disabled={formik.isSubmitting}
+                     multiline
+                     autoFocus
+                     minRows={3}
+                     maxRows={12}
+                     inputProps={{
+                        maxLength: maxMessageLength,
+                     }}
+                     placeholder="Click here to see our open positions"
+                     label="Job Description"
+                     value={formik.values.message}
+                     onChange={formik.handleChange}
+                     error={
+                        formik.touched.message && Boolean(formik.errors.message)
+                     }
+                     helperText={
+                        formik.touched.message && formik.errors.message
+                     }
+                  />
+               </Collapse>
+            </Grid>
+            <Grid xs={12} sm={12} item>
+               <TextField
+                  fullWidth
+                  variant="outlined"
+                  id="buttonUrl"
+                  name="buttonUrl"
+                  disabled={formik.isSubmitting}
+                  placeholder="https://mywebsite.com/careers/"
+                  label={`Job Posting Url*`}
+                  value={formik.values.buttonUrl}
+                  onChange={formik.handleChange}
+                  error={
+                     formik.touched.buttonUrl &&
+                     Boolean(formik.errors.bwuttonUrl)
+                  }
+                  helperText={
+                     formik.touched.buttonUrl && formik.errors.buttonUrl
+                  }
+               />
+            </Grid>
+         </Grid>
+      );
+   }
+);
+
+export default JobPostingCtaForm;
