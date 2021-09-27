550164522ca6efcbc68659a1f5ff8025b6849746
habib <habib@careerfairy.io>
2021-09-27 17:13:30 +0200

    removed handraise banner for mobile viewports added coloring to floating call to action icon

--- src/app/components/views/streaming/video-container/Streams/index.js
+++ src/app/components/views/streaming/video-container/Streams/index.js
@@ -39,8 +39,9 @@ const Streams = memo(
      const classes = useStyles();
 
      useEffect(() => {
-       setBannersBottom(Boolean(mobile && !presenter && !mobile))
-     }, [mobile, presenter, mobile]);
+       setBannersBottom(Boolean(mobile && !presenter))
+     }, [mobile, presenter]);
+
      
 
       useEffect(() => {
@@ -144,6 +145,7 @@ const Streams = memo(
                <Banners
                   presenter={presenter}
                   handRaiseActive={handRaiseActive}
+                  mobile={mobile}
                />
             )}
             <div className={classes.videoElementsWrapper}>
@@ -164,6 +166,7 @@ const Streams = memo(
                  isBottom
                   presenter={presenter}
                   handRaiseActive={handRaiseActive}
+                 mobile={mobile}
                />
             }
          </div>
