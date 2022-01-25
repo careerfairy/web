8f880eb6343c944dedd2e5c834d3f400c4939254
Habib Kadiri <habib@careerfairy.io>
2022-01-25 16:44:49 +0100

    added sx styling to button

--- src/app/components/views/common/LoginButton.js
+++ src/app/components/views/common/LoginButton.js
@@ -1,21 +1,14 @@
 import React from "react";
-import makeStyles from '@mui/styles/makeStyles';
 import { Button } from "@mui/material";
 import Link from "materialUI/NextNavLink";
 
-const useStyles = makeStyles((theme) => ({
-   root: {
-      color: [theme.palette.common.white, "important"],
-   },
-}));
-
 const LoginButton = ({}) => {
-   const classes = useStyles();
-
    return (
       <Button
          fullWidth
-         className={classes.root}
+         sx={{
+            color: (theme) => [theme.palette.common.white, "important"],
+         }}
          component={Link}
          href="/login"
          style={{ textDecoration: "none" }}
