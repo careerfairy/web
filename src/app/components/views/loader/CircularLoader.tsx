6b553a544b5ea5f2c593091d4243eb6e6f4270d7
Carlos Florêncio <carlos@careerfairy.io>
2022-02-03 16:07:27 +0100

    User profile changes

---
+++ src/app/components/views/loader/CircularLoader.tsx
@@ -0,0 +1,10 @@
+import {Box, CircularProgress} from "@mui/material";
+import React from "react";
+
+const CircularLoader = (props) => (
+	<Box display="flex" justifyContent="center" {...props}>
+		<CircularProgress />
+	</Box>
+)
+
+export default CircularLoader
