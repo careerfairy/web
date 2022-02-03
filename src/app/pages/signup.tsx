35062abcb9c81e67cbddc6da7264cfeeb449d0bb
Carlos Florêncio <carlos@careerfairy.io>
2022-02-03 16:07:27 +0100

    User profile changes

--- src/app/pages/signup.tsx
+++ src/app/pages/signup.tsx
@@ -1,19 +1,18 @@
-import React, { Fragment, useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
+import React, {Fragment, useState, useEffect} from "react";
+import {useRouter} from "next/router";
 import Head from "next/head";
 import {
-   Box,
-   Container,
-   Typography,
-   Stepper,
-   Step,
-   StepLabel,
+  Box,
+  Container,
+  Typography,
+  Stepper,
+  Step,
+  StepLabel,
 } from "@mui/material";
 import makeStyles from '@mui/styles/makeStyles';
-import { TealBackground } from "../materialUI/GlobalBackground/GlobalBackGround";
-import { useAuth } from "../HOCs/AuthProvider";
-import { createStyles } from '@mui/styles';
+import {TealBackground} from "../materialUI/GlobalBackground/GlobalBackGround";
+import {useAuth} from "../HOCs/AuthProvider";
+import {createStyles} from '@mui/styles';
 import SignUpPinForm from "../components/views/signup/SignUpPinForm";
 import SignUpUserForm from "../components/views/signup/SignUpUserForm";
 import MultiStepWrapper, {
@@ -21,6 +20,8 @@ import MultiStepWrapper, {
 } from "../components/views/signup/MultiStepWrapper";
 import PersonaliseSteps from "../components/views/signup/PersonaliseSteps";
 import {MainLogo} from './../components/logos'
+
+export const SIGNUP_REDIRECT_PATH = "/profile"
 
 const initialStates: MultiStepComponentType[] = [
   {
@@ -32,21 +33,20 @@ const initialStates: MultiStepComponentType[] = [
     description: "Email Verification"
   },
   {
-    component: () => PersonaliseSteps,
-    // Change this to "Personalise" when we have multiple steps
-    description: "Join Groups",
+    component: () => PersonaliseSteps, // contains multiple steps
+    description: "Personalise",
   }
 ]
 
 const SignUp = () => {
-  const {authenticatedUser: user, userData} = useAuth();
+  const {authenticatedUser: user} = useAuth();
   const [currentStep, setCurrentStep] = useState(0);
   const [steps, setSteps] = useState(initialStates)
   const {query: {absolutePath}} = useRouter();
 
   useEffect(() => {
-    // When redirecting, we want to skip the last step
-    if(absolutePath && steps.length === 3) {
+    // When redirecting, we want to skip the Personalise step
+    if (absolutePath && steps.length === 3) {
       setSteps(prevSteps => prevSteps.slice(0, 2))
     }
   }, [steps, absolutePath])
@@ -54,7 +54,6 @@ const SignUp = () => {
   useEffect(() => {
     if (!user.isLoaded || user.isEmpty) return
 
-    console.log("useEffect signup", user)
     if (!user.emailVerified) {
       return setCurrentStep(1);
     }
@@ -62,23 +61,34 @@ const SignUp = () => {
 
   return (
     <SignUpPageLayout>
-      <Stepper
-        style={{padding: "0 0 24px 0"}}
-        activeStep={currentStep}
-        alternativeLabel
-      >
-        {steps.map((step, i) => (
-          <Step key={i}>
-            <StepLabel>{step.description}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
+      {currentStep < 2 ? (
+        <Box mb={2}>
+          <SignupStepper steps={steps} currentStep={currentStep} />
+        </Box>
+      ) : null}
+
       <MultiStepWrapper steps={steps} currentStep={currentStep}
                         setCurrentStep={setCurrentStep}/>
     </SignUpPageLayout>
 
   )
 }
+
+export const SignupStepper = ({
+                                currentStep,
+                                steps
+                              }: { currentStep: number, steps: MultiStepComponentType[] }) => (
+  <Stepper
+    activeStep={currentStep}
+    alternativeLabel
+  >
+    {steps.map((step, i) => (
+      <Step key={i}>
+        <StepLabel>{step.description}</StepLabel>
+      </Step>
+    ))}
+  </Stepper>
+)
 
 const useStyles = makeStyles((theme) => createStyles({
   box: {
@@ -120,11 +130,7 @@ export const SignUpPageLayout = ({children}) => {
       </Head>
       <TealBackground>
         <header>
-          <Link href="/">
-            <a>
-              <MainLogo className={classes.logo} white={true} />
-            </a>
-          </Link>
+          <MainLogo className={classes.logo} white={true}/>
         </header>
         <Typography className={classes.title}>Sign Up</Typography>
         <Container maxWidth="sm">
