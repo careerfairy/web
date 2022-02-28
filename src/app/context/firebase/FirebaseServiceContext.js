b51625b678a75dd9d5e4796e2e8e86ad55911045
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-02-28 16:09:40 +0000

    Firestore Long Polling Transport fallback (#19)
    * Refactored firebase instance creation
    
    * Only use sentry when building from the CI
    
    * Fix eslint warning about the component name be lowercase
    
    * Auto enable long polling when Firestore connectivity fails
    
    * Converted FirebaseService to typescript
    
    * Converted FirebaseService to typescript
    
    * Fix build, opened access to the FirebaseService properties
    
    * Added warnings

--- src/app/context/firebase/FirebaseServiceContext.js
+++ src/app/context/firebase/FirebaseServiceContext.js
@@ -1,27 +1,29 @@
-import React, {useContext} from 'react';
-import {firebaseServiceInstance} from "../../data/firebase/FirebaseService"
+import React, { useContext } from "react";
+import { firebaseServiceInstance } from "../../data/firebase/FirebaseService";
 
-const FirebaseServiceContext = React.createContext(firebaseServiceInstance)
+const FirebaseServiceContext = React.createContext(firebaseServiceInstance);
 
-export const withFirebase = Component => props => (
-    <FirebaseServiceContext.Consumer>
-        {firebase => <Component {...props} firebase={firebase} />}
-    </FirebaseServiceContext.Consumer>
-);
+// eslint-disable-next-line react/display-name
+export const withFirebase = (Component) => (props) =>
+   (
+      <FirebaseServiceContext.Consumer>
+         {(firebase) => <Component {...props} firebase={firebase} />}
+      </FirebaseServiceContext.Consumer>
+   );
 
-export const withFirebasePage = Page => {
-    let element = props => {
-        return(
-            <FirebaseServiceContext.Consumer>
-                {firebase => <Page {...props} firebase={firebase} />}
-            </FirebaseServiceContext.Consumer>
-        );
-    };
-    if  (Page.getInitialProps) {
-        element.getInitialProps = Page.getInitialProps
-    }
-    return element;
-}
+export const withFirebasePage = (Page) => {
+   let element = (props) => {
+      return (
+         <FirebaseServiceContext.Consumer>
+            {(firebase) => <Page {...props} firebase={firebase} />}
+         </FirebaseServiceContext.Consumer>
+      );
+   };
+   if (Page.getInitialProps) {
+      element.getInitialProps = Page.getInitialProps;
+   }
+   return element;
+};
 
 export const useFirebaseService = () => useContext(FirebaseServiceContext);
 
