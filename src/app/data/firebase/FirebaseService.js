f37e4453d195504979a289e2a2e7627af0e9449b
Habib Kadiri <habib@careerfairy.io>
2022-03-03 17:23:24 +0100

    - Added livestreamId and authId to the registrant sub-collection document write

--- src/app/data/firebase/FirebaseService.js
+++ src/app/data/firebase/FirebaseService.js
@@ -2429,13 +2429,17 @@ class FirebaseService {
                // To be depreciated
                transaction.set(registeredUsersRef, {
                   ...user,
+                  livestreamId,
                   dateRegistered: this.getServerTimestamp(),
+                  authId: uid,
                });
 
                // To be used from now on
                transaction.set(registrantSubCollectionRef, {
                   ...user,
+                  livestreamId,
                   dateRegistered: this.getServerTimestamp(),
+                  authId: uid,
                });
             });
          }
