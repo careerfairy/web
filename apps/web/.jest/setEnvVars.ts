7f4ea31f7c0b95ff4a93dcdd3bf1c2581baa24f8
Habib Kadiri <habib@careerfairy.io>
2022-03-23 14:48:59 +0100

    -reinstalled app - fixed jest testing error in useCollection.ts hook

--- apps/web/.jest/setEnvVars.ts
+++ apps/web/.jest/setEnvVars.ts
@@ -1,8 +1,3 @@
-process.env.REACT_APP_FIREBASE_API_KEY =
-   "AIzaSyAMx1wVVxqo4fooh0OMVSeSTOqNKzMbch0"
-process.env.REACT_APP_FIREBASE_AUTH_DOMAIN = "careerfairy-e1fd9.firebaseapp.com"
-process.env.REACT_APP_FIREBASE_DATABASE_URL =
-   "https://careerfairy-e1fd9.firebaseio.com"
-process.env.REACT_APP_FIREBASE_PROJECT_ID = "careerfairy-e1fd9"
-process.env.REACT_APP_FIREBASE_STORAGE_BUCKET = "careerfairy-e1fd9.appspot.com"
-process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID = "993933306494"
+require("dotenv").config({
+   ...(!process.env.CI && { path: "../../.env.test.local" }),
+})
