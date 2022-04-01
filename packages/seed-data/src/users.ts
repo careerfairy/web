86d5cf757f0277895cccd39c094b0d877f482c15
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-04-01 13:52:05 +0100

    Init seed package

---
+++ packages/seed-data/src/users.ts
@@ -0,0 +1,57 @@
+import { auth, firestore } from "./lib/firebase"
+import { capitalizeFirstLetter } from "./utils/utils"
+import { UserData } from "@careerfairy/shared-lib/dist/users"
+
+interface UserSeed {
+   /**
+    * Creates a user on firebase auth (already verified) and a corresponding
+    * userData document
+    *
+    * @param email The password will be the string on the left side of the @
+    * @param extraData fields that will be stored on the userData document
+    */
+   createUser(email: string, extraData?: UserData): Promise<UserData>
+}
+
+class UserFirebaseSeed implements UserSeed {
+   /**
+    * Creates a user on firebase auth (already verified) and a corresponding
+    * userData document
+    *
+    * @param email The password will be the string on the left side of the @
+    * @param extraData fields that will be stored on the userData document
+    */
+   async createUser(email: string, extraData?: UserData) {
+      const username = email.split("@")[0]
+      const userRecord = await auth.createUser({
+         email: email,
+         password: username,
+         emailVerified: true,
+      })
+
+      const userData = Object.assign(
+         {
+            authId: userRecord.uid,
+            id: email,
+            firstName: capitalizeFirstLetter(username),
+            lastName: "Doe",
+            userEmail: email,
+            university: {
+               name: "Other",
+               code: "other",
+            },
+            universityCountryCode: "CH",
+            unsubscribed: false,
+         },
+         extraData
+      )
+
+      await firestore.collection("userData").doc(email).set(userData)
+
+      return userData
+   }
+}
+
+const instance: UserSeed = new UserFirebaseSeed()
+
+export default instance
