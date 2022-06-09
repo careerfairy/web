607853940b1c5832250227c37e95bb601937ded8
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-06-09 15:24:09 +0100

    Save UTM Params on event registration (#150)

--- apps/web/util/SessionStorageUtil.ts
+++ apps/web/util/SessionStorageUtil.ts
@@ -6,4 +6,13 @@ export default class SessionStorageUtil {
    static setIsLongPollingMode = (value) => {
       return sessionStorage?.setItem("compat_longpolling", value)
    }
+
+   static setUTMParams = (value: object) => {
+      return sessionStorage?.setItem("utm_params", JSON.stringify(value))
+   }
+
+   static getUTMParams = () => {
+      const existing = sessionStorage?.getItem("utm_params")
+      return existing ? JSON.parse(existing) : null
+   }
 }
