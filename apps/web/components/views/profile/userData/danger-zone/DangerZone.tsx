01ea16673b2b76d1c76220ff5f375f44ae7e5d75
goncaloarnauth <goncaloarnauth@outlook.com>
2022-06-29 17:24:44 +0200

    Feature: Delete User Account - update layout to based on UX/UI

--- apps/web/components/views/profile/userData/danger-zone/DangerZone.tsx
+++ apps/web/components/views/profile/userData/danger-zone/DangerZone.tsx
@@ -3,22 +3,18 @@ import { useRouter } from "next/router"
 import { useDispatch } from "react-redux"
 
 import * as actions from "store/actions"
-import { Button, Grid, Typography } from "@mui/material"
+import { Grid, Link, Typography } from "@mui/material"
 import { StylesProps, sxStyles } from "../../../../../types/commonTypes"
 import DeleteAccountDialog from "./DeleteAccountDialog"
 
 const styles: StylesProps = sxStyles({
-   submitBtn: {
-      margin: "unset",
-   },
    section: {
-      marginTop: "unset",
+      marginBottom: 1,
    },
    subtitle: {
       textTransform: "uppercase",
       fontSize: "0.8rem !important",
       fontWeight: "bold",
-      marginBottom: 1,
    },
 })
 
@@ -44,39 +40,33 @@ const DangerZone = ({ userEmail }: Props) => {
    }, [dispatch, router])
 
    return (
-      <Grid container spacing={2} sx={styles.section}>
-         <Grid item xs={12} sm={8}>
+      <>
+         <Grid sx={styles.section}>
             <Typography sx={styles.subtitle} variant="h5">
-               Delete Account
+               Wish no longer joint the events?
             </Typography>
             <Typography variant="body2" component="p">
                Once you delete your Account, there is no going back. Please be
                certain.
             </Typography>
          </Grid>
-
-         <Grid item xs={12} sm={4}>
-            <Button
-               type="button"
-               fullWidth
-               variant="outlined"
-               color="error"
-               sx={styles.submitBtn}
-               onClick={() => setIsDeleteOverlayOpen(true)}
-               data-testid={"delete-account-button"}
-            >
-               {"Delete Account"}
-            </Button>
-            {isDeleteOverlayOpen && (
-               <DeleteAccountDialog
-                  userEmail={userEmail}
-                  onAccountDeletion={handleAccountDeletion}
-                  onClose={setIsDeleteOverlayOpen}
-                  data-testid={"delete-account-dialog"}
-               />
-            )}
-         </Grid>
-      </Grid>
+         <Link
+            href="#"
+            underline="always"
+            color="error"
+            onClick={() => setIsDeleteOverlayOpen(true)}
+         >
+            Delete my account
+         </Link>
+         {isDeleteOverlayOpen && (
+            <DeleteAccountDialog
+               userEmail={userEmail}
+               onAccountDeletion={handleAccountDeletion}
+               onClose={setIsDeleteOverlayOpen}
+               data-testid={"delete-account-dialog"}
+            />
+         )}
+      </>
    )
 }
 
