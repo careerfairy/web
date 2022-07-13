ff59dcb50c038f71c853daf872ebed56ed8f8f7b
goncaloarnauth <goncaloarnauth@outlook.com>
2022-07-13 23:15:19 +0100

    Remove Personalise Step. Create utils file. Add genders options to constants file

---
+++ apps/web/components/views/signup/utils.ts
@@ -0,0 +1,15 @@
+export type Option = {
+   id: string
+   name: string
+   groupId?: string
+}
+export const formatToOptionArray = (
+   selectedIds: string[],
+   allOptions: Option[]
+): Option[] => {
+   return allOptions.filter(({ id }) => selectedIds?.includes(id))
+}
+
+export const mapOptions = (options: Option[]): string[] => {
+   return options.map((option) => option.id)
+}
