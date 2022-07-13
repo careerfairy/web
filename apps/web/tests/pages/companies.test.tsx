8f5207f8e0a6334c7df71716d152ab7c85e6419c
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-07-13 13:00:22 +0100

    Commented test because it can't run

--- apps/web/tests/pages/companies.test.tsx
+++ apps/web/tests/pages/companies.test.tsx
@@ -3,12 +3,12 @@ import React from "react"
 import CompaniesPage from "../../pages/companies/index"
 import { render, screen } from "../test-utils"
 
-// describe("Companies Page Tests", () => {
-//    it("Should render the 'Book a Demo' button with a valid link", () => {
-//       render(<CompaniesPage />)
-//       expect(screen.getAllByText("Book a Demo")[0]).toHaveAttribute(
-//          "href",
-//          "https://library.careerfairy.io/demo-request-1"
-//       )
-//    })
-// })
+describe("Companies Page Tests", () => {
+   it("Should render the 'Book a Demo' button with a valid link", () => {
+      render(<CompaniesPage />)
+      expect(screen.getAllByText("Book a Demo")[0]).toHaveAttribute(
+         "href",
+         "https://library.careerfairy.io/demo-request-1"
+      )
+   })
+})
