16caa4ff747d16d296d83d0e8d37388b6ecc75a4
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-07-15 12:14:07 +0100

    Adding Suspense to reactfire

--- apps/web/components/custom-hook/useUserRewards.ts
+++ apps/web/components/custom-hook/useUserRewards.ts
@@ -2,23 +2,23 @@ import { collection, query, orderBy } from "firebase/firestore"
 import { useFirestore, useFirestoreCollectionData } from "reactfire"
 import { Reward } from "../../types/reward"
 
+/**
+ * Fetch the rewards for a given user
+ *
+ * This hook will suspend when loading data
+ * Use an ErrorBoundary to handle errors
+ *
+ * @param userId
+ */
 const useUserRewards = (userId: string) => {
    const collectionRef = query(
       collection(useFirestore(), "userData", userId, "rewards"),
       orderBy("createdAt", "desc")
    )
 
-   let { status, data } = useFirestoreCollectionData<Reward>(
-      collectionRef as any,
-      {
-         idField: "id", // this field will be added to the firestore object
-      }
-   )
-
-   return {
-      isLoading: status === "loading",
-      data: data,
-   }
+   return useFirestoreCollectionData<Reward>(collectionRef as any, {
+      idField: "id", // this field will be added to the firestore object
+   })
 }
 
 export default useUserRewards
