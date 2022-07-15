347f14996e0824a0e2dbae9224ba4e673c16705f
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-07-15 09:16:46 +0100

    Rebase to Firebase v9 and ReactFire

--- packages/shared-lib/src/groups/groups.ts
+++ packages/shared-lib/src/groups/groups.ts
@@ -1,5 +1,5 @@
 import { Identifiable } from "../commonTypes"
-import firebase from "firebase"
+import firebase from "firebase/compat/app"
 
 // CareerCenterData collection
 export interface Group extends Identifiable {
