552de81cb0f7e065e558000974c51c4c3433ca53
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-07-15 11:42:12 +0100

    Rebase from develop

--- apps/web/pages/group/[groupId]/admin/ats-integration.tsx
+++ apps/web/pages/group/[groupId]/admin/ats-integration.tsx
@@ -1,9 +1,7 @@
-import React, { useMemo } from "react"
+import React, { Suspense } from "react"
 import GroupDashboardLayout from "../../../../layouts/GroupDashboardLayout"
 import DashboardHead from "../../../../layouts/GroupDashboardLayout/DashboardHead"
 import AtsIntegrationContent from "../../../../components/views/group/admin/ats-integration/AtsIntegrationContent"
-import { useRouter } from "next/router"
-import { useFirestoreConnect } from "react-redux-firebase"
 import ErrorBoundary from "../../../../components/ErrorBoundary"
 
 const AtsIntegration = () => (
@@ -14,41 +12,5 @@ const AtsIntegration = () => (
       </ErrorBoundary>
    </GroupDashboardLayout>
 )
-
-/**
- * Fetch the ATS information for the group and stores on redux state
- * for the child components to read
- *
- * Needs to have the groupId in the current path
- * @param children
- * @constructor
- */
-const GroupATSAccountsReduxFetcher = ({ children }) => {
-   const router = useRouter()
-
-   const groupId = router.query.groupId as string
-
-   const query = useMemo(
-      () => [
-         {
-            collection: "careerCenterData",
-            doc: groupId,
-            subcollections: [
-               {
-                  collection: "ats",
-               },
-            ],
-            storeAs: "ats",
-         },
-      ],
-      [groupId]
-   )
-
-   useFirestoreConnect(query)
-
-   console.log("groupId", groupId)
-
-   return children
-}
 
 export default AtsIntegration
