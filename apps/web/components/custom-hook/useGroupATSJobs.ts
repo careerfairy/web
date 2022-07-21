9e6e4ded28670edebaa790602e21807cf064b4b2
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-07-21 12:35:05 +0100

    Added custom hooks to fetch data from ATS

---
+++ apps/web/components/custom-hook/useGroupATSJobs.ts
@@ -0,0 +1,37 @@
+import useSWR from "swr"
+import { Job } from "@careerfairy/shared-lib/dist/ats/Job"
+import useFunctionsSWR, {
+   reducedRemoteCallsOptions,
+} from "./utils/useFunctionsSWRFetcher"
+import { useMemo } from "react"
+
+type Result = {
+   jobs: Job[]
+}
+
+const useGroupATSJobs = (groupId: string, integrationId: string): Result => {
+   const fetcher = useFunctionsSWR<Job[]>()
+
+   const { data } = useSWR(
+      [
+         "fetchATSJobs",
+         {
+            groupId,
+            integrationId,
+         },
+      ],
+      fetcher,
+      reducedRemoteCallsOptions
+   )
+
+   return useMemo(() => {
+      // map to business model (convert plain object to class object)
+      const jobs = data.map((job) => Job.createFromPlainObject(job))
+
+      return {
+         jobs,
+      }
+   }, [data])
+}
+
+export default useGroupATSJobs
