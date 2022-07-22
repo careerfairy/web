5502cb1d17128ed2057b2a880a5e039600add679
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-07-22 09:30:56 +0100

    Added props types to TabPanel component

--- apps/web/layouts/UserLayout/UserView.tsx
+++ apps/web/layouts/UserLayout/UserView.tsx
@@ -16,7 +16,15 @@ import { Container } from "@mui/material"
 import MyRecruitersTab from "../../components/views/profile/saved-recruiters/MyRecruitersTab"
 import CareerSkills from "../../components/views/profile/career-skills/CareerSkills"
 
-function TabPanel(props) {
+type TabPanelProps = {
+   children: () => JSX.Element
+   value: string
+   path: string
+   // All other props
+   [x: string]: any
+}
+
+function TabPanel(props: TabPanelProps) {
    const { children, value, path, ...other } = props
    return (
       <div
