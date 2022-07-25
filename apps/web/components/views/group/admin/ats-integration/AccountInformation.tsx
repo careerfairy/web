dac807c47c99be4324f419dc0014ba59dba43c8b
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-07-25 13:17:31 +0100

    Refactor columns and remove generateColumnsFromObject()

--- apps/web/components/views/group/admin/ats-integration/AccountInformation.tsx
+++ apps/web/components/views/group/admin/ats-integration/AccountInformation.tsx
@@ -1,16 +1,37 @@
 import useGroupATSJobs from "../../../../custom-hook/useGroupATSJobs"
 import MaterialTable from "@material-table/core"
 import Box from "@mui/material/Box"
-import { capitalizeFirstLetter } from "../../../../../util/CommonUtil"
 import { useMemo } from "react"
 import { Job } from "@careerfairy/shared-lib/dist/ats/Job"
 import { GroupATSAccount } from "@careerfairy/shared-lib/dist/groups/GroupATSAccount"
-import { Button } from "@mui/material"
 import SyncStatusButtonDialog from "./SyncStatusButtonDialog"
 
 type Props = {
    atsAccount: GroupATSAccount
 }
+
+const columns = [
+   {
+      title: "Name",
+      field: "name",
+   },
+   {
+      title: "Description",
+      field: "description",
+   },
+   {
+      title: "Status",
+      field: "status",
+   },
+   {
+      title: "Hiring Manager",
+      field: "hiringManager",
+   },
+   {
+      title: "Created At",
+      field: "createdAt",
+   },
+]
 
 const AccountInformation = ({ atsAccount }: Props) => {
    const { jobs } = useGroupATSJobs(atsAccount.groupId, atsAccount.id)
@@ -18,31 +39,6 @@ const AccountInformation = ({ atsAccount }: Props) => {
    const jobsToRows = useMemo(() => {
       return mapJobsToTableRows(jobs)
    }, [jobs])
-
-   const columns = useMemo(() => {
-      return [
-         {
-            title: "Name",
-            field: "name",
-         },
-         {
-            title: "Description",
-            field: "description",
-         },
-         {
-            title: "Status",
-            field: "status",
-         },
-         {
-            title: "Hiring Manager",
-            field: "hiringManager",
-         },
-         {
-            title: "Created At",
-            field: "createdAt",
-         },
-      ]
-   }, [])
 
    return (
       <>
@@ -72,24 +68,13 @@ type TableProps<T> = {
 }
 
 const Table = <T extends object>(props: TableProps<T>) => {
-   const columns = props.columns ?? generateColumnsFromObject(props.data[0])
-
    return (
-      <MaterialTable columns={columns} data={props.data} title={props.title} />
+      <MaterialTable
+         columns={props.columns}
+         data={props.data}
+         title={props.title}
+      />
    )
-}
-
-function generateColumnsFromObject<T>(obj: T) {
-   const columns = []
-
-   for (let objKey in obj) {
-      columns.push({
-         title: capitalizeFirstLetter(objKey),
-         field: objKey,
-      })
-   }
-
-   return columns
 }
 
 function mapJobsToTableRows(jobs: Job[]) {
