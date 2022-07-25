2b6f3a1c0e66cf8a5b2c665960d373d3cec14199
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-07-25 13:40:02 +0100

    Added custom hook to ease error/success notifications

--- apps/web/store/actions/snackbarActions.js
+++ apps/web/store/actions/snackbarActions.js
@@ -48,31 +48,34 @@ export const removeSnackbar = (key) => ({
    key,
 })
 
-export const sendGeneralError = (errorInstance) => async (dispatch) => {
-   const error = errorInstance || ""
-   console.error("error", error)
-   Sentry.captureException(error)
-   let message = GENERAL_ERROR
-   if (process.env.NODE_ENV === "development") {
-      const devInfo =
-         "This type of error only appears in development, it will just show a general error in production."
-      if (typeof error === "string") {
-         message = `${error} - ${devInfo}`
+export const sendGeneralError =
+   (errorInstance, message = GENERAL_ERROR, extra = {}) =>
+   async (dispatch) => {
+      const error = errorInstance || ""
+      console.error("error", error)
+      Sentry.captureException(error, { extra })
+
+      if (process.env.NODE_ENV === "development") {
+         const devInfo =
+            "This type of error only appears in development, it will just show a general error in production."
+         if (typeof error === "string") {
+            message = `${error} - ${devInfo}`
+         }
+         if (typeof error?.message === "string") {
+            message = `${error.message} - ${devInfo}`
+         }
       }
-      if (typeof error?.message === "string") {
-         message = `${error.message} - ${devInfo}`
-      }
+
+      dispatch(
+         enqueueSnackbar({
+            message: message,
+            options: {
+               variant: "error",
+               preventDuplicate: true,
+            },
+         })
+      )
    }
-   dispatch(
-      enqueueSnackbar({
-         message: message,
-         options: {
-            variant: "error",
-            preventDuplicate: true,
-         },
-      })
-   )
-}
 
 export const sendSuccessMessage =
    (message = "Success") =>
