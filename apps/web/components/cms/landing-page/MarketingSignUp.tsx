285ce3fb09d6276567af4cead2e155de3957ad1a
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-08-05 11:02:05 +0200

    Updated MarketingSignUp

--- apps/web/components/cms/landing-page/MarketingSignUp.tsx
+++ apps/web/components/cms/landing-page/MarketingSignUp.tsx
@@ -1,5 +1,31 @@
+import Container from "@mui/material/Container"
+import Box from "@mui/material/Box"
+import { sxStyles } from "../../../types/commonTypes"
+import { Grid, Typography } from "@mui/material"
+
+const styles = sxStyles({
+   container: {
+      backgroundColor: "primary.main",
+   },
+})
+
 const MarketingSignUp = () => {
-   return <div>MarketingSignUp</div>
+   return (
+      <Box sx={styles.container}>
+         <Container>
+            <Box p={3}>
+               <Typography variant="h4">Stay connected</Typography>
+               <Typography variant="subtitle1">
+                  {"You'll"} receive emails for future livestream events
+               </Typography>
+
+               <Grid container>
+                  <Grid item>grid</Grid>
+               </Grid>
+            </Box>
+         </Container>
+      </Box>
+   )
 }
 
 export default MarketingSignUp
