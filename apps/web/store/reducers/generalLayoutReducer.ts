de15b46eb7e26c4b4d845e14edb20754dee34c1f
goncaloarnauth <goncaloarnauth@outlook.com>
2022-08-09 09:35:34 +0100

    add isOnLandingPage Redux field and use it

--- apps/web/store/reducers/generalLayoutReducer.ts
+++ apps/web/store/reducers/generalLayoutReducer.ts
@@ -4,12 +4,13 @@ import { GeneralLayoutState } from "./index"
 const initialState = {
    layout: {
       drawerOpen: false,
+      isOnLandingPage: false,
    },
 } as GeneralLayoutState
 
 const generalLayoutReducer = (
    state: GeneralLayoutState = initialState,
-   { type }
+   { type}
 ) => {
    switch (type) {
       case actions.OPEN_NAV_DRAWER:
@@ -20,6 +21,22 @@ const generalLayoutReducer = (
          return {
             ...state,
             layout: { ...state.layout, drawerOpen: !state.layout.drawerOpen },
+         }
+      case actions.IN_LANDING_PAGE:
+         return {
+            ...state,
+            layout: {
+               ...state.layout,
+               isOnLandingPage: true,
+            },
+         }
+      case actions.OUT_OF_LANDING_PAGE:
+         return {
+            ...state,
+            layout: {
+               ...state.layout,
+               isOnLandingPage: false,
+            },
          }
       default:
          return state
