2116f58d7e9b4c276b7d37a373a9d8be84e8be78
Habib Kadiri <habib@careerfairy.io>
2022-08-09 12:19:53 +0200

    add gql template string syntax highlighting

--- apps/web/data/graphcms/MarketingPageRepository.ts
+++ apps/web/data/graphcms/MarketingPageRepository.ts
@@ -1,5 +1,6 @@
 import { fetchAPI } from "./index"
 import { Slug, Variables } from "../../types/cmsTypes"
+import gql from "graphql-tag"
 
 export interface IMarketingPageRepository {
    getAllMarketingPageSlugs(): Promise<{ slug: Slug }[]>
@@ -11,8 +12,8 @@ class GraphCMSMarketingPageRepository implements IMarketingPageRepository {
    constructor() {}
 
    async getAllMarketingPageSlugs(): Promise<{ slug: Slug }[]> {
-      const data = await fetchAPI(`
-          {
+      const data = await fetchAPI(gql`
+         {
             marketingLandingPages {
                slug
             }
@@ -23,97 +24,97 @@ class GraphCMSMarketingPageRepository implements IMarketingPageRepository {
 
    async getMarketingPage(variables: Variables): Promise<any> {
       return await fetchAPI(
-         `
-         query MarketingLandingPageQuery($slug: String!, $stage: Stage!) {
-  marketingLandingPage(stage: $stage, where: {slug: $slug}) {
-    id
-    stage
-      title
-    subtitle
-    updatedAt
-    createdBy {
-      entryId: id
-      name
-      picture
-      kind
-      isActive
-    }
-    updatedBy {
-      entryId: id
-      name
-      picture
-      kind
-      isActive
-    }
-    blocks(first: 500) {
-      ... on Testimonial {
-        id
-        stage
-        updatedAt
-      }
-      ... on Grid {
-        id
-        stage
-        updatedAt
-        Grid_title: title
-        Grid_slug: slug
-      }
-      ... on LogoCloud {
-        id
-        stage
-        updatedAt
-        LogoCloud_title: title
-      }
-    }
-    createdAt
-    hero {
-      id
-      stage
-      updatedAt
-      slug
-    }
-    id
-    marketing(first: 500) {
-      ... on Banner {
-        id
-        stage
-        updatedAt
-        Banner_slug: slug
-      }
-      ... on MarketingSignup {
-        id
-        stage
-        updatedAt
-        MarketingSignup_title: title
-      }
-    }
-    navigationLabel
-    pageType
-    publishedAt
-    slug
-    updatedAt
-    seo {
-      id
-      title
-      description
-      keywords
-      image {
-        id
-        stage
-        updatedAt
-        alt
-        handle
-        fileName
-        mimeType
-        width
-        height
-        size
-        url
-      }
-      noIndex
-    }
-  }
-}
+         gql`
+            query MarketingLandingPageQuery($slug: String!, $stage: Stage!) {
+               marketingLandingPage(stage: $stage, where: { slug: $slug }) {
+                  id
+                  stage
+                  title
+                  subtitle
+                  updatedAt
+                  createdBy {
+                     entryId: id
+                     name
+                     picture
+                     kind
+                     isActive
+                  }
+                  updatedBy {
+                     entryId: id
+                     name
+                     picture
+                     kind
+                     isActive
+                  }
+                  blocks(first: 500) {
+                     ... on Testimonial {
+                        id
+                        stage
+                        updatedAt
+                     }
+                     ... on Grid {
+                        id
+                        stage
+                        updatedAt
+                        Grid_title: title
+                        Grid_slug: slug
+                     }
+                     ... on LogoCloud {
+                        id
+                        stage
+                        updatedAt
+                        LogoCloud_title: title
+                     }
+                  }
+                  createdAt
+                  hero {
+                     id
+                     stage
+                     updatedAt
+                     slug
+                  }
+                  id
+                  marketing(first: 500) {
+                     ... on Banner {
+                        id
+                        stage
+                        updatedAt
+                        Banner_slug: slug
+                     }
+                     ... on MarketingSignup {
+                        id
+                        stage
+                        updatedAt
+                        MarketingSignup_title: title
+                     }
+                  }
+                  navigationLabel
+                  pageType
+                  publishedAt
+                  slug
+                  updatedAt
+                  seo {
+                     id
+                     title
+                     description
+                     keywords
+                     image {
+                        id
+                        stage
+                        updatedAt
+                        alt
+                        handle
+                        fileName
+                        mimeType
+                        width
+                        height
+                        size
+                        url
+                     }
+                     noIndex
+                  }
+               }
+            }
          `,
          {
             variables: {
