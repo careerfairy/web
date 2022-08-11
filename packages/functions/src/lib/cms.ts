b16000fac092efa44f762780e2b51dd75c4d3027
Habib Kadiri <habib@careerfairy.io>
2022-08-12 01:30:34 +0200

    added full sync between hygraph and firebase fields of studies

--- packages/functions/src/lib/cms.ts
+++ packages/functions/src/lib/cms.ts
@@ -1,10 +1,15 @@
 import { admin } from "../api/firestoreAdmin"
 
-export const getFieldsOfStudy = async () => {
-   const ref = await admin.firestore().collection("fieldsOfStudy").get()
+export const getFieldOfStudyById = async (fieldOfStudyId: string) => {
+   const ref = await admin
+      .firestore()
+      .collection("fieldsOfStudy")
+      .doc(fieldOfStudyId)
+      .get()
 
-   return ref.docs.map((doc) => ({
-      ...doc.data(),
-      id: doc.id,
-   }))
+   if (!ref.exists) {
+      return null
+   }
+
+   return { ...ref.data(), id: ref.id }
 }
