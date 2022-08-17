2d62c31e43cdb0819f5c302b30198a3899b74b08
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-08-17 11:18:59 +0100

    Updated livestream forms to add target students

---
+++ apps/web/components/views/draftStreamForm/FieldsOfStudyMultiSelector.tsx
@@ -0,0 +1,39 @@
+import { useFieldsOfStudy } from "../../custom-hook/useCollection"
+import { FieldOfStudy } from "@careerfairy/shared-lib/dist/fieldOfStudy"
+import MultiListSelect from "../common/MultiListSelect"
+
+type Props = {
+   selectedItems: FieldOfStudy[]
+   setFieldValue: (name, value) => void
+}
+
+const FieldsOfStudyMultiSelector = ({
+   selectedItems,
+   setFieldValue,
+}: Props) => {
+   const { isLoading, data: allFieldsOfStudy } = useFieldsOfStudy()
+
+   if (isLoading) {
+      return <div>Loading..</div>
+   }
+
+   return (
+      <MultiListSelect
+         inputName="targetFieldsOfStudy"
+         selectedItems={selectedItems}
+         setFieldValue={setFieldValue}
+         allValues={allFieldsOfStudy}
+         limit={5}
+         inputProps={{
+            label: "Select Fields of Study",
+            placeholder: "Select up to 5 Fields of Study",
+         }}
+         chipProps={{
+            variant: "outlined",
+         }}
+         getValueFn={(value) => value}
+      />
+   )
+}
+
+export default FieldsOfStudyMultiSelector
