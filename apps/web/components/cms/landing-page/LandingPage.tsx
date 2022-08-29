cf02a407e2bddabbcdbfea9cde3010d6cf6199c8
goncaloarnauth <goncaloarnauth@outlook.com>
2022-08-29 18:19:48 +0100

    Add button to Landing Page selector

--- apps/web/components/cms/landing-page/LandingPage.tsx
+++ apps/web/components/cms/landing-page/LandingPage.tsx
@@ -3,10 +3,17 @@ import {
    HygraphResponseHero,
 } from "../../../types/cmsTypes"
 import { sxStyles } from "../../../types/commonTypes"
-import React from "react"
+import React, { useCallback } from "react"
 import { Hero } from "../blocks"
-import { MenuItem, TextField, Box } from "@mui/material"
-import Link from "../../views/common/Link"
+import {
+   MenuItem,
+   TextField,
+   Box,
+   CircularProgress,
+   Button,
+} from "@mui/material"
+import { Formik } from "formik"
+import { useRouter } from "next/router"
 
 type Props = {
    fieldsOfStudy: HygraphRemoteFieldOfStudyResponse[]
@@ -34,39 +41,101 @@ const styles = sxStyles({
    selectorItem: {
       color: "black",
    },
+   button: {
+      mt: 2,
+      width: { xs: "100%", md: "auto" },
+      minWidth: "150px",
+   },
 })
 
+const initialValues = { selectedFieldOfStudyName: "" }
+
 const LandingPage = ({ fieldsOfStudy, hero }: Props) => {
+   const { push } = useRouter()
+
+   const handleSubmit = useCallback(
+      ({ selectedFieldOfStudyName }) => {
+         const fieldOfStudy = fieldsOfStudy.find(
+            ({ fieldOfStudyId }) =>
+               fieldOfStudyId.toUpperCase() ===
+               selectedFieldOfStudyName.toUpperCase()
+         )
+
+         void push(
+            `/landing/${fieldOfStudy.marketingLandingPage?.slug}?fieldOfStudyId=${fieldOfStudy.fieldOfStudyId}`
+         )
+      },
+      [fieldsOfStudy, push]
+   )
+
    return (
       <Hero {...hero}>
          {fieldsOfStudy.length > 0 && (
             <Box sx={styles.wrapper}>
-               <TextField
-                  select
-                  inputProps={{
-                     sx: styles.input,
-                  }}
-                  InputLabelProps={{ shrink: false }}
-                  id="fieldOfStudy"
-                  name="fieldOfStudy"
-                  label={hero?.selectorLabel}
-                  fullWidth
-                  variant="filled"
-                  sx={styles.selector}
+               <Formik
+                  initialValues={initialValues}
+                  enableReinitialize
+                  onSubmit={handleSubmit}
                >
-                  {fieldsOfStudy.map((entry) => (
-                     <Link
-                        href={`/landing/${entry.marketingLandingPage?.slug}?fieldOfStudyId=${entry.fieldOfStudyId}`}
-                        noLinkStyle
-                        key={entry.fieldOfStudyId}
-                        sx={styles.selectorItem}
-                     >
-                        <MenuItem value={entry.fieldOfStudyId}>
-                           {entry.fieldOfStudyName}
-                        </MenuItem>
-                     </Link>
-                  ))}
-               </TextField>
+                  {({
+                     values,
+                     handleChange,
+                     handleBlur,
+                     handleSubmit,
+                     isSubmitting,
+                  }) => (
+                     <form onSubmit={handleSubmit}>
+                        <TextField
+                           select
+                           inputProps={{
+                              sx: styles.input,
+                           }}
+                           id="selectedFieldOfStudyName"
+                           name="selectedFieldOfStudyName"
+                           label={hero?.selectorLabel}
+                           fullWidth
+                           variant="filled"
+                           sx={styles.selector}
+                           value={values.selectedFieldOfStudyName}
+                           onChange={handleChange}
+                           onBlur={handleBlur}
+                           disabled={isSubmitting}
+                        >
+                           {fieldsOfStudy.map((entry) => (
+                              <MenuItem
+                                 key={entry.fieldOfStudyId}
+                                 value={entry.fieldOfStudyId}
+                              >
+                                 {entry.fieldOfStudyName}
+                              </MenuItem>
+                           ))}
+                        </TextField>
+
+                        <Box>
+                           <Button
+                              type="submit"
+                              fullWidth
+                              variant="contained"
+                              disabled={
+                                 values.selectedFieldOfStudyName.length === 0 ||
+                                 isSubmitting
+                              }
+                              endIcon={
+                                 isSubmitting && (
+                                    <CircularProgress
+                                       size={20}
+                                       color="inherit"
+                                    />
+                                 )
+                              }
+                              sx={styles.button}
+                           >
+                              {hero.selectorLabel || "Send"}
+                           </Button>
+                        </Box>
+                     </form>
+                  )}
+               </Formik>
             </Box>
          )}
       </Hero>
