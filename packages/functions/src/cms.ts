fc9ef1784423a3d93df6ee1a0d1cad7741a094d1
Habib Kadiri <habib@careerfairy.io>
2022-08-30 19:37:59 +0200

    removed fieldsOfStudy cloud function

--- packages/functions/src/cms.ts
+++ packages/functions/src/cms.ts
@@ -1,10 +1,6 @@
 import functions = require("firebase-functions")
 import { isLocalEnvironment, logGraphqlErrorAndThrow } from "./util"
 import HygraphClient from "./api/hygraph"
-import * as config from "./config"
-import { getFieldsOfStudy } from "./lib/cms"
-
-const SECRET = "bizzy"
 
 export const syncFieldsOfStudyToHygraph = functions
    // Make the secret available to this function
@@ -51,30 +47,4 @@ export const syncFieldsOfStudyToHygraph = functions
             e
          )
       }
-   })
-
-
-/**
- * Return the list of field of studies
- * This endpoint is used by Hygraph cms (custom landing pages)
- *
- * A secret header is used to hide the content from internet crawlers
- * Request needs to have the header secret: $SECRET
- */
-export const fieldsOfStudy = functions
-   .region(config.region)
-   .https.onRequest(async (req, res) => {
-      if (req.method !== "GET") {
-         res.status(401).end()
-         return
-      }
-
-      if (req.get("secret") !== SECRET) {
-         res.status(401).end()
-         return
-      }
-
-      const fieldsOfStudy = await getFieldsOfStudy()
-
-      res.send(fieldsOfStudy)
    })
