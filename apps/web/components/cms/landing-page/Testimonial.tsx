38c2d1366ed0b21eb439726f9677a0c0f32154da
goncaloarnauth <goncaloarnauth@outlook.com>
2022-08-30 11:14:00 +0100

    Add bubble background for the testimonial and update layout

--- apps/web/components/cms/landing-page/Testimonial.tsx
+++ apps/web/components/cms/landing-page/Testimonial.tsx
@@ -1,6 +1,7 @@
 import { Rating, Typography } from "@mui/material"
 import Box from "@mui/material/Box"
 import { sxStyles } from "../../../types/commonTypes"
+import { quoteBackgroundLightPink } from "../../../constants/images"
 
 type Props = {
    title: string
@@ -13,40 +14,47 @@ const styles = sxStyles({
       color: (theme) => theme.palette.primary.main,
    },
    wrapper: {
-      paddingY: 6,
-      marginX: {
-         xs: 4,
-         md: 32,
-         lg: 52,
-         xl: 83,
-      },
+      paddingTop: 10,
+      maxWidth: { xs: "80vw", md: "60vw", lg: "50vw", xl: "30vw" },
+      height: "fit-content",
+   },
+   background: {
+      height: "60vh",
+      background: `url(${quoteBackgroundLightPink}) no-repeat`,
+      backgroundSize: "110vh",
+      backgroundPosition: "center",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
    },
 })
 
 const Testimonial = ({ title, content, rating }: Props): JSX.Element => {
    return (
-      <Box sx={styles.wrapper}>
-         <Typography variant="h2">{title}</Typography>
-         {content && (
-            <Typography variant="body1" marginY={3}>
-               {content}
-            </Typography>
-         )}
-         {rating && (
-            <Box display={"flex"}>
-               <Rating
-                  readOnly
-                  name={"testimonial-rating"}
-                  value={Number(rating)}
-                  precision={0.5}
-                  size="large"
-                  sx={styles.stars}
-               />
-               <Typography variant="h5" ml={1} alignSelf="flex-end">
-                  {rating}
+      <Box sx={styles.background}>
+         <Box sx={styles.wrapper}>
+            <Typography variant="h2">{title}</Typography>
+            {content && (
+               <Typography variant="body1" marginY={3}>
+                  {content}
                </Typography>
-            </Box>
-         )}
+            )}
+            {rating && (
+               <Box display={"flex"}>
+                  <Rating
+                     readOnly
+                     name={"testimonial-rating"}
+                     value={Number(rating)}
+                     precision={0.5}
+                     size="large"
+                     sx={styles.stars}
+                  />
+                  <Typography variant="h5" ml={1} alignSelf="flex-end">
+                     {rating}
+                  </Typography>
+               </Box>
+            )}
+         </Box>
       </Box>
    )
 }
