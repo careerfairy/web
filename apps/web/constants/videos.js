dfcce579823658e7883117a89faf0f31b2cc94db
goncaloarnauth <goncaloarnauth@outlook.com>
2022-09-13 11:15:49 +0100

    Update smilingStreamerVideoUrl for Zippy video

--- apps/web/constants/videos.js
+++ apps/web/constants/videos.js
@@ -1,4 +1,4 @@
 export const smilingStreamerVideoUrl =
-   "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/landing-videos%2Fengaged-streamer.mp4?alt=media&token=d54f257b-7cca-455e-ada3-ccfd4f259b09"
+   "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/landing-videos%2Fzippy-waving-video.mp4?alt=media&token=d54f257b-7cca-455e-ada3-ccfd4f259b09"
 export const analyticsShowCaseVideoUrl =
    "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/landing-videos%2Fanalytics-showcase.mp4?alt=media&token=fd11cee1-2a3e-4084-822e-965018159adc"
