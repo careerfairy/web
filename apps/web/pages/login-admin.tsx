8c2798a27ec72e675215c37d923a519ba016ad8e
habib <kadirit@hotmail.com>
2022-09-26 17:10:58 +0200

    removed serverside validation for admin login page

--- apps/web/pages/login-admin.tsx
+++ apps/web/pages/login-admin.tsx
@@ -1,32 +1,11 @@
 import React from "react"
 import LogInForm from "../components/views/login/LoginForm"
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
-import { groupRepo } from "../data/RepositoryInstances"
 import LoginLayout from "../layouts/LoginLayout"
-import InvalidInviteDisplay from "../components/views/common/auth/InvalidInviteDisplay"
 
-type Props = InferGetServerSidePropsType<typeof getServerSideProps>
-const LogInPage = ({ validInvite }: Props) => (
+const LogInPage = () => (
    <LoginLayout>
-      {validInvite ? (
-         <LogInForm groupAdmin />
-      ) : (
-         <InvalidInviteDisplay type={"login"} />
-      )}
+      <LogInForm groupAdmin />
    </LoginLayout>
 )
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-   const { groupDashboardInviteId } = context.query
-
-   const validInvite = await groupRepo.getGroupDashboardInviteById(
-      groupDashboardInviteId as string
-   )
-   return {
-      props: {
-         validInvite: !!validInvite,
-      },
-   }
-}
 
 export default LogInPage
