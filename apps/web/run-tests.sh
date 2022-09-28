908872f988596488b6f2de9370dbe559c30e87a4
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-09-28 13:59:37 +0100

    force playwright logs upload

--- apps/web/run-tests.sh
+++ apps/web/run-tests.sh
@@ -2,8 +2,8 @@
 #docker run -p 9323:9323 -v $(pwd)/apps/web/:/app/apps/web -it --entrypoint "" -e DEBUG=pw:webserver tests \
 #  npm run test:e2e-webapp -w @careerfairy/webapp -- --project=firefox apps/web/tests/e2e/pages/livestream-registration.spec.ts
 
-#docker run -p 9323:9323 -p 8080:8080 -v $(pwd)/apps/web/:/app/apps/web -it --entrypoint "" -e DEBUG=pw:webserver tests \
-#  npm run test:e2e-webapp -w @careerfairy/webapp -- --project=firefox -g "livestream has already started, confirm the redirection without any registration"
+docker run -p 9323:9323 -p 8080:8080 -v $(pwd)/apps/web/:/app/apps/web -it --entrypoint "" -e DEBUG=pw:webserver tests \
+  npm run test:e2e-webapp -w @careerfairy/webapp -- -g "Streamer hand raise functionality"
 
-docker run -p 9323:9323 -v $(pwd)/apps/web/:/app/apps/web -it --entrypoint "" -e DEBUG=pw:webserver tests \
-  npm run test:e2e-webapp -w @careerfairy/webapp -- --project=chromium
+#docker run -p 9323:9323 -v $(pwd)/apps/web/:/app/apps/web -it --entrypoint "" -e DEBUG=pw:webserver tests \
+#  npm run test:e2e-webapp -w @careerfairy/webapp -- --project=firefox
