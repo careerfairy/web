6df89bfe319030c468a77f4a623d67d86e3ca3ab
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-09-28 11:24:59 +0100

    Refactored registration context

--- apps/web/run-tests.sh
+++ apps/web/run-tests.sh
@@ -6,4 +6,4 @@
 #  npm run test:e2e-webapp -w @careerfairy/webapp -- --project=firefox -g "livestream has already started, confirm the redirection without any registration"
 
 docker run -p 9323:9323 -v $(pwd)/apps/web/:/app/apps/web -it --entrypoint "" -e DEBUG=pw:webserver tests \
-  npm run test:e2e-webapp -w @careerfairy/webapp
+  npm run test:e2e-webapp -w @careerfairy/webapp -- --project=chromium
