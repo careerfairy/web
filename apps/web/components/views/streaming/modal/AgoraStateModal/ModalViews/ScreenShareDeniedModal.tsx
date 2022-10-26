ff9d33bf3184b037d0009ce2cdbf3d92884efdf9
habib <kadirit@hotmail.com>
2022-10-26 15:06:28 +0200

    Added link to screen share issues article for screen share denied dialog

--- apps/web/components/views/streaming/modal/AgoraStateModal/ModalViews/ScreenShareDeniedModal.tsx
+++ apps/web/components/views/streaming/modal/AgoraStateModal/ModalViews/ScreenShareDeniedModal.tsx
@@ -7,6 +7,24 @@ import ScreenShareIcon from "@mui/icons-material/ScreenShare"
 import { useDispatch, useSelector } from "react-redux"
 import * as actions from "store/actions"
 import RootState from "store/reducers"
+import Box from "@mui/material/Box"
+import { sxStyles } from "../../../../../../types/commonTypes"
+
+const styles = sxStyles({
+   title: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   icon: {
+      mr: 1,
+   },
+   link: {
+      color: "primary.main",
+      display: "inline",
+      fontWeight: "bold",
+   },
+})
 
 const ScreenShareDeniedModal = () => {
    const dispatch = useDispatch()
@@ -20,23 +38,32 @@ const ScreenShareDeniedModal = () => {
 
    return (
       <Dialog open={screenSharePermissionDenied}>
-         <DialogTitle
-            sx={{
-               display: "flex",
-               justifyContent: "center",
-               alignItems: "center",
-            }}
-         >
-            <ScreenShareIcon sx={{ mr: 1 }} />
+         <DialogTitle sx={styles.title}>
+            <ScreenShareIcon sx={styles.icon} />
             Screen Share Not Allowed
          </DialogTitle>
          <DialogContent dividers>
-            <DialogContentText>
+            <DialogContentText align={"center"}>
                Unfortunately, your browser was not allowed to share your screen.
                If you did not actively cancel screen sharing, this indicates
                that your browser is missing the necessary permission for screen
-               sharing. Please check your system's Security & Privacy settings
-               to authorize your browser to share your screen.
+               sharing.
+               <br />
+               <br />
+               Please check your system's Security & Privacy settings to
+               authorize your browser to share your screen or{" "}
+               <Box
+                  sx={styles.link}
+                  component={"a"}
+                  href={
+                     "https://support.careerfairy.io/en/article/screen-sharing-issues-ctnit3"
+                  }
+                  target={"_blank"}
+                  rel="noreferrer"
+               >
+                  read our documentation
+               </Box>{" "}
+               on how to fix this issue
             </DialogContentText>
          </DialogContent>
          <DialogActions>
