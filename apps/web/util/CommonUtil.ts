6479a07d671bf52d369d8862de503fd63734cf41
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-11-04 14:49:33 +0000

    Added isMobileBrowser helper

--- apps/web/util/CommonUtil.ts
+++ apps/web/util/CommonUtil.ts
@@ -248,3 +248,14 @@ export const getDictValues = <K extends keyof any, T>(
       ? valueKeys.map((stepId) => dict[stepId]).filter(Boolean)
       : Object.values(dict)
 }
+
+/**
+ * Check if the browser is from a mobile device
+ */
+export const isMobileBrowser = () => {
+   const toMatch = [/Android/i, /webOS/i, /iPhone/i, /iPad/i]
+
+   return toMatch.some((toMatchItem) => {
+      return navigator?.userAgent?.match(toMatchItem)
+   })
+}
