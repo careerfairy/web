ad5b088a2dd5d74856334ca2c9f7845925f0ec0a
habib <kadirit@hotmail.com>
2022-11-11 11:49:07 +0100

    add the option to download participants data in the events table

---
+++ apps/web/components/useLivestreamCsvData.tsx
@@ -0,0 +1,189 @@
+import StatsUtil from "data/util/StatsUtil"
+import React, { useCallback, useEffect, useMemo, useState } from "react"
+import { CircularProgress } from "@mui/material"
+import RegisteredUsersIcon from "@mui/icons-material/People"
+import ButtonWithHint from "./views/group/admin/events/events-table/ButtonWithHint"
+import { useTheme } from "@mui/material/styles"
+import {
+   LivestreamEvent,
+   LivestreamUserAction,
+} from "@careerfairy/shared-lib/dist/livestreams"
+import { livestreamRepo } from "../data/RepositoryInstances"
+import { CSVDownloadUserData } from "@careerfairy/shared-lib/dist/users"
+import { useGroup } from "../layouts/GroupDashboardLayout"
+import { CSVDialogDownload } from "./custom-hook/useMetaDataActions"
+import { errorLogAndNotify } from "../util/CommonUtil"
+
+interface MetaDataActionsProps {
+   // The type of user we are downloading data for
+   userType: LivestreamUserAction
+   // Whether the user can download the csv
+   canDownload?: boolean
+   // The target stream we are downloading data for
+   targetStream?: LivestreamEvent
+}
+
+export function useLivestreamCsvData({
+   userType,
+   canDownload,
+   targetStream,
+}: MetaDataActionsProps) {
+   const title = getTitle(userType)
+   const description = getDescription(userType)
+
+   const { groupPresenter, groupQuestions, group } = useGroup()
+
+   const theme = useTheme()
+
+   // Dictionary of livestreams to their respective csv data
+   const [userDataDictionary, setUserDataDictionary] = useState<
+      Record<string, CSVDownloadUserData[]>
+   >({})
+
+   // Dictionary of loading states for each stream
+   const [loadingData, setLoadingData] = useState<Record<string, boolean>>({})
+
+   useEffect(() => {
+      if (targetStream) {
+         ;(async function fetchUsers() {
+            setLoadingData((prevState) => ({
+               ...prevState,
+               [targetStream.id]: true,
+            }))
+            const targetUsers = userDataDictionary[targetStream.id]
+            try {
+               if (!targetUsers) {
+                  let users = await livestreamRepo.getLivestreamUsers(
+                     targetStream.id,
+                     userType
+                  )
+                  // We only filter out the users if the group is a university group and the usertype is not talent pool
+                  // This is because we want to show all the users in the talent pool no matter what kind of group they are (request from sales)
+                  if (group.universityCode && userType !== "talentPool") {
+                     users = users.filter((data) =>
+                        groupPresenter.isUniversityStudent(data.user)
+                     )
+                  }
+                  const csvData = StatsUtil.getCsvData(
+                     group,
+                     targetStream,
+                     users,
+                     groupQuestions
+                  )
+                  setUserDataDictionary({
+                     ...userDataDictionary,
+                     [targetStream.id]: csvData,
+                  })
+               }
+            } catch (e) {
+               errorLogAndNotify(e, {
+                  message: "Failed to fetch livestream users for csv",
+                  targetStream,
+                  userType,
+                  group,
+               })
+            }
+            setLoadingData((prevState) => ({
+               ...prevState,
+               [targetStream.id]: false,
+            }))
+         })()
+      }
+   }, [
+      targetStream,
+      groupQuestions,
+      userDataDictionary,
+      userType,
+      group,
+      groupPresenter,
+   ])
+
+   const action = useCallback(
+      (rowData) => {
+         const actionLoading = loadingData[rowData?.id]
+         const targetUserData = userDataDictionary[rowData?.id]
+         const canDownloadUserData = Boolean(canDownload)
+         const hintTitle = `Download ${title}`
+
+         return {
+            icon: actionLoading ? (
+               <CircularProgress size={15} color="inherit" />
+            ) : (
+               <RegisteredUsersIcon color="action" />
+            ),
+            hintTitle,
+            hintDescription: description,
+            tooltip: targetUserData
+               ? `Download ${title}`
+               : actionLoading
+               ? `Getting ${title}...`
+               : `Get ${title}`,
+            onClick: () => {},
+            hidden: !canDownloadUserData,
+            disabled: actionLoading || !canDownloadUserData,
+            loadedButton: targetUserData && (
+               <CSVDialogDownload
+                  title={hintTitle}
+                  data={targetUserData}
+                  filename={`${title} ${rowData?.company} ${rowData?.id}.csv`}
+               >
+                  <ButtonWithHint
+                     startIcon={<RegisteredUsersIcon color="action" />}
+                     hintTitle={hintTitle}
+                     style={{
+                        marginTop: theme.spacing(0.5),
+                     }}
+                     hintDescription={description}
+                  >
+                     Download {title}
+                  </ButtonWithHint>
+               </CSVDialogDownload>
+            ),
+         }
+      },
+      [loadingData, userDataDictionary, canDownload, title, description, theme]
+   )
+
+   const getNumberOfUsers = useCallback(
+      (rowData) => {
+         const targetUserData = userDataDictionary?.[rowData?.id]
+         return targetUserData?.length
+      },
+      [userDataDictionary]
+   )
+
+   return useMemo(
+      () => ({ action, getNumberOfUsers }),
+      [action, getNumberOfUsers]
+   )
+}
+
+const getTitle = (userType: LivestreamUserAction) => {
+   switch (userType) {
+      case "talentPool":
+         return "Talent Pool"
+      case "jobApplications":
+         return "Job Applicants"
+      case "registered":
+         return "Registered Users"
+      case "participated":
+         return "Participants"
+      default:
+         return "Users"
+   }
+}
+
+const getDescription = (userType: LivestreamUserAction) => {
+   switch (userType) {
+      case "talentPool":
+         return "Download a CSV with the details of the students who opted to put themselves in the talent pool"
+      case "jobApplications":
+         return "Download a CSV with the details of the students who applied for a job"
+      case "registered":
+         return "Download a CSV with the details of the students who registered to your event"
+      case "participated":
+         return "Download a CSV with the details of the students who participated in your event"
+      default:
+         return "Download a CSV with the details of the students who joined your event"
+   }
+}
