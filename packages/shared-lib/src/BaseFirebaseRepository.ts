e78e23caf0fcd788f7e4449072c4464954b26adb
habib <kadirit@hotmail.com>
2022-11-14 15:19:42 +0100

    fixed import issue

--- packages/shared-lib/src/BaseFirebaseRepository.ts
+++ packages/shared-lib/src/BaseFirebaseRepository.ts
@@ -109,10 +109,12 @@ export function removeDuplicateDocuments<T extends Identifiable>(
 // max of 10 events to allow for firestore query limit
 export type FirebaseInArrayLimit = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
 
-export const dateFromFirebaseTimestamp = (timestamp?: Timestamp) => {
+export const dateFromFirebaseTimestamp = (
+   timestamp?: firebase.firestore.Timestamp
+) => {
    return timestamp?.toDate?.() || null
 }
 
 export const dateToFirebaseTimestamp = (date?: Date | number) => {
-   return date ? Timestamp.fromDate(new Date(date)) : null
+   return date ? firebase.firestore.Timestamp.fromDate(new Date(date)) : null
 }
