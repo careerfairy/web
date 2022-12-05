86b119bb1adf412430c25b534448a5d0a88dd8a2
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-12-05 11:45:32 +0000

    Added 1min cache to recommended events function call

--- packages/functions/src/recommendation.ts
+++ packages/functions/src/recommendation.ts
@@ -1,11 +1,10 @@
 import functions = require("firebase-functions")
-import {
-   logAndThrow,
-   validateData,
-   validateUserAuthExists,
-} from "./lib/validations"
-import { number, object } from "yup"
+import { logAndThrow } from "./lib/validations"
+import { number } from "yup"
 import { userEventRecommendationService } from "./api/services"
+import { middlewares } from "./middlewares/middlewares"
+import { cacheOnCallValues } from "./middlewares/cacheMiddleware"
+import { dataValidation, userAuthExists } from "./middlewares/validations"
 
 /**
  * Get Recommended Events
@@ -14,25 +13,28 @@ import { userEventRecommendationService } from "./api/services"
  * @returns {Promise<string[]>} - A list of recommended event Ids in order of relevance
  * */
 export const getRecommendedEvents = functions.https.onCall(
-   async (data, context) => {
-      try {
-         const { email } = await validateUserAuthExists(context)
-         const { limit } = await validateData(
-            data,
-            object({
-               limit: number().default(10),
+   middlewares(
+      dataValidation({
+         limit: number().default(10).max(30),
+      }),
+      userAuthExists(),
+      cacheOnCallValues(
+         "recommendedEvents",
+         (data, context) => [context.auth.token.email, data.limit],
+         60 // 1m
+      ),
+      async (data, context) => {
+         try {
+            return await userEventRecommendationService.getRecommendations(
+               context.auth.token.email,
+               data.limit
+            )
+         } catch (error) {
+            logAndThrow("Error in getting recommended events", {
+               data,
+               error,
             })
-         )
-
-         return await userEventRecommendationService.getRecommendations(
-            email,
-            limit
-         )
-      } catch (error) {
-         logAndThrow("Error in getting recommended events", {
-            data,
-            error,
-         })
+         }
       }
-   }
+   )
 )
