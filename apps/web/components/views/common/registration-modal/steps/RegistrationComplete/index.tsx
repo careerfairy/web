7a6d7503ad3aaf672e8b0d718b9c3d1125047386
goncaloarnauth <goncaloarnauth@outlook.com>
2022-12-12 09:17:42 +0000

    Add confetti zIndex

--- apps/web/components/views/common/registration-modal/steps/RegistrationComplete/index.tsx
+++ apps/web/components/views/common/registration-modal/steps/RegistrationComplete/index.tsx
@@ -99,6 +99,7 @@ const RegistrationComplete = ({ isFirstRegistration }: Props) => {
             origin: { y: isMobile ? 0.9 : 0.7 },
             decay: isMobile ? null : 0.93,
             startVelocity: isMobile ? null : 45,
+            zIndex: 99999,
          })
       }
    }, [isFirstRegistration, isMobile])
