03aca377cbd34aece72a791ad5f9275e50f95f1f
Carlos Florêncio <carlosmflorencio@gmail.com>
2022-12-21 13:51:27 +0000

    Added applicationTestCompletedAt field to group ats document

--- packages/shared-lib/src/groups/GroupATSAccount.ts
+++ packages/shared-lib/src/groups/GroupATSAccount.ts
@@ -8,7 +8,8 @@ export class GroupATSAccount {
       public readonly image?: string,
       public readonly slug?: string,
       public readonly lastFetchedAt?: Date,
-      public readonly firstSyncCompletedAt?: Date
+      public readonly firstSyncCompletedAt?: Date,
+      public readonly applicationTestCompletedAt?: Date
    ) {}
 
    static createFromDocument(account: GroupATSAccountDocument) {
@@ -19,11 +20,16 @@ export class GroupATSAccount {
          account.merge.image,
          account.merge.slug,
          account.merge.lastFetchedAt?.toDate(),
-         account.merge.firstSyncCompletedAt?.toDate()
+         account.merge.firstSyncCompletedAt?.toDate(),
+         account.merge.applicationTestCompletedAt?.toDate()
       )
    }
 
    isFirstSyncComplete() {
       return Boolean(this.firstSyncCompletedAt)
+   }
+
+   isApplicationTestComplete() {
+      return Boolean(this.applicationTestCompletedAt)
    }
 }
