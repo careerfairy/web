b5941f7e9084ce665639aee4611a5c1412f3ca51
habib <kadirit@hotmail.com>
2023-01-03 14:51:48 +0100

    adds jsdoc to useBadgeStepProgress hook

--- apps/web/components/custom-hook/useBadgeStepProgress.ts
+++ apps/web/components/custom-hook/useBadgeStepProgress.ts
@@ -2,7 +2,29 @@ import { useMemo } from "react"
 import { Badge } from "@careerfairy/shared-lib/dist/badges/badges"
 import UserPresenter from "@careerfairy/shared-lib/dist/users/UserPresenter"
 
-const useBadgeStepProgress = (userPresenter: UserPresenter, badge: Badge) => {
+type Result = {
+   steps: Badge[]
+   activeStep: number
+   /*
+    * How close you are to completing the entire badge chain (0-100)
+    * */
+   percentProgress: number
+}
+
+/**
+ * Returns the steps of a badge, the active step and the percent progress
+ * This hook is designed to provide information about the progress a user has made in earning a specific badge.
+ *
+ *
+ * @param userPresenter The user presenter
+ * @param badge The badge we wish to get the progress of
+ *
+ * @returns The total steps of a badge, the currently active step the user is on and the overall percent progress to completion of all the badges in the chain
+ * */
+const useBadgeStepProgress = (
+   userPresenter: UserPresenter,
+   badge: Badge
+): Result => {
    return useMemo(() => {
       const steps = badge.getBadgesArray()
 
