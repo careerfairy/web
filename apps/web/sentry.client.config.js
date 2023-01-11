4f393ed4aee47d5f1b43be3348d79a718f756635
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-01-11 12:23:33 +0000

    Updated sentry config to take consent in consideration

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs"
+import { isConsentEnabled } from "./util/ConsentUtils"
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
@@ -12,8 +13,38 @@ Sentry.init({
       "https://6852108b71ce4fbab24839792f82fa90@o362689.ingest.sentry.io/4261031",
    // Adjust this value in production, or use tracesSampler for greater control
    tracesSampleRate: 0.05,
+   autoSessionTracking: false, // gdpr compliance
    // ...
    // Note: if you want to override the automatic release value, do not set a
    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
    // that it will also get attached to your source maps
+   beforeSend: (event) => {
+      if (isDisabled()) {
+         return null
+      }
+
+      return event
+   },
+
+   beforeBreadcrumb: (breadcrumb) => {
+      if (isDisabled()) {
+         return null
+      }
+
+      return breadcrumb
+   },
+
+   tracesSampler: () => {
+      if (isDisabled()) {
+         // Drop this transaction, by setting its sample rate to 0%
+         return 0
+      }
+
+      // Default sample rate for all others (replaces tracesSampleRate)
+      return 0.05
+   },
 })
+
+const isDisabled = () => {
+   return !isConsentEnabled("Sentry Error Monitoring")
+}
