730c85594507cd74c2d05ca38ddf18b5dbef30c7
habib <kadirit@hotmail.com>
2023-01-20 11:19:37 +0100

    refactor change wording

--- packages/shared-lib/src/groups/stats.ts
+++ packages/shared-lib/src/groups/stats.ts
@@ -7,10 +7,10 @@ export interface GroupStats extends Identifiable {
    /**
     * The overall stats of the event
     * */
-   generalStats: Stats
+   generalStats: GroupStatsMap
    universityStats: {
       // The key is the university code, and the value is the stats for that university. We exclude numberOfPeopleReached because it is not relevant for the university stats
-      [universityCode: string]: Omit<Stats, "numberOfPeopleReached">
+      [universityCode: string]: Omit<GroupStatsMap, "numberOfPeopleReached">
    }
 }
 
@@ -30,7 +30,7 @@ export const createGroupStatsDoc = <T extends string>(
    }
 }
 
-export type Stats = {
+export type GroupStatsMap = {
    // Total number of views across all live stream details pages for every livestream of the group
    numberOfPeopleReached: number
    // Total number of registrations across all live streams for every livestream of the group
@@ -43,11 +43,11 @@ type GeneralStatsKey = keyof Pick<GroupStats, "generalStats">
 
 type UniversityStatsKey = keyof Pick<GroupStats, "universityStats">
 
-export function getPropertyToUpdate<TField extends keyof Stats>(
+export function getPropertyToUpdate<TField extends keyof GroupStatsMap>(
    field: TField
 ): `${GeneralStatsKey}.${TField}`
 export function getPropertyToUpdate<
-   TField extends keyof Stats,
+   TField extends keyof GroupStatsMap,
    TUniversityCode extends string
 >(
    field: TField,
@@ -60,7 +60,7 @@ export function getPropertyToUpdate<
  * @returns The string path in dot notation to the field to update Example: universityStats.${universityCode}.numberOfRegistrations or generalStats.numberOfRegistrations
  * */
 export function getPropertyToUpdate<
-   TField extends keyof Stats,
+   TField extends keyof GroupStatsMap,
    TUniversityCode extends string | undefined
 >(
    field: TField,
