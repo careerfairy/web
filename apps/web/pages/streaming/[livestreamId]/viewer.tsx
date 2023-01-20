5711a3032a23be3ff2d3fb62b9f23522dad7805e
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-01-20 09:54:58 +0000

    feat: refactored useCloseUsercentrics hook into an external file

--- apps/web/pages/streaming/[livestreamId]/viewer.tsx
+++ apps/web/pages/streaming/[livestreamId]/viewer.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from "react"
+import React from "react"
 import Head from "next/head"
 import dynamic from "next/dynamic"
 import StreamingLoader from "../../../components/views/loader/StreamingLoader"
-import { useRouter } from "next/router"
-import { closePrivacyWall } from "util/ConsentUtils"
+import { useCloseUsercentrics } from "components/custom-hook/consent/useCloseUsercentrics"
 
 const ViewerLayout = dynamic(() => import("../../../layouts/ViewerLayout"), {
    ssr: false,
@@ -32,33 +31,6 @@ const ViewerPage = () => {
          </ViewerLayout>
       </React.Fragment>
    )
-}
-
-/**
- * Close the Usercentrics dialog if we're on a recording session
- */
-const useCloseUsercentrics = () => {
-   const { query } = useRouter()
-   useEffect(() => {
-      let interval: NodeJS.Timeout
-      if (query.isRecordingWindow) {
-         closePrivacyWall()
-
-         let tries = 7
-         // just to be completely sure there isn't a race condition anywhere
-         // try to close the dialog after some seconds as well
-         interval = setInterval(() => {
-            closePrivacyWall()
-            if (--tries <= 0) {
-               clearInterval(interval)
-            }
-         }, 1000)
-      }
-
-      return () => {
-         clearInterval(interval)
-      }
-   }, [query.isRecordingWindow])
 }
 
 export default ViewerPage
