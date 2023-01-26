d46a69e338c13bb3320178596d46899c61a750bb
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-01-26 10:04:56 +0100

    feat: improve typescript module resolution

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -191,6 +191,9 @@ const moduleExports = {
          },
       ]
    },
+   experimental: {
+      externalDir: true,
+   },
    images: {
       domains: [
          "firebasestorage.googleapis.com",
