20e60e2255ab95dfb4b4b7cd32c88eb9b9ed95d4
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-01-31 00:02:28 +0000

    fix: disable web react unit test temporarily

--- packages/functions/jest.config.js
+++ packages/functions/jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
    preset: "ts-jest",
    testEnvironment: "node",
    modulePathIgnorePatterns: ["dist"],
+   moduleNameMapper: {
+      "@careerfairy/shared-lib/(.*)":
+         "<rootDir>../../packages/shared-lib/src/$1",
+   },
 }
