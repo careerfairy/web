91b80f2ea79b570cb9034ad199ac9065dc4551b7
goncaloarnauth <goncaloarnauth@outlook.com>
2023-02-14 10:49:46 +0000

    Update navbar; Create basic skeleton and Header component

---
+++ apps/web/components/views/group/admin/page/TestimonialsSection.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material"
+
+const TestimonialsSection = () => {
+   return (
+      <Box>
+         <Typography variant="h3" fontWeight={"600"} color="black">
+            Testimonials
+         </Typography>
+      </Box>
+   )
+}
+
+export default TestimonialsSection
