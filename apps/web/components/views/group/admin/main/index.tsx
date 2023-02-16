a0b2aa828d5ea935383b22ec3241940be77fe6b7
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-02-16 13:01:06 +0000

    feat: main page skeleton

---
+++ apps/web/components/views/group/admin/main/index.tsx
@@ -0,0 +1,38 @@
+import { Card, CardContent, Container, Grid, Tooltip } from "@mui/material"
+import { Box } from "@mui/system"
+
+const MainPageContent = () => {
+   return (
+      <Box py={2}>
+         <Container maxWidth={false}>
+            <Grid container spacing={3}>
+               <Grid xs={12} md={6} item>
+                  <Card>
+                     <CardContent>Next Livestreams</CardContent>
+                  </Card>
+               </Grid>
+
+               <Grid xs={12} md={6} item>
+                  <Card>
+                     <CardContent>Analytics</CardContent>
+                  </Card>
+               </Grid>
+
+               <Grid xs={12} md={6} item>
+                  <Card>
+                     <CardContent>Registration Sources</CardContent>
+                  </Card>
+               </Grid>
+
+               <Grid xs={12} md={6} item>
+                  <Card>
+                     <CardContent>Feedback</CardContent>
+                  </Card>
+               </Grid>
+            </Grid>
+         </Container>
+      </Box>
+   )
+}
+
+export default MainPageContent
