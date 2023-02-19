5e538d61ef6bae5f9a44f3420c25075d65c8556f
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-02-19 23:22:31 +0000

    feat: added pagination helper

---
+++ packages/shared-lib/src/utils/pagination.ts
@@ -0,0 +1,31 @@
+export default class PaginationHelper {
+   constructor(
+      public total: number,
+      public perPageItems: number,
+      public currentPage = 1
+   ) {}
+
+   totalPages() {
+      return Math.round(this.total / this.perPageItems)
+   }
+
+   canNavigateToNext() {
+      return this.currentPage < this.total / this.perPageItems
+   }
+
+   canNavigateToPrevious() {
+      return this.currentPage > 1
+   }
+
+   nextPage() {
+      if (this.canNavigateToNext()) {
+         this.currentPage++
+      }
+   }
+
+   previousPage() {
+      if (this.canNavigateToPrevious()) {
+         this.currentPage--
+      }
+   }
+}
