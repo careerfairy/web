e26d7198a27884d54d26d94b7122b9e72bcbabbf
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-02-22 10:39:49 +0000

    fix: same height across all cards

--- apps/web/components/views/group/admin/main/analytics/AggregatedAnalytics.tsx
+++ apps/web/components/views/group/admin/main/analytics/AggregatedAnalytics.tsx
@@ -1,12 +1,12 @@
-import { LiveStreamStats } from "@careerfairy/shared-lib/livestreams/stats"
+import { GroupStats } from "@careerfairy/shared-lib/groups/stats"
 import { Box, Grid, Typography } from "@mui/material"
 import Link from "components/views/common/Link"
 import { useGroup } from "layouts/GroupDashboardLayout"
 import { ChevronRight } from "react-feather"
 import { sxStyles } from "types/commonTypes"
 import CardCustom from "../CardCustom"
-import { useMainPageContext } from "../MainPageProvider"
 import { AggregatedTalentPoolValue } from "./AggregatedTalentPoolValue"
+import AverageRegistrationsCard from "./AverageRegistrationsCard"
 
 const styles = sxStyles({
    value: {
@@ -20,56 +20,50 @@ const styles = sxStyles({
       width: "18px",
       marginLeft: "5px",
    },
+   card: {
+      width: "100%",
+   },
+   gridItem: {
+      display: "flex",
+   },
 })
 
 const AggregatedAnalytics = () => {
-   const { livestreamStats } = useMainPageContext()
-   const { group } = useGroup()
+   const { group, stats } = useGroup()
 
    return (
       <Grid container spacing={2}>
-         <Grid item xs={6}>
+         <Grid item xs={6} style={styles.gridItem}>
             <CardAnalytic
                title="Total people reached"
                tooltip="Total number of people exposed to your company"
-               value={totalGeneralStats(
-                  livestreamStats,
-                  "numberOfPeopleReached"
-               )}
+               value={totalPeopleReached(stats)}
             />
          </Grid>
 
-         <Grid item xs={6}>
+         <Grid item xs={6} style={styles.gridItem}>
             <CardAnalytic
                title="Total registered users"
-               tooltip="Total number of registrations for all your streams"
-               value={totalGeneralStats(
-                  livestreamStats,
-                  "numberOfRegistrations"
-               )}
+               tooltip="Total number of registrations to all your streams"
+               value={stats.generalStats.numberOfRegistrations}
             />
          </Grid>
 
-         <Grid item xs={6}>
+         <Grid item xs={6} style={styles.gridItem}>
             {group.atsAdminPageFlag ? (
                <CardAnalytic
                   title="Total applications"
-                  value={totalGeneralStats(
-                     livestreamStats,
-                     "numberOfApplicants"
-                  )}
+                  // @ts-ignore TODO: remove this ignore when numberOfApplications field has been added
+                  value={stats.generalStats.numberOfApplications ?? 0}
                   linkDescription={"Go to applicants"}
                   link={`/group/${group.id}/admin/ats-integration?section=1`}
                />
             ) : (
-               <CardAnalytic
-                  title="Average registrations per stream"
-                  value={averageRegistrationsPerLivestream(livestreamStats)}
-               />
+               <AverageRegistrationsCard />
             )}
          </Grid>
 
-         <Grid item xs={6}>
+         <Grid item xs={6} style={styles.gridItem}>
             <CardAnalytic
                title="Talent pool profiles"
                value={<AggregatedTalentPoolValue />}
@@ -88,7 +82,8 @@ type Props = {
    link?: string
    linkDescription?: string
 }
-const CardAnalytic = ({
+
+export const CardAnalytic = ({
    title,
    tooltip,
    value,
@@ -100,7 +95,12 @@ const CardAnalytic = ({
    ) : undefined
 
    return (
-      <CardCustom title={title} helpTooltip={tooltip} subHeader={subHeader}>
+      <CardCustom
+         sx={styles.card}
+         title={title}
+         helpTooltip={tooltip}
+         subHeader={subHeader}
+      >
          <Typography mt={1} sx={styles.value} align="right">
             {value}
          </Typography>
@@ -119,36 +119,24 @@ const SubheaderLink = ({ link, title }: { link: string; title: string }) => {
    )
 }
 
-function totalGeneralStats(
-   stats: LiveStreamStats[],
-   stat: keyof LiveStreamStats["generalStats"]
-) {
-   return (
-      stats?.reduce((acc, cur) => {
-         if (cur.generalStats[stat]) {
-            return acc + cur.generalStats[stat]
-         } else {
-            return acc
-         }
-      }, 0) ?? 0
-   )
-}
+/**
+ * Sum the total of people reached
+ *
+ * Since the numberOfPeopleReached was introduced afterwards, it still has a small value for
+ * some groups, lets sum it with the registrations until it catches up
+ */
+function totalPeopleReached(stats: GroupStats) {
+   if (
+      stats.generalStats.numberOfPeopleReached <
+      stats.generalStats.numberOfRegistrations
+   ) {
+      return (
+         stats.generalStats.numberOfPeopleReached +
+         stats.generalStats.numberOfRegistrations
+      )
+   }
 
-function averageRegistrationsPerLivestream(stats: LiveStreamStats[]) {
-   if (!stats || stats.length === 0) return 0
-
-   let len = 0
-   const sum =
-      stats.reduce((acc, cur) => {
-         if (cur.generalStats["numberOfRegistrations"]) {
-            len++
-            return acc + cur.generalStats["numberOfRegistrations"]
-         } else {
-            return acc
-         }
-      }, 0) ?? 0
-
-   return Math.round(sum / len)
+   return stats.generalStats.numberOfPeopleReached
 }
 
 export default AggregatedAnalytics
