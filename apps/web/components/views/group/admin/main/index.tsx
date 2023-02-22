452b75ea0763021aa40cab9dbbb4f4ccb332621d
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-02-22 18:39:25 +0000

    feat: make all the cards respect the height of sibling card

--- apps/web/components/views/group/admin/main/index.tsx
+++ apps/web/components/views/group/admin/main/index.tsx
@@ -1,30 +1,36 @@
 import { Card, CardContent, Container, Grid } from "@mui/material"
 import { Box } from "@mui/system"
+import { sxStyles } from "types/commonTypes"
 import AggregatedAnalytics from "./analytics/AggregatedAnalytics"
 import AggregatedFeedbackCard from "./feedback/AggregatedFeedbackCard"
+import { AggregatedRegistrationSourcesCard } from "./registration-sources/AggregatedRegistrationSourcesCard"
+
+const styles = sxStyles({
+   gridItem: {
+      display: "flex",
+   },
+})
 
 const MainPageContent = () => {
    return (
       <Box py={2}>
          <Container maxWidth={false}>
             <Grid container spacing={3}>
-               <Grid xs={12} md={6} item>
+               <Grid xs={12} md={6} item style={styles.gridItem}>
                   <Card>
                      <CardContent>Next Livestreams</CardContent>
                   </Card>
                </Grid>
 
-               <Grid xs={12} md={6} item>
+               <Grid xs={12} md={6} item style={styles.gridItem}>
                   <AggregatedAnalytics />
                </Grid>
 
-               <Grid xs={12} md={6} item>
-                  <Card>
-                     <CardContent>Registration Sources</CardContent>
-                  </Card>
+               <Grid xs={12} md={6} item style={styles.gridItem}>
+                  <AggregatedRegistrationSourcesCard />
                </Grid>
 
-               <Grid xs={12} md={6} item>
+               <Grid xs={12} md={6} item style={styles.gridItem}>
                   <AggregatedFeedbackCard />
                </Grid>
             </Grid>
