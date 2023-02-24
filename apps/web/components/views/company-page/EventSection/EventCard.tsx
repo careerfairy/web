c59f16f8d739b9e4412fc92b82fa15ecd21fcd64
goncaloarnauth <goncaloarnauth@outlook.com>
2023-02-24 12:32:18 +0000

    Add event section; Create new carousel and event card

---
+++ apps/web/components/views/company-page/EventSection/EventCard.tsx
@@ -0,0 +1,91 @@
+import { sxStyles } from "../../../../types/commonTypes"
+import { LivestreamEvent } from "@careerfairy/shared-lib/livestreams"
+import { Box, Button, Stack, Typography } from "@mui/material"
+import { getResizedUrl } from "../../../helperFunctions/HelperFunctions"
+import { placeholderBanner } from "../../../../constants/images"
+import React from "react"
+import { useCompanyPage } from "../index"
+
+const styles = sxStyles({
+   wrapper: {
+      minHeight: (theme) => theme.spacing(40),
+      width: "100%",
+      borderRadius: "12px",
+
+      pr: 1,
+   },
+   backgroundImage: {
+      width: "100%",
+      height: "158px",
+      transition: (theme) =>
+         theme.transitions.create(["height", "transform"], {
+            easing: theme.transitions.easing.easeInOut,
+            duration: theme.transitions.duration.standard,
+         }),
+      objectFit: "cover",
+      borderRadius: "12px 12px 0 0",
+   },
+   content: {
+      border: "1px solid #EDE7FD",
+      borderTop: "none",
+      borderRadius: "0 0 12px 12px",
+      p: 2,
+   },
+   button: {
+      display: "flex",
+      justifyContent: "end",
+      mt: (theme) => `${theme.spacing(3)} !important`,
+   },
+})
+
+type Props = {
+   event: LivestreamEvent
+   handleClick: (event: LivestreamEvent) => void
+}
+
+const EventCard = ({ event, handleClick }: Props) => {
+   const { group } = useCompanyPage()
+
+   return (
+      <Box sx={styles.wrapper}>
+         <Box
+            component="img"
+            sx={styles.backgroundImage}
+            src={
+               getResizedUrl(group?.bannerImageUrl, "sm") || placeholderBanner
+            }
+            alt="thumbnail"
+            loading="lazy"
+         />
+         <Box sx={styles.content}>
+            <Stack spacing={1}>
+               <Typography
+                  fontWeight={400}
+                  color={"text.secondary"}
+                  fontSize={13}
+               >
+                  December 8th, 13:00
+               </Typography>
+
+               <Typography fontWeight={600} fontSize={15}>
+                  MAHLE Trainee Talk - Discover our international trainee
+                  program
+               </Typography>
+
+               <Box sx={styles.button}>
+                  <Button
+                     size="small"
+                     variant="contained"
+                     color="primary"
+                     onClick={() => handleClick(event)}
+                  >
+                     MANAGE LIVE STREAM
+                  </Button>
+               </Box>
+            </Stack>
+         </Box>
+      </Box>
+   )
+}
+
+export default EventCard
---
+++ apps/web/components/views/company-page/EventSection/index.tsx
@@ -0,0 +1,145 @@
+import { Box, Button, Grid, IconButton, Typography } from "@mui/material"
+import NewStreamModal from "components/views/group/admin/events/NewStreamModal"
+import { StreamCreationProvider } from "../../draftStreamForm/StreamForm/StreamCreationProvider"
+import { useCompanyPage } from "../index"
+import React, { useCallback, useRef, useState } from "react"
+import { sxStyles } from "../../../../types/commonTypes"
+import { useTheme } from "@mui/material/styles"
+import { Add } from "@mui/icons-material"
+import Link from "next/link"
+import { LivestreamEvent } from "@careerfairy/shared-lib/livestreams"
+import EventCarousel from "./EventCarousel"
+import { ArrowLeft, ArrowRight } from "react-feather"
+
+const styles = sxStyles({
+   titleSection: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+   },
+   addEvent: {
+      borderRadius: "10px",
+      height: (theme) => theme.spacing(40),
+      width: (theme) => theme.spacing(35),
+      border: "dashed",
+      borderColor: (theme) => theme.palette.grey.A400,
+      fontSize: "16px",
+
+      "&:hover": {
+         border: "dashed",
+      },
+   },
+   arrowIcon: {
+      padding: 0,
+      minHeight: { xs: "25px", md: "30px" },
+      minWidth: { xs: "25px", md: "30px" },
+      ml: 2,
+   },
+})
+
+const EventSection = () => {
+   const { group } = useCompanyPage()
+   const upcomingLivestreams = [
+      { id: "1234", title: "title cenas" },
+      { id: "6789", title: "title cenas 2" },
+      { id: "14", title: "title cenas 3" },
+      { id: "1334", title: "title cenas 4" },
+   ] as LivestreamEvent[]
+   const [openDialog, setOpenDialog] = useState(false)
+   const [eventToEdit, setEventToEdit] = useState(null)
+   const { spacing } = useTheme()
+   const sliderRef = useRef(null)
+
+   const handleNext = useCallback(() => {
+      sliderRef.current?.slickNext()
+   }, [])
+
+   const handlePrev = useCallback(() => {
+      sliderRef.current?.slickPrev()
+   }, [])
+
+   const handleCloseDialog = useCallback(() => {
+      setOpenDialog(false)
+   }, [])
+
+   const handleOpenEvent = useCallback((event: LivestreamEvent) => {
+      setEventToEdit(event)
+      setOpenDialog(true)
+   }, [])
+
+   return (
+      <Box minHeight={{ md: spacing(50) }}>
+         <Box sx={styles.titleSection}>
+            <Typography variant="h4" fontWeight={"600"} color="black">
+               Next Live Stream
+            </Typography>
+            {upcomingLivestreams?.length > 2 ? (
+               <Box>
+                  <IconButton
+                     color="inherit"
+                     sx={styles.arrowIcon}
+                     onClick={handlePrev}
+                  >
+                     <ArrowLeft fontSize={"large"} />
+                  </IconButton>
+                  <IconButton
+                     color="inherit"
+                     sx={styles.arrowIcon}
+                     onClick={handleNext}
+                  >
+                     <ArrowRight fontSize={"large"} />
+                  </IconButton>
+               </Box>
+            ) : null}
+         </Box>
+         <Box mt={2}>
+            {upcomingLivestreams.length > 0 ? (
+               <Box>
+                  <Typography
+                     variant="h6"
+                     fontWeight={"400"}
+                     color="textSecondary"
+                  >
+                     Below are your published live streams, these will be shown
+                     on your company page.
+                  </Typography>
+                  <Grid container xs={12}>
+                     <EventCarousel
+                        events={upcomingLivestreams}
+                        sliderRef={sliderRef}
+                        handleOpenEvent={handleOpenEvent}
+                     />
+                  </Grid>
+               </Box>
+            ) : (
+               <Link href={`/group/${group.id}/admin/events`}>
+                  <a>
+                     <Button color="secondary" sx={styles.addEvent}>
+                        <Box>
+                           <Add sx={{ height: "58px", width: "58px" }} />
+                           <Typography variant="subtitle2" color={"black"}>
+                              Create new live stream
+                           </Typography>
+                        </Box>
+                     </Button>
+                  </a>
+               </Link>
+            )}
+         </Box>
+
+         <StreamCreationProvider>
+            <NewStreamModal
+               group={group}
+               typeOfStream={"upcoming"}
+               open={openDialog}
+               handlePublishStream={() => {}}
+               handleResetCurrentStream={() => {}}
+               currentStream={eventToEdit}
+               onClose={handleCloseDialog}
+            />
+         </StreamCreationProvider>
+      </Box>
+   )
+}
+
+export default EventSection
