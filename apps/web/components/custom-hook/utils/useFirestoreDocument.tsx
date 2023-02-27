8fc0d37b3c542e9705b377f1314e4976da8f5c4e
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-02-27 11:28:06 +0000

    chore: added converter to useFirestoreDocument hook

--- apps/web/components/custom-hook/utils/useFirestoreDocument.tsx
+++ apps/web/components/custom-hook/utils/useFirestoreDocument.tsx
@@ -1,19 +1,24 @@
+import { createGenericConverter } from "@careerfairy/shared-lib/BaseFirebaseRepository"
+import { Identifiable } from "@careerfairy/shared-lib/commonTypes"
 import { FirestoreInstance } from "data/firebase/FirebaseInstance"
 import { doc } from "firebase/firestore"
-import { ReactFireOptions, useFirestore, useFirestoreDocData } from "reactfire"
+import { ReactFireOptions, useFirestoreDocData } from "reactfire"
 
 /**
  * Subscribe a Firestore document for updates
  */
-export const useFirestoreDocument = <T,>(
+export const useFirestoreDocument = <T extends Identifiable>(
    collection: string,
    pathSegments: string[],
    options: ReactFireOptions = {
       idField: "id", // this field will be added to the firestore object
    }
 ) => {
-   // @ts-ignore
-   const docRef = doc(FirestoreInstance, collection, ...pathSegments)
+   const docRef = doc(
+      FirestoreInstance,
+      collection,
+      ...pathSegments
+   ).withConverter(createGenericConverter<T>())
 
-   return useFirestoreDocData<T>(docRef as any, options)
+   return useFirestoreDocData<T>(docRef, options)
 }
