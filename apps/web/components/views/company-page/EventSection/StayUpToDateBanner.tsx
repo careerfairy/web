61fdd6a19cfd735a41cc18189e03c356fe5c4c4a
goncaloarnauth <goncaloarnauth@outlook.com>
2023-02-28 11:06:40 +0000

    Add Stay Up-To-Date component

---
+++ apps/web/components/views/company-page/EventSection/StayUpToDateBanner.tsx
@@ -0,0 +1,62 @@
+import { Box, Button, Grid, Typography } from "@mui/material"
+import { sxStyles } from "../../../../types/commonTypes"
+import React from "react"
+import Image from "next/image"
+
+const styles = sxStyles({
+   wrapper: {
+      border: "1px solid #EDE7FD",
+      borderRadius: "12px",
+      p: 4,
+   },
+})
+
+type Props = {
+   handleFollow: () => void
+}
+
+const StayUpToDateBanner = ({ handleFollow }: Props) => {
+   return (
+      <Box sx={styles.wrapper}>
+         <Grid container>
+            <Grid item xs={3}>
+               <Image
+                  src="/illustrations/undraw_video_streaming.png"
+                  width="100px"
+                  height="90px"
+                  alt="Stay up to date illustration"
+                  objectFit={"contain"}
+               />
+            </Grid>
+            <Grid item xs={9}>
+               <Grid container spacing={2}>
+                  <Grid item>
+                     <Typography variant={"h6"} fontWeight={600}>
+                        Stay up-to-date!
+                     </Typography>
+                  </Grid>
+                  <Grid item>
+                     <Typography variant={"body1"} fontSize={14}>
+                        Atlassian Corporation does not have any upcoming live
+                        stream planned. Follow the company to show your interest
+                        and notified as soon as they publish a new stream.
+                     </Typography>
+                  </Grid>
+                  <Grid item>
+                     <Button
+                        onClick={handleFollow}
+                        variant={"contained"}
+                        color={"secondary"}
+                        size={"medium"}
+                     >
+                        Follow
+                     </Button>
+                  </Grid>
+               </Grid>
+            </Grid>
+         </Grid>
+      </Box>
+   )
+}
+
+export default StayUpToDateBanner
