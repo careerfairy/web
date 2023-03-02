2a80b404fe6d15eed93eb6310fa2c92e2d570375
habib <kadirit@hotmail.com>
2023-03-02 17:41:37 +0100

    feat(upload-banner):Added Banner upload

--- apps/web/components/views/company-page/Header/BannerIllustration.tsx
+++ apps/web/components/views/company-page/Header/BannerIllustration.tsx
@@ -3,8 +3,13 @@ import { useCompanyPage } from "../index"
 import { getResizedUrl } from "../../../helperFunctions/HelperFunctions"
 import { placeholderBanner } from "../../../../constants/images"
 import BackgroundImage from "../../../views/common/BackgroundImage"
-import { Box } from "@mui/material"
+import { Box, LinearProgress } from "@mui/material"
 import BannerUploadButton from "./BannerUploadButton"
+import useFirebaseUpload from "../../../custom-hook/useFirebaseUpload"
+import { groupRepo } from "../../../../data/RepositoryInstances"
+import useSWRMutation from "swr/mutation"
+import useSnackbarNotifications from "../../../custom-hook/useSnackbarNotifications"
+import { v4 as uuid } from "uuid"
 
 const styles = sxStyles({
    imageWrapper: {
@@ -19,12 +24,49 @@ const styles = sxStyles({
       px: 3,
       py: 2,
    },
+   progress: {
+      zIndex: 1,
+   },
 })
 const BannerIllustration = () => {
-   const { group, tabValue, changeTabValue, editMode } = useCompanyPage()
+   const { group, groupPresenter, editMode } = useCompanyPage()
+
+   const [uploadFile, uploadProgress, isUploading] = useFirebaseUpload()
+
+   const { successNotification, errorNotification } = useSnackbarNotifications()
+
+   const { trigger, isMutating } = useSWRMutation(
+      `update-group-${group.id}-banner-image`,
+      handleUpdateBannerImage,
+      {
+         onSuccess: () => {
+            successNotification("Banner image has been successfully updated")
+         },
+         onError: (err) => {
+            errorNotification(err.message)
+         },
+         throwOnError: false, // We don't want to throw an error, we want to handle it ourselves in the onError callback above
+      }
+   )
+
+   const handleUploadBannerPhoto = async (photo: File) => {
+      const bannerImageId = uuid()
+      const downloadURL = await uploadFile(
+         photo,
+         groupPresenter.getGroupBannerStorageImagePath(bannerImageId)
+      )
+      return trigger({ groupId: group.id, bannerImageUrl: downloadURL })
+   }
 
    return (
       <Box sx={styles.imageWrapper}>
+         {isUploading ? (
+            <LinearProgress
+               sx={styles.progress}
+               variant="determinate"
+               value={uploadProgress}
+            />
+         ) : null}
          <BackgroundImage
             image={
                getResizedUrl(group.bannerImageUrl, "lg") || placeholderBanner
@@ -35,10 +77,26 @@ const BannerIllustration = () => {
             backgroundImageSx={undefined}
          />
          <Box sx={styles.buttonWrapper}>
-            <BannerUploadButton />
+            {editMode ? (
+               <BannerUploadButton
+                  disabled={isUploading || isMutating}
+                  handleUploadBannerPhoto={handleUploadBannerPhoto}
+                  loading={isUploading || isMutating}
+               />
+            ) : null}
          </Box>
       </Box>
    )
 }
+
+type Arguments = {
+   groupId: string
+   bannerImageUrl: string
+}
+
+const handleUpdateBannerImage = (
+   _: string,
+   { arg: { bannerImageUrl, groupId } }: { arg: Arguments }
+) => groupRepo.updateGroupBannerPhoto(groupId, bannerImageUrl)
 
 export default BannerIllustration
