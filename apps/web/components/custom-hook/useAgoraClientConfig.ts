5b2dad57e3fea70be133a7da417fabe6f2831526
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-03-06 13:44:24 +0000

    chore: refactor use agora client events

--- apps/web/components/custom-hook/useAgoraClientConfig.ts
+++ apps/web/components/custom-hook/useAgoraClientConfig.ts
@@ -9,7 +9,6 @@ import {
 } from "agora-rtc-sdk-ng"
 import { IRemoteStream } from "types/streaming"
 import useAgoraError from "./useAgoraError"
-import { useSessionStorage } from "react-use"
 import { errorLogAndNotify } from "../../util/CommonUtil"
 
 export const SESSION_PROXY_KEY = "should-use-cloud-proxy"
@@ -23,176 +22,230 @@ export default function useAgoraClientConfig(
       downlinkNetworkQuality: 0,
       uplinkNetworkQuality: 0,
    })
-   const [sessionShouldUseCloudProxy, setSessionShouldUseProxy] =
-      useSessionStorage<boolean>(SESSION_PROXY_KEY, false)
 
    const { handleRtcError } = useAgoraError()
 
    const dispatch = useDispatch()
 
-   const handleSessionShouldUseCloudProxy = useCallback(
-      (shouldUse: boolean) => {
-         setSessionShouldUseProxy(shouldUse)
-         dispatch(actions.setSessionShouldUseCloudProxy(shouldUse))
+   const configureAgoraClient = useCallback(
+      (rtcClient: IAgoraRTCClient, streamerId: UID) => {
+         /**
+          * Occurs when the SDK automatically switches to TCP/TLS 443.
+          * This fires when the RTC is using the proxy mode (when automatically or manually enabled)
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_is_using_cloud_proxy
+          */
+         rtcClient.on("is-using-cloud-proxy", (isUsing) => {
+            logRTCEvent("is-using-cloud-proxy", isUsing)
+
+            dispatch(actions.setSessionIsUsingCloudProxy(true))
+         })
+
+         /**
+          * Occurs when the SDK automatically switches to TCP/TLS 443.
+          * This is not firing when the RTC automatically enables the proxy mode
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_join_fallback_to_proxy
+          */
+         rtcClient.on("join-fallback-to-proxy", (proxyServer) => {
+            logRTCEvent("join-fallback-to-proxy", proxyServer)
+
+            // sets the UI proxy indicator
+            dispatch(actions.setSessionShouldUseCloudProxy(true))
+         })
+
+         /**
+          * Occurs when a remote user or host joins the channel.
+          *
+          * In a live-broadcast channel, this callback indicates that a host joins the channel.
+          * In a communication channel, this callback indicates that another user joins the channel and reports the ID of that user.
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_user_joined
+          */
+         rtcClient.on("user-joined", async (remoteUser) => {
+            setRemoteStreams((prevRemoteStreams) => {
+               let cleanedRemoteStreams = removeStreamFromList(
+                  remoteUser.uid,
+                  prevRemoteStreams
+               )
+               return [...cleanedRemoteStreams, { uid: remoteUser.uid }]
+            })
+         })
+
+         /**
+          * Occurs when a remote user becomes offline.
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_user_left
+          */
+         rtcClient.on("user-left", async (remoteUser) => {
+            setRemoteStreams((prevRemoteStreams) => {
+               return removeStreamFromList(remoteUser.uid, prevRemoteStreams)
+            })
+         })
+
+         /**
+          * Occurs when the state of the connection between the SDK and
+          * the server changes.
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_connection_state_change
+          */
+         rtcClient.on(
+            "connection-state-change",
+            (curState, prevState, reason) => {
+               logRTCEvent("connection-state-change", {
+                  curState,
+                  prevState,
+                  reason,
+               })
+               dispatch(
+                  actions.setAgoraRtcConnectionState({
+                     curState,
+                     prevState,
+                     reason,
+                  })
+               )
+            }
+         )
+
+         /**
+          * Occurs when a remote user publishes an audio or video track.
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_user_published
+          */
+         rtcClient.on("user-published", async (remoteUser, mediaType) => {
+            return rtcClient
+               .subscribe(remoteUser, mediaType)
+               .then(() => {
+                  setRemoteStreams((prevRemoteStreams) => {
+                     return prevRemoteStreams.map((user) => {
+                        if (user.uid === remoteUser.uid) {
+                           if (mediaType === "audio") {
+                              user.audioTrack = remoteUser.audioTrack
+                              user.audioMuted = false
+                              try {
+                                 // We don't play the audiotrack for the screen share track if its being shared by the local
+                                 // client, else it echoes with the sound of the original media player.
+                                 if (user.uid !== `${streamerId}screen`) {
+                                    remoteUser?.audioTrack?.play?.()
+                                 }
+                              } catch (e) {
+                                 dispatch(actions.sendGeneralError(e))
+                              }
+                           } else if (mediaType === "video") {
+                              user.videoTrack = remoteUser.videoTrack
+                              user.videoMuted = false
+                           }
+                        }
+                        return user
+                     })
+                  })
+
+                  /**
+                   * To benefit from the dual streams feature (on the sender)
+                   * Set a stream fallback option to automatically switch remote video quality when network conditions degrade
+                   *
+                   * https://github.com/AgoraIO-Community/AgoraWebSDK-NG/blob/eb8a5b2ef2/Docs/en/stream_fallback.md
+                   */
+                  rtcClient
+                     .setStreamFallbackOption(remoteUser.uid, 1) // 1 - fallback to low quality video
+                     .catch((e) => {
+                        console.error(
+                           "Failed to set setStreamFallbackOption",
+                           remoteUser,
+                           e
+                        )
+                        errorLogAndNotify(e)
+                     })
+               })
+               .catch(handleRtcError)
+         })
+
+         /**
+          * Occurs when a remote user unpublishes an audio or video track.
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_user_unpublished
+          */
+         rtcClient.on("user-unpublished", async (remoteUser, mediaType) =>
+            rtcClient
+               .unsubscribe(remoteUser, mediaType)
+               .then(() => {
+                  setRemoteStreams((prevRemoteStreams) => {
+                     return prevRemoteStreams.map((user) => {
+                        if (user.uid === remoteUser.uid) {
+                           if (mediaType === "audio") {
+                              user.audioTrack = null
+                              user.audioMuted = true
+                           } else if (mediaType === "video") {
+                              user.videoTrack = null
+                              user.videoMuted = true
+                           }
+                        }
+                        return user
+                     })
+                  })
+               })
+               .catch(handleRtcError)
+         )
+
+         /**
+          * Reports the network quality of the local user.
+          *
+          * After the local user joins the channel, the SDK triggers this callback to
+          * report the uplink and downlink network conditions of the local user once every two second.
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_network_quality
+          */
+         rtcClient.on("network-quality", (networkStats) => {
+            const isEqualToCurrentValue =
+               JSON.stringify(networkStats) === JSON.stringify(networkQuality)
+
+            // prevent setting unnecessary a new value to avoid re-renders
+            if (!isEqualToCurrentValue) {
+               setNetworkQuality(getQualityObjectReference(networkStats))
+            }
+         })
+
+         /**
+          * Occurs when the type of a remote video stream changes.
+          *
+          * The SDK triggers this callback when a high-quality video stream
+          * changes to a low-quality video stream, or vice versa.
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_stream_type_changed
+          */
+         rtcClient.on("stream-type-changed", (uid, streamType) => {
+            logRTCEvent("stream-type-changed", { uid, streamType })
+         })
+
+         /**
+          * Occurs when a remote video stream falls back to an audio stream due
+          * to unreliable network conditions or switches back to video after the network conditions improve.
+          *
+          * https://api-ref.agora.io/en/video-sdk/web/4.x/interfaces/iagorartcclient.html#event_stream_fallback
+          */
+         rtcClient.on("stream-fallback", (uid, isFallbackOrRecover) => {
+            logRTCEvent("stream-fallback", { uid, isFallbackOrRecover })
+         })
       },
-      [dispatch, setSessionShouldUseProxy]
+      [dispatch, handleRtcError, networkQuality]
    )
 
    useEffect(() => {
-      handleSessionShouldUseCloudProxy(sessionShouldUseCloudProxy) // Only ever listen to this once in the app
-   }, [dispatch, handleSessionShouldUseCloudProxy, sessionShouldUseCloudProxy])
-
-   useEffect(() => {
       if (rtcClient) {
-         configureAgoraClient()
+         console.log(
+            "here calling configureAgoraClient for ",
+            rtcClient,
+            streamerId
+         )
+         configureAgoraClient(rtcClient, streamerId)
       }
-   }, [rtcClient])
+   }, [configureAgoraClient, rtcClient, streamerId])
 
    const removeStreamFromList = (
       uid: IAgoraRTCRemoteUser["uid"],
-      streamList
+      streamList: IRemoteStream[]
    ) => {
       return streamList.filter((entry) => entry.uid !== uid)
-   }
-
-   const configureAgoraClient = () => {
-      /**
-       * The RTC sdk 4.11.0 automatically switches to proxy mode after the initial request fails
-       * release notes: https://docs.agora.io/en/Video/release_web_ng?platform=Web#v4110
-       * docs: https://docs.agora.io/en/Video/API%20Reference/web_ng/interfaces/iagorartcclient.html#event_join_fallback_to_proxy
-       */
-      rtcClient.on("join-fallback-to-proxy", (proxyServer) => {
-         handleSessionShouldUseCloudProxy(true)
-      })
-      /**
-       * This emit only triggers when the RTC client join method SUCCEEDS when attempting to join a channel with proxy
-       * docs: https://docs.agora.io/en/Video/API%20Reference/web_ng/interfaces/iagorartcclient.html#event_is_using_cloud_proxy
-       * */
-      rtcClient.on("is-using-cloud-proxy", (isUsing) => {
-         const isUsingProxy = Boolean(isUsing)
-         if (isUsingProxy) {
-            handleSessionShouldUseCloudProxy(true)
-         }
-         dispatch(actions.setSessionIsUsingCloudProxy(Boolean(isUsingProxy)))
-      })
-      rtcClient.on("user-joined", async (remoteUser) => {
-         setRemoteStreams((prevRemoteStreams) => {
-            let cleanedRemoteStreams = removeStreamFromList(
-               remoteUser.uid,
-               prevRemoteStreams
-            )
-            return [...cleanedRemoteStreams, { uid: remoteUser.uid }]
-         })
-      })
-      rtcClient.on("user-left", async (remoteUser) => {
-         setRemoteStreams((prevRemoteStreams) => {
-            return removeStreamFromList(remoteUser.uid, prevRemoteStreams)
-         })
-      })
-
-      rtcClient.on("connection-state-change", (curState, prevState, reason) => {
-         dispatch(
-            actions.setAgoraRtcConnectionState({ curState, prevState, reason })
-         )
-      })
-
-      rtcClient.on("user-published", async (remoteUser, mediaType) => {
-         return rtcClient
-            .subscribe(remoteUser, mediaType)
-            .then(() => {
-               setRemoteStreams((prevRemoteStreams) => {
-                  return prevRemoteStreams.map((user) => {
-                     if (user.uid === remoteUser.uid) {
-                        if (mediaType === "audio") {
-                           user.audioTrack = remoteUser.audioTrack
-                           user.audioMuted = false
-                           try {
-                              // We don't play the audiotrack for the screen share track if its being shared by the local
-                              // client, else it echoes with the sound of the original media player.
-                              if (user.uid !== `${streamerId}screen`) {
-                                 remoteUser?.audioTrack?.play?.()
-                              }
-                           } catch (e) {
-                              dispatch(actions.sendGeneralError(e))
-                           }
-                        } else if (mediaType === "video") {
-                           user.videoTrack = remoteUser.videoTrack
-                           user.videoMuted = false
-                        }
-                     }
-                     return user
-                  })
-               })
-
-               /**
-                * To benefit from the dual streams feature (on the sender)
-                * Set a stream fallback option to automatically switch remote video quality when network conditions degrade
-                *
-                * https://github.com/AgoraIO-Community/AgoraWebSDK-NG/blob/eb8a5b2ef2/Docs/en/stream_fallback.md
-                */
-               rtcClient
-                  .setStreamFallbackOption(remoteUser.uid, 1) // 1 - fallback to low quality video
-                  .catch((e) => {
-                     console.error(
-                        "Failed to set setStreamFallbackOption",
-                        remoteUser,
-                        e
-                     )
-                     errorLogAndNotify(e)
-                  })
-            })
-            .catch(handleRtcError)
-      })
-
-      rtcClient.on("user-unpublished", async (remoteUser, mediaType) =>
-         rtcClient
-            .unsubscribe(remoteUser, mediaType)
-            .then(() => {
-               setRemoteStreams((prevRemoteStreams) => {
-                  return prevRemoteStreams.map((user) => {
-                     if (user.uid === remoteUser.uid) {
-                        if (mediaType === "audio") {
-                           user.audioTrack = null
-                           user.audioMuted = true
-                        } else if (mediaType === "video") {
-                           user.videoTrack = null
-                           user.videoMuted = true
-                        }
-                     }
-                     return user
-                  })
-               })
-            })
-            .catch(handleRtcError)
-      )
-
-      rtcClient.on("network-quality", (networkStats) => {
-         const isEqualToCurrentValue =
-            JSON.stringify(networkStats) === JSON.stringify(networkQuality)
-
-         // prevent setting unnecessary a new value to avoid re-renders
-         if (!isEqualToCurrentValue) {
-            setNetworkQuality(getQualityObjectReference(networkStats))
-         }
-      })
-
-      // https://api-ref.agora.io/en/voice-sdk/web/4.x/interfaces/iagorartcclient.html#event_stream_type_changed
-      rtcClient.on("stream-type-changed", (uid, streamType) => {
-         console.log(
-            "RTC Event stream-type-changed (stream quality changed)",
-            uid,
-            streamType
-         )
-      })
-
-      // https://api-ref.agora.io/en/voice-sdk/web/4.x/interfaces/iagorartcclient.html#event_stream_fallback
-      rtcClient.on("stream-fallback", (uid, isFallbackOrRecover) => {
-         console.log(
-            "RTC Event stream-fallback (stream toggle between audio mode only)",
-            uid,
-            isFallbackOrRecover
-         )
-      })
    }
 
    const demoStreamHandlers = useMemo(
@@ -220,6 +273,10 @@ export default function useAgoraClientConfig(
    )
 }
 
+function logRTCEvent(event: string, ...args: any[]) {
+   console.log(`RTC Event: ${event}`, ...args)
+}
+
 /**
  * Map that holds pointers to objects
  * By re-using the same object instance, we avoid re-renders
@@ -230,7 +287,7 @@ export default function useAgoraClientConfig(
  */
 const mapQualityObjectReferences = {}
 
-const getQualityObjectReference = (currentReport) => {
+const getQualityObjectReference = (currentReport: NetworkQuality) => {
    const key = `${currentReport.downlinkNetworkQuality} - ${currentReport.uplinkNetworkQuality}`
 
    if (mapQualityObjectReferences[key]) {
