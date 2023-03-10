c54d1dec4d643da6294e8293d085de9a72a0d2de
habib <kadirit@hotmail.com>
2023-03-10 11:25:35 +0100

    feat(group-stats): Add script to back-fill group stats

---
+++ packages/firebase-scripts/src/migrations/groups/backFillGroupStats/README.md
@@ -0,0 +1,19 @@
+# Back-fill Live Stream Stats
+
+Will fetch all the userLivestreamData and aggregate their stats in memory.
+Once aggregated in memory, it will SET the stats for each livestream in batches of 200.
+The script is designed to be run multiple times in case it fails or times out.
+It will only update the specific stats as opposed to overwriting the entire stats object.
+
+### Stats that are updated (general and per university code):
+
+-  numberOfParticipants
+-  numberOfRegistrations
+-  numberOfTalentPoolProfiles
+-  numberOfApplicants
+
+## Run
+
+```sh
+npm run script -w @careerfairy/firebase-scripts -- scriptPath=./migrations/groups/backFillGroupStats
+```
---
+++ packages/firebase-scripts/src/migrations/groups/backFillGroupStats/index.ts
@@ -0,0 +1,157 @@
+import Counter from "../../../lib/Counter"
+import { throwMigrationError } from "../../../util/misc"
+import { firestore } from "../../../lib/firebase"
+import { groupRepo, livestreamRepo } from "../../../repositories"
+import { writeProgressBar } from "../../../util/bulkWriter"
+import { DataWithRef } from "../../../util/types"
+import { LiveStreamStats } from "@careerfairy/shared-lib/dist/livestreams/stats"
+import { DeepPartial } from "@careerfairy/shared-lib/dist/utils/types"
+import { logAction } from "../../../util/logger"
+import { GroupStats } from "@careerfairy/shared-lib/dist/groups/stats"
+import { merge } from "lodash"
+import { pickPublicDataFromGroup } from "@careerfairy/shared-lib/dist/groups"
+
+const counter = new Counter()
+
+// types
+type LivestreamStatsWithRef = DataWithRef<true, LiveStreamStats>
+type GroupStatsToUpdate = DeepPartial<GroupStats>
+
+// cached globally
+let statsToUpdateDict: Record<
+   // livestreamId
+   string,
+   // the livestream stats we would like to update, we don't want to overwrite the stats that are not set by this script.
+   // This will allow us to run this script multiple times without overwriting the stats that are already set
+   GroupStatsToUpdate
+> = {}
+const groupStatsId = "groupStats"
+export async function run() {
+   try {
+      Counter.log("Fetching all livestreams and user livestream data")
+
+      const stats = await logAction(
+         () => livestreamRepo.getAllLivestreamStats(true),
+         "Fetching all livestream stats"
+      )
+      Counter.log(`Fetched ${stats.length} livestream stats`)
+      counter.addToReadCount(stats.length)
+      sumUpStats(stats)
+
+      await handleSaveGroupStatsInFirestore()
+   } catch (error) {
+      console.error(error)
+      throwMigrationError(error.message)
+   } finally {
+      counter.print()
+   }
+}
+
+const sumUpStats = (livestreamStats: LivestreamStatsWithRef[]) => {
+   livestreamStats.forEach((stat) => {
+      const groupIds = stat.livestream?.groupIds || []
+
+      groupIds.forEach((groupId) => {
+         if (!statsToUpdateDict[groupId]) {
+            // If stats doc doesn't exist, create it
+
+            statsToUpdateDict[groupId] = {
+               // We create a partial object to update the stats, so we don't overwrite the stats that are not set by this script
+               id: groupStatsId,
+               generalStats: {
+                  numberOfParticipants: 0,
+                  numberOfRegistrations: 0,
+                  // numberOfPeopleReached: 0, - we don't want to overwrite this
+                  // numberOfPeopleReachedCompanyPage: 0, - we don't want to overwrite this
+               },
+               universityStats: {},
+            }
+         }
+
+         // Sum up the number of participants and registrations for each group
+         statsToUpdateDict[groupId].generalStats.numberOfParticipants +=
+            stat.generalStats.numberOfParticipants
+         statsToUpdateDict[groupId].generalStats.numberOfRegistrations +=
+            stat.generalStats.numberOfRegistrations
+
+         Object.keys(stat.universityStats).forEach((universityCode) => {
+            if (!statsToUpdateDict[groupId].universityStats[universityCode]) {
+               statsToUpdateDict[groupId].universityStats[universityCode] = {
+                  numberOfRegistrations: 0,
+                  numberOfParticipants: 0,
+                  // numberOfPeopleReached: 0, - we don't want to overwrite this
+                  // numberOfPeopleReachedCompanyPage: 0 - we don't want to overwrite this
+               }
+            }
+
+            // Sum up the number of participants and registrations for each university
+            statsToUpdateDict[groupId].universityStats[
+               universityCode
+            ].numberOfParticipants +=
+               stat.universityStats[universityCode].numberOfParticipants
+            statsToUpdateDict[groupId].universityStats[
+               universityCode
+            ].numberOfRegistrations +=
+               stat.universityStats[universityCode].numberOfRegistrations
+         })
+      })
+   })
+}
+
+const handleSaveGroupStatsInFirestore = async () => {
+   let batchSize = 200 // Batch size for firestore, 200 or fewer works consistently
+
+   const totalDocs = Object.entries(statsToUpdateDict)
+   const totalNumDocs = totalDocs.length
+
+   writeProgressBar.start(totalNumDocs, 0)
+
+   for (let i = 0; i < totalNumDocs; i += batchSize) {
+      const batch = firestore.batch()
+
+      const statsData = totalDocs.slice(i, i + batchSize) // Slice the data into batches
+
+      for (const [groupId, stats] of statsData) {
+         writeProgressBar.increment() // Increment progress bar
+
+         const groupStatsRef = firestore
+            .collection("careerCenterData")
+            .doc(groupId)
+            .collection("stats")
+            .doc(groupStatsId)
+
+         // check if group stats exists
+         const [groupStatsSnap, group] = await Promise.all([
+            groupStatsRef.get(),
+            groupRepo.getGroupById(groupId),
+         ])
+
+         if (!group) {
+            Counter.log(`Group with id ${groupId} does not exist`)
+            continue
+         }
+
+         if (groupStatsSnap.exists) {
+            const groupStats = {
+               ...groupStatsSnap.data(),
+               id: groupStatsSnap.id,
+            } as GroupStats
+
+            // Perform a deep merge of the stats
+            const mergedStats = merge(groupStats, stats) // The stats that are not set by this script will not be overwritten by this merge :)
+
+            batch.update(groupStatsRef, mergedStats) // Upsert the stats data
+         } else {
+            stats.group = pickPublicDataFromGroup(group)
+            batch.set(groupStatsRef, stats) // Upsert the stats data
+         }
+
+         counter.writeIncrement() // Increment write counter
+      }
+
+      await batch.commit() // Wait for batch to commit
+   }
+
+   writeProgressBar.stop()
+   Counter.log("All batches committed! :)")
+}
