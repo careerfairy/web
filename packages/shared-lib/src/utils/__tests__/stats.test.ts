baca7676c3ab8a3c44638cf2199d5d2f6d608682
habib <kadirit@hotmail.com>
2023-03-14 17:43:37 +0100

    fix(new-stat-aggregations): Updated Livestream stats migration script, added unit tests for livestream stat operations, updated cloud functions to aggregate new points

--- packages/shared-lib/src/utils/__tests__/stats.test.ts
+++ packages/shared-lib/src/utils/__tests__/stats.test.ts
@@ -1,5 +1,8 @@
 import { getAValidGroupStatsUpdateField } from "../../groups/stats"
-import { getAValidLivestreamStatsUpdateField } from "../../livestreams/stats"
+import {
+   getAValidLivestreamStatsUpdateField,
+   NestedObjectOptions,
+} from "../../livestreams/stats"
 
 describe("getPropertyToUpdate", () => {
    it("should return the correct property path for a general stat", () => {
@@ -31,5 +34,49 @@ describe("getPropertyToUpdate", () => {
       const result = getAValidGroupStatsUpdateField(field, universityCode)
 
       expect(result).toEqual("universityStats.ucl.invalidField")
+   })
+})
+
+describe("getAValidLivestreamStatsUpdateField", () => {
+   it("should return a valid field path for general stats", () => {
+      const field = "numberOfRegistrations"
+      const result = getAValidLivestreamStatsUpdateField(field)
+      expect(result).toBe(`generalStats.${field}`)
+   })
+
+   it("should return a valid field path for university stats", () => {
+      const field = "numberOfRegistrations"
+      const options: NestedObjectOptions = {
+         statsObjectKey: "universityStats",
+         statsObjectProperty: "universityCode123",
+      }
+      const result = getAValidLivestreamStatsUpdateField(field, options)
+      expect(result).toBe(
+         `universityStats.${options.statsObjectProperty}.${field}`
+      )
+   })
+
+   it("should return a valid field path for field of study stats", () => {
+      const field = "numberOfRegistrations"
+      const options: NestedObjectOptions = {
+         statsObjectKey: "fieldOfStudyStats",
+         statsObjectProperty: "fieldOfStudyId123",
+      }
+      const result = getAValidLivestreamStatsUpdateField(field, options)
+      expect(result).toBe(
+         `fieldOfStudyStats.${options.statsObjectProperty}.${field}`
+      )
+   })
+
+   it("should return a valid field path for country stats", () => {
+      const field = "numberOfRegistrations"
+      const options: NestedObjectOptions = {
+         statsObjectKey: "countryStats",
+         statsObjectProperty: "countryCode123",
+      }
+      const result = getAValidLivestreamStatsUpdateField(field, options)
+      expect(result).toBe(
+         `countryStats.${options.statsObjectProperty}.${field}`
+      )
    })
 })
--- packages/shared-lib/src/utils/types.ts
+++ packages/shared-lib/src/utils/types.ts
@@ -11,3 +11,32 @@ export type DeepPartial<T> = T extends Function
    : T extends object
    ? DeepPartialObject<T>
    : T | undefined
+
+type FlattenObjectPathsHelper<T> = T extends Record<string, unknown>
+   ? {
+        [K in keyof T]-?: `${K & string}${FlattenObjectPathsHelper<
+           T[K]
+        > extends ""
+           ? ""
+           : "."}${FlattenObjectPathsHelper<T[K]>}`
+     }[keyof T]
+   : ""
+
+/**
+ * This utility type recursively flattens all possible paths of an object as string dot notation.
+ * @example
+ * type Obj = {
+ *   a: {
+ *     b: {
+ *       c: string;
+ *     };
+ *     d: number;
+ *   };
+ *   e: boolean;
+ * };
+ * type Paths = FlattenObjectPaths<Obj>;
+ * // type Paths = "a.b.c" | "a.d" | "e"
+ */
+export type FlattenPaths<T> = FlattenObjectPathsHelper<{
+   [K in keyof T]: T[K] // This is to convert Interfaces to mapped types
+}>
