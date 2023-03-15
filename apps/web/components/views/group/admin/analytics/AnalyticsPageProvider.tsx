b5cebac4f9d20d9f4c7c7164ebb0c43aedbac2f7
habib <kadirit@hotmail.com>
2023-03-15 18:29:36 +0100

    feat(analytics): Setup Provider and general layout

--- apps/web/components/views/group/admin/analytics/AnalyticsPageProvider.tsx
+++ apps/web/components/views/group/admin/analytics/AnalyticsPageProvider.tsx
@@ -1,15 +1,11 @@
 import { useGroup } from "layouts/GroupDashboardLayout"
-import React, { createContext, useContext, useMemo, useState } from "react"
+import React, { createContext, FC, useContext, useMemo, useState } from "react"
 import { LiveStreamStats } from "@careerfairy/shared-lib/livestreams/stats"
-import { Identifiable } from "../../../../../types/commonTypes"
-import { collection, query, where } from "firebase/firestore"
+import { collectionGroup, query, where } from "firebase/firestore"
 import { FirestoreInstance } from "../../../../../data/firebase/FirebaseInstance"
-import { ReactFireOptions } from "reactfire"
-import {
-   FieldOfStudy,
-   LevelOfStudy,
-} from "@careerfairy/shared-lib/fieldOfStudy"
+import { FieldOfStudy } from "@careerfairy/shared-lib/fieldOfStudy"
 import { useFirestoreCollection } from "../../../../custom-hook/utils/useFirestoreCollection"
+import { createLookup } from "@careerfairy/shared-lib/utils"
 
 type IAnalyticsPageContext = {
    /**
@@ -19,32 +15,57 @@ type IAnalyticsPageContext = {
     */
    livestreamStats: LiveStreamStats[] | null | undefined
    fieldsOfStudyLookup: Record<string, string>
-
-   levelsOfStudyLookup: Record<string, string>
-   setLivestreamStatsTimeFrame: React.Dispatch<typeof timeFrames[number]>
+   setLivestreamStatsTimeFrame: React.Dispatch<React.SetStateAction<TimeFrame>>
+   livestreamStatsTimeFrame: TimeFrame
 }
+export const TimeFrames = {
+   "Last 30 days": {
+      start: new Date(new Date().setDate(new Date().getDate() - 30)),
+      end: new Date(),
+   },
+   "Last 6 months": {
+      start: new Date(new Date().setDate(new Date().getDate() - 180)),
+      end: new Date(),
+   },
+   "Last 1 year": {
+      start: new Date(new Date().setDate(new Date().getDate() - 365)),
+      end: new Date(),
+   },
+   "Last 2 years": {
+      start: new Date(new Date().setDate(new Date().getDate() - 730)),
+      end: new Date(),
+   },
+   "All time": {
+      start: new Date(0),
+      end: new Date(),
+   },
+} as const
+
+export type TimeFrame = keyof typeof TimeFrames
 
 const initialValues: IAnalyticsPageContext = {
    livestreamStats: undefined,
    fieldsOfStudyLookup: {},
-   levelsOfStudyLookup: {},
    setLivestreamStatsTimeFrame: () => {},
+   livestreamStatsTimeFrame: "Last 2 years",
 }
 
 const AnalyticsPageContext = createContext<IAnalyticsPageContext>(initialValues)
 
-export const AnalyticsPageProvider = ({ children }) => {
+export const AnalyticsPageProvider: FC = ({ children }) => {
    const { group } = useGroup()
 
    const [livestreamStatsTimeFrame, setLivestreamStatsTimeFrame] =
-      useState<typeof timeFrames[number]>(defaultTimeFrame)
+      useState<TimeFrame>("Last 2 years")
 
    const livestreamStatsQuery = useMemo(() => {
+      const timeFrame = TimeFrames[livestreamStatsTimeFrame]
       return query(
-         collection(FirestoreInstance, "livestreamStats"),
+         collectionGroup(FirestoreInstance, "stats"),
+         where("id", "==", "livestreamStats"),
          where("livestream.groupIds", "array-contains", group.id),
-         where("livestream.start", ">=", livestreamStatsTimeFrame.start),
-         where("livestream.start", "<=", livestreamStatsTimeFrame.end)
+         where("livestream.start", ">=", timeFrame.start),
+         where("livestream.start", "<=", timeFrame.end)
       )
    }, [group.id, livestreamStatsTimeFrame])
 
@@ -58,44 +79,25 @@ export const AnalyticsPageProvider = ({ children }) => {
       queryOptions
    )
 
-   const { data: levelsOfStudy } = useFirestoreCollection<LevelOfStudy>(
-      "levelsOfStudy",
-      queryOptions
-   )
-
    const fieldsOfStudyLookup = useMemo(
       () => createLookup(fieldsOfStudy, "name"),
       [fieldsOfStudy]
-   )
-   const levelsOfStudyLookup = useMemo(
-      () => createLookup(levelsOfStudy, "name"),
-      [levelsOfStudy]
    )
 
    const value = useMemo<IAnalyticsPageContext>(() => {
       return {
          fieldsOfStudyLookup,
-         levelsOfStudyLookup,
          livestreamStats,
          setLivestreamStatsTimeFrame,
+         livestreamStatsTimeFrame,
       }
-   }, [fieldsOfStudyLookup, levelsOfStudyLookup, livestreamStats])
+   }, [fieldsOfStudyLookup, livestreamStats, livestreamStatsTimeFrame])
 
    return (
       <AnalyticsPageContext.Provider value={value}>
          {children}
       </AnalyticsPageContext.Provider>
    )
-}
-
-const createLookup = <T extends Identifiable>(
-   array: T[],
-   propertyName: keyof T
-): Record<string, T[keyof T]> => {
-   return array.reduce((acc, curr) => {
-      acc[curr.id] = curr[propertyName]
-      return acc
-   }, {})
 }
 
 export const useAnalyticsPageContext = () => {
@@ -108,44 +110,7 @@ export const useAnalyticsPageContext = () => {
    return context
 }
 
-type TimeFrame = {
-   start: Date
-   end: Date
-   name: string
-}
-
-// Array of timeframes for the last month,6 months, 1 year, 2 years, all time
-export const timeFrames = [
-   {
-      start: new Date(new Date().setDate(new Date().getDate() - 30)),
-      end: new Date(),
-      name: "Last 30 days",
-   },
-   {
-      start: new Date(new Date().setDate(new Date().getDate() - 180)),
-      end: new Date(),
-      name: "Last 6 months",
-   },
-   {
-      start: new Date(new Date().setDate(new Date().getDate() - 365)),
-      end: new Date(),
-      name: "Last 1 year",
-   },
-   {
-      start: new Date(new Date().setDate(new Date().getDate() - 730)),
-      end: new Date(),
-      name: "Last 2 years",
-   },
-   {
-      start: new Date(0),
-      end: new Date(),
-      name: "All time",
-   },
-] as const satisfies readonly TimeFrame[]
-
 const queryOptions = {
    suspense: true,
    idField: "id",
-} as const satisfies ReactFireOptions
-
-const defaultTimeFrame = timeFrames.find((t) => t.name === "Last 2 years")
+} as const
