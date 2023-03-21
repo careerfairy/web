b93dfa8da686d33523c6d9ef5eb04b7fbd9f5afb
habib <kadirit@hotmail.com>
2023-03-21 18:12:18 +0100

    feat(analytics): Added Live streams KPIs Overview component

---
+++ apps/web/components/views/group/admin/common/Tabs.tsx
@@ -0,0 +1,96 @@
+import { styled } from "@mui/material/styles"
+import Tabs, { tabsClasses } from "@mui/material/Tabs"
+import Tab, { tabClasses } from "@mui/material/Tab"
+import React, { FC } from "react"
+import Skeleton from "@mui/material/Skeleton"
+
+type TabOption = {
+   label: string
+   value: string
+}
+type Props = {
+   tabOptions: TabOption[] | readonly TabOption[]
+   value: string
+
+   onChange: (event: React.SyntheticEvent, value: string) => void
+}
+export const TabsComponent: FC<Props> = ({ tabOptions, onChange, value }) => {
+   return (
+      <StyledTabs
+         value={value}
+         onChange={onChange}
+         color={"secondary"}
+         aria-label="basic tabs example"
+      >
+         {tabOptions.map((option) => (
+            <StyledTab
+               key={option.value}
+               label={option.label}
+               value={option.value}
+            />
+         ))}
+      </StyledTabs>
+   )
+}
+
+const mobileTabHeight = 35
+const tabletTabHeight = 45
+
+export const StyledTabs = styled(Tabs)(({ theme }) => ({
+   [`& .${tabsClasses.indicator}`]: {
+      top: 0,
+      height: "100%",
+      borderRadius: theme.spacing(5),
+      zIndex: 0,
+      backgroundColor: theme.palette.secondary.main,
+      [theme.breakpoints.down("lg")]: {
+         // make tabs smaller on tablet
+         height: tabletTabHeight,
+      },
+      [theme.breakpoints.down("sm")]: {
+         // make tabs smaller on mobile
+         height: mobileTabHeight,
+      },
+   },
+   [`& .${tabsClasses.flexContainer}`]: {
+      width: "auto",
+      flex: "0 1 auto",
+      display: "inline-flex",
+      overflow: "hidden",
+      borderRadius: theme.spacing(5),
+      backgroundColor: theme.palette.tertiary.main,
+   },
+})) as typeof Tabs
+
+const borderRadius = 5
+export const StyledTab = styled(Tab)(({ theme }) => ({
+   zIndex: 1,
+   transition: theme.transitions.create(["color", "background-color"]),
+   borderRadius: theme.spacing(borderRadius),
+   textTransform: "none",
+   fontWeight: 600,
+   color: theme.palette.grey[500],
+   [`&.${tabClasses.selected}`]: {
+      color: "white",
+   },
+   [theme.breakpoints.down("lg")]: {
+      padding: theme.spacing(2, 1.2),
+      // make tabs smaller on tablet
+      minHeight: tabletTabHeight,
+      fontSize: theme.typography.pxToRem(12),
+   },
+   [theme.breakpoints.down("sm")]: {
+      // make tabs smaller on mobile
+      minWidth: 0,
+      minHeight: mobileTabHeight,
+      padding: theme.spacing(0.8, 1),
+      fontSize: theme.typography.pxToRem(10),
+   },
+})) as unknown as typeof Tab
+
+export const TabsSkeleton = styled(Skeleton)(({ theme }) => ({
+   display: "inline-flex",
+   borderRadius: theme.spacing(borderRadius),
+   width: 100,
+   height: tabletTabHeight,
+})) as typeof Skeleton
