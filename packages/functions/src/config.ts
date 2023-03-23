b564548f56f3573a00e6919f6843535aafc3d69c
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-03-23 15:59:00 +0000

    feat: export bundles to firesbase hosting

--- packages/functions/src/config.ts
+++ packages/functions/src/config.ts
@@ -10,6 +10,9 @@ const config = {
       livestreamCreated:
          "https://hooks.slack.com/services/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",
    },
+
+   // Firebase Hosting Domain
+   hostingUrl: "https://cdn.careerfairy.io",
 }
 
 if (process.env.NODE_ENV !== "production") {
@@ -20,6 +23,10 @@ if (process.env.NODE_ENV !== "production") {
 
    config.slackWebhooks.livestreamAlerts = testSlackIntegrationsChannel
    config.slackWebhooks.livestreamCreated = testSlackIntegrationsChannel
+
+   // Target the firebase functions emulator when loading the bundles
+   // locally, no need to use cache (firebase hosting)
+   config.hostingUrl = `http://127.0.0.1:5001/careerfairy-e1fd9/${config.region}/`
 }
 
 export default config
