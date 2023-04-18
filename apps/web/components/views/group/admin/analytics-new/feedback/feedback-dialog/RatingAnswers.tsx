7067b2e1c3d2ba160e308e9299407d319ffd630c
habib <kadirit@hotmail.com>
2023-04-18 15:00:55 +0200

    feat(feedback): added voters list with skeleton

--- apps/web/components/views/group/admin/analytics-new/feedback/feedback-dialog/RatingAnswers.tsx
+++ apps/web/components/views/group/admin/analytics-new/feedback/feedback-dialog/RatingAnswers.tsx
@@ -1,10 +1,11 @@
 import Stack from "@mui/material/Stack"
 import React, { FC } from "react"
 import { LiveStreamStats } from "@careerfairy/shared-lib/livestreams/stats"
-import { useFirestoreCollection } from "../../../../../../custom-hook/utils/useFirestoreCollection"
-import { collection, query } from "firebase/firestore"
-import { FirestoreInstance } from "../../../../../../../data/firebase/FirebaseInstance"
-import { useFirestoreDocument } from "../../../../../../custom-hook/utils/useFirestoreDocument"
+import useLivestreamRatingVoters from "./useLivestreamRatingVoters"
+import useLivestreamRating from "./useLivestreamRating"
+import { Divider, Skeleton, Typography } from "@mui/material"
+import { StyledRating } from "../../../common/inputs"
+import { normalizeRating } from "@careerfairy/shared-lib/livestreams/ratings"
 
 type FeedbackAnswersContentProps = {
    livestreamStats: LiveStreamStats
@@ -14,33 +15,75 @@ export const RatingAnswers: FC<FeedbackAnswersContentProps> = ({
    livestreamStats,
    feedbackQuestionId,
 }) => {
-   console.log("-> livestreamStats", livestreamStats)
-   console.log("-> feedbackQuestionId", feedbackQuestionId)
-   const { data: voters } = useFirestoreCollection(
-      query(
-         collection(
-            FirestoreInstance,
-            "livestreams",
-            livestreamStats.livestream.id,
-            "rating",
-            feedbackQuestionId,
-            "voters"
-         )
-      )
-   )
-   console.log("-> voters", voters)
-   const { data: feedbackQuestion } = useFirestoreDocument("livestreams", [
+   const { data: feedbackQuestion } = useLivestreamRating(
       livestreamStats.livestream.id,
-      "rating",
-      feedbackQuestionId,
-   ])
-   console.log("-> feedbackQuestion", feedbackQuestion)
+      feedbackQuestionId
+   )
 
-   return <Stack spacing={3}>User feedback answers</Stack>
+   const { data: voters } = useLivestreamRatingVoters(
+      livestreamStats.livestream.id,
+      feedbackQuestionId,
+      feedbackQuestion.hasText
+   )
+
+   return (
+      <Stack divider={<Divider flexItem />} spacing={3}>
+         {voters.map((voter) => (
+            <Stack spacing={2} key={voter.id}>
+               <Stack
+                  direction={{
+                     xs: "column",
+                     sm: "row",
+                  }}
+                  justifyContent="space-between"
+                  spacing={1}
+               >
+                  <Typography fontWeight={600} variant="body1">
+                     {voter.id}
+                  </Typography>
+                  <StyledRating
+                     value={normalizeRating(feedbackQuestion, voter)}
+                     readOnly
+                     color="primary.main"
+                  />
+               </Stack>
+               {voter.message ? (
+                  <Typography variant="body2">{voter.message}</Typography>
+               ) : null}
+            </Stack>
+         ))}
+      </Stack>
+   )
 }
 
 export const RatingAnswersSkeleton: FC = () => {
-   return <Stack spacing={3}>User feedback answers</Stack>
+   return (
+      <Stack divider={<Divider flexItem />} spacing={3}>
+         {Array.from({ length: 10 }).map((_, idx) => (
+            <Stack spacing={2} key={idx}>
+               <Stack
+                  direction={{
+                     xs: "column",
+                     sm: "row",
+                  }}
+                  justifyContent="space-between"
+                  spacing={1}
+               >
+                  <Typography fontWeight={600} variant="body1">
+                     <Skeleton variant="text" width={100} />
+                  </Typography>
+                  <StyledRating value={5} readOnly color="grey.500" />
+               </Stack>
+               <Typography variant="body2">
+                  <Skeleton
+                     variant="text"
+                     width={`calc(100% * ${Math.random() * (1 - 0.5) + 0.5})`}
+                  />
+               </Typography>
+            </Stack>
+         ))}
+      </Stack>
+   )
 }
 
 export default RatingAnswers
