27cbcf937dd690b559ed9815f4f596e59c3ad9c6
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-04-19 10:47:36 +0100

    feat: add user cv upload reward trigger

--- apps/web/components/views/profile/userData/user-resume/UserResume.js
+++ apps/web/components/views/profile/userData/user-resume/UserResume.js
@@ -8,6 +8,9 @@ import { useDispatch } from "react-redux"
 import { withFirebase } from "context/firebase/FirebaseServiceContext"
 import ContentCardTitle from "../../../../../layouts/UserLayout/ContentCardTitle"
 import UploadCvButton from "./UploadCvButton"
+import { rewardService } from "data/firebase/RewardService"
+import { errors } from "@playwright/test"
+import { errorLogAndNotify } from "util/CommonUtil"
 
 const useStyles = makeStyles((theme) => ({
    cvContainer: {
@@ -100,6 +103,11 @@ const UserResume = ({
             function () {
                //Upload completed successfully, now we can get the download URL
                console.log("upload successful")
+
+               rewardService
+                  .userAction("USER_CV_UPLOAD")
+                  .catch(errorLogAndNotify)
+
                uploadTask.snapshot.ref
                   .getDownloadURL()
                   .then(function (downloadURL) {
