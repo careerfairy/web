968afb6f60e9d8d7b8b961988b19636fefa5cb7c
goncaloarnauth <goncaloarnauth@outlook.com>
2023-04-20 18:51:40 +0100

    update coinIcon

--- apps/web/components/views/common/CareerCoinIcon.tsx
+++ apps/web/components/views/common/CareerCoinIcon.tsx
@@ -1,20 +1,21 @@
-import Image from "next/image"
-import { ImageProps } from "next/dist/client/image"
-
 type Props = {
    width?: number
    height?: number
-   imageProps?: ImageProps
+   withoutBorder?: boolean
 }
 
-const CareerCoinIcon = ({ width = 22, height = 24, imageProps }: Props) => {
+const careerCoinWithBorder =
+   "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/ui-icons%2FCF%20Coins.svg?alt=media&token=191ad2af-96eb-4cea-94e7-93ad21617160"
+const careerCoin =
+   "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/ui-icons%2FCF%20Coins%20border.svg?alt=media&token=efa26a24-cdfc-4f85-8387-c0cc72840a35"
+
+const CareerCoinIcon = ({ width = 22, height = 24, withoutBorder }: Props) => {
    return (
-      <Image
-         src={"/cf_coin.svg"}
+      <img
+         src={withoutBorder ? careerCoinWithBorder : careerCoin}
          width={width}
          height={height}
          alt={"CareerFairy Coin"}
-         {...imageProps}
       />
    )
 }
