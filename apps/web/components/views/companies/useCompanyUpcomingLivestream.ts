5f9cea263796deea08a73eada70173c13a275b9a
goncaloarnauth <goncaloarnauth@outlook.com>
2023-05-03 17:01:08 +0100

    update companies upcoming livestreams logic

--- apps/web/components/views/companies/useCompanyUpcomingLivestream.ts
+++ apps/web/components/views/companies/useCompanyUpcomingLivestream.ts
@@ -1,16 +1,26 @@
 import { collection, orderBy, query, where } from "firebase/firestore"
 import { LivestreamEvent } from "@careerfairy/shared-lib/livestreams"
 import { FirestoreInstance } from "../../../data/firebase/FirebaseInstance"
-import { limit } from "@firebase/firestore"
+import { limit, QueryConstraint } from "@firebase/firestore"
 import { useFirestoreCollection } from "../../custom-hook/utils/useFirestoreCollection"
 
 const now = new Date()
-const useCompanyUpcomingLivestream = (groupId: string) => {
+const useCompanyUpcomingLivestream = (
+   groupId: string,
+   getHiddenEvents?: boolean
+) => {
+   const constraints: QueryConstraint[] = []
+
+   if (!getHiddenEvents) {
+      constraints.push(where("hidden", "==", false))
+   }
+
    const q = query<LivestreamEvent>(
       // @ts-ignore
       collection(FirestoreInstance, "livestreams"),
       where("groupIds", "array-contains", groupId),
       where("start", ">", now),
+      ...constraints,
       orderBy("start", "desc"),
       limit(1)
    )
