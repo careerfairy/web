1da9d6251012db64f6ead987ba9d892aaa3eb102
goncaloarnauth <goncaloarnauth@outlook.com>
2023-05-05 12:31:35 +0100

    Add university country to the recommendation system

--- packages/shared-lib/src/recommendation/IRecommendationService.ts
+++ packages/shared-lib/src/recommendation/IRecommendationService.ts
@@ -137,6 +137,7 @@ export default class RecommendationServiceCore {
          .userInterests()
          .userFieldsOfStudy()
          .userCountriesOfInterest()
+         .userUniversityCountry()
          .get()
    }
 }
