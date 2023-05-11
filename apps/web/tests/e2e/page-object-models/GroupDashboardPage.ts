65cc8515ad94fd5cd6ed9e35bf6733d6e73c79ef
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-05-11 10:50:32 +0100

    feat: added GrouDashboard page model

---
+++ apps/web/tests/e2e/page-object-models/GroupDashboardPage.ts
@@ -0,0 +1,34 @@
+import { Page } from "@playwright/test"
+import { Group } from "@careerfairy/shared-lib/groups"
+import { CommonPage } from "./CommonPage"
+
+export class GroupDashboardPage extends CommonPage {
+   constructor(public readonly page: Page, protected readonly group: Group) {
+      super(page)
+   }
+
+   async open() {
+      await this.page.goto(`/group/${this.group.id}/admin`)
+   }
+
+   public assertMainPageHeader() {
+      return this.assertTextIsVisible("Main Page")
+   }
+
+   public topCreateLivestreamButton() {
+      return this.page
+         .getByRole("banner")
+         .getByRole("button", { name: "Create New Live Stream" })
+   }
+
+   /**
+    * Dismiss tooltips about new features
+    */
+   public async clickBackdropIfPresent() {
+      const backdrop = this.page.locator(".MuiBackdrop-root")
+
+      if (await backdrop.isVisible()) {
+         await backdrop.click()
+      }
+   }
+}
