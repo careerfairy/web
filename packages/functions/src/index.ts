98ba4b32064ff498fc7a0d4d50cd3e90061549b0
Habib Kadiri <habib@careerfairy.io>
2023-05-17 14:43:14 +0200

    Hotfix - Fixing Timezone Issue in Livestream event confirmation email iCalendar ICS file - CF-422 (#635)

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -104,7 +104,7 @@ exports.manualNewsletter = newsletter.manualNewsletter
 // Livestreams
 exports.setFirstCommentOfQuestionOnCreate =
    livestreams.setFirstCommentOfQuestionOnCreate
-exports.sendLivestreamRegistrationConfirmationEmail_eu =
+exports.sendLivestreamRegistrationConfirmationEmail_v2 =
    livestreams.sendLivestreamRegistrationConfirmationEmail
 exports.sendPhysicalEventRegistrationConfirmationEmail_eu =
    livestreams.sendPhysicalEventRegistrationConfirmationEmail
@@ -114,7 +114,7 @@ exports.notifySlackWhenALivestreamStarts =
    livestreams.notifySlackWhenALivestreamStarts
 exports.notifySlackWhenALivestreamIsCreated =
    livestreams.notifySlackWhenALivestreamIsCreated
-exports.getLivestreamICalendarEvent_eu = livestreams.getLivestreamICalendarEvent
+exports.getLivestreamICalendarEvent_v2 = livestreams.getLivestreamICalendarEvent
 
 // Postmark webhooks
 exports.postmarkWebhook = postmark.postmarkWebhook
