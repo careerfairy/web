e87017ee70858e7393ec3032733172beb1c5ade1
goncaloarnauth <goncaloarnauth@outlook.com>
2023-05-18 12:12:46 +0100

    update past request to have a limit of 6 months; Add key to chip component to remove error

--- apps/web/components/views/common/stream-cards/EventPreviewCardChipLabels.tsx
+++ apps/web/components/views/common/stream-cards/EventPreviewCardChipLabels.tsx
@@ -45,19 +45,30 @@ const EventPreviewCardChipLabels = ({
 
    if (hasParticipated && isPast) {
       rightChip = (
-         <Chip icon={<CheckIcon />} color="primary" label={"Attended"} />
+         <Chip
+            key={"attend-chip"}
+            icon={<CheckIcon />}
+            color="primary"
+            label={"Attended"}
+         />
       )
    }
 
    if (hasRegistered && !isPast) {
       rightChip = (
-         <Chip icon={<CheckIcon />} color="primary" label={"Booked!"} />
+         <Chip
+            key={"booked-chip"}
+            icon={<CheckIcon />}
+            color="primary"
+            label={"Booked!"}
+         />
       )
    }
 
    if (isLive) {
       leftChips.push(
          <Chip
+            key={"live-chip"}
             icon={<LiveIcon />}
             color="error"
             label={"LIVE"}
@@ -69,6 +80,7 @@ const EventPreviewCardChipLabels = ({
    if (hasJobToApply) {
       leftChips.push(
          <Chip
+            key={"easy-apply-chip"}
             deleteIcon={
                <Badge
                   color="primary"
@@ -106,6 +118,7 @@ const EventPreviewCardChipLabels = ({
 
       leftChips.push(
          <Chip
+            key={"available-chip"}
             sx={{ pr: 1 }}
             color={"info"}
             label={
