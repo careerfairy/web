a7ec05496b47368352e29fe39b1b43a5808f1e49
habib <kadirit@hotmail.com>
2023-05-19 11:10:12 +0200

    Add reasons to join the live stream to AboutLivestream view.

--- apps/web/components/views/livestream-dialog/views/livestream-details/main-content/AboutLivestream.tsx
+++ apps/web/components/views/livestream-dialog/views/livestream-details/main-content/AboutLivestream.tsx
@@ -1,15 +1,104 @@
-import { FC } from "react"
+import React, { FC } from "react"
 import Box from "@mui/material/Box"
 import SectionTitle from "./SectionTitle"
-import { DummyContent } from "../LivestreamDetailsView"
+import LinkifyText from "../../../../../util/LinkifyText"
+import { Typography } from "@mui/material"
+import { LivestreamPresenter } from "@careerfairy/shared-lib/livestreams/LivestreamPresenter"
+import { sxStyles } from "../../../../../../types/commonTypes"
+import Stack from "@mui/material/Stack"
 
-interface Props {}
+const styles = sxStyles({
+   companyName: {
+      fontSize: "2.28rem",
+   },
+   summary: {
+      fontSize: "1.285rem",
+      whiteSpace: "pre-line",
+   },
+   customBullet: {
+      listStyleType: "none",
+      paddingLeft: "1em",
+      position: "relative",
+   },
+   list: {
+      listStyleType: "none",
+      pl: 0,
+      mb: 0,
+   },
+   listItem: {
+      fontSize: "1.1428rem",
+      display: "list-item",
+      position: "relative",
+      paddingLeft: 3,
+      "&:not(:last-child)": {
+         mb: 2.25,
+      },
+      "&::before": {
+         content: "''",
+         position: "absolute",
+         left: 0,
+         top: "0.36em",
+         width: 15,
+         height: 15,
+         borderRadius: "50%",
+         backgroundColor: "primary.main",
+         background: `url(/icons/themed-check.svg)`,
+         backgroundSize: "contain",
+         backgroundRepeat: "no-repeat",
+      },
+   },
+})
 
-const AboutLivestream: FC<Props> = () => {
+interface Props {
+   presenter: LivestreamPresenter
+}
+
+const AboutLivestream: FC<Props> = ({ presenter }) => {
    return (
       <Box>
          <SectionTitle>About the live stream</SectionTitle>
-         <DummyContent />
+         <Typography sx={styles.companyName} component="h3">
+            {presenter.company}
+         </Typography>
+         <Stack spacing={6}>
+            <LinkifyText>
+               <Typography sx={styles.summary}>{presenter.summary}</Typography>
+            </LinkifyText>
+            <ReasonsToJoin presenter={presenter} />
+         </Stack>
+      </Box>
+   )
+}
+
+type ReasonToJoinProps = {
+   presenter: LivestreamPresenter
+}
+
+const ReasonsToJoin: FC<ReasonToJoinProps> = ({ presenter }) => {
+   const reasonsToJoinLivestream = presenter.reasonsToJoinLivestream.trim()
+   const hasReasonsToJoinLivestream = Boolean(reasonsToJoinLivestream)
+
+   const reasonsList = reasonsToJoinLivestream.split("\n").map((reason) => {
+      // replace the first occurrence of "-" but exclude any subsequent occurrences
+      return reason.replace(/^-(?!.*-)/, "").trim()
+   })
+
+   if (!hasReasonsToJoinLivestream) {
+      return null
+   }
+
+   return (
+      <Box>
+         <SectionTitle>Why should you join the Live Stream?</SectionTitle>
+         <Box sx={styles.list} component="ul">
+            {reasonsList.map((point) => (
+               <Box sx={styles.listItem} component="li" key={point}>
+                  <LinkifyText>
+                     <Typography fontSize="inherit">{point}</Typography>
+                  </LinkifyText>
+               </Box>
+            ))}
+         </Box>
       </Box>
    )
 }
