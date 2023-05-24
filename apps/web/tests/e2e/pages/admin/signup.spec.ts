049fb4af4534617107bb0c61625abee48651947a
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-05-24 11:44:51 +0100

    feat: re-use existing fixture

--- apps/web/tests/e2e/pages/admin/signup.spec.ts
+++ apps/web/tests/e2e/pages/admin/signup.spec.ts
@@ -1,45 +1,11 @@
-import { Group, GROUP_DASHBOARD_ROLE } from "@careerfairy/shared-lib/groups"
 import GroupSeed from "@careerfairy/seed-data/dist/groups"
 import UserSeed from "@careerfairy/seed-data/dist/users"
-import { test as base, expect } from "@playwright/test"
-import { credentials } from "../../../constants"
-import { LoginPage } from "../../page-object-models/LoginPage"
+import { expect } from "@playwright/test"
 import { GroupDashboardPage } from "../../page-object-models/GroupDashboardPage"
-import {
-   clearAuthData,
-   clearFirestoreData,
-} from "@careerfairy/seed-data/dist/emulators"
 import { waitForData } from "../../utils"
 import ProfilePage from "../../page-object-models/ProfilePage"
 import { AdminSignupPage } from "../../page-object-models/AdminSignupPage"
-
-/**
- * Test Fixture
- * Setup Pages and requirements
- */
-const test = base.extend<{
-   group: Group
-   groupPage: GroupDashboardPage
-}>({
-   group: async ({}, use) => {
-      await clearAuthData()
-      await clearFirestoreData()
-
-      const { group } = await setupData()
-
-      await use(group)
-   },
-   groupPage: async ({ page, group }, use) => {
-      const newUrl = `/group/${group.id}/admin`
-      await LoginPage.login(page, { waitForURL: newUrl })
-
-      const groupPage = new GroupDashboardPage(page, group)
-
-      await groupPage.clickBackdropIfPresent()
-
-      await use(groupPage)
-   },
-})
+import { groupAdminFixture as test } from "../../fixtures"
 
 test.describe("Admin Signup", () => {
    test("Sign in with a group admin account should redirect to the group dashboard page", async ({
@@ -145,21 +111,4 @@ const creds = {
    lastName: "Swanson",
    email: "ron@careerfairy.io",
    password: "password123A",
-}
-
-async function setupData(overrideGroupDetails: Partial<Group> = {}) {
-   const group = await GroupSeed.createGroup(
-      Object.assign({}, overrideGroupDetails)
-   )
-
-   const user = await UserSeed.createUser(credentials.correctEmail)
-
-   await GroupSeed.addGroupAdmin(
-      user,
-      group,
-      // can't use GROUP_DASHBOARD_ROLE.OWNER, build fails
-      "OWNER" as GROUP_DASHBOARD_ROLE
-   )
-
-   return { group, user }
 }
