d81319ec1b0fb6a7b49fe5c1e00fe08287f867b0
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-06-23 11:46:45 +0100

    feat: add welcome dialog and views

--- apps/web/components/views/welcome-dialog/VideoView.tsx
+++ apps/web/components/views/welcome-dialog/VideoView.tsx
@@ -1,8 +1,72 @@
-export const VideoView = () => {
+import { Box, Button, Stack } from "@mui/material"
+import { sxStyles } from "../../../types/commonTypes"
+import { useVideo } from "react-use"
+import useIsMobile from "../../custom-hook/useIsMobile"
+import {
+   ONBOARDING_VIDEO_URL_DESKTOP,
+   ONBOARDING_VIDEO_URL_MOBILE,
+} from "../../util/constants"
+import { useCallback } from "react"
+
+const styles = sxStyles({
+   video: {
+      width: "100%",
+      borderRadius: 2,
+   },
+
+   videoMobile: {
+      width: "90%",
+   },
+
+   button: {
+      boxShadow: "none",
+      padding: "5px 15px",
+      textTransform: "none",
+   },
+})
+
+export const VideoView = ({ onComplete }: { onComplete: () => void }) => {
+   const isMobile = useIsMobile()
+
+   const videoUrl = isMobile
+      ? ONBOARDING_VIDEO_URL_MOBILE
+      : ONBOARDING_VIDEO_URL_DESKTOP
+
+   const onEnded = useCallback(() => {
+      onComplete()
+   }, [onComplete])
+
+   const [video] = useVideo(
+      <Box
+         sx={[styles.video, isMobile && styles.videoMobile]}
+         webkit-playsInline
+         // @ts-ignore
+         webkitPlaysInline
+         playsInline
+         component="video"
+         controls
+         disablePictureInPicture={true}
+         src={videoUrl}
+         onEnded={onEnded}
+         controlsList="nodownload"
+      />
+   )
+
    return (
-      <div>
-         <h1>VideoView</h1>
-      </div>
+      <Box>
+         <Box textAlign="center">{video}</Box>
+         <Box textAlign={isMobile ? "center" : "right"} pb={1}>
+            <Button
+               size="small"
+               sx={styles.button}
+               variant="contained"
+               color="primary"
+               onClick={onEnded}
+            >
+               Skip video
+            </Button>
+         </Box>
+      </Box>
    )
 }
 
