6d4e89c9257481865bce0bfaba4825ff3db6c2cc
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-06-26 19:05:48 +0100

    feat: new welcome video e2e tests

--- apps/web/tests/e2e/page-object-models/PortalPage.ts
+++ apps/web/tests/e2e/page-object-models/PortalPage.ts
@@ -1,15 +1,27 @@
-import { Locator, Page } from "@playwright/test"
 import { CommonPage } from "./CommonPage"
+import { Locator, expect, Page } from "@playwright/test"
 
 export class PortalPage extends CommonPage {
    readonly cookieAcceptButton: Locator
    readonly UpcomingEventsHeader: Locator
    readonly NextEventsHeader: Locator
+   readonly skipVideoButton: Locator
 
    constructor(page: Page) {
       super(page)
       this.UpcomingEventsHeader = page.locator("text=COMING UP NEXT")
       this.NextEventsHeader = page.locator("text=MY NEXT EVENTS")
       this.cookieAcceptButton = page.locator("id=rcc-confirm-button")
+      this.skipVideoButton = this.page.getByRole("button", {
+         name: "Skip video",
+      })
+   }
+
+   async assertWelcomeDialog() {
+      await expect(this.skipVideoButton).toBeVisible()
+      await this.skipVideoButton.click()
+
+      // welcome view
+      await expect(this.page.getByText("Welcome to CareerFairy!")).toBeVisible()
    }
 }
