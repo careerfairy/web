6f53110649ef5bb43cd34d36c23d65e10cdefd6e
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-06-30 14:03:40 +0200

    feat: fix tests

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -47,7 +47,7 @@ const config: PlaywrightTestConfig = {
       // baseURL: 'http://localhost:3000',
 
       /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-      trace: "on-first-retry",
+      trace: process.env.CI ? "on-first-retry" : "retain-on-failure",
       screenshot: "only-on-failure",
       video: "retain-on-failure",
    },
