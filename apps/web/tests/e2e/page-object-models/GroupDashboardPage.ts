199dec8d2a3f15c7d34740c4bc8aa7b9cc4e0f08
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-06-30 14:26:16 +0200

    fix: tests

--- apps/web/tests/e2e/page-object-models/GroupDashboardPage.ts
+++ apps/web/tests/e2e/page-object-models/GroupDashboardPage.ts
@@ -53,7 +53,7 @@ export class GroupDashboardPage extends CommonPage {
          name: "Company size *",
       })
 
-      this.companyInformationAboutInput = this.page.getByLabel("About")
+      this.companyInformationAboutInput = this.page.getByLabel("About *")
 
       this.companyPageTestimonialSectionEditButton = this.page.locator(
          "data-testid=testimonial-section-edit-button"
@@ -332,7 +332,7 @@ export class GroupDashboardPage extends CommonPage {
       await sleep(1000)
 
       await this.page
-         .getByLabel("Testimonial")
+         .getByPlaceholder("Say something about their story")
          .fill(
             "I am about to finish my Bachelor’s degree in Applied Linguistics at ZHAW. I wanted to start my professional career in an environment that values my ideas and ultimately helps students, like me, to make their first steps in their professional world."
          )
