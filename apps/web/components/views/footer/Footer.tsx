ff22c1daf2b46b5d844b816d3d192fefcadb38f4
Alvaro Paço <alvaro.paco@ip-192-168-0-154.ec2.internal>
2023-07-04 18:38:00 +0100

    Moving styles to Box MUI component

--- apps/web/components/views/footer/Footer.tsx
+++ apps/web/components/views/footer/Footer.tsx
@@ -1,23 +1,19 @@
 import React from "react"
 import { useRouter } from "next/router"
-import { Theme, useTheme } from "@mui/material/styles"
+import { Theme } from "@mui/material/styles"
 import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
-import { SystemStyleObject } from "@mui/system"
-
+import { MAIL_TO_ADDRESS } from "../../../constants/links"
 import CareerInspiration from "./CareerInpiration"
 
 type FooterProps = {
    background?: string
-   sx?: SystemStyleObject<Theme>
    bottom?: boolean
 }
 
-const MAIL_TO_ADDRESS = "info@careerfairy.io"
-
-const Footer = ({ background, sx, bottom }: FooterProps) => {
+const Footer = ({ background, bottom }: FooterProps) => {
    const router = useRouter()
-   const shouldDisplayCareerInspiration = !![
+   const shouldDisplayCareerInspiration = [
       "/",
       "/career-center",
       "/about-us",
@@ -25,27 +21,15 @@ const Footer = ({ background, sx, bottom }: FooterProps) => {
 
    return (
       <Box
-         sx={[
-            {
-               background: (theme: Theme) =>
-                  background || theme.palette.common.white,
-            },
-            sx,
-            bottom && { mt: "auto" },
-         ]}
+         bgcolor={(theme: Theme) => background ?? theme.palette.common.white}
+         mt={bottom && "auto"}
          id="page-footer"
       >
-         {shouldDisplayCareerInspiration && <CareerInspiration />}
+         {shouldDisplayCareerInspiration ? <CareerInspiration /> : null}
          <Typography align="center">
             {new Date().getFullYear()} - CareerFairy AG - Made in Zurich,
             Switzerland - Contact:
-            <Box
-               component="a"
-               sx={{
-                  marginLeft: 5,
-               }}
-               href={`mailto:${MAIL_TO_ADDRESS}`}
-            >
+            <Box component="a" ml={5} href={`mailto:${MAIL_TO_ADDRESS}`}>
                {MAIL_TO_ADDRESS}
             </Box>
          </Typography>
--- apps/web/constants/links.ts
+++ apps/web/constants/links.ts
@@ -2,5 +2,5 @@ export const hubSpotFunnelLink = "https://library.careerfairy.io/demo-request-1"
 export const hubSpotFunnelLinkCareerCenter =
    "https://meetings.hubspot.com/lisa-brandenberger"
 export const facebookAppId = "2046877438758567"
-
 export const supportPageLink = "https://support.careerfairy.io/"
+export const MAIL_TO_ADDRESS = "info@careerfairy.io"
