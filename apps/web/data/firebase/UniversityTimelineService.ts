ed7bfb1f10ae0bc05bc7f90c1b37aed22546972c
Valentine Tscharner <valentine.tscharner@hotmail.com>
2023-07-07 14:27:18 +0000

    fix: add UniversityPeriodType enum & better comments

--- apps/web/data/firebase/UniversityTimelineService.ts
+++ apps/web/data/firebase/UniversityTimelineService.ts
@@ -4,27 +4,14 @@ import {
    deleteDoc,
    doc,
    Firestore,
-   getDoc,
-   getDocs,
-   where,
-   query,
    Timestamp,
    setDoc,
 } from "firebase/firestore"
 import { FirestoreInstance } from "./FirebaseInstance"
-import {
-   UniversityPeriodType,
-   TimelineUniversity,
-} from "@careerfairy/shared-lib/universities/universityTimeline"
-import {
-   createGenericConverter,
-   mapFirestoreDocuments,
-} from "@careerfairy/shared-lib/BaseFirebaseRepository"
+import { UniversityPeriodType } from "@careerfairy/shared-lib/universities/universityTimeline"
 
 export class UniversityTimelineService {
    constructor(private readonly firestore: Firestore) {}
-
-   // UNIVERSITIES
 
    /**
     * Adds a TimelineUniversity to the database
@@ -33,41 +20,66 @@ export class UniversityTimelineService {
     *
     * @param name - university name
     * @param country - university country location
+    * @returns A promise to a DocumentReference of the newly created document
     */
    addTimelineUniversity(name: string, country: string) {
-      // let firestore generate a document id
       return addDoc(collection(this.firestore, "timelineUniversities"), {
          name: name,
          country: country,
       })
    }
 
-   updateTimelineUniversity(name: string, country: string, docId: string) {
-      setDoc(doc(this.firestore, "timelineUniversities", docId), {
+   /**
+    * Updates a TimelineUniversity in the database
+    *
+    * A promise will be created but not awaited
+    *
+    * @param name - name of the university
+    * @param country - country of the university
+    * @param docId - firestore document id of the university
+    */
+   updateTimelineUniversity(name: string, country: string, uniId: string) {
+      setDoc(doc(this.firestore, "timelineUniversities", uniId), {
          name: name,
          country: country,
       })
    }
 
+   /**
+    * Updates a TimelineUniversity in the database
+    *
+    * A promise will be created but not awaited
+    *
+    * @param uniId - the firestore document id of the university
+    */
    removeTimelineUniversity(uniId: string) {
       deleteDoc(doc(this.firestore, "timelineUniversities", uniId))
    }
 
-   // PERIODS
-
+   /**
+    * Adds a UniversityPeriod to a given TimelineUniversity
+    *
+    * A promise will be created but not awaited
+    *
+    * @param uniId - the university document id
+    * @param type - the type of the period
+    * @param start - the start date of the period
+    * @param end - the end date of the period
+    * @returns A promise to a DocumentReference to the newly created document
+    */
    addUniversityPeriod(
       uniId: string,
       type: UniversityPeriodType,
       start: Date,
       end: Date
    ) {
-      let periodRef = collection(
+      const periodRef = collection(
          this.firestore,
          "timelineUniversities",
          uniId,
          "periods"
       )
-      let data = {
+      const data = {
          type: type,
          start: Timestamp.fromDate(start),
          end: Timestamp.fromDate(end),
@@ -75,6 +87,17 @@ export class UniversityTimelineService {
       return addDoc(periodRef, data)
    }
 
+   /**
+    * Updates a UniversityPeriod in the database
+    *
+    * A promise will be created but not awaited
+    *
+    * @param uniId - the TimelineUniversity's document id
+    * @param type - the new type of the period
+    * @param start - the new start date of the period
+    * @param end - the new end date of the period
+    * @param periodId - the id of the period to update
+    */
    updateUniversityPeriod(
       uniId: string,
       type: UniversityPeriodType,
@@ -82,14 +105,14 @@ export class UniversityTimelineService {
       end: Date,
       periodId: string
    ) {
-      let period = doc(
+      const period = doc(
          this.firestore,
          "timelineUniversities",
          uniId,
          "periods",
          periodId
       )
-      let data = {
+      const data = {
          type: type,
          start: Timestamp.fromDate(start),
          end: Timestamp.fromDate(end),
@@ -97,6 +120,14 @@ export class UniversityTimelineService {
       setDoc(period, data)
    }
 
+   /**
+    * Deletes a UniversityPeriod from the database
+    *
+    * A promise will be created but not awaited
+    *
+    * @param uniId - the document id of the university
+    * @param periodId - the document id of the period
+    */
    removeUniversityPeriod(uniId: string, periodId: string) {
       deleteDoc(
          doc(this.firestore, "timelineUniversities", uniId, "periods", periodId)
