41a09c1231f6e7b9c0e35313a235930657cad733
habib <kadirit@hotmail.com>
2023-07-18 10:23:48 +0200

    Refactor code in sparks-dialog/views/CreateCreatorView and sparks-dialog/views/CreatorSelectedView
    - Fixes bug related to displaying the avatar image in CreateCreatorView
    - Adds functionality to edit the selected creator in CreatorSelectedView

--- apps/web/components/views/admin/sparks/sparks-dialog/views/CreatorSelectedView.tsx
+++ apps/web/components/views/admin/sparks/sparks-dialog/views/CreatorSelectedView.tsx
@@ -1,52 +1,102 @@
-import { Box } from "@mui/material"
+import { Box, Stack } from "@mui/material"
 import { sxStyles } from "types/commonTypes"
 import SparksDialog, { useSparksForm } from "../SparksDialog"
 import { Chip } from "@mui/material"
+import CreatorFetchWrapper from "HOCs/creator/CreatorFetchWrapper"
+import { useSelector } from "react-redux"
+import useUploadCreatorAvatar from "components/custom-hook/creator/useUploadCreatorAvatar"
+import { useGroup } from "layouts/GroupDashboardLayout"
+import { sparksSelectedCreatorId } from "store/selectors/adminSparksSelectors"
+import EditIcon from "@mui/icons-material/ModeEditOutlineOutlined"
+import { useCallback } from "react"
+import { Avatar } from "@mui/material"
 
 const styles = sxStyles({
-   root: {
-      m: {
-         xs: 0,
-         mobile: "auto",
-      },
-      width: "100%",
-   },
+   root: {},
    cretaorDetailsWrapper: {
       display: "flex",
-      height: 474,
+      height: 488,
       backgroundColor: "background.paper",
       position: "relative",
+      width: 494,
+      borderRadius: 2,
+      border: "1px solid #F8F8F8",
+   },
+   editButton: {
+      top: 0,
+      right: 0,
+      position: "absolute",
+      padding: 3.5,
+      "& .MuiChip-label": {
+         color: "#A0A0A0",
+         fontSize: "1.14286rem",
+      },
+      color: "#A0A0A0",
+   },
+   avatar: {
+      width: 136,
+      height: 136,
    },
 })
 
 const CreatorSelectedView = () => {
-   const { stepper, goToCreatorSelectedView } = useSparksForm()
+   const { stepper, goToCreateOrEditCreatorView } = useSparksForm()
+   const { group } = useGroup()
 
-   const handleClickEdit = () => {}
+   const selectedCreatorId = useSelector(sparksSelectedCreatorId)
+
+   const handleClickEdit = useCallback(
+      (creatorId: string) => {
+         goToCreateOrEditCreatorView(creatorId)
+      },
+      [goToCreateOrEditCreatorView]
+   )
 
    return (
-      <SparksDialog.Container
-         onMobileBack={() => stepper.goToStep("select-creator")}
-         sx={styles.root}
+      <CreatorFetchWrapper
+         selectedCreatorId={selectedCreatorId}
+         groupId={group.id}
+         shouldFetch={Boolean(selectedCreatorId)}
       >
-         <SparksDialog.Title pl={2}>
-            <Box component="span" color="secondary.main">
-               Creator
-            </Box>{" "}
-            selected!
-         </SparksDialog.Title>
-         <SparksDialog.Subtitle>
-            Please check if that’s the correct creator
-         </SparksDialog.Subtitle>
-         <Box sx={styles.cretaorDetailsWrapper}>
-            <Chip
-               label="Custom delete icon"
-               onClick={handleClick}
-               onDelete={handleDelete}
-               deleteIcon={<DoneIcon />}
-            />
-         </Box>
-      </SparksDialog.Container>
+         {(creator) => (
+            <SparksDialog.Container
+               onMobileBack={() => stepper.goToStep("select-creator")}
+               sx={styles.root}
+            >
+               <SparksDialog.Title pl={2}>
+                  <Box component="span" color="secondary.main">
+                     Creator
+                  </Box>{" "}
+                  selected!
+               </SparksDialog.Title>
+               <SparksDialog.Subtitle>
+                  Please check if that’s the correct creator
+               </SparksDialog.Subtitle>
+               <Stack
+                  spacing={1}
+                  p={3.5}
+                  alignItems="center"
+                  sx={styles.cretaorDetailsWrapper}
+               >
+                  <Box sx={styles.editButton}>
+                     <Chip
+                        label="Edit"
+                        onDelete={() => handleClickEdit(creator.id)}
+                        onClick={() => handleClickEdit(creator.id)}
+                        deleteIcon={<EditIcon />}
+                     />
+                  </Box>
+                  <Avatar
+                     alt={`${creator.firstName} ${creator.lastName}`}
+                     src={creator.avatarUrl}
+                     sx={styles.avatar}
+                  >
+                     {creator.firstName[0]} {creator.lastName[0]}
+                  </Avatar>
+               </Stack>
+            </SparksDialog.Container>
+         )}
+      </CreatorFetchWrapper>
    )
 }
 
