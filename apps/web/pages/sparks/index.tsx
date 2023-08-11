cfc1d0207dd0e16a4431299433cac74837560a45
Valentine Tscharner <valentine.tscharner@hotmail.com>
2023-08-11 10:50:59 +0000

    fix: add empty page

---
+++ apps/web/pages/sparks/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import GenericDashboardLayout from "../../layouts/GenericDashboardLayout"
+import { Box } from "@mui/material"
+const SparksPage = () => {
+   return (
+      <GenericDashboardLayout pageDisplayName={""}>
+         <Box></Box>
+      </GenericDashboardLayout>
+   )
+}
+export default SparksPage
