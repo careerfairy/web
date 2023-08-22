e758ef61a5558f7ed250ab08a850a9a1781fa532
Valentine Tscharner <valentine.tscharner@hotmail.com>
2023-08-22 14:01:02 +0000

    feature: sparks share

--- apps/web/components/views/common/ReferralWidget.tsx
+++ apps/web/components/views/common/ReferralWidget.tsx
@@ -1,13 +1,22 @@
-import React from "react"
+import React, { useCallback } from "react"
 import Stack, { StackProps } from "@mui/material/Stack"
 import { alpha, useTheme } from "@mui/material/styles"
-import { IconButton, Tooltip, useMediaQuery } from "@mui/material"
+import {
+   Box,
+   Button,
+   IconButton,
+   Tooltip,
+   Typography,
+   useMediaQuery,
+} from "@mui/material"
 import { StylesProps, sxStyles } from "../../../types/commonTypes"
 import { SocialIconProps } from "../../custom-hook/useSocials"
+import Image from "next/image"
 
 const mobileProp = "sm"
 
 interface WidgetButtonProps extends StackProps {
+   isImageIcon?: boolean
    iconsColor?: "grey" | "primary" | "secondary"
    noBackgroundColor?: boolean
    iconStyle?: any
@@ -32,9 +41,24 @@ const styles = sxStyles({
          backgroundColor: "transparent",
       },
    },
+   socialContainer: {
+      display: "flex",
+      flexDirection: "column",
+      textAlign: "center",
+      alignItems: "center",
+   },
+   imageBox: {
+      width: "48px",
+   },
+   iconText: {
+      color: "tertiary.dark",
+      fontSize: "16px",
+      mt: "2px",
+   },
 })
 
 export const ReferralWidget = ({
+   isImageIcon,
    iconsColor = "secondary",
    noBackgroundColor,
    iconStyle,
@@ -43,6 +67,50 @@ export const ReferralWidget = ({
 }: WidgetButtonProps) => {
    const theme = useTheme()
    const mobile = useMediaQuery(theme.breakpoints.down(mobileProp))
+
+   const getIconButton = useCallback(
+      (icon) => (
+         <IconButton
+            sx={[
+               styles.iconButton,
+               {
+                  color:
+                     iconsColor === "grey"
+                        ? "common.black"
+                        : `${iconsColor}.main`,
+                  width: mobile ? "15%" : "unset",
+               },
+            ]}
+            size={"large"}
+            onClick={icon.onClick}
+            href={icon.href}
+         >
+            <icon.icon
+               color={"inherit"}
+               sx={[styles.icon, iconStyle && { ...iconStyle }]}
+            />
+         </IconButton>
+      ),
+      [iconStyle, iconsColor, mobile]
+   )
+
+   const getImageButton = useCallback(
+      (icon) => (
+         <Box sx={styles.socialContainer}>
+            <Box sx={styles.imageBox} onClick={icon.onClick}>
+               <Image
+                  src={icon.imageLink}
+                  alt={icon.name}
+                  objectFit={"contain"}
+                  height={100}
+                  width={100}
+               />
+            </Box>
+            <Typography sx={styles.iconText}> {icon.name} </Typography>
+         </Box>
+      ),
+      []
+   )
 
    return (
       <Stack
@@ -57,26 +125,7 @@ export const ReferralWidget = ({
       >
          {socials.map((icon) => (
             <Tooltip key={icon.name} arrow title={icon.name}>
-               <IconButton
-                  sx={[
-                     styles.iconButton,
-                     {
-                        color:
-                           iconsColor === "grey"
-                              ? "common.black"
-                              : `${iconsColor}.main`,
-                        width: mobile ? "15%" : "unset",
-                     },
-                  ]}
-                  size={"large"}
-                  onClick={icon.onClick}
-                  href={icon.href}
-               >
-                  <icon.icon
-                     color={"inherit"}
-                     sx={[styles.icon, iconStyle && { ...iconStyle }]}
-                  />
-               </IconButton>
+               {isImageIcon ? getImageButton(icon) : getIconButton(icon)}
             </Tooltip>
          ))}
       </Stack>
