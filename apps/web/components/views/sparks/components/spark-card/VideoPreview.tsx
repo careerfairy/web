55489b4a2e9783e7af25f887414a4299f9ff3ead
habib <kadirit@hotmail.com>
2023-08-28 19:52:26 +0200

    feat: Add video transformation using ImageKit
    This commit introduces changes to the code that allow for video transformation using ImageKit. A new utility function, `imageKitLoader`, has been added to handle the transformation parameters and generate the transformed video URL. The `getTransformedVideoUrl()` method in `SparkPresenter` class now utilizes this utility function to retrieve the video URL with the appropriate transformations. The transformations include maintaining a 16:9 aspect ratio, resizing the video to 640x360 pixels, reducing the quality to 40%, and converting the format to mp4.

--- apps/web/components/views/sparks/components/spark-card/VideoPreview.tsx
+++ apps/web/components/views/sparks/components/spark-card/VideoPreview.tsx
@@ -1,7 +1,11 @@
+import LinearProgress, {
+   linearProgressClasses,
+} from "@mui/material/LinearProgress"
 import Box from "@mui/material/Box"
-import { FC, Fragment } from "react"
+import { FC, Fragment, useCallback, useEffect, useState } from "react"
 import ReactPlayer from "react-player/file"
 import { sxStyles } from "types/commonTypes"
+import { BaseReactPlayerProps, OnProgressProps } from "react-player/base"
 
 const styles = sxStyles({
    root: {
@@ -32,6 +36,18 @@ const styles = sxStyles({
          },
       },
    },
+   progress: {
+      background: "none",
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+      width: "100%",
+      [`& .${linearProgressClasses.bar}`]: {
+         bgcolor: "primary.600",
+      },
+      zIndex: (theme) => theme.zIndex.drawer + 1,
+      height: 4.5,
+   },
 })
 
 type Props = {
@@ -47,6 +63,30 @@ const VideoPreview: FC<Props> = ({
    playing,
    containThumbnail,
 }) => {
+   const [progress, setProgress] = useState(0)
+
+   useEffect(() => {
+      return () => {
+         setProgress(0)
+      }
+   }, [])
+
+   const handleProgress = useCallback((progress: OnProgressProps) => {
+      setProgress(progress.played * 100)
+   }, [])
+
+   const handleError: BaseReactPlayerProps["onError"] = (
+      error,
+      data,
+      hlsINstance,
+      hlsGlobal
+   ) => {
+      console.log("🚀 ~ file: VideoPreview.tsx:84 ~ hlsGlobal:", hlsGlobal)
+      console.log("🚀 ~ file: VideoPreview.tsx:84 ~ hlsINstance:", hlsINstance)
+      console.log("🚀 ~ file: VideoPreview.tsx:84 ~ data:", data)
+      console.log("🚀 ~ file: VideoPreview.tsx:96 ~ error:", error)
+   }
+
    return (
       <Box sx={styles.root}>
          <Box
@@ -62,13 +102,25 @@ const VideoPreview: FC<Props> = ({
                width="100%"
                height="100%"
                className="player"
+               onProgress={handleProgress}
+               onError={handleError}
+               progressInterval={500}
                url={videoUrl}
                light={!playing && thumbnailUrl}
                playIcon={<Fragment />}
             />
+            <LinearProgress
+               sx={styles.progress}
+               variant="determinate"
+               value={progress}
+            />
          </Box>
       </Box>
    )
+}
+
+type FallBackComponentProps = {
+   thumbnailUrl: string
 }
 
 export default VideoPreview
