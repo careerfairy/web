f2320f5b8335e21d7810669ad5a04de0449fd8c6
Alvaro Paco <alvaropaconeto@gmail.com>
2023-08-30 08:50:24 -0300

    Fixing questions creation behavior

--- apps/web/components/views/common/QuestionnaireCreation/createAGroupQuestion.ts
+++ apps/web/components/views/common/QuestionnaireCreation/createAGroupQuestion.ts
@@ -1,11 +1,24 @@
 import { GroupQuestion } from "@careerfairy/shared-lib/groups"
 import { v4 as uuidv4 } from "uuid"
+import { createAGroupQuestionOption } from "./createAGroupQuestionOption"
 
-export const createAGroupQuestion = () =>
-   ({
+const setQuestionOptions = (question) => {
+   for (let i = 0; i < 2; ++i) {
+      const newQuestion = createAGroupQuestionOption()
+      question.options[newQuestion.id] = { ...newQuestion }
+   }
+}
+
+export const createAGroupQuestion = () => {
+   const newQuestion = {
       id: uuidv4(),
       name: "",
       options: {},
       hidden: false,
       questionType: "custom",
-   } as GroupQuestion)
+   } as GroupQuestion
+
+   setQuestionOptions(newQuestion)
+
+   return { ...newQuestion }
+}
