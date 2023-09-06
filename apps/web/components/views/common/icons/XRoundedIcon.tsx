835df305033fdcc5fd438aa182dc41e9b68a87da
habib <kadirit@hotmail.com>
2023-09-06 19:05:22 +0200

    feat: Update XRoundedIcon to correct Icon

--- apps/web/components/views/common/icons/XRoundedIcon.tsx
+++ apps/web/components/views/common/icons/XRoundedIcon.tsx
@@ -3,7 +3,18 @@ import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon"
 import ReactComponent from "public/icons/x-rounded-icon.svg"
 
 const XRoundedIcon = (props: SvgIconProps) => {
-   return <SvgIcon component={ReactComponent} viewBox="0 0 49 48" {...props} />
+   return (
+      <SvgIcon
+         component={ReactComponent}
+         viewBox="0 0 962.7 962.7"
+         style={styles}
+         {...props}
+      />
+   )
+}
+
+const styles = {
+   color: "black",
 }
 
 export default XRoundedIcon
