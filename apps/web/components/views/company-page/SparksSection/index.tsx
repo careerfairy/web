28cd9cd8cc459a01ac7528172ec3c0622f2a741e
Alvaro Paco <alvaropaconeto@gmail.com>
2023-09-07 07:05:40 -0300

    Handle sparks clicked

--- apps/web/components/views/company-page/SparksSection/index.tsx
+++ apps/web/components/views/company-page/SparksSection/index.tsx
@@ -1,12 +1,30 @@
 import SparksCarouselWithSuspenseComponent from "components/views/portal/sparks/SparksCarouselWithSuspenseComponent"
 import { FC } from "react"
+import { useCompanyPage } from ".."
+import { useMountedState } from "react-use"
+import { useRouter } from "next/router"
+import { Spark } from "@careerfairy/shared-lib/sparks/sparks"
 
 type Props = {
    groupId: String
 }
 
 const SparksSection: FC<Props> = ({ groupId }) => {
-   const handleSparksClicked = async () => {}
+   const {
+      group,
+      upcomingLivestreams,
+      pastLivestreams,
+      sectionRefs: { eventSectionRef },
+   } = useCompanyPage()
+   const isMounted = useMountedState()
+   const router = useRouter()
+
+   const handleSparksClicked = async (spark: Spark) => {
+      if (!spark) return
+      router.push(`/sparks/${spark.id}`)
+      return
+   }
+
    return (
       <SparksCarouselWithSuspenseComponent
          groupId={groupId}
