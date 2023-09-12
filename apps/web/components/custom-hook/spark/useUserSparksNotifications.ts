155fa7250597d26d5a52d5c258d0b93c6c0ca0c1
goncaloarnauth <goncaloarnauth@outlook.com>
2023-09-12 10:13:20 +0100

    update rules;

--- apps/web/components/custom-hook/spark/useUserSparksNotifications.ts
+++ apps/web/components/custom-hook/spark/useUserSparksNotifications.ts
@@ -5,19 +5,19 @@ import { useFirestoreCollection } from "../utils/useFirestoreCollection"
 import { UserSparksNotification } from "@careerfairy/shared-lib/users"
 import { fromSerializedDate } from "@careerfairy/shared-lib/BaseModel"
 
-const useUserSparksNotifications = (userId = "userId") => {
-   const sparkNotificationsQuery = useMemo(
-      () =>
-         query(
-            collection(
-               FirestoreInstance,
-               "userData",
-               userId,
-               "sparksNotifications"
-            )
-         ),
-      [userId]
-   )
+const useUserSparksNotifications = (userId: string) => {
+   const sparkNotificationsQuery = useMemo(() => {
+      return userId
+         ? query(
+              collection(
+                 FirestoreInstance,
+                 "userData",
+                 userId || "userId", // to not break when there is no user
+                 "sparksNotifications"
+              )
+           )
+         : null
+   }, [userId])
 
    const { data } = useFirestoreCollection<UserSparksNotification>(
       sparkNotificationsQuery,
