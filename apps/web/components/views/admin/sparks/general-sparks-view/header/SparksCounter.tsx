4446cca2002bbccc9afce49523d88e669bcdf974
goncaloarnauth <goncaloarnauth@outlook.com>
2023-09-12 14:02:16 +0100

    add sparks progress banner;

---
+++ apps/web/components/views/admin/sparks/general-sparks-view/header/SparksCounter.tsx
@@ -0,0 +1,103 @@
+import { FC } from "react"
+import {
+   Box,
+   LinearProgress,
+   linearProgressClasses,
+   tooltipClasses,
+   Typography,
+} from "@mui/material"
+import { sxStyles } from "../../../../../../types/commonTypes"
+import { Spark } from "@careerfairy/shared-lib/sparks/sparks"
+import BrandedTooltip from "../../../../common/tooltips/BrandedTooltip"
+import { SPARK_CONSTANTS } from "@careerfairy/shared-lib/sparks/constants"
+
+const styles = sxStyles({
+   tooltip: {
+      maxWidth: 350,
+      [`& .${tooltipClasses.tooltip}`]: {
+         maxWidth: 350,
+      },
+   },
+   progressBar: {
+      width: "100%",
+      height: 10,
+      borderRadius: 5,
+      bgcolor: "grey.300",
+      [`& .${linearProgressClasses.bar}`]: {
+         borderRadius: 5,
+      },
+   },
+   progressCount: {
+      color: (theme) => theme.palette.grey.A700,
+   },
+   headerMessage: {
+      mb: 2,
+      fontWeight: "bold",
+   },
+})
+
+type Props = {
+   isCriticalState: boolean
+   publicSparks: Spark[]
+   maxPublicSparks: number
+}
+
+const SparksCounter: FC<Props> = ({
+   isCriticalState,
+   publicSparks,
+   maxPublicSparks,
+}) => {
+   return (
+      <BrandedTooltip
+         title={
+            isCriticalState
+               ? renderCriticalMessage()
+               : renderDefaultMessage(maxPublicSparks)
+         }
+         sx={styles.tooltip}
+      >
+         <Box>
+            <Typography variant={"body1"} sx={styles.progressCount}>
+               {publicSparks.length}/{SPARK_CONSTANTS.MAX_PUBLIC_SPARKS} Spark
+               slots
+            </Typography>
+
+            <LinearProgress
+               sx={styles.progressBar}
+               color={isCriticalState ? "error" : "secondary"}
+               variant="determinate"
+               value={
+                  publicSparks.length
+                     ? (publicSparks.length / maxPublicSparks) * 100
+                     : 0
+               }
+            />
+         </Box>
+      </BrandedTooltip>
+   )
+}
+
+const renderCriticalMessage = () => (
+   <Box>
+      <Typography variant={"h6"} sx={styles.headerMessage}>
+         You’re running out of Spark slots!
+      </Typography>
+      <Typography variant={"body1"} fontSize={16}>
+         Your company is nearing its Spark slot limit. Consider getting more
+         slots to continue engaging your talent community with more Sparks.
+      </Typography>
+   </Box>
+)
+const renderDefaultMessage = (maxPublicSparks: number) => (
+   <Box>
+      <Typography variant={"h6"} sx={styles.headerMessage}>
+         Your company Sparks are visible!
+      </Typography>
+      <Typography variant={"body1"} fontSize={16}>
+         {`Congrats! Your Sparks are now live on CareerFairy. Currently you can
+            publish a maximum of ${maxPublicSparks} Sparks.`}
+      </Typography>
+   </Box>
+)
+
+export default SparksCounter
--- apps/web/components/views/admin/sparks/general-sparks-view/header/SparksProgressIndicator.tsx
+++ apps/web/components/views/admin/sparks/general-sparks-view/header/SparksProgressIndicator.tsx
@@ -1,95 +1,215 @@
-import { FC } from "react"
-import {
-   Box,
-   LinearProgress,
-   linearProgressClasses,
-   tooltipClasses,
-   Typography,
-} from "@mui/material"
+import { Box, CircularProgress, Stack, Typography } from "@mui/material"
 import { sxStyles } from "../../../../../../types/commonTypes"
-import { Spark } from "@careerfairy/shared-lib/sparks/sparks"
-import BrandedTooltip from "../../../../common/tooltips/BrandedTooltip"
+import { useGroup } from "../../../../../../layouts/GroupDashboardLayout"
+import { Upload } from "react-feather"
+import CreateSparkButton from "../../components/CreateSparkButton"
+import React, { FC, useMemo } from "react"
+import useGroupSparks from "../../../../../custom-hook/spark/useGroupSparks"
+import { SPARK_CONSTANTS } from "@careerfairy/shared-lib/sparks/constants"
 
 const styles = sxStyles({
-   tooltip: {
-      maxWidth: 350,
-      [`& .${tooltipClasses.tooltip}`]: {
-         maxWidth: 350,
-      },
+   root: {
+      display: "flex",
+      flexDirection: { xs: "column", md: "row" },
+      bgcolor: "#F8F5FF",
+      p: 2,
+      borderRadius: 4,
+      border: "1px solid #E0D9FF",
    },
-   progressBar: {
+   info: {
+      maxWidth: { xs: "unset", md: "50%" },
+   },
+   progress: {
       width: "100%",
-      height: 10,
-      borderRadius: 5,
-      bgcolor: "grey.300",
-      [`& .${linearProgressClasses.bar}`]: {
-         borderRadius: 5,
-      },
+      alignItems: "center",
+      justifyContent: "space-evenly",
+      mt: { xs: 4, md: "unset" },
    },
-   progressCount: {
+   singleProgress: {
+      alignItems: "center",
+   },
+   creator: {
       color: (theme) => theme.palette.grey.A700,
    },
-   headerMessage: {
-      mb: 2,
-      fontWeight: "bold",
+   circle: {
+      borderRadius: "60%",
+      boxShadow: "inset 0 0 0 6px #C9C0DE",
+   },
+   circularProgress: {
+      position: "relative",
+      display: "inline-flex",
+   },
+   percentageValue: {
+      top: 3,
+      left: 3,
+      bottom: 0,
+      right: 0,
+      position: "absolute",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+   },
+   createBtn: {
+      width: { xs: "100%", md: "fit-content" },
    },
 })
 
-type Props = {
-   isCriticalState: boolean
-   publicSparks: Spark[]
+type CreatorWithSparksNumber = {
+   creatorId: string
+   numberOfSparks: number
+   progress?: number
 }
 
-const SparksProgressIndicator: FC<Props> = ({
-   isCriticalState,
-   publicSparks,
-}) => {
-   return (
-      <BrandedTooltip
-         title={
-            isCriticalState ? renderCriticalMessage() : renderDefaultMessage()
-         }
-         sx={styles.tooltip}
-      >
-         <Box>
-            <Typography variant={"body1"} sx={styles.progressCount}>
-               {publicSparks.length}/15 Spark slots
-            </Typography>
+const SparksProgressIndicator = () => {
+   const { group } = useGroup()
+   const { data: sparks } = useGroupSparks(group.groupId)
 
-            <LinearProgress
-               sx={styles.progressBar}
-               color={isCriticalState ? "error" : "secondary"}
-               variant="determinate"
-               value={
-                  publicSparks.length ? (publicSparks.length / 15) * 100 : 0
+   const creatorsToValidate = useMemo(() => {
+      // to get the number of public sparks per creator
+      const creatorsWithSparks = sparks.reduce((acc, spark) => {
+         if (spark.published) {
+            const existingCreatorIndex = acc.findIndex(
+               ({ creatorId }) => creatorId === spark.creator.id
+            )
+
+            if (existingCreatorIndex >= 0) {
+               acc[existingCreatorIndex] = {
+                  ...acc[existingCreatorIndex],
+                  numberOfSparks: acc[existingCreatorIndex]?.numberOfSparks + 1,
                }
-            />
-         </Box>
-      </BrandedTooltip>
+            } else {
+               acc.push({
+                  creatorId: spark.creator.id,
+                  numberOfSparks: 1,
+               })
+            }
+         }
+
+         return acc
+      }, [] as CreatorWithSparksNumber[])
+
+      const sortedCreatorsWithSparks = creatorsWithSparks.sort(
+         (a, b) => b.numberOfSparks - a.numberOfSparks
+      )
+
+      // In case there are less than 3 creators, add an empty object for the missing elements
+      while (
+         sortedCreatorsWithSparks.length <
+         SPARK_CONSTANTS.MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS
+      ) {
+         sortedCreatorsWithSparks.push({
+            creatorId: "",
+            numberOfSparks: 0,
+         })
+      }
+
+      // To get only the minimum creators need to publish sparks from the sorted array
+      // Add the progress percentage to each element of the array
+      return sortedCreatorsWithSparks
+         .slice(0, SPARK_CONSTANTS.MINIMUM_CREATORS_TO_PUBLISH_SPARKS)
+         .map((creatorWithSparks) => {
+            const progress =
+               creatorWithSparks.numberOfSparks >
+               SPARK_CONSTANTS.MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS
+                  ? 100
+                  : Math.floor(
+                       (creatorWithSparks.numberOfSparks /
+                          SPARK_CONSTANTS.MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS) *
+                          100
+                    )
+
+            return {
+               ...creatorWithSparks,
+               progress,
+            }
+         })
+   }, [sparks])
+
+   return (
+      <Box sx={styles.root}>
+         <Stack spacing={2} sx={styles.info}>
+            <Typography variant={"h4"} fontWeight={"bold"}>
+               Your Sparks are not published yet!
+            </Typography>
+            <Typography variant={"body1"} fontSize={16}>
+               Almost there! Providing your talent community with valuable
+               insights into your company requires some initial content.
+               Remember, hidden Sparks don't count!
+            </Typography>
+            <CreateSparkButton
+               startIcon={<Upload size={18} />}
+               sx={styles.createBtn}
+            >
+               Upload a new Spark
+            </CreateSparkButton>
+         </Stack>
+         <Stack spacing={4} direction={"row"} sx={styles.progress}>
+            {creatorsToValidate.map((creatorWithSparks, index) => (
+               <ProgressIndicator
+                  key={index}
+                  creatorWithSparks={creatorWithSparks}
+                  index={index + 1}
+               />
+            ))}
+         </Stack>
+      </Box>
    )
 }
 
-const renderCriticalMessage = () => (
-   <Box>
-      <Typography variant={"h6"} sx={styles.headerMessage}>
-         You’re running out of Spark slots!
-      </Typography>
-      <Typography variant={"body1"} fontSize={16}>
-         Your company is nearing its Spark slot limit. Consider getting more
-         slots to continue engaging your talent community with more Sparks.
-      </Typography>
-   </Box>
-)
-const renderDefaultMessage = () => (
-   <Box>
-      <Typography variant={"h6"} sx={styles.headerMessage}>
-         Your company Sparks are visible!
-      </Typography>
-      <Typography variant={"body1"} fontSize={16}>
-         Congrats! Your Sparks are now live on CareerFairy. Currently you can
-         publish a maximum of 15 Sparks.
-      </Typography>
-   </Box>
-)
-
 export default SparksProgressIndicator
+
+type ProgressIndicator = {
+   creatorWithSparks: CreatorWithSparksNumber
+   index: number
+}
+const ProgressIndicator: FC<ProgressIndicator> = ({
+   creatorWithSparks,
+   index,
+}) => {
+   const numberOfSparks = useMemo(
+      () =>
+         creatorWithSparks.numberOfSparks >
+         SPARK_CONSTANTS.MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS
+            ? SPARK_CONSTANTS.MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS
+            : creatorWithSparks.numberOfSparks,
+      [creatorWithSparks.numberOfSparks]
+   )
+
+   const isValid = useMemo(
+      () =>
+         creatorWithSparks.numberOfSparks >=
+         SPARK_CONSTANTS.MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS,
+      [creatorWithSparks.numberOfSparks]
+   )
+
+   return (
+      <Stack
+         spacing={1}
+         sx={styles.singleProgress}
+         key={creatorWithSparks.creatorId}
+      >
+         <Box sx={styles.circularProgress}>
+            <CircularProgress
+               variant="determinate"
+               value={creatorWithSparks.progress}
+               size={60}
+               thickness={4}
+               sx={styles.circle}
+               color={isValid ? "primary" : "secondary"}
+            />
+            <Box sx={styles.percentageValue}>
+               <Typography variant="body1" color="text.primary">
+                  {creatorWithSparks.progress}%
+               </Typography>
+            </Box>
+         </Box>
+         <Typography variant={"h4"} fontWeight={"bold"}>
+            {numberOfSparks}/
+            {SPARK_CONSTANTS.MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS}
+         </Typography>
+         <Typography variant={"h6"} sx={styles.creator}>
+            Creator {index}
+         </Typography>
+      </Stack>
+   )
+}
