8f9a7770b87b77d4d39fcd7b67d6cf0e38cce7dd
habib <kadirit@hotmail.com>
2023-09-14 17:49:57 +0200

    Update parameter names and types in userSparks.ts
    - Change `sparkEvents` to `events` in `markSparkAsSeenByUser` and `trackSparkEvents` functions.
    - Change `sparkSecondsWatched` to `events` in `trackSparkSecondsWatched` function.
    
    Refactor common client and server properties in analytics.ts
    
    - Rename `SparkEventClient` to `SparkEventClient` and remove `stringTimestamp` field.
    - Rename `SparkSecondsWatchedClient` to `SparkSecondWatchedClient` and remove `stringTimestamp` field.
    - Create new type `CommonClientFields` to include common client properties.
    - Create new type `CommonServerFields` to include common server properties.
    - Update `SparkEventClient` to extend `CommonClientFields`.
    - Update `SparkSecondWatchedClient` to extend `CommonClientFields`.
    - Update `SparkEvent` to extend `CommonServerFields`.
    - Update `SparkSecondWatched` to extend `CommonServerFields`.
    - Update `SparkClientEventsPayload` and `SparkSecondsWatchedClientPayload` to use new `EventsPayload` type.

--- packages/shared-lib/src/sparks/analytics.ts
+++ packages/shared-lib/src/sparks/analytics.ts
@@ -1,18 +1,45 @@
 /**
+ * Common client properties for SparkEventClient and SparkSecondWatchedClient
+ */
+type CommonClientFields = {
+   /** Unique identifier for the Spark */
+   sparkId: string
+   /** Visitor ID to help identify unique users in case they are not logged in */
+   visitorId: string
+   /** A sessionId is a unique identifier generated each time a user views a specific spark. When the user scrolls to a new spark, a new sessionId is generated. */
+   sessionId: string
+   /** ISO Alpha-2 Country code of the user's university at the time of the event */
+   universityCountry: string | null
+   /**
+    * ISO 8601 formatted string representing the timestamp of the event.
+    * This timestamp is generated on the client side when the event occurs.
+    */
+   stringTimestamp: string
+}
+
+/**
+ * Common server properties for SparkEvent and SparkSecondWatched
+ * These properties are added by the cloud function.
+ * They are not sent by the client.
+ */
+type CommonServerFields = {
+   /** User AuthUID, null if not logged in */
+   userId: string | null
+   /** Timestamp of the event */
+   timestamp: Date
+   /** ISO Alpha-2 Country code of the user at the time of the event */
+   countryCode: string | null
+}
+
+/**
  * Type representing the data that the client sends to the callable function.
  * It includes all the fields except `userId`, `timestamp`, and `countryCode`.
  */
 export type SparkEventClient = {
-   /** Unique identifier for the Spark */
-   sparkId: string
    /** Original Spark ID from the feed (first one) */
    originalSparkId: string | null
-   /** Visitor ID to help identify unique users in case they are not logged in */
-   visitorId: string
    /** Document referrer information */
    referrer: string | null
-   /** A sessionId is a unique identifier generated each time a user views a specific spark. When the user scrolls to a new spark, a new sessionId is generated. */
-   sessionId: string
    /** Referral code of a user from the URL */
    referralCode: string | null
    /** UTM Source */
@@ -27,75 +54,43 @@ export type SparkEventClient = {
    utm_content: string | null
    /** Type of action (Share, Like, Impression, Click on career page CTA, Spark completely watched, Played spark) */
    actionType: SparkEventActionType
-   /** ISO Alpha-2 Country code of the user's university at the time of the event */
-   universityCountry: string | null
-   /**
-    * ISO 8601 formatted string representing the timestamp of the event.
-    * This timestamp is generated on the client side when the event occurs.
-    */
-   stringTimestamp: string
-}
-
-/**
- * Type representing the complete data being inserted into BigQuery.
- * It includes all fields from `SparkEventClient` plus `userId`, `timestamp`, and `countryCode` which are added by the cloud function.
- * The `stringTimestamp` field from `SparkEventClient` is omitted because it's replaced by the `timestamp` field, which is a Date object.
- */
-export type SparkEvent = Omit<SparkEventClient, "stringTimestamp"> & {
-   /** User AuthUID, null if not logged in */
-   userId: string | null
-   /** Timestamp of the event */
-   timestamp: Date
-   /** ISO Alpha-2 Country code of the user at the time of the event */
-   countryCode: string | null
-}
-
-export type SparkClientEventsPayload = {
-   sparkEvents: SparkEventClient[]
-}
+} & CommonClientFields
 
 /**
  * Type representing the data that the client sends to the callable function for SparkSecondsWatched.
  * It includes all the fields except `userId`, `timestamp`, and `countryCode`.
  */
 export type SparkSecondWatchedClient = {
-   /** Unique identifier for the Spark */
-   sparkId: string
-   /** User AuthUID, null if not logged in */
-   userId: string | null
-   /** Visitor ID to help identify unique users in case they are not logged in */
-   visitorId: string
    /** Position (in seconds, allowing for fractions up to 2 decimal places) in the spark at the event time */
    videoEventPositionInSeconds: number
-   /** A sessionId is a unique identifier generated each time a user views a specific spark. When the user scrolls to a new spark, a new sessionId is generated. */
-   sessionId: string
-   /** ISO Alpha-2 Country code of the user's university at the time of the event */
-   universityCountry: string | null
-   /**
-    * ISO 8601 formatted string representing the timestamp of the event.
-    * This timestamp is generated on the client side when the event occurs.
-    */
-   stringTimestamp: string
-}
+} & CommonClientFields
+
+/**
+ * Type representing the complete data being inserted into BigQuery for SparkEvents.
+ */
+export type SparkEvent = Omit<SparkEventClient, "stringTimestamp"> &
+   CommonServerFields
 
 /**
  * Type representing the complete data being inserted into BigQuery for SparkSecondsWatched.
- * It includes all fields from `SparkSecondsWatchedClient` plus `userId`, `timestamp`, and `countryCode` which are added by the cloud function.
- * The `stringTimestamp` field from `SparkSecondsWatchedClient` is omitted because it's replaced by the `timestamp` field, which is a Date object.
  */
 export type SparkSecondWatched = Omit<
    SparkSecondWatchedClient,
    "stringTimestamp"
-> & {
-   /** Timestamp of the event */
-   timestamp: Date
-   /** ISO Alpha-2 Country code of the user at the time of the event */
-   countryCode: string | null
+> &
+   CommonServerFields
+
+/**
+ * The payload that the client sends to the callable function.
+ */
+export type EventsPayload<TEvent> = {
+   events: TEvent[]
 }
 
-export type SparkSecondsWatchedClientPayload = {
-   sparkSecondsWatched: SparkSecondWatchedClient[]
-}
+export type SparkClientEventsPayload = EventsPayload<SparkEventClient>
+
+export type SparkSecondsWatchedClientPayload =
+   EventsPayload<SparkSecondWatchedClient>
 
 export const SparkEventActions = {
    Share_WhatsApp: "Share_WhatsApp",
