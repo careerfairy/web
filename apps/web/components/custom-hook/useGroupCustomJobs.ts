fc068e92c7d3e83d79a24938556bae6bb67f467f
goncaloarnauth <goncaloarnauth@outlook.com>
2023-10-06 11:46:48 +0100

    add custom hook to support customJobs real time data

---
+++ apps/web/components/custom-hook/useGroupCustomJobs.ts
@@ -0,0 +1,39 @@
+import { collection } from "firebase/firestore"
+import { useFirestore } from "reactfire"
+import { useMemo } from "react"
+import {
+   CustomJob,
+   pickPublicDataFromCustomJob,
+} from "@careerfairy/shared-lib/groups/customJobs"
+import { useFirestoreCollection } from "./utils/useFirestoreCollection"
+
+/**
+ * Fetch Group Custom Jobs
+ *
+ * @param groupId
+ */
+const useGroupCustomJobs = (groupId: string) => {
+   const collectionRef = collection(
+      useFirestore(),
+      "careerCenterData",
+      groupId,
+      "customJobs"
+   )
+
+   const { data } = useFirestoreCollection<CustomJob>(collectionRef, {
+      idField: "id", // this field will be added to the firestore object
+      suspense: false,
+   })
+
+   return useMemo(() => {
+      // map to business model
+      const jobs =
+         data?.map((document) => pickPublicDataFromCustomJob(document)) || []
+
+      return {
+         data: jobs,
+      }
+   }, [data])
+}
+
+export default useGroupCustomJobs
