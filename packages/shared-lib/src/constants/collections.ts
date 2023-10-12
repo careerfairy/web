dbcba2ab55b3064d297415d67d9ee1edb6975d5c
habib <kadirit@hotmail.com>
2023-10-12 09:40:49 +0200

    Add "customJobs" to the list of project collections in the shared library.
    This commit adds "customJobs" to the list of project collections in the shared library's constants file. This change ensures that "customJobs" is included when retrieving all project collection IDs.

--- packages/shared-lib/src/constants/collections.ts
+++ packages/shared-lib/src/constants/collections.ts
@@ -16,6 +16,7 @@ export const ALL_PROJECT_COLLECTION_IDS = [
    "companiesUserFollows",
    "companyPageViews",
    "creators",
+   "customJobs",
    "deletedSparks",
    "deletedUsers",
    "detailPageViews",
