a62baae72ab17dd97276e57e0e974ce73d731efe
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-10-18 12:10:42 +0100

    Adds tutorial video url to constants file.

--- apps/web/constants/videos.js
+++ apps/web/constants/videos.js
@@ -2,3 +2,5 @@ export const smilingStreamerVideoUrl =
    "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/landing-videos%2Fzippy-waving-video.mp4?alt=media&token=d54f257b-7cca-455e-ada3-ccfd4f259b09"
 export const analyticsShowCaseVideoUrl =
    "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/landing-videos%2Fanalytics-showcase.mp4?alt=media&token=fd11cee1-2a3e-4084-822e-965018159adc"
+export const sparksTutorialVideoUrl =
+   "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fmisc%2Fsparks-creation-flow-25.mp4?alt=media&token=ce50a4f3-1dfa-4beb-bcd4-5004250f817d&_gl=1*tafkh4*_ga*MTYyMTA3Njk0My4xNjk3MDQyOTc4*_ga_CW55HF8NVT*MTY5NzYyNjQwMi40LjEuMTY5NzYyNjY2NC4zLjAuMA.."
