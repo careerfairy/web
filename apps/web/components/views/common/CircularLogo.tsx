7754b026230ffe0662b2d95837cb4cfe7971a7c7
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-10-23 17:43:22 +0100

    Refactors RoundedLogo component to circular.

---
+++ apps/web/components/views/common/CircularLogo.tsx
@@ -0,0 +1,55 @@
+import { FC } from "react"
+import { Avatar, Box } from "@mui/material"
+import Image, { ImageProps } from "next/image"
+import { sxStyles } from "types/commonTypes"
+
+const getStyles = (size: number) =>
+   sxStyles({
+      root: {
+         width: size,
+         height: size,
+         backgroundColor: "white",
+         border: "solid 2px white",
+         "& > *": {
+            display: "flex",
+         },
+      },
+   })
+
+type Props = {
+   src: string
+   alt: string
+   size?: number
+   borderRadius?: number
+   objectFit?: ImageProps["objectFit"]
+   quality?: number
+}
+
+const CircularLogo: FC<Props> = ({
+   src,
+   alt,
+   size = 50,
+   borderRadius = 50,
+   objectFit = "contain",
+   quality = 70,
+}) => {
+   const adjustedSize = size - 8 // 8 is the padding
+   const styles = getStyles(adjustedSize)
+
+   return (
+      <Avatar variant="circular" sx={styles.root}>
+         <Box borderRadius={borderRadius}>
+            <Image
+               src={src}
+               width={adjustedSize}
+               height={adjustedSize}
+               objectFit={objectFit}
+               alt={alt}
+               quality={quality}
+            />
+         </Box>
+      </Avatar>
+   )
+}
+
+export default CircularLogo
