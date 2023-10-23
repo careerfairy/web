a8157e750e8993500ee5bdc58843845fe3dcd9dd
goncaloarnauth <goncaloarnauth@outlook.com>
2023-10-23 12:00:09 +0100

    add custom job details view on livestream-dialog views; Improve useLivestreamJob and useIsAtsJob hooks;
    (cherry picked from commit fa9b35c00aa821759de0f35f6f5222c31cce79ba)

--- apps/web/components/custom-hook/useIsAtsJob.ts
+++ apps/web/components/custom-hook/useIsAtsJob.ts
@@ -2,7 +2,7 @@ import { Job } from "@careerfairy/shared-lib/ats/Job"
 import { PublicCustomJob } from "@careerfairy/shared-lib/groups/customJobs"
 
 const useIsAtsJob = (job: Job | PublicCustomJob): job is Job => {
-   return "getHiringManager" in job
+   return job ? "getHiringManager" in job : false
 }
 
 export default useIsAtsJob
