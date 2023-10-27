fd6b3724823878226402b8a616d6eaea0213e2cd
goncaloarnauth <goncaloarnauth@outlook.com>
2023-10-27 12:44:10 +0100

    update notification date

--- apps/web/util/DateUtil.js
+++ apps/web/util/DateUtil.js
@@ -288,4 +288,33 @@ export default class DateUtil {
       const formattedDate = dayjs(JSDate).format("DD MMM YYYY")
       return `Live streamed on: ${formattedDate}` // Release date: 15 Dec 2022
    }
+
+   static calculateAndFormatTimeAgo(pastDate) {
+      const now = new Date()
+      const diff = now - pastDate
+
+      const seconds = Math.floor(diff / 1000)
+      const minutes = Math.floor(seconds / 60)
+      const hours = Math.floor(minutes / 60)
+      const days = Math.floor(hours / 24)
+      const weeks = Math.floor(days / 7)
+      const months = Math.floor(days / 30)
+      const years = Math.floor(days / 365)
+
+      if (seconds < 60) {
+         return "just now"
+      } else if (minutes < 60) {
+         return `${minutes} minutes ago`
+      } else if (hours < 24) {
+         return `${hours} hours ago`
+      } else if (days < 7) {
+         return `${days} days ago`
+      } else if (weeks < 4) {
+         return `${weeks} weeks ago`
+      } else if (months < 12) {
+         return `${months} months ago`
+      } else {
+         return `${years} years ago`
+      }
+   }
 }
