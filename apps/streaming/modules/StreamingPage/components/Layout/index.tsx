cc22eead138069389a9b45a597f94ba82ee13e64
habibKadiri <kadirit@hotmail.com>
2023-11-14 09:33:46 +0100

    Simplify side panel height calculation in MiddleContent component

--- apps/streaming/modules/StreamingPage/components/Layout/index.tsx
+++ apps/streaming/modules/StreamingPage/components/Layout/index.tsx
@@ -4,11 +4,11 @@ import { sxStyles } from "@careerfairy/shared-ui"
 
 const styles = sxStyles({
    root: {
-      border: "1px solid black",
       display: "flex",
       flexDirection: "column",
       height: "100vh",
       background: "#F7F8FC",
+      maxHeight: "100vh",
    },
 })
 
