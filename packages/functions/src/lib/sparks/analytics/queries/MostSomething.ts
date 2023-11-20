6fecdb89c896f8350a91dc8abfcbfce495cbdccb
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-20 20:55:48 +0000

    Creates SQL queries for sparks analytics.

--- packages/functions/src/lib/sparks/analytics/queries/MostSomething.ts
+++ packages/functions/src/lib/sparks/analytics/queries/MostSomething.ts
@@ -1,52 +1,43 @@
-export function mostWatched(timePeriod: string) {
-   return `
-    SELECT 
-      sparkId, 
-      count(sparkId) as num_views
-    FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents 
-    WHERE groupId = @groupId
-      AND actionType = "Played_Spark"
-      AND timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL ${timePeriod}))
-    GROUP BY sparkId
-    ORDER BY count(sparkId) DESC
-    LIMIT 3
-  `
-}
+export const mostWatched = `
+  SELECT 
+    sparkId, 
+    count(sparkId) as num_views
+  FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents 
+  WHERE actionType = "Played_Spark"
+    and timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
+  GROUP BY sparkId
+  ORDER BY count(sparkId) DESC
+  LIMIT 3
+`
+export const mostLiked = `
+  SELECT
+    sparkId,
+    COUNT(sparkId) AS total_liked_Sparks
+  FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
+  WHERE actionType = "Like"
+    and timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
+  GROUP BY sparkId
+  ORDER BY COUNT(sparkId) DESC
+  LIMIT 3
+`
 
-export function mostLiked(timePeriod: string) {
-   return `
-    SELECT
-      sparkId,
-      COUNT(sparkId) AS total_liked_Sparks
-    FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
-    WHERE groupId = @groupId
-      AND actionType = "Like"
-      AND timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL ${timePeriod}))
-    GROUP BY sparkId
-    ORDER BY COUNT(sparkId) DESC
-    LIMIT 3
-  `
-}
-
-export function mostShared(timePeriod: string) {
-   return `
-    SELECT
-      sparkId,
-      COUNT(sparkId) AS total_shared_Sparks
-    FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
-    WHERE groupId = @groupId
-      AND actionType LIKE 'Share_%'
-      AND timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL ${timePeriod}))
-    GROUP BY sparkId
-    ORDER BY COUNT(sparkId) DESC
-    LIMIT 3
-  `
-}
+export const mostShared = `
+  SELECT
+    sparkId,
+    COUNT(sparkId) AS total_shared_Sparks
+  FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
+  WHERE actionType in ("Share_Clipboard", "Share_X", 
+      "Share_Facebook", "Share_LinkedIn", "Share_WhatsApp", 
+      "Share_Mobile", "Share_Email", "Share_Other")
+    and timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
+  GROUP BY sparkId
+  ORDER BY COUNT(sparkId) DESC
+  LIMIT 3
+`
 
 export const mostRecent = `
   SELECT spark_id
   FROM careerfairy-e1fd9.firestore_export.sparkStats_schema_sparkStats_latest
-  WHERE spark_group_id = @groupId
   ORDER BY spark_createdAt DESC
   LIMIT 3
 `
