ef3f714659966367c6f04bec0d0f37ab9fe96498
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-20 16:30:13 +0000

    Refactors current BigQueryServiceCore.

--- packages/functions/src/lib/bigQuery/IBigQueryService.ts
+++ packages/functions/src/lib/bigQuery/IBigQueryService.ts
@@ -1,25 +1,32 @@
-import { BigQuery } from "@google-cloud/bigquery"
+import { BigQuery, TableMetadata } from "@google-cloud/bigquery"
+import { getBigQueryTablePrefix } from "../../util"
 
 /**
  * Class to handle BigQuery operations.
  */
 class BigQueryServiceCore {
    protected bigQueryClient: BigQuery
+   protected datasetId: string
+   protected tableId: string
+   protected tableOptions: TableMetadata
 
    /**
     * Create a new BigQueryServiceCore.
     * @param {BigQuery} bigQueryClient - The BigQuery client.
+    * @param {string} datasetId - The ID of the dataset.
+    * @param {string} tableId - The ID of the table.
+    * @param {TableMetadata} tableOptions - The options for creating the table.
     */
-   constructor(bigQueryClient: BigQuery) {
+   constructor(
+      bigQueryClient: BigQuery,
+      datasetId: string,
+      tableId: string,
+      tableOptions: TableMetadata
+   ) {
       this.bigQueryClient = bigQueryClient
-   }
-
-   public async query(sqlQuery: string, params?: object) {
-      const query = {
-         query: sqlQuery,
-         params: params,
-      }
-      return this.bigQueryClient.query(query)
+      this.datasetId = datasetId
+      this.tableId = `${tableId}${getBigQueryTablePrefix()}`
+      this.tableOptions = tableOptions
    }
 }
 
