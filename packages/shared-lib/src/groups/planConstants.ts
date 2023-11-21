30bd5c0060e99c659f602d9278442d3f8f8cd884
habibKadiri <kadirit@hotmail.com>
2023-11-21 13:56:49 +0100

    feat(groups): Add plan constants and plan duration
    - Add `PlanConstants` and `planDurationDays` to `GroupPresenter` class.
    - Create `getPlanConstants` function to retrieve plan constants based on
    plan type.
    - Set `planConstants` in the constructor of `GroupPresenter` class.
    - `getMaxPublicSparks` returns `this.planConstants.MAX_PUBLIC_SPARKS`.
    - `getMinimumCreatorsToPublishSparks` returns
    `this.planConstants.MINIMUM_CREATORS_TO_PUBLISH_SPARKS`.
    - `getMinimumSparksPerCreatorToPublishSparks` returns
    `this.planConstants.MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS`.
    - `getMaxSparkCreatorCount` returns
    `this.planConstants.MAX_SPARK_CREATOR_COUNT`.
    - `assignPlanConstants` function sets `this.planConstants` based on the
    plan type in the constructor.
    - Create `PlanConstants` interface with constants for minimum creators,
    minimum sparks per creator, max public sparks, max spark creator count,
    and plan duration days.
    - Create `PLAN_CONSTANTS` object that maps plan types to plan constants.
    - If the plan type is not found, `getPlanConstants` defaults to the
    'sparks' plan constants.

--- packages/shared-lib/src/groups/planConstants.ts
+++ packages/shared-lib/src/groups/planConstants.ts
@@ -1,6 +1,6 @@
 import { GroupPlanType } from "."
 
-interface PlanConstants {
+export interface PlanConstants {
    /** The minimum number of creators required to publish sparks. */
    MINIMUM_CREATORS_TO_PUBLISH_SPARKS: number
    /** The minimum number of sparks required per creator to publish sparks. */
@@ -9,6 +9,8 @@ interface PlanConstants {
    MAX_PUBLIC_SPARKS: number
    /** The maximum number of creators for a specific group. */
    MAX_SPARK_CREATOR_COUNT: number
+   /** The duration of the plan in days. */
+   PLAN_DURATION_DAYS: number
 }
 
 const PLAN_CONSTANTS: Record<GroupPlanType, PlanConstants> = {
@@ -17,15 +19,24 @@ const PLAN_CONSTANTS: Record<GroupPlanType, PlanConstants> = {
       MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS: 3,
       MAX_PUBLIC_SPARKS: 6,
       MAX_SPARK_CREATOR_COUNT: 1,
+      PLAN_DURATION_DAYS: 75, // 2.5 months
    },
    sparks: {
       MINIMUM_CREATORS_TO_PUBLISH_SPARKS: 3,
       MINIMUM_SPARKS_PER_CREATOR_TO_PUBLISH_SPARKS: 3,
       MAX_PUBLIC_SPARKS: 15,
       MAX_SPARK_CREATOR_COUNT: 100, // No limit
+      PLAN_DURATION_DAYS: 365, // 1 year
    },
 }
 
+/**
+ * This function retrieves the plan constants for a given plan type.
+ * If the plan type is not found, it defaults to the 'sparks' plan constants.
+ *
+ * @param planType - The type of the plan for which constants are to be retrieved.
+ * @returns The constants for the given plan type.
+ */
 export const getPlanConstants = (planType: GroupPlanType): PlanConstants => {
    return PLAN_CONSTANTS[planType] || PLAN_CONSTANTS.sparks
 }
