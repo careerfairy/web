a472c2355569a5dfcda6aaa620d76f2131839e5f
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-21 19:56:33 +0000

    Updates registrations query for new event

--- packages/functions/src/lib/sparks/analytics/queries/Engagement.ts
+++ packages/functions/src/lib/sparks/analytics/queries/Engagement.ts
@@ -23,17 +23,13 @@ export const timeseriesSharesPastYear = `
 `
 
 export const timeseriesRegistrationsPastYear = `
-DECLARE start_date TIMESTAMP DEFAULT TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR));
-
-SELECT 
-  TIMESTAMP_TRUNC(uld.timestamp, DAY) AS x,
-  COUNT(uld.registered_sparkId) AS y
-FROM \`careerfairy-e1fd9.firestore_export.userLivestreamData_schema_userLivestreamData_latest\` uld
-INNER JOIN \`careerfairy-e1fd9.firestore_export.livestreams_schema_livestreams_latest\` l
-  ON uld.livestreamId = l.document_id
-WHERE l.author_groupId = @groupId
-  AND uld.timestamp >= start_date
-  AND uld.registered_sparkId is not null
+  SELECT
+    TIMESTAMP_TRUNC(timestamp, DAY) AS x,
+    COUNT(sparkId) AS y
+  FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
+  WHERE groupId = @groupId
+    AND actionType = "Register_Event"
+    AND timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR))
   GROUP BY x
   ORDER BY x
 ;
