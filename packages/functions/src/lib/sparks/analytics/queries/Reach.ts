949e2801f47d6649f85efb5e2510abdf5c4bd7aa
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-21 16:49:58 +0000

    SQL queries are now scoped to group.

--- packages/functions/src/lib/sparks/analytics/queries/Reach.ts
+++ packages/functions/src/lib/sparks/analytics/queries/Reach.ts
@@ -3,8 +3,9 @@ export const totalViewsPastYear = `
     TIMESTAMP_TRUNC(timestamp, DAY) AS x,
     COUNT(sparkId) AS y
   FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
-  WHERE timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR))
+  WHERE groupId = @groupId
     AND actionType = "Played_Spark"
+    AND timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR))
   GROUP BY x
   ORDER BY x
 `
@@ -14,8 +15,9 @@ export const uniqueViewersPastYear = `
     TIMESTAMP_TRUNC(timestamp, DAY) AS x,
     COUNT(distinct(ifNull(userId, visitorId))) AS y
   FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
-  WHERE timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR))
+  WHERE groupId = @groupId
     AND actionType = "Played_Spark"
+    AND timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR))
   GROUP BY x
   ORDER BY x
 `
