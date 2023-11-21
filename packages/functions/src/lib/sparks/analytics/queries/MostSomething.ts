7041fb1d252971d2f4c0424a52326a222899bf9a
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-21 16:49:58 +0000

    SQL queries are now scoped to group.

--- packages/functions/src/lib/sparks/analytics/queries/MostSomething.ts
+++ packages/functions/src/lib/sparks/analytics/queries/MostSomething.ts
@@ -3,8 +3,9 @@ export const mostWatched = `
     sparkId, 
     count(sparkId) as num_views
   FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents 
-  WHERE actionType = "Played_Spark"
-    and timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
+  WHERE groupId = @groupId
+    AND actionType = "Played_Spark"
+    AND timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
   GROUP BY sparkId
   ORDER BY count(sparkId) DESC
   LIMIT 3
@@ -14,8 +15,9 @@ export const mostLiked = `
     sparkId,
     COUNT(sparkId) AS total_liked_Sparks
   FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
-  WHERE actionType = "Like"
-    and timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
+  WHERE groupId = @groupId
+    AND actionType = "Like"
+    AND timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
   GROUP BY sparkId
   ORDER BY COUNT(sparkId) DESC
   LIMIT 3
@@ -26,10 +28,9 @@ export const mostShared = `
     sparkId,
     COUNT(sparkId) AS total_shared_Sparks
   FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
-  WHERE actionType in ("Share_Clipboard", "Share_X", 
-      "Share_Facebook", "Share_LinkedIn", "Share_WhatsApp", 
-      "Share_Mobile", "Share_Email", "Share_Other")
-    and timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
+  WHERE groupId = @groupId
+    AND actionType LIKE 'Share_%'
+    AND timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @timePeriod)
   GROUP BY sparkId
   ORDER BY COUNT(sparkId) DESC
   LIMIT 3
@@ -38,6 +39,7 @@ export const mostShared = `
 export const mostRecent = `
   SELECT spark_id
   FROM careerfairy-e1fd9.firestore_export.sparkStats_schema_sparkStats_latest
+  WHERE spark_group_id = @groupId
   ORDER BY spark_createdAt DESC
   LIMIT 3
 `
