e3693e39af73479e8e3e57fac89dd4520bfb7321
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-23 15:07:31 +0000

    Revert (#878)
    This merge was agreed through slack.

--- packages/shared-lib/src/sparks/analytics.ts
+++ packages/shared-lib/src/sparks/analytics.ts
@@ -132,7 +132,6 @@ export const SparkEventActions = {
    Watched_CompleteSpark: "Watched_CompleteSpark",
    Played_Spark: "Played_Spark",
    Register_Event: "Register_Event",
-   Create: "Create",
 } as const
 
 export type SparkEventActionType =
