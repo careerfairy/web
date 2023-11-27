fd70f5fa8846fdfa719e19526320c13160a62b69
goncaloarnauth <goncaloarnauth@outlook.com>
2023-11-27 15:48:31 +0000

    create skeleton of job details page; add search functionality;

---
+++ apps/web/pages/group/[groupId]/admin/jobs/index.tsx
@@ -0,0 +1,34 @@
+import GroupDashboardLayout from "../../../../../layouts/GroupDashboardLayout"
+import DashboardHead from "../../../../../layouts/GroupDashboardLayout/DashboardHead"
+import JobsContent from "../../../../../components/views/group/admin/jobs"
+import { SkeletonAdminPage } from "../../../../../components/util/Skeletons"
+import { SuspenseWithBoundary } from "../../../../../components/ErrorBoundary"
+import PrivacyPolicyDialog from "../../../../../components/views/group/admin/jobs/PrivacyPolicyDialog"
+import CreateJobButton from "../../../../../components/views/admin/jobs/components/CreateJobButton"
+import JobFormDialog from "../../../../../components/views/group/admin/jobs/JobFormDialog"
+import { GetServerSidePropsContext } from "next"
+
+const Jobs = ({ groupId }) => (
+   <GroupDashboardLayout
+      titleComponent={"Jobs"}
+      groupId={groupId as string}
+      topBarCta={<CreateJobButton />}
+   >
+      <DashboardHead title="CareerFairy | Jobs" />
+      <SuspenseWithBoundary fallback={<SkeletonAdminPage />}>
+         <JobsContent />
+         <PrivacyPolicyDialog />
+      </SuspenseWithBoundary>
+      <JobFormDialog />
+   </GroupDashboardLayout>
+)
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+   const { groupId } = context.params
+   return {
+      props: {
+         groupId,
+      },
+   }
+}
+export default Jobs
