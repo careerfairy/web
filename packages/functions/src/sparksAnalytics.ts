4477e0532377f3bddcfd9af6f1159139a80fd078
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-27 15:53:04 +0000

    Adds logging and fixes a bug.

--- packages/functions/src/sparksAnalytics.ts
+++ packages/functions/src/sparksAnalytics.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions"
 import config from "./config"
+import { logAndThrow } from "./lib/validations"
 import { middlewares } from "./middlewares/middlewares"
 import {
    dataValidation,
@@ -72,131 +73,174 @@ export const getSparksAnalytics = functions.region(config.region).https.onCall(
       }),
       userShouldBeGroupAdmin(),
       cache((data) => registrationSourcesCacheKey({ ...data })),
-      async (data) => {
+      async (data, context) => {
          const groupId = data.groupId
          const sparksAnalyticsRepo = getSparksAnalyticsRepoInstance(groupId)
 
-         const [
-            totalViews,
-            uniqueViewers,
-            likes,
-            shares,
-            registrations,
-            pageClicks,
-            [
-               mostWatched7days,
-               mostWatched30days,
-               mostWatched6months,
-               mostWatched1year,
-            ],
-            [mostLiked7days, mostLiked30days, mostLiked6months, mostLiked1year],
-            [
-               mostShared7days,
-               mostShared30days,
-               mostShared6months,
-               mostShared1year,
-            ],
-            mostRecent,
-            [
-               topCountries7days,
-               topCountries30days,
-               topCountries6months,
-               topCountries1year,
-            ],
-            [
-               topUniversities7days,
-               topUniversities30days,
-               topUniversities6months,
-               topUniversities1year,
-            ],
-            [
-               topFieldsOfStudy7days,
-               topFieldsOfStudy30days,
-               topFieldsOfStudy6months,
-               topFieldsOfStudy1year,
-            ],
-            [
-               levelsOfStudy7days,
-               levelsOfStudy30days,
-               levelsOfStudy6months,
-               levelsOfStudy1year,
-            ],
-         ] = await Promise.all([
-            sparksAnalyticsRepo.getTotalViewsPastYear(),
-            sparksAnalyticsRepo.getUniqueViewersPastYear(),
-            sparksAnalyticsRepo.getLikesPastYear(),
-            sparksAnalyticsRepo.getSharesPastYear(),
-            sparksAnalyticsRepo.getRegistrationsPastYear(),
-            sparksAnalyticsRepo.getPageClicksPastYear(),
-            fetchTimePeriodData(sparksAnalyticsRepo.getMostWatchedSparks),
-            fetchTimePeriodData(sparksAnalyticsRepo.getMostLikedSparks),
-            fetchTimePeriodData(sparksAnalyticsRepo.getMostSharedSparks),
-            sparksAnalyticsRepo.getMostRecentSparks(),
-            fetchTimePeriodData(sparksAnalyticsRepo.getTopCountries),
-            fetchTimePeriodData(sparksAnalyticsRepo.getTopUniversities),
-            fetchTimePeriodData(sparksAnalyticsRepo.getTopFieldsOfStudy),
-            fetchTimePeriodData(sparksAnalyticsRepo.getLevelsOfStudy),
-         ])
+         functions.logger.info(
+            `Fetching sparks analytics for group ${groupId}...`
+         )
 
-         const SparksAnalyticsPayload: SparksAnalyticsPayload = {
-            reach: {
-               totalViews: totalViews,
-               uniqueViewers: uniqueViewers,
-            },
-            engagement: {
-               likes: likes,
-               shares: shares,
-               registrations: registrations,
-               pageClicks: pageClicks,
-            },
-            most: {
-               watched: {
-                  "7days": mostWatched7days,
-                  "30days": mostWatched30days,
-                  "6months": mostWatched6months,
-                  "1year": mostWatched1year,
+         try {
+            const [
+               totalViews,
+               uniqueViewers,
+               likes,
+               shares,
+               registrations,
+               pageClicks,
+               [
+                  mostWatched7days,
+                  mostWatched30days,
+                  mostWatched6months,
+                  mostWatched1year,
+               ],
+               [
+                  mostLiked7days,
+                  mostLiked30days,
+                  mostLiked6months,
+                  mostLiked1year,
+               ],
+               [
+                  mostShared7days,
+                  mostShared30days,
+                  mostShared6months,
+                  mostShared1year,
+               ],
+               mostRecent,
+               [
+                  topCountries7days,
+                  topCountries30days,
+                  topCountries6months,
+                  topCountries1year,
+               ],
+               [
+                  topUniversities7days,
+                  topUniversities30days,
+                  topUniversities6months,
+                  topUniversities1year,
+               ],
+               [
+                  topFieldsOfStudy7days,
+                  topFieldsOfStudy30days,
+                  topFieldsOfStudy6months,
+                  topFieldsOfStudy1year,
+               ],
+               [
+                  levelsOfStudy7days,
+                  levelsOfStudy30days,
+                  levelsOfStudy6months,
+                  levelsOfStudy1year,
+               ],
+            ] = await Promise.all([
+               sparksAnalyticsRepo.getTotalViewsPastYear(),
+               sparksAnalyticsRepo.getUniqueViewersPastYear(),
+               sparksAnalyticsRepo.getLikesPastYear(),
+               sparksAnalyticsRepo.getSharesPastYear(),
+               sparksAnalyticsRepo.getRegistrationsPastYear(),
+               sparksAnalyticsRepo.getPageClicksPastYear(),
+               fetchTimePeriodData(
+                  sparksAnalyticsRepo.getMostWatchedSparks.bind(
+                     sparksAnalyticsRepo
+                  )
+               ),
+               fetchTimePeriodData(
+                  sparksAnalyticsRepo.getMostLikedSparks.bind(
+                     sparksAnalyticsRepo
+                  )
+               ),
+               fetchTimePeriodData(
+                  sparksAnalyticsRepo.getMostSharedSparks.bind(
+                     sparksAnalyticsRepo
+                  )
+               ),
+               sparksAnalyticsRepo.getMostRecentSparks(),
+               fetchTimePeriodData(
+                  sparksAnalyticsRepo.getTopCountries.bind(sparksAnalyticsRepo)
+               ),
+               fetchTimePeriodData(
+                  sparksAnalyticsRepo.getTopUniversities.bind(
+                     sparksAnalyticsRepo
+                  )
+               ),
+               fetchTimePeriodData(
+                  sparksAnalyticsRepo.getTopFieldsOfStudy.bind(
+                     sparksAnalyticsRepo
+                  )
+               ),
+               fetchTimePeriodData(
+                  sparksAnalyticsRepo.getLevelsOfStudy.bind(sparksAnalyticsRepo)
+               ),
+            ])
+
+            const SparksAnalyticsPayload: SparksAnalyticsPayload = {
+               reach: {
+                  totalViews: totalViews,
+                  uniqueViewers: uniqueViewers,
                },
-               liked: {
-                  "7days": mostLiked7days,
-                  "30days": mostLiked30days,
-                  "6months": mostLiked6months,
-                  "1year": mostLiked1year,
+               engagement: {
+                  likes: likes,
+                  shares: shares,
+                  registrations: registrations,
+                  pageClicks: pageClicks,
                },
-               shared: {
-                  "7days": mostShared7days,
-                  "30days": mostShared30days,
-                  "6months": mostShared6months,
-                  "1year": mostShared1year,
+               most: {
+                  watched: {
+                     "7days": mostWatched7days,
+                     "30days": mostWatched30days,
+                     "6months": mostWatched6months,
+                     "1year": mostWatched1year,
+                  },
+                  liked: {
+                     "7days": mostLiked7days,
+                     "30days": mostLiked30days,
+                     "6months": mostLiked6months,
+                     "1year": mostLiked1year,
+                  },
+                  shared: {
+                     "7days": mostShared7days,
+                     "30days": mostShared30days,
+                     "6months": mostShared6months,
+                     "1year": mostShared1year,
+                  },
+                  recent: mostRecent,
                },
-               recent: mostRecent,
-            },
-            topCountries: {
-               "7days": topCountries7days,
-               "30days": topCountries30days,
-               "6months": topCountries6months,
-               "1year": topCountries1year,
-            },
-            topUniversities: {
-               "7days": topUniversities7days,
-               "30days": topUniversities30days,
-               "6months": topUniversities6months,
-               "1year": topUniversities1year,
-            },
-            topFieldsOfStudy: {
-               "7days": topFieldsOfStudy7days,
-               "30days": topFieldsOfStudy30days,
-               "6months": topFieldsOfStudy6months,
-               "1year": topFieldsOfStudy1year,
-            },
-            levelsOfStudy: {
-               "7days": levelsOfStudy7days,
-               "30days": levelsOfStudy30days,
-               "6months": levelsOfStudy6months,
-               "1year": levelsOfStudy1year,
-            },
+               topCountries: {
+                  "7days": topCountries7days,
+                  "30days": topCountries30days,
+                  "6months": topCountries6months,
+                  "1year": topCountries1year,
+               },
+               topUniversities: {
+                  "7days": topUniversities7days,
+                  "30days": topUniversities30days,
+                  "6months": topUniversities6months,
+                  "1year": topUniversities1year,
+               },
+               topFieldsOfStudy: {
+                  "7days": topFieldsOfStudy7days,
+                  "30days": topFieldsOfStudy30days,
+                  "6months": topFieldsOfStudy6months,
+                  "1year": topFieldsOfStudy1year,
+               },
+               levelsOfStudy: {
+                  "7days": levelsOfStudy7days,
+                  "30days": levelsOfStudy30days,
+                  "6months": levelsOfStudy6months,
+                  "1year": levelsOfStudy1year,
+               },
+            }
+
+            functions.logger.info("Fetching successful.")
+
+            return SparksAnalyticsPayload
+         } catch (error) {
+            logAndThrow("Error in generating sparks analytics", {
+               data,
+               error,
+               context,
+            })
          }
-
-         return SparksAnalyticsPayload
       }
    )
 )
