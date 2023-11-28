adc41f3545b59248d459db5b494bb8e8caaf69d9
habibKadiri <kadirit@hotmail.com>
2023-11-28 17:24:51 +0100

    Refactor ActionButton component and related components and modules
    This commit refactors the ActionButton component and its related
    components and modules. The code changes include:
    - Remove unused imports and variables
    - Improve readability and organization of the code
    - Simplify logic and remove redundant code
    - Update dependencies and imports
    
    These changes aim to enhance the maintainability and readability of the
    codebase, improve performance, and eliminate unused code.

---
+++ apps/web/components/views/admin/company-plans/plan-confirmation-dialog/SucessView.tsx
@@ -0,0 +1,51 @@
+import DialogBody from "components/views/admin/company-plans/plan-confirmation-dialog/DialogBody"
+import ConfettiIcon from "components/views/common/icons/ConfettiIcon"
+import { usePlanConfirmationDialog } from "./CompanyPlanConfirmationDialog"
+import { GroupPlanType } from "@careerfairy/shared-lib/groups"
+import { PLAN_CONSTANTS } from "@careerfairy/shared-lib/groups/planConstants"
+import DateUtil from "util/DateUtil"
+
+const SucessView = () => {
+   const { handleClose, isMutating, groupToManage, updatedGroupPlanType } =
+      usePlanConfirmationDialog()
+
+   return (
+      <DialogBody
+         handleClose={handleClose}
+         actions={
+            <DialogBody.ActionButton
+               buttonType="primary"
+               loading={isMutating}
+               onClick={handleClose}
+            >
+               Done
+            </DialogBody.ActionButton>
+         }
+         icon={<ConfettiIcon />}
+         title="Plan active!"
+         content={`${
+            groupToManage?.universityName
+         }  plan is now active and will be last until ${getExpirationDate(
+            updatedGroupPlanType
+         )}`}
+      />
+   )
+}
+
+/**
+ * This function calculates the expiration date of a plan based on its type.
+ * It uses the PLAN_CONSTANTS to find the duration of the plan in milliseconds,
+ * then adds this duration to the current date and time.
+ * The resulting date is then formatted as a string.
+ *
+ * @param {GroupPlanType} groupPlanType - The type of the plan.
+ * @returns {string} - The expiration date of the plan, formatted as a string.
+ */
+const getExpirationDate = (groupPlanType: GroupPlanType) => {
+   const durattion =
+      PLAN_CONSTANTS[groupPlanType]?.sparks.PLAN_DURATION_MILLISECONDS || 0
+
+   return DateUtil.formatDateToString(new Date(Date.now() + durattion))
+}
+
+export default SucessView
