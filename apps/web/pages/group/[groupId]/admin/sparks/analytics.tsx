7c2bd3faf3480e7652da55f562ed5aa2c4bd89cc
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-28 09:30:43 +0000

    Creates spark's video and analytics pages.

--- apps/web/pages/group/[groupId]/admin/sparks/analytics.tsx
+++ apps/web/pages/group/[groupId]/admin/sparks/analytics.tsx
@@ -1,26 +1,25 @@
 import Sparks from "components/views/admin/sparks"
 import CreateSparkButton from "components/views/admin/sparks/components/CreateSparkButton"
 import { useRouter } from "next/router"
-import GroupDashboardLayout from "../../../../layouts/GroupDashboardLayout"
-import DashboardHead from "../../../../layouts/GroupDashboardLayout/DashboardHead"
+import GroupDashboardLayout from "../../../../../layouts/GroupDashboardLayout"
+import DashboardHead from "../../../../../layouts/GroupDashboardLayout/DashboardHead"
 import SparkPreviewDialog from "components/views/admin/sparks/general-sparks-view/SparkPreviewDialog"
 
-const AdminSparksPage = () => {
+const AdminSparksAnalyticsPage = () => {
    const {
       query: { groupId },
    } = useRouter()
 
    return (
       <GroupDashboardLayout
-         titleComponent={"Sparks"}
+         titleComponent={"Sparks Analytics"}
          groupId={groupId as string}
          topBarCta={<CreateSparkButton />}
       >
-         <DashboardHead title="CareerFairy | My Sparks" />
-         <Sparks />
-         <SparkPreviewDialog />
+         <DashboardHead title="CareerFairy | My Sparks Analytics" />
+         <h1>🧐 ✨✨✨ 📈📉📈 🧐</h1>
       </GroupDashboardLayout>
    )
 }
 
-export default AdminSparksPage
+export default AdminSparksAnalyticsPage
