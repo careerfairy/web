2cd6a26826078e4bdfc47089b31ce5252837ca51
wgoncalvescf <walter.goncalves@careerfairy.io>
2023-11-28 19:49:34 +0000

    Added Tooltip for Sparks CreateOrEditCreator and constants in pages.ts

--- apps/web/constants/pages.ts
+++ apps/web/constants/pages.ts
@@ -1,1 +1,3 @@
 export const wishListBorderRadius = 2
+export const EMAIL_TOOLTIP_INFO =
+   "The email address won't be exposed to your talent community, but will be used to send technical documentation to prepare for live streams and Sparks."
