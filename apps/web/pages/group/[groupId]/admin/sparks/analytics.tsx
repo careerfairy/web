7f818b27054c45d2a7f6109a23e724150ae6f34c
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-28 09:30:43 +0000

    Creates spark's video and analytics pages.

--- apps/web/pages/group/[groupId]/admin/sparks/analytics.tsx
+++ apps/web/pages/group/[groupId]/admin/sparks/analytics.tsx
@@ -4,6 +4,16 @@ import GroupSparkAnalytics from "components/views/admin/sparks/analytics"
 import CreateSparkButton from "components/views/admin/sparks/components/CreateSparkButton"
 import GroupDashboardLayout from "../../../../../layouts/GroupDashboardLayout"
 import DashboardHead from "../../../../../layouts/GroupDashboardLayout/DashboardHead"
+import SparksDialog from "components/views/admin/sparks/sparks-dialog/SparksDialog"
+
+const CreateSparkButtonWrapper: FC = () => {
+   return (
+      <>
+         <CreateSparkButton />
+         <SparksDialog />
+      </>
+   )
+}
 
 const AdminSparksAnalyticsPage: FC = () => {
    const {
@@ -14,7 +24,7 @@ const AdminSparksAnalyticsPage: FC = () => {
       <GroupDashboardLayout
          titleComponent={"Sparks Analytics"}
          groupId={groupId as string}
-         topBarCta={<CreateSparkButton />}
+         topBarCta={<CreateSparkButtonWrapper />}
       >
          <DashboardHead title="CareerFairy | My Sparks Analytics" />
          <GroupSparkAnalytics />
