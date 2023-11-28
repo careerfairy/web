0b48c4d94f0e7d88fbc1ea4ed0706c9472ef089a
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-28 09:30:43 +0000

    Creates spark's video and analytics pages.

--- apps/web/layouts/GroupDashboardLayout/GroupNavList.tsx
+++ apps/web/layouts/GroupDashboardLayout/GroupNavList.tsx
@@ -86,6 +86,20 @@ const GroupNavList = () => {
                     pathname: `/${baseHrefPath}/${baseParam}/admin/sparks`,
                     Icon: SparksIcon,
                     title: "Sparks",
+                    childLinks: [
+                       {
+                          id: "videos",
+                          href: `/${baseHrefPath}/${group.id}/admin/sparks`,
+                          pathname: `/${baseHrefPath}/${baseParam}/admin/sparks`,
+                          title: "Videos",
+                       },
+                       {
+                          id: "analytics",
+                          href: `/${baseHrefPath}/${group.id}/admin/sparks/analytics`,
+                          pathname: `/${baseHrefPath}/${baseParam}/admin/sparks/analytics`,
+                          title: "Analytics",
+                       },
+                    ],
                  },
               ]
             : []),
