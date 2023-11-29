a248c912f6569e7593929e3540450f5c7d717b6c
Gonçalo Arnauth <goncalo@careerfairy.io>
2023-11-29 16:25:01 +0000

    Feature/cf 601 - Job section #1 (#873)
    * add Jobs leftMenu section; Update SteppedDialog component to be more generic; Create PrivacyPolicy dialog
    * improve mobile experience
    * add basic redux for admin job section; Create no jobs view; Update privacyPolicy with redux logic
    * add CreateJobButton component;

---
+++ apps/web/components/views/admin/jobs/components/CreateJobButton.tsx
@@ -0,0 +1,44 @@
+import { Button, ButtonProps } from "@mui/material"
+import { FC, useCallback } from "react"
+import { sxStyles } from "../../../../../types/commonTypes"
+import { useDispatch } from "react-redux"
+import {
+   openJobFormDialog,
+   openPrivacyPolicyDialog,
+} from "../../../../../store/reducers/adminJobsReducer"
+import { PlusCircle } from "react-feather"
+import useGroupFromState from "../../../../custom-hook/useGroupFromState"
+
+const styles = sxStyles({
+   root: {
+      textTransform: "none",
+   },
+})
+
+const CreateJobButton: FC<ButtonProps> = ({ sx, children, ...props }) => {
+   const dispatch = useDispatch()
+   const { group } = useGroupFromState()
+
+   const handleCreateJob = useCallback(() => {
+      if (group.privacyPolicyActive) {
+         dispatch(openJobFormDialog())
+      } else {
+         dispatch(openPrivacyPolicyDialog())
+      }
+   }, [dispatch, group.privacyPolicyActive])
+
+   return (
+      <Button
+         onClick={handleCreateJob}
+         color="secondary"
+         sx={[styles.root, ...(Array.isArray(sx) ? sx : [sx])]}
+         variant="contained"
+         startIcon={<PlusCircle />}
+         {...props}
+      >
+         {children || "Create new job"}
+      </Button>
+   )
+}
+
+export default CreateJobButton
