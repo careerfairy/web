79f4bafe010d01998955c1e0ea35915ad1e8bbb0
goncaloarnauth <goncaloarnauth@outlook.com>
2023-11-29 18:33:23 +0000

    add transaction to verify that the user possesses enough credits for the deduction operation;

--- packages/shared-lib/src/rewards/RewardRepository.ts
+++ packages/shared-lib/src/rewards/RewardRepository.ts
@@ -4,6 +4,7 @@ import BaseFirebaseRepository, {
 } from "../BaseFirebaseRepository"
 import { Create } from "../commonTypes"
 import { RewardAction, RewardDoc, REWARDS } from "./rewards"
+import { UserData } from "../users"
 
 export interface IRewardRepository {
    /**
@@ -90,15 +91,35 @@ export class FirebaseRewardRepository
       return querySnapshot.docs[0].data()
    }
 
+   /**
+    * Applies or deducts credits to/from a user's account.
+    *
+    * This method is responsible for updating the credits in the user's account.
+    * It uses Firestore transactions to ensure data consistency.
+    *
+    * @example
+    * // Applying 10 credits to a user's account
+    * await applyCreditsToUser('user@example.com', 10);
+    *
+    * // Deducting 5 credits from a user's account
+    * await applyCreditsToUser('user@example.com', -5);
+    */
    async applyCreditsToUser(userEmail: string, credits: number): Promise<void> {
       if (!credits) return // skip 0/null/undefined credits
 
-      await this.firestore
-         .collection("userData")
-         .doc(userEmail)
-         .update({
-            credits: this.fieldValue.increment(credits),
-         })
+      const userRef = this.firestore.collection("userData").doc(userEmail)
+
+      await this.firestore.runTransaction(async (transaction) => {
+         const userDoc = await transaction.get(userRef)
+         const user = userDoc.data() as UserData
+
+         // Verify that the user possesses enough credits for the deduction operation
+         if (credits > 0 || user.credits >= credits) {
+            await userRef.update({
+               credits: this.fieldValue.increment(credits),
+            })
+         }
+      })
    }
 }
 
