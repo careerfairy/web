06b95b8b083f4cbfbba48cdd20e5e79c8da15913
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-11-30 15:40:41 +0000

    Isolates timeframe selector into its own component. UI fixes.

---
+++ apps/web/components/views/admin/sparks/analytics/components/GroupSparkAnalyticsCardContainer.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import { Box, Typography } from "@mui/material"
+import { sxStyles } from "types/commonTypes"
+
+const styles = sxStyles({
+   root: {
+      width: "100%",
+      backgroundColor: "#FFFFFF",
+      borderRadius: "8px",
+      padding: "16px 12px",
+   },
+   title: {
+      fontSize: "20px",
+      fontWeight: 600,
+      lineHeight: "30px",
+      letterSpacing: "0em",
+      textAlign: "left",
+      marginBottom: "21px",
+   },
+})
+
+type GroupSparkAnalyticsCardContainerProps = {
+   title: string
+   children: React.ReactNode
+}
+
+export const GroupSparkAnalyticsCardContainer: React.FC<
+   GroupSparkAnalyticsCardContainerProps
+> = ({ title, children }) => {
+   return (
+      <Box sx={styles.root}>
+         <Typography sx={styles.title}>{title}</Typography>
+         {children}
+      </Box>
+   )
+}
