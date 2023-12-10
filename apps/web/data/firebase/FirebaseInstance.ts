7f4aa8b662559869d8dad73412b4f172df493190
wgoncalvescf <walter.goncalves@careerfairy.io>
2023-12-10 22:41:52 +0000

    Renamed FIREBASE starting variables to original

--- apps/web/data/firebase/FirebaseInstance.ts
+++ apps/web/data/firebase/FirebaseInstance.ts
@@ -8,13 +8,13 @@ import { shouldUseEmulators } from "../../util/CommonUtil"
 import { fromDateFirestoreFn } from "@careerfairy/shared-lib/dist/firebaseTypes"
 
 export const firebaseConfig = {
-   apiKey: process.env.FIREBASE_API_KEY,
-   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-   databaseURL: process.env.FIREBASE_DATABASE_URL,
-   projectId: process.env.FIREBASE_PROJECT_ID,
-   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-   appId: process.env.FIREBASE_APP_ID,
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 export const region = "europe-west1"
@@ -73,7 +73,6 @@ const getFirestoreSettings = (
 const firebaseApp: firebase.app.App = createFirebaseInstance("[DEFAULT]")
 
 export const FirestoreInstance = firebaseApp.firestore()
-console.log(firebaseConfig)
 export const AuthInstance = firebaseApp.auth()
 export const FunctionsInstance = firebaseApp.functions(region)
 
