1d3aee89e434e29a655e49087248f54462e9b66d
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-12-11 12:25:24 +0000

    Rename files to match PR requests.

--- packages/functions/src/sparksAnalytics.ts
+++ packages/functions/src/sparksAnalytics.ts
@@ -19,7 +19,7 @@ import {
    PieChartWithPastData,
    TimePeriodParams,
    TimeseriesDataPoint,
-} from "./lib/sparks/analytics/SparksAnalytics"
+} from "./lib/sparks/analytics/GroupSparksAnalyticsTypes"
 
 type ReachData = {
    totalViews: TimeseriesDataPoint[]
