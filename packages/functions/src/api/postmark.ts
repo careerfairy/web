ee147f8ec765d199cb45a269e5fbd1ac7bdc674b
wgoncalvescf <walter.goncalves@careerfairy.io>
2023-12-11 21:43:54 +0000

    Added variables to pipeline YML

--- packages/functions/src/api/postmark.ts
+++ packages/functions/src/api/postmark.ts
@@ -3,11 +3,10 @@ import * as postmark from "postmark"
 import { ServerClient } from "postmark"
 import { Callback, MessageSendingResponse } from "postmark/dist/client/models"
 import { isLocalEnvironment, isTestEnvironment } from "../util"
-let serverToken = "3f6d5713-5461-4453-adfd-71f5fdad4e63"
+let serverToken = process.env.POSTMARK_SERVER_TOKEN
 
 // on local emulators use the sandbox environment
 if (isLocalEnvironment()) {
-   serverToken = "40c62a86-6189-432d-b3f8-f25b345184aa"
    console.log(
       "Using postmark sandbox environment, you need to check the sent emails on their dashboard"
    )
