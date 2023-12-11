8c78237bb1dac384038080b74ac51b7d4ead62c9
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-12-11 09:32:36 +0000

    Fixes sparks creation dialog.

--- apps/web/components/views/admin/sparks/components/CreateSparkButton.tsx
+++ apps/web/components/views/admin/sparks/components/CreateSparkButton.tsx
@@ -3,6 +3,7 @@ import { FC, useCallback } from "react"
 import { useDispatch } from "react-redux"
 import { openSparkDialog } from "store/reducers/adminSparksReducer"
 import { sxStyles } from "types/commonTypes"
+import SparksDialog from "../sparks-dialog/SparksDialog"
 
 const styles = sxStyles({
    root: {
@@ -18,15 +19,18 @@ const CreateSparkButton: FC<ButtonProps> = ({ sx, children, ...props }) => {
    }, [dispatch])
 
    return (
-      <Button
-         onClick={handleOpen}
-         color="secondary"
-         sx={[styles.root, ...(Array.isArray(sx) ? sx : [sx])]}
-         variant="contained"
-         {...props}
-      >
-         {children || "Upload a new Spark"}
-      </Button>
+      <>
+         <Button
+            onClick={handleOpen}
+            color="secondary"
+            sx={[styles.root, ...(Array.isArray(sx) ? sx : [sx])]}
+            variant="contained"
+            {...props}
+         >
+            {children || "Upload a new Spark"}
+         </Button>
+         <SparksDialog />
+      </>
    )
 }
 
