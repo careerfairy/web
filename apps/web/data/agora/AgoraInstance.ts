e980e294c8c4f6145e5c40dfb381554498e85940
wgoncalvescf <walter.goncalves@careerfairy.io>
2023-12-12 13:56:02 +0000

    Debugging instructions

--- apps/web/data/agora/AgoraInstance.ts
+++ apps/web/data/agora/AgoraInstance.ts
@@ -2,7 +2,7 @@ import { shouldUseEmulators } from "../../util/CommonUtil"
 
 // Project: Production
 let appID = process.env.AGORA_APP_ID
-
+console.log("WG-TBD-ENV: " + JSON.stringify(process.env))
 if (shouldUseEmulators()) {
    // Project: CareerFairyStreaming
 
