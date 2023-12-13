d407d87569dd806701782de7b08d544d880dc707
goncaloarnauth <goncaloarnauth@outlook.com>
2023-12-13 16:58:02 +0000

    Add privacy policy validation for the applicants view

---
+++ apps/web/components/views/group/admin/jobs/details/jobApplicants/NoApplicantsData.tsx
@@ -0,0 +1,56 @@
+import { Box, Stack, Typography } from "@mui/material"
+import { sxStyles } from "../../../../../../../types/commonTypes"
+import { AlertOctagon } from "react-feather"
+
+const styles = sxStyles({
+   wrapper: {
+      display: "flex",
+      flexDirection: "column",
+      height: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      mt: { md: "100px" },
+      px: { md: 4 },
+   },
+   content: {
+      maxWidth: "610px",
+      alignItems: "center",
+      textAlign: "center",
+      background: "white",
+      borderRadius: "16px",
+      py: 4,
+      px: { xs: 1, md: 4 },
+   },
+   title: {
+      fontSize: "20px",
+      fontWeight: "600",
+   },
+   message: {
+      fontSize: "16px",
+      fontWeight: "400",
+   },
+})
+const NoApplicantsData = () => {
+   return (
+      <Box sx={styles.wrapper}>
+         <Stack spacing={2} sx={styles.content}>
+            <AlertOctagon size={68} color={"#6749EA"} />
+
+            <Typography sx={styles.title}>
+               Unable to display applicants’ data
+            </Typography>
+
+            <Typography sx={styles.message}>
+               {/* eslint-disable-next-line react/no-unescaped-entities */}
+               Your company's privacy policy is necessary to display applicant
+               data for new job openings. It wasn’t added to your company
+               profile when this job opening was created. As a result, you won’t
+               be able to access applicants’ data for this particular job
+               opening.
+            </Typography>
+         </Stack>
+      </Box>
+   )
+}
+
+export default NoApplicantsData
