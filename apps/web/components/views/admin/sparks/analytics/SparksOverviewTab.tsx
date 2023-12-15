497dfcebc30830c157bc60edad97a864f8333504
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-12-15 17:50:26 +0000

    Adds responsive select drawer to overview tab.

--- apps/web/components/views/admin/sparks/analytics/SparksOverviewTab.tsx
+++ apps/web/components/views/admin/sparks/analytics/SparksOverviewTab.tsx
@@ -1,9 +1,106 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import { Box, Stack } from "@mui/material"
 import { GroupSparkAnalyticsCardContainer } from "./components/GroupSparkAnalyticsCardContainer"
 import ChartSwitchButton from "./components/charts/ChartSwitchButton"
 import { CFLineChart } from "./components/charts/CFLineChart"
 import SparksStaticCard from "./components/SparksStaticCard"
+import { ResponsiveSelectWithDrawer } from "./components/ResponsiveSelectWithDrawer"
+import { sxStyles } from "types/commonTypes"
+
+const styles = sxStyles({
+   sparksContainer: {
+      display: "flex",
+      flexDirection: {
+         xs: "column",
+         md: "row",
+      },
+      gap: 1.5,
+   },
+   mostSomethingContainerTitleContainer: {
+      fontSize: "20px",
+      fontWeight: 600,
+      lineHeight: "30px",
+      letterSpacing: "0em",
+      textAlign: "left",
+      marginBottom: "21px",
+      display: "flex",
+      alignItems: "center",
+   },
+   mostSomethingSelect: {
+      width: "100%",
+      textTransform: "lowercase",
+      color: "#6749EA",
+      fontSize: "20px",
+      fontWeight: 600,
+      lineHeight: "30px",
+      letterSpacing: "0em",
+   },
+   mostSomethingSelectMenu: {
+      marginTop: 4,
+      marginLeft: "-3px",
+      ".MuiPaper-root": {
+         minWidth: "0 !important",
+      },
+   },
+   mostSomethingSelectContainer: {
+      "& .MuiSelect-select": {
+         paddingLeft: "0 !important",
+         paddingTop: "0 !important",
+         paddingBottom: "0 !important",
+      },
+      "& fieldset": {
+         "&.MuiOutlinedInput-notchedOutline": {
+            border: "0 !important",
+         },
+      },
+   },
+   chartSwitchButtonsContainers: {
+      display: {
+         xs: "none",
+         md: "flex",
+      },
+      gap: 1.5,
+   },
+})
+
+const MostSomethingTitle = ({
+   selectMostSomething,
+   setSelectMostSomething,
+   options,
+}) => {
+   return (
+      <Box sx={styles.mostSomethingContainerTitleContainer}>
+         Your&nbsp;
+         <ResponsiveSelectWithDrawer
+            selectValue={selectMostSomething}
+            setSelectValue={setSelectMostSomething}
+            options={options}
+            selectProps={{
+               sx: styles.mostSomethingSelect,
+            }}
+            selectMenuProps={{
+               anchorOrigin: {
+                  vertical: "top",
+                  horizontal: "left",
+               },
+               transformOrigin: {
+                  vertical: "top",
+                  horizontal: "left",
+               },
+               sx: styles.mostSomethingSelectMenu,
+            }}
+            selectContainerProps={{ sx: styles.mostSomethingSelectContainer }}
+         />
+      </Box>
+   )
+}
+
+const mostSomethingSelectOptions = [
+   { value: "watched", label: "Most watched Sparks" },
+   { value: "liked", label: "Most liked Sparks" },
+   { value: "shared", label: "Most shared Sparks" },
+   { value: "recent", label: "Most recent Sparks" },
+]
 
 type SparksOverviewTabProps = {
    timeFilter: any
@@ -21,17 +118,14 @@ const SparksOverviewTab: FC<SparksOverviewTabProps> = ({
    engagementData,
    mostSomethingData,
 }) => {
+   const [selectMostSomething, setSelectMostSomething] = useState("watched")
+
    return (
       <Stack spacing={5} marginBottom={10}>
          <GroupSparkAnalyticsCardContainer
             title={`Reach for the past ${timeFilter}`}
          >
-            <Box
-               sx={{
-                  display: "flex",
-                  gap: 1.5,
-               }}
-            >
+            <Box sx={styles.chartSwitchButtonsContainers}>
                <ChartSwitchButton
                   label={"Total views"}
                   value={"0"}
@@ -48,12 +142,7 @@ const SparksOverviewTab: FC<SparksOverviewTabProps> = ({
          <GroupSparkAnalyticsCardContainer
             title={`Engagement for the past ${timeFilter}`}
          >
-            <Box
-               sx={{
-                  display: "flex",
-                  gap: 1.5,
-               }}
-            >
+            <Box sx={styles.chartSwitchButtonsContainers}>
                <ChartSwitchButton label={"Likes"} value={"450"} active={true} />
                <ChartSwitchButton
                   label={"Shares"}
@@ -79,36 +168,22 @@ const SparksOverviewTab: FC<SparksOverviewTabProps> = ({
                />
             </Box>
          </GroupSparkAnalyticsCardContainer>
-         <GroupSparkAnalyticsCardContainer title={<MostSomethingTitle />}>
-            <Box
-               sx={{
-                  display: "flex",
-                  gap: 1.5,
-               }}
-            >
+         <GroupSparkAnalyticsCardContainer
+            title={
+               <MostSomethingTitle
+                  selectMostSomething={selectMostSomething}
+                  setSelectMostSomething={setSelectMostSomething}
+                  options={mostSomethingSelectOptions}
+               />
+            }
+         >
+            <Box sx={styles.sparksContainer}>
                <SparksStaticCard spark={mostSomethingData} />
                <SparksStaticCard spark={mostSomethingData} />
                <SparksStaticCard spark={mostSomethingData} />
             </Box>
          </GroupSparkAnalyticsCardContainer>
       </Stack>
-   )
-}
-
-const MostSomethingTitle = () => {
-   return (
-      <Box
-         sx={{
-            fontSize: "20px",
-            fontWeight: 600,
-            lineHeight: "30px",
-            letterSpacing: "0em",
-            textAlign: "left",
-            marginBottom: "21px",
-         }}
-      >
-         Your <span style={{ color: "#6749EA" }}>most watched Sparks</span>
-      </Box>
    )
 }
 
