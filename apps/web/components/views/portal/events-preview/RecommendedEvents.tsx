83bd2ad8fed191b68357723ac4c9f90996dfd5a1
wgoncalvescf <walter.goncalves@careerfairy.io>
2023-12-19 10:15:50 +0000

    Base event using sparks, refactors to do yet

--- apps/web/components/views/portal/events-preview/RecommendedEvents.tsx
+++ apps/web/components/views/portal/events-preview/RecommendedEvents.tsx
@@ -1,12 +1,20 @@
-import React, { useMemo } from "react"
+import React, { useMemo, useRef } from "react"
 import EventsPreview, { EventsTypes } from "./EventsPreview"
 import { useAuth } from "../../../../HOCs/AuthProvider"
 import useRecommendedEvents from "../../../custom-hook/useRecommendedEvents"
 import { FirebaseInArrayLimit } from "@careerfairy/shared-lib/dist/BaseFirebaseRepository"
+import EventsPreviewCarousel, { ChildRefType } from "./EventsPreviewCarousel"
+import { EmblaOptionsType } from "embla-carousel-react"
 
 const RecommendedEvents = ({ limit = 10, hideTitle }: Props) => {
    const { authenticatedUser } = useAuth()
+   const emblaOptions: EmblaOptionsType = {
+      slidesToScroll: "auto",
+      containScroll: "trimSnaps",
+      axis: "y",
+   }
 
+   const childRef = useRef<ChildRefType | null>(null)
    const options = useMemo(
       () => ({
          limit,
@@ -16,19 +24,63 @@ const RecommendedEvents = ({ limit = 10, hideTitle }: Props) => {
 
    const { loading, events } = useRecommendedEvents(options)
 
+   const emblaOptionsMemoized = useMemo<EmblaOptionsType>(
+      () => ({
+         axis: "x",
+         loop: true,
+         align: "center",
+         // duration: 15,
+         dragThreshold: 0.5,
+         // asdsa: "dsfs",
+         // dragFree: false,
+         dragFree: false,
+         inViewThreshold: 0,
+         /**
+          * Custom function to watch for changes to the slides.
+          * Reloads the Embla Carousel whenever the slides (sparks) are updated,
+          * to prevent flickering.
+          */
+         watchSlides: (emblaApi) => {
+            const reloadEmbla = (): void => {
+               const oldEngine = emblaApi.internalEngine()
+               emblaApi.reInit()
+
+               console.log("Re-Init Embla in Watch")
+            }
+
+            reloadEmbla()
+         },
+      }),
+      [events]
+   )
+
    if (!authenticatedUser?.email || !events?.length) {
       return null
    }
-
+   // Switch to emblaApi
    return (
-      <EventsPreview
-         limit={limit}
-         title={!hideTitle && "RECOMMENDED FOR YOU"}
-         events={events}
-         type={EventsTypes.recommended}
-         loading={loading}
-         isRecommended
-      />
+      <div>
+         <EventsPreview
+            limit={limit}
+            title={!hideTitle && "RECOMMENDED FOR YOU"}
+            events={events}
+            type={EventsTypes.recommended}
+            loading={loading}
+            isRecommended
+         />
+         <EventsPreviewCarousel
+            options={emblaOptionsMemoized}
+            limit={limit}
+            title={!hideTitle && "RECOMMENDED FOR YOU - EMBLA"}
+            events={events}
+            type={EventsTypes.recommended}
+            loading={loading}
+            isRecommended
+            isAdmin={true}
+            hidePreview={false}
+            ref={childRef}
+         />
+      </div>
    )
 }
 
