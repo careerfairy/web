d037d1aebbc4823d0f8764c73d5290a7c1a6b433
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-12-21 17:36:18 +0000

    Adds data transformer

--- apps/web/components/custom-hook/spark/analytics/useSparksAnalytics.tsx
+++ apps/web/components/custom-hook/spark/analytics/useSparksAnalytics.tsx
@@ -1,7 +1,9 @@
+import { useMemo } from "react"
 import useSWR from "swr"
 import useFunctionsSWR, {
    reducedRemoteCallsOptions,
-} from "./utils/useFunctionsSWRFetcher"
+} from "../../utils/useFunctionsSWRFetcher"
+import { convertToClientModel } from "components/custom-hook/spark/analytics/dataTransformers"
 
 const useSparksAnalytics = (groupId: string) => {
    const fetcher = useFunctionsSWR()
@@ -12,7 +14,9 @@ const useSparksAnalytics = (groupId: string) => {
       reducedRemoteCallsOptions
    )
 
-   return data
+   return useMemo(() => {
+      return convertToClientModel(data)
+   }, [data])
 }
 
 export default useSparksAnalytics
