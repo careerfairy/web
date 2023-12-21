acad9500aa7c383d874ebf84c4841c617c06c1c3
goncaloarnauth <goncaloarnauth@outlook.com>
2023-12-21 15:47:31 +0000

    improve mobile DeleteJobDialog experience

--- apps/web/store/reducers/adminJobsReducer.ts
+++ apps/web/store/reducers/adminJobsReducer.ts
@@ -3,12 +3,14 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 interface IJobsState {
    jobsDialogOpen: boolean
    deleteJobDialogOpen: boolean
+   deleteJobWithLinkedLivestreamsDialogOpen: boolean
    selectedJobId: string | null
 }
 
 const initialState: IJobsState = {
    jobsDialogOpen: false,
    deleteJobDialogOpen: false,
+   deleteJobWithLinkedLivestreamsDialogOpen: false,
    selectedJobId: null,
 }
 
@@ -24,6 +26,7 @@ export const adminJobsSlice = createSlice({
          state.jobsDialogOpen = false
          state.deleteJobDialogOpen = false
          state.selectedJobId = null
+         state.deleteJobWithLinkedLivestreamsDialogOpen = false
       },
       openDeleteJobDialogOpen: (
          state,
@@ -32,12 +35,19 @@ export const adminJobsSlice = createSlice({
          state.deleteJobDialogOpen = true
          state.selectedJobId = action.payload
       },
+      openDeleteJobWithLinkedLivestreams: (state) => {
+         state.deleteJobWithLinkedLivestreamsDialogOpen = true
+      },
    },
 })
 
 // Export actions
-export const { openJobsDialog, closeJobsDialog, openDeleteJobDialogOpen } =
-   adminJobsSlice.actions
+export const {
+   openDeleteJobWithLinkedLivestreams,
+   openJobsDialog,
+   closeJobsDialog,
+   openDeleteJobDialogOpen,
+} = adminJobsSlice.actions
 
 // Export reducer
 export default adminJobsSlice.reducer
