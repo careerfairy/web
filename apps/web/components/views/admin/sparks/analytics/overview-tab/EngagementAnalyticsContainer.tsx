04d52a10aff75097cc0a571ef53fcf73d0da32b0
Carlos Rijo <carlos.rijo@careerfairy.io>
2023-12-21 17:46:01 +0000

    Isolates engagement, reach and most analytics to their own components

---
+++ apps/web/components/views/admin/sparks/analytics/overview-tab/EngagementAnalyticsContainer.tsx
@@ -0,0 +1,148 @@
+import { FC, useState } from "react"
+import { Box } from "@mui/material"
+import { AbstractButtonSelect } from "./util"
+import {
+   EngagementData,
+   TimePeriodParams,
+} from "@careerfairy/shared-lib/sparks/analytics"
+import { numberToString } from "util/CommonUtil"
+import { useGroup } from "layouts/GroupDashboardLayout"
+import useIsMobile from "components/custom-hook/useIsMobile"
+import useSparksAnalytics from "components/custom-hook/spark/analytics/useSparksAnalytics"
+import { GroupSparkAnalyticsCardContainer } from "../components/GroupSparkAnalyticsCardContainer"
+import { GroupSparkAnalyticsCardContainerTitle } from "../components/GroupSparkAnalyticsCardTitle"
+import BrandedSwipeableViews from "../components/BrandedSwipeableViews"
+import ChartSwitchButton from "../components/charts/ChartSwitchButton"
+import ChartSwitchButtonGroupContainer from "../components/ChartSwitchButtonGroupContainer"
+import { CFLineChart } from "../components/charts/CFLineChart"
+
+const engagementSelectOptions: AbstractButtonSelect<EngagementData> = {
+   likes: "Likes",
+   shares: "Shares",
+   registrations: "Registrations",
+   pageClicks: "Clicks",
+} as const
+
+type EngagementAnalyticsContainerProps = {
+   timeFilter: TimePeriodParams
+   timeFrameLabel: string
+}
+
+const EngagementAnalyticsContainer: FC<EngagementAnalyticsContainerProps> = ({
+   timeFilter,
+   timeFrameLabel,
+}) => {
+   const isMobile = useIsMobile()
+   const { group } = useGroup()
+   const { engagement } = useSparksAnalytics(group.id)[timeFilter]
+
+   const [engagementSelectValue, setEngagementSelectValue] =
+      useState<keyof EngagementData>("likes")
+
+   const totalLikesCount = engagement.likes.totalCount
+   const totalSharesCount = engagement.shares.totalCount
+   const totalRegistrationsCount = engagement.registrations.totalCount
+   const totalPageClicksCount = engagement.pageClicks.totalCount
+
+   return (
+      <>
+         {isMobile ? (
+            <GroupSparkAnalyticsCardContainer>
+               <BrandedSwipeableViews>
+                  <>
+                     <GroupSparkAnalyticsCardContainerTitle>
+                        {`${numberToString(
+                           totalLikesCount
+                        )} likes for the ${timeFrameLabel}`}
+                     </GroupSparkAnalyticsCardContainerTitle>
+                     <CFLineChart
+                        tooltipLabel={engagementSelectOptions.likes}
+                        xAxis={engagement.likes.xAxis}
+                        series={engagement.likes.series}
+                     />
+                  </>
+                  <>
+                     <GroupSparkAnalyticsCardContainerTitle>
+                        {`${numberToString(
+                           totalSharesCount
+                        )} shares for the ${timeFrameLabel}`}
+                     </GroupSparkAnalyticsCardContainerTitle>
+                     <CFLineChart
+                        tooltipLabel={engagementSelectOptions.shares}
+                        xAxis={engagement.shares.xAxis}
+                        series={engagement.shares.series}
+                     />
+                  </>
+                  <>
+                     <GroupSparkAnalyticsCardContainerTitle>
+                        {`${numberToString(
+                           totalRegistrationsCount
+                        )} registrations for the ${timeFrameLabel}`}
+                     </GroupSparkAnalyticsCardContainerTitle>
+                     <CFLineChart
+                        tooltipLabel={engagementSelectOptions.registrations}
+                        xAxis={engagement.registrations.xAxis}
+                        series={engagement.registrations.series}
+                     />
+                  </>
+                  <>
+                     <GroupSparkAnalyticsCardContainerTitle>
+                        {`${numberToString(
+                           totalPageClicksCount
+                        )} page clicks for the ${timeFrameLabel}`}
+                     </GroupSparkAnalyticsCardContainerTitle>
+                     <CFLineChart
+                        tooltipLabel={engagementSelectOptions.pageClicks}
+                        xAxis={engagement.pageClicks.xAxis}
+                        series={engagement.pageClicks.series}
+                     />
+                  </>
+               </BrandedSwipeableViews>
+            </GroupSparkAnalyticsCardContainer>
+         ) : (
+            <GroupSparkAnalyticsCardContainer>
+               <GroupSparkAnalyticsCardContainerTitle>
+                  {`Engagement for the ${timeFrameLabel}`}
+               </GroupSparkAnalyticsCardContainerTitle>
+               <ChartSwitchButtonGroupContainer>
+                  <ChartSwitchButton
+                     label={"Likes"}
+                     value={totalLikesCount}
+                     active={engagementSelectValue === "likes"}
+                     onClick={() => setEngagementSelectValue("likes")}
+                  />
+                  <ChartSwitchButton
+                     label={"Shares"}
+                     value={totalSharesCount}
+                     active={engagementSelectValue === "shares"}
+                     onClick={() => setEngagementSelectValue("shares")}
+                  />
+                  <ChartSwitchButton
+                     label={"Registrations"}
+                     value={totalRegistrationsCount}
+                     active={engagementSelectValue === "registrations"}
+                     onClick={() => setEngagementSelectValue("registrations")}
+                  />
+                  <ChartSwitchButton
+                     label={"Page clicks"}
+                     value={totalPageClicksCount}
+                     active={engagementSelectValue === "pageClicks"}
+                     onClick={() => setEngagementSelectValue("pageClicks")}
+                  />
+               </ChartSwitchButtonGroupContainer>
+               <Box>
+                  <CFLineChart
+                     tooltipLabel={
+                        engagementSelectOptions[engagementSelectValue]
+                     }
+                     xAxis={engagement[engagementSelectValue].xAxis}
+                     series={engagement[engagementSelectValue].series}
+                  />
+               </Box>
+            </GroupSparkAnalyticsCardContainer>
+         )}
+      </>
+   )
+}
+
+export default EngagementAnalyticsContainer
---
+++ apps/web/components/views/admin/sparks/analytics/overview-tab/ReachAnalyticsContainer.tsx
@@ -0,0 +1,103 @@
+import { FC, useState } from "react"
+import { AbstractButtonSelect } from "./util"
+import {
+   ReachData,
+   TimePeriodParams,
+} from "@careerfairy/shared-lib/sparks/analytics"
+import { numberToString } from "util/CommonUtil"
+import { useGroup } from "layouts/GroupDashboardLayout"
+import useSparksAnalytics from "components/custom-hook/spark/analytics/useSparksAnalytics"
+import useIsMobile from "components/custom-hook/useIsMobile"
+import { GroupSparkAnalyticsCardContainer } from "../components/GroupSparkAnalyticsCardContainer"
+import { GroupSparkAnalyticsCardContainerTitle } from "../components/GroupSparkAnalyticsCardTitle"
+import ChartSwitchButton from "../components/charts/ChartSwitchButton"
+import ChartSwitchButtonGroupContainer from "../components/ChartSwitchButtonGroupContainer"
+import { CFLineChart } from "../components/charts/CFLineChart"
+import BrandedSwipeableViews from "../components/BrandedSwipeableViews"
+
+const reachSelectOptions: AbstractButtonSelect<ReachData> = {
+   totalViews: "Views",
+   uniqueViewers: "Viewers",
+} as const
+
+type ReachAnalyticsContainerProps = {
+   timeFilter: TimePeriodParams
+   timeFrameLabel: string
+}
+
+const ReachAnalyticsContainer: FC<ReachAnalyticsContainerProps> = ({
+   timeFilter,
+   timeFrameLabel,
+}) => {
+   const isMobile = useIsMobile()
+   const { group } = useGroup()
+   const { reach } = useSparksAnalytics(group.id)[timeFilter]
+
+   const [reachSelectValue, setReachSelectValue] =
+      useState<keyof ReachData>("totalViews")
+
+   const totalViewsCount = reach.totalViews.totalCount
+   const totalUniqueViewersCount = reach.uniqueViewers.totalCount
+
+   return (
+      <>
+         {isMobile ? (
+            <GroupSparkAnalyticsCardContainer>
+               <BrandedSwipeableViews>
+                  <>
+                     <GroupSparkAnalyticsCardContainerTitle>
+                        {`${numberToString(
+                           totalViewsCount
+                        )} views for the ${timeFrameLabel}`}
+                     </GroupSparkAnalyticsCardContainerTitle>
+                     <CFLineChart
+                        tooltipLabel={reachSelectOptions.totalViews}
+                        xAxis={reach.totalViews.xAxis}
+                        series={reach.totalViews.series}
+                     />
+                  </>
+                  <>
+                     <GroupSparkAnalyticsCardContainerTitle>
+                        {`${numberToString(
+                           totalUniqueViewersCount
+                        )} unique viewers for the ${timeFrameLabel}`}
+                     </GroupSparkAnalyticsCardContainerTitle>
+                     <CFLineChart
+                        tooltipLabel={reachSelectOptions.uniqueViewers}
+                        xAxis={reach.uniqueViewers.xAxis}
+                        series={reach.uniqueViewers.series}
+                     />
+                  </>
+               </BrandedSwipeableViews>
+            </GroupSparkAnalyticsCardContainer>
+         ) : (
+            <GroupSparkAnalyticsCardContainer>
+               <GroupSparkAnalyticsCardContainerTitle>
+                  {`Reach for the ${timeFrameLabel}`}
+               </GroupSparkAnalyticsCardContainerTitle>
+               <ChartSwitchButtonGroupContainer>
+                  <ChartSwitchButton
+                     label={"Total views"}
+                     value={totalViewsCount}
+                     active={reachSelectValue === "totalViews"}
+                     onClick={() => setReachSelectValue("totalViews")}
+                  />
+                  <ChartSwitchButton
+                     label={"Unique viewers"}
+                     value={totalUniqueViewersCount}
+                     active={reachSelectValue === "uniqueViewers"}
+                     onClick={() => setReachSelectValue("uniqueViewers")}
+                  />
+               </ChartSwitchButtonGroupContainer>
+               <CFLineChart
+                  tooltipLabel={reachSelectOptions[reachSelectValue]}
+                  xAxis={reach[reachSelectValue].xAxis}
+                  series={reach[reachSelectValue].series}
+               />
+            </GroupSparkAnalyticsCardContainer>
+         )}
+      </>
+   )
+}
+
+export default ReachAnalyticsContainer
