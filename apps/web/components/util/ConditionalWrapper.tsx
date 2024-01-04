6134135b54fee47b4318eeac4c065e69fc693a74
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-01-04 22:21:47 +0000

    Removed unused declarations

--- apps/web/components/util/ConditionalWrapper.tsx
+++ apps/web/components/util/ConditionalWrapper.tsx
@@ -1,8 +1,8 @@
 /**
  * Conditionally renders a set of components based on input boolean condition @param condition.
  * @param input Object containing rendering options, where components specified in field condition are rendered following
- * a ternary evaluation, with @param input.trutyChildren being rendered when @param input.condition is of truthful value, otherwise
- * the value to be rendered is @param input.falsyChildren .
+ * a ternary evaluation, with @param input.children being rendered when @param input.condition is of truthful value, otherwise
+ * the value to be rendered is @param input.fallback .
  * @returns Any type, dictated by truthy or falsy children.
  */
 const ConditionalWrapper = (input: ConditionalWrapperType) =>
