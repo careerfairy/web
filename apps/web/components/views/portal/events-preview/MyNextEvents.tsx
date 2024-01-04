16c5571da3253178582b8dc53727bdb5e798b83c
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-01-04 12:32:22 +0000

    Base MyNextEvents UI for empty registrations

--- apps/web/components/views/portal/events-preview/MyNextEvents.tsx
+++ apps/web/components/views/portal/events-preview/MyNextEvents.tsx
@@ -5,12 +5,97 @@ import { livestreamRepo } from "../../../../data/RepositoryInstances"
 import { useFirestoreCollection } from "components/custom-hook/utils/useFirestoreCollection"
 import EventsPreviewCarousel, { EventsTypes } from "./EventsPreviewCarousel"
 import { EmblaOptionsType } from "embla-carousel-react"
+import { Box, Button, Stack, Typography } from "@mui/material"
+import { sxStyles } from "types/commonTypes"
+import Heading from "../common/Heading"
 
 const config = {
    suspense: false,
    initialData: [],
 }
 
+const desktopCircleWidth = 45
+const mobileCircleWidth = 85
+
+const styles = sxStyles({
+   eventsBackground: {
+      background: "black",
+   },
+   cardWrapperContainerCircle: {
+      border: "20px solid #229584",
+      background: "#2ABAA5",
+      width: {
+         xs: "100vw",
+         md: "60vw",
+         lg: "45vw",
+      },
+      height: {
+         xs: "80vw",
+         md: "60vw",
+         lg: "45vw",
+      },
+      borderRadius: "50%",
+      overflow: "hidden",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      boxShadow:
+         "0px 11px 15px -7px rgba(34, 149, 132,0.2),0px 24px 38px 3px rgba(34, 149, 132, 0.14),0px 9px 46px 100px rgba(34, 149, 132, 0.12)",
+   },
+   cardWrapper: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: "16px",
+      border: "1px solid #D6D6E0",
+      boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
+      overflow: "hidden",
+      background: "#3D3D47",
+      marginTop: 2,
+      paddingTop: 5,
+      paddingBottom: 4,
+      backgroundColor: "black",
+      // display: "flex",
+      marginLeft: 2,
+      position: "relative",
+      width: "100%",
+      maxHeight: "292px",
+
+      cardInnerWrapper: {
+         background: "#2ABAA5",
+         borderRadius: "100%",
+         boxShadow: "0.375em 0.375em 0 0 rgba(15, 28, 63, 0.125)",
+         // height: "371px",
+         //   width: "5em"
+      },
+   },
+   noRegistrations: {
+      overflow: "hidden",
+      title: {
+         color: "white",
+         opacity: 1,
+         fontWeight: "bold",
+      },
+      description: {
+         color: "white",
+         textAlign: "center",
+         fontFamily: "Poppins",
+         fontSize: "16px",
+         fontStyle: "normal",
+         fontWeight: "400",
+         lineHeight: "27px" /* 168.75% */,
+         paddingX: "15px",
+      },
+      descriptionWrapper: {
+         maxWidth: "600px",
+      },
+      redirectButton: {
+         color: "#229584",
+         backgroundColor: "white",
+         textTransform: "none",
+      },
+   },
+})
 const MyNextEvents = ({ limit }: Props) => {
    const { authenticatedUser } = useAuth()
 
@@ -26,18 +111,6 @@ const MyNextEvents = ({ limit }: Props) => {
       config
    )
 
-   const eventsCarouselEmblaOptions = useMemo<EmblaOptionsType>(
-      () => ({
-         axis: "x",
-         loop: false,
-         align: "center",
-         dragThreshold: 0.5,
-         dragFree: true,
-         inViewThreshold: 0,
-      }),
-      []
-   )
-
    const isLoading = status === "loading"
 
    if (!authenticatedUser.email) {
@@ -51,8 +124,51 @@ const MyNextEvents = ({ limit }: Props) => {
          events={events}
          isEmpty={Boolean(!isLoading && !events.length)}
          title={"My registrations"}
-         loading={isLoading || (!isLoading && !events.length)}
-      />
+         loading={isLoading}
+         styling={{
+            backgroundSx: styles.eventsBackground,
+         }}
+      >
+         <Box sx={styles.cardWrapper}>
+            {/* <Box sx={styles.cardWrapperContainerCircleOne}> */}
+            <Box sx={styles.cardWrapperContainerCircle}>
+               <Stack
+                  spacing={1.25}
+                  alignItems="center"
+                  flexDirection={"column"}
+               >
+                  <Box>
+                     {
+                        <Heading sx={styles.noRegistrations.title}>
+                           No registrations Yet!
+                        </Heading>
+                     }
+                  </Box>
+                  <Box sx={styles.noRegistrations.descriptionWrapper}>
+                     <Typography
+                        sx={styles.noRegistrations.description}
+                        variant="h6"
+                        fontWeight={"400"}
+                        color="textSecondary"
+                     >
+                        You don’t have any registrations to upcoming live
+                        streams! Browse, register, discover new opportunities
+                        and kick-start your career.
+                     </Typography>
+                  </Box>
+                  <Box>
+                     <Button
+                        sx={styles.noRegistrations.redirectButton}
+                        variant="contained"
+                     >
+                        <Typography>Check all live streams</Typography>
+                     </Button>
+                  </Box>
+               </Stack>
+               {/* </Box> */}
+            </Box>
+         </Box>
+      </EventsPreviewCarousel>
    )
 }
 
