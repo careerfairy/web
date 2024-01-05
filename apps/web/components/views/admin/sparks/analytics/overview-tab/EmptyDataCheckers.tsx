4c91e174eaa9399f1bcbabc9d6b32fa57c41c1b7
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-01-05 11:49:57 +0000

    Addresses code review concerns

--- apps/web/components/views/admin/sparks/analytics/overview-tab/EmptyDataCheckers.tsx
+++ apps/web/components/views/admin/sparks/analytics/overview-tab/EmptyDataCheckers.tsx
@@ -1,46 +1,47 @@
 import { Stack, Typography } from "@mui/material"
 import { WarningIcon } from "./WarningIcon"
+import { sxStyles } from "types/commonTypes"
+
+const styles = sxStyles({
+   root: {
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      height: "309px",
+      marginTop: "-21px",
+   },
+   container: {
+      width: {
+         xs: "100%",
+         md: "34%",
+      },
+      alignItems: "center",
+      textAlign: "center",
+   },
+   icon: {
+      fontSize: 48,
+   },
+   header: {
+      color: "#7A7A8E",
+      fontWeight: 600,
+      fontSize: "1.425rem",
+      lineHeight: "30px",
+   },
+   body: {
+      color: "#9999B1",
+      fontWeight: 400,
+      fontSize: "1.15rem",
+      lineHeight: "27px",
+   },
+})
 
 const EmptyDataCheckerForMostSomething = () => {
    return (
-      <Stack
-         direction="row"
-         justifyContent="center"
-         alignItems="center"
-         width="100%"
-         height="309px"
-         marginTop="-21px"
-      >
-         <Stack
-            direction="column"
-            width={{ xs: "100%", md: "34%" }}
-            alignItems="center"
-            textAlign="center"
-            spacing={3}
-         >
-            <WarningIcon
-               sx={{
-                  fontSize: 48,
-               }}
-            />
-            <Typography
-               sx={{
-                  color: "#7A7A8E",
-                  fontWeight: 600,
-                  fontSize: "1.425rem",
-                  lineHeight: "30px",
-               }}
-            >
-               Not enough content yet
-            </Typography>
-            <Typography
-               sx={{
-                  color: "#9999B1",
-                  fontWeight: 400,
-                  fontSize: "1.15rem",
-                  lineHeight: "27px",
-               }}
-            >
+      <Stack direction="row" sx={styles.root}>
+         <Stack direction="column" spacing={3} sx={styles.container}>
+            <WarningIcon sx={styles.icon} />
+            <Typography sx={styles.header}>Not enough content yet</Typography>
+            <Typography sx={styles.body}>
                More content is needed before displaying the top performing
                Sparks from your company.
             </Typography>
