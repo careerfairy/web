e9cc0465726285d9b8cf20f460fc22c9c362ed1d
habibkadiri <kadirit@hotmail.com>
2024-01-08 10:36:19 +0100

    Add condition to hide TrialModeNotice if not in trial plan

--- apps/web/components/views/admin/sparks/general-sparks-view/header/TrialModeNotice/TrialModeNotice.tsx
+++ apps/web/components/views/admin/sparks/general-sparks-view/header/TrialModeNotice/TrialModeNotice.tsx
@@ -35,6 +35,9 @@ function TrialModeNotice() {
    const isMobile = useIsMobile()
    const [statusDrawerOpen, handleOpenStatusDrawer, handleCloseStatusDrawer] =
       useDialogStateHandler()
+
+   if (!groupPresenter.isTrialPlan()) return null
+
    return (
       <>
          <Box
