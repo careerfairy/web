cc78a3e7aeb6be9ca555bce92b0b071da8951807
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-01-16 16:49:32 +0000

    Created Filter in page with 2 filters, with the company country being applied. Additional ones to follow

--- apps/web/components/views/companies/Companies.tsx
+++ apps/web/components/views/companies/Companies.tsx
@@ -1,5 +1,5 @@
 import { FC, useMemo } from "react"
-import { Group } from "@careerfairy/shared-lib/groups"
+import { FilterCompanyOptions, Group } from "@careerfairy/shared-lib/groups"
 import { Grid } from "@mui/material"
 import useInfiniteCompanies, {
    UseInfiniteCompanies,
@@ -8,6 +8,9 @@ import { sxStyles } from "../../../types/commonTypes"
 import CompanyCard, { CompanyCardSkeleton } from "./CompanyCard"
 import CustomInfiniteScroll from "../common/CustomInfiniteScroll"
 import { COMPANIES_PAGE_SIZE } from "components/util/constants"
+import { useRouter } from "next/router"
+import { ParsedUrlQuery } from "querystring"
+import { useMountedState } from "react-use"
 
 const styles = sxStyles({
    flexItem: {
@@ -23,13 +26,22 @@ type Props = {
 }
 
 const Companies: FC<Props> = ({ initialData }) => {
+   const isMounted = useMountedState()
+   const { query } = useRouter()
+
+   const {
+      companyCountries,
+      // TODO:WG add additinal query variables
+   } = useMemo(() => getQueryVariables(query), [query])
+
+   const hasFilters = companyCountries?.length > 0 || false
    const options = useMemo<UseInfiniteCompanies>(
       () => ({
-         filters: {},
-         initialData,
+         filters: { companyCountries: companyCountries },
+         initialData: hasFilters ? [] : initialData,
          limit: COMPANIES_PAGE_SIZE,
       }),
-      [initialData]
+      [initialData, companyCountries, hasFilters]
    )
 
    const {
@@ -38,11 +50,11 @@ const Companies: FC<Props> = ({ initialData }) => {
       hasMore,
       loading,
    } = useInfiniteCompanies(options)
-
+   const renderCompanies = isMounted() ? companies : initialData
    return (
       <CustomInfiniteScroll hasMore={hasMore} next={getMore} loading={loading}>
          <Grid sx={styles.root} container spacing={2}>
-            {companies?.map((company) => (
+            {renderCompanies.map((company) => (
                <Grid
                   sx={styles.flexItem}
                   key={company.id}
@@ -63,6 +75,34 @@ const Companies: FC<Props> = ({ initialData }) => {
          </Grid>
       </CustomInfiniteScroll>
    )
+}
+
+const queryParamToArr = (
+   queryParam: string | string[] | undefined
+): string[] => {
+   if (!queryParam) return []
+   if (Array.isArray(queryParam)) return queryParam.sort()
+   return queryParam.split(",").sort() // to make sure the order is always the same for caching the key
+}
+
+const queryParamToBool = (
+   queryParam: string | string[] | undefined
+): boolean => {
+   if (!queryParam || Array.isArray(queryParam)) return false
+   return queryParam?.toLowerCase() === "true" || false
+}
+/**
+ *
+ * @param query Query string object, all aplied filters are passed by query string parameters.
+ * @returns @type FilterCompanyOptions mapped from the query string object
+ */
+const getQueryVariables = (query: ParsedUrlQuery): FilterCompanyOptions => {
+   return {
+      companyCountries: queryParamToArr(query.companyCountries),
+      companyIndustries: queryParamToArr(query.companyIndustries),
+      publicSparks: queryParamToBool(query.publicSparks as string),
+      companySize: queryParamToArr(query.companySize),
+   }
 }
 
 export default Companies
--- apps/web/components/views/companies/CompaniesPageOverview.tsx
+++ apps/web/components/views/companies/CompaniesPageOverview.tsx
@@ -3,12 +3,15 @@ import { Container, Grid } from "@mui/material"
 import Companies from "./Companies"
 import { Group } from "@careerfairy/shared-lib/groups"
 import CompanySearch from "./CompanySearch"
+import { useRouter } from "next/router"
 
 type Props = {
    serverSideCompanies: Group[]
 }
 
 const CompaniesPageOverview: FC<Props> = ({ serverSideCompanies }) => {
+   const { query } = useRouter()
+   // TODO:WG Destructure query parameters onto custom type, removing the need for the JSON.stringify
    return (
       <Container maxWidth="xl">
          <Grid container spacing={2}>
@@ -16,7 +19,10 @@ const CompaniesPageOverview: FC<Props> = ({ serverSideCompanies }) => {
                <CompanySearch />
             </Grid>
             <Grid item xs={12}>
-               <Companies initialData={serverSideCompanies} />
+               <Companies
+                  initialData={serverSideCompanies}
+                  key={JSON.stringify(query)}
+               />
             </Grid>
          </Grid>
       </Container>
