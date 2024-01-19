6022fb6577570b7ddcc429bcb570f5b6422c055d
habibkadiri <kadirit@hotmail.com>
2024-01-19 16:12:47 +0100

    Improve access denied messaging for new streaming pages

--- apps/web/pages/streaming/viewer/[streamId].tsx
+++ apps/web/pages/streaming/viewer/[streamId].tsx
@@ -1,10 +1,17 @@
+import { Typography } from "@mui/material"
 import { useAuth } from "HOCs/AuthProvider"
 import { StreamingPage } from "components/views/streaming-page"
 
 const StreamingViewer = () => {
    const { userData } = useAuth()
 
-   return userData?.isAdmin ? <StreamingPage isHost={false} /> : null
+   return userData?.isAdmin ? (
+      <StreamingPage isHost={false} />
+   ) : (
+      <Typography variant="brandedH4" color="error">
+         Please log in as cf admin to access this page
+      </Typography>
+   )
 }
 
 export default StreamingViewer
