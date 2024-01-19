83d0ba8b8ee8a847ceaf665ff304a59c183d4a88
habibkadiri <kadirit@hotmail.com>
2024-01-19 14:26:47 +0100

    Restrict streaming page access to admins only

--- apps/web/pages/streaming/host/[streamId].tsx
+++ apps/web/pages/streaming/host/[streamId].tsx
@@ -1,5 +1,10 @@
+import { useAuth } from "HOCs/AuthProvider"
 import { StreamingPage } from "components/views/streaming-page"
 
-const StreamingHost = () => <StreamingPage isHost />
+const StreamingHost = () => {
+   const { userData } = useAuth()
+
+   return userData.isAdmin ? <StreamingPage isHost /> : null
+}
 
 export default StreamingHost
--- apps/web/pages/streaming/viewer/[streamId].tsx
+++ apps/web/pages/streaming/viewer/[streamId].tsx
@@ -1,5 +1,10 @@
+import { useAuth } from "HOCs/AuthProvider"
 import { StreamingPage } from "components/views/streaming-page"
 
-const StreamingViewer = () => <StreamingPage isHost={false} />
+const StreamingViewer = () => {
+   const { userData } = useAuth()
+
+   return userData.isAdmin ? <StreamingPage /> : null
+}
 
 export default StreamingViewer
