f3f2696e65f7d9b592231c431ec1d39dcb984fb6
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-01-22 17:06:21 +0000

    Adds styling and navigation behavior to bottom bar navigation

--- apps/web/pages/group/[groupId]/admin/events/[livestreamId]/navigation/LivestreamAdminDetailBottomBarNavigation.tsx
+++ apps/web/pages/group/[groupId]/admin/events/[livestreamId]/navigation/LivestreamAdminDetailBottomBarNavigation.tsx
@@ -1,10 +1,11 @@
-import { FC } from "react"
-import { Box, Button } from "@mui/material"
+import { ElementType, FC } from "react"
 import { sxStyles } from "types/commonTypes"
+import { Box, Button, IconButton } from "@mui/material"
+import { ChevronLeft, ChevronRight, IconProps } from "react-feather"
 import useIsMobile from "components/custom-hook/useIsMobile"
 import { TAB_VALUES } from "./LivestreamAdminDetailTopBarNavigation"
-import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded"
-import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded"
+
+const isDisabledClassName = "isDisabled"
 
 const styles = sxStyles({
    root: {
@@ -13,45 +14,137 @@ const styles = sxStyles({
          xs: "left",
          md: "space-between",
       },
-      paddingBottom: 4,
+      alignItems: "center",
+      backgroundColor: "#FFFFFFF2",
+      boxShadow: "0px 0px 10px 0px #BBBBC20A",
+      padding: {
+         xs: "20px 16px",
+         md: "20px 32px",
+      },
+      gap: {
+         xs: 2,
+      },
+   },
+   navigationButton: {
+      backgroundColor: {
+         xs: "#F6F6FA",
+         md: "inherit",
+      },
+      [`&.${isDisabledClassName}`]: {
+         opacity: {
+            xs: "20%",
+            md: "initial",
+         },
+      },
+      padding: {
+         xs: 0,
+      },
+      svg: {
+         stroke: (theme) => ({
+            xs: "black",
+            md: theme.palette.primary.main,
+         }),
+      },
    },
 })
 
+type ButtonOrIconButtonProps = React.ComponentProps<typeof Button> &
+   React.ComponentProps<typeof IconButton>
+
+const ButtonOrIconButton: FC<ButtonOrIconButtonProps> = ({
+   children,
+   ...props
+}) => {
+   const isMobile = useIsMobile()
+
+   return isMobile ? (
+      <IconButton {...props}>{children}</IconButton>
+   ) : (
+      <Button {...props}>{children}</Button>
+   )
+}
+
+type ButtonContentProps = {
+   label: string
+   iconPositionOnDesktop: "left" | "right"
+   Icon: ElementType<IconProps>
+}
+
+const ButtonContent: FC<ButtonContentProps> = ({
+   label,
+   iconPositionOnDesktop,
+   Icon,
+}) => {
+   const isMobile = useIsMobile()
+
+   const desktopIconSize = 18
+   const mobileIconSize = 32
+
+   return isMobile ? (
+      <Icon size={mobileIconSize} />
+   ) : (
+      <>
+         {iconPositionOnDesktop === "left" ? (
+            <>
+               <Icon size={desktopIconSize} /> {label}
+            </>
+         ) : (
+            <>
+               {label} <Icon size={desktopIconSize} />
+            </>
+         )}
+      </>
+   )
+}
+
 const changeToNewTab = (
    currentTab: TAB_VALUES,
-   changeTab: LivestreamAdminDetailTopBarNavigationProps["changeTab"],
+   changeTab: LivestreamAdminDetailBottomBarNavigationProps["changeTab"],
    step: number
-) => {
+): void => {
    const tabValues = Object.values(TAB_VALUES)
    const currentTabIndex = tabValues.indexOf(currentTab)
    const newTab = tabValues[currentTabIndex + step]
    changeTab(newTab)
 }
 
-type LivestreamAdminDetailTopBarNavigationProps = {
+type LivestreamAdminDetailBottomBarNavigationProps = {
    currentTab: TAB_VALUES
    changeTab: (TAB_VALUE) => void
 }
 
 const LivestreamAdminDetailBottomBarNavigation: FC<
-   LivestreamAdminDetailTopBarNavigationProps
+   LivestreamAdminDetailBottomBarNavigationProps
 > = ({ currentTab, changeTab }) => {
-   const isMobile = useIsMobile()
+   const isBackButtonDisabled = currentTab === TAB_VALUES.GENERAL
+   const isNextButtonDisabled = currentTab === TAB_VALUES.JOBS
 
    return (
       <Box sx={styles.root}>
-         <Button
-            disabled={currentTab === TAB_VALUES.GENERAL}
+         <ButtonOrIconButton
+            sx={styles.navigationButton}
+            disabled={isBackButtonDisabled}
             onClick={() => changeToNewTab(currentTab, changeTab, -1)}
+            className={Boolean(isBackButtonDisabled) && isDisabledClassName}
          >
-            {isMobile ? <ArrowBackIosNewRoundedIcon /> : "Back"}
-         </Button>
-         <Button
+            <ButtonContent
+               label={"Back"}
+               iconPositionOnDesktop="left"
+               Icon={ChevronLeft}
+            />
+         </ButtonOrIconButton>
+         <ButtonOrIconButton
+            sx={styles.navigationButton}
             disabled={currentTab === TAB_VALUES.JOBS}
             onClick={() => changeToNewTab(currentTab, changeTab, 1)}
+            className={Boolean(isNextButtonDisabled) && isDisabledClassName}
          >
-            {isMobile ? <ArrowForwardIosRoundedIcon /> : "Next"}
-         </Button>
+            <ButtonContent
+               label={"Next"}
+               iconPositionOnDesktop="right"
+               Icon={ChevronRight}
+            />
+         </ButtonOrIconButton>
       </Box>
    )
 }
