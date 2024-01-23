ec35855076bfc36072815357ccc9f68c509bf301
matilderamosCF <matilde.ramos@careerfairy.io>
2024-01-23 12:45:49 +0000

    feature/cf-711-trial-activation-email

--- packages/functions/src/groupPlans.ts
+++ packages/functions/src/groupPlans.ts
@@ -9,6 +9,8 @@ import { GroupPlanTypes } from "@careerfairy/shared-lib/groups"
 import { StartPlanData } from "@careerfairy/shared-lib/groups/planConstants"
 import { dataValidation, userShouldBeCFAdmin } from "./middlewares/validations"
 import { groupRepo } from "./api/repositories"
+import { addUtmTagsToLink } from "@careerfairy/shared-lib/utils"
+import { client } from "./api/postmark"
 
 const setgroupPlanSchema: SchemaOf<StartPlanData> = object().shape({
    planType: mixed().oneOf(Object.values(GroupPlanTypes)).required(),
@@ -26,6 +28,41 @@ export const startPlan = functions.region(config.region).https.onCall(
             functions.logger.info(
                `Successfully set group plan for group ${data.groupId} to ${data.planType}`
             )
+
+            if (data.planType === GroupPlanTypes.Trial) {
+               const admins = await groupRepo.getGroupAdmins(data.groupId)
+
+               const emails = admins?.map(({ email, firstName, groupId }) => ({
+                  TemplateId: Number(
+                     process.env.POSTMARK_TEMPLATE_SPARKS_TRIAL_WELCOME
+                  ),
+                  From: "CareerFairy <noreply@careerfairy.io>",
+                  To: email,
+                  TemplateModel: {
+                     user_name: firstName,
+                     company_sparks_link: addUtmTagsToLink({
+                        link: `https://www.careerfairy.io/group/${groupId}/admin/sparks`,
+                        campaign: "sparks",
+                        content: "trial_welcome",
+                     }),
+                  },
+               }))
+
+               client.sendEmailBatchWithTemplates(emails).then(
+                  (responses) => {
+                     responses.forEach(() =>
+                        functions.logger.log(
+                           "Successfully sent batch sparks trial welcome email"
+                        )
+                     )
+                  },
+                  (error) => {
+                     functions.logger.error(
+                        "Error sending sparks trial welcome email:" + error
+                     )
+                  }
+               )
+            }
          } catch (error) {
             logAndThrow("Error in setting group plan", {
                data,
