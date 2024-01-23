32105d7ae48aa03c341b14634cc11075ef7edd2f
habibkadiri <kadirit@hotmail.com>
2024-01-23 13:58:40 +0100

    Optimize useLivestreamData hook docs

--- apps/web/components/custom-hook/streaming/useLivestreamData.ts
+++ apps/web/components/custom-hook/streaming/useLivestreamData.ts
@@ -3,12 +3,36 @@ import { useRouter } from "next/router"
 import { useFirestoreDocument } from "../utils/useFirestoreDocument"
 
 /**
- * This hook is crafted for the streaming application to retrieve livestream data from Firestore using the streamId from the router.
+ * This hook retrieves livestream data from Firestore using the streamId from the router.
  * @returns {LivestreamEvent} The data for the livestream.
  *
- * CAUTION: This hook should be utilized with consideration due to the frequent updates of livestream data,
+ * CAUTION: This hook should be used sparingly due to the frequent updates of livestream data when the event is live,
  * which can lead to substantial re-rendering and potential performance degradation.
- * For scenarios where only the livestreamId is required, the useStreamingContext is a more performance-optimized alternative.
+ *
+ * If you only need the livestreamId, consider using the `useStreamingContext` hook as a more performance-optimized alternative.
+ *
+ * If you need the livestream data, use this hook at the smallest possible component to minimize the DOM node re-rendering,
+ * or ensure that all the children of the component are memoized and not re-rendering.
+ *
+ * @example
+ * // Good usage
+ * const LivestreamTitle = () => {
+ *   const livestreamData = useLivestreamData();
+ *   return <h1>{livestreamData.title}</h1>;
+ * }
+ *
+ * // Bad usage
+ * const LivestreamPage = () => {
+ *   const livestreamData = useLivestreamData();
+ *   // The entire page re-renders whenever livestreamData updates
+ *   return (
+ *     <div>
+ *       <h1>{livestreamData.title}</h1>
+ *       <p>{livestreamData.description}</p>
+ *       // ... more components ...
+ *     </div>
+ *   );
+ * }
  */
 export const useLivestreamData = (): LivestreamEvent => {
    const {
