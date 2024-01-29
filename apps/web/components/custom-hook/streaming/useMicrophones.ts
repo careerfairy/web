85b9530c9bbb086a6a82857d23792c713b9528cf
habibkadiri <kadirit@hotmail.com>
2024-01-29 15:43:29 +0100

    Prevent server-side camera/mic detection

--- apps/web/components/custom-hook/streaming/useMicrophones.ts
+++ apps/web/components/custom-hook/streaming/useMicrophones.ts
@@ -1,9 +1,11 @@
 import { type IAgoraRTCError } from "agora-rtc-react"
+import { isServer } from "components/helperFunctions/HelperFunctions"
 import { getAgoraRTC } from "components/views/streaming-page/util"
 import useSWR from "swr"
 import { errorLogAndNotify } from "util/CommonUtil"
 
 const fetcher = async () => {
+   if (isServer()) return []
    const AgoraRTC = await getAgoraRTC()
    return AgoraRTC.getMicrophones()
 }
