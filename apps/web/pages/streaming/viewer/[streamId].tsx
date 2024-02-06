05560e84f38bf954ce07aa5c8abe9ddd7f7620c9
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-02-06 16:56:31 +0000

    Fixed redirect bug

--- apps/web/pages/streaming/viewer/[streamId].tsx
+++ apps/web/pages/streaming/viewer/[streamId].tsx
@@ -1,17 +1,27 @@
+import { GetServerSideProps } from "next"
 import dynamic from "next/dynamic"
-import { useMountedState } from "react-use"
-
+import { useEffect, useState } from "react"
 const StreamingPage = dynamic(
    // Use next/dynamic to import the StreamingPage component without ssr as the Agora SDK uses the window object
    // https://github.com/AgoraIO-Community/Agora-RTC-React/blob/master/example/nextjs/pages/index.tsx
    import("components/views/streaming-page").then((a) => a.StreamingPage),
-   { ssr: false }
+   { ssr: false, loading: () => <div>Loading...</div> }
 )
-
 const StreamingViewer = () => {
-   const mounted = useMountedState()
-
-   return mounted() ? <StreamingPage isHost={false} /> : null
+   const [loaded, setLoaded] = useState(false)
+   useEffect(() => {
+      setLoaded(true)
+   }, [])
+   return loaded ? <StreamingPage isHost={false} /> : null
 }
-
+/**
+ * Force the page to be initially rendered on the server
+ *
+ * This is required to allow the _document.tsx to access the correct request
+ * context data (query string params) and disable Usercentrics for certain scenarios
+ * like the recording session
+ */
+export const getServerSideProps: GetServerSideProps = async () => {
+   return { props: {} }
+}
 export default StreamingViewer
