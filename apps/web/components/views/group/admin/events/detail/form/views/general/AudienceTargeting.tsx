e584f97d50f5d3f4e35ec8fc08a02934f92ef528
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-02-08 16:40:52 +0000

    Adds the Audience Targeting section to the form

---
+++ apps/web/components/views/group/admin/events/detail/form/views/general/AudienceTargeting.tsx
@@ -0,0 +1,153 @@
+import { useCallback } from "react"
+import { useFormikContext } from "formik"
+import {
+   useUniversityCountries,
+   useFieldsOfStudy,
+   useLevelsOfStudy,
+} from "components/custom-hook/useCollection"
+import { LivestreamFormValues } from "../../types"
+import { CompanyCountryValues } from "constants/forms"
+import FormSectionHeader from "../../FormSectionHeader"
+import MultiChipSelect from "./components/MultiChipSelect"
+import { GROUP_CONSTANTS } from "@careerfairy/shared-lib/groups/constants"
+import BrandedTextField from "components/views/common/inputs/BrandedTextField"
+import {
+   GroupOption,
+   GroupTargetUniversity,
+} from "@careerfairy/shared-lib/groups"
+import {
+   University,
+   UniversityCountry,
+   universityCountryMap,
+} from "@careerfairy/shared-lib/universities"
+
+type UniversityOption = University & {
+   country: string
+}
+
+const groupByUniversities = (uni: GroupTargetUniversity) =>
+   universityCountryMap?.[uni.country] || uni.country
+
+const getUniversityOptions = (
+   universityCountries: UniversityCountry[],
+   universityCountryCodes: string[]
+): UniversityOption[] => {
+   let universityCountriesArray = [...universityCountries] // if no countries are selected, show all universities
+   if (universityCountryCodes.length > 0) {
+      // if countries are selected, filter universities by country
+      universityCountriesArray = universityCountries.filter(
+         (universityCountry) =>
+            universityCountryCodes.includes(universityCountry.id)
+      )
+   }
+   const universityOptions = universityCountriesArray.reduce<
+      UniversityOption[]
+   >(
+      (acc, universityCountry) =>
+         acc.concat(
+            universityCountry.universities.map((uni) => {
+               return {
+                  ...uni,
+                  country: universityCountry.countryId,
+               }
+            })
+         ),
+      []
+   )
+
+   return universityOptions
+}
+
+const AudienceTargeting = () => {
+   const {
+      values: { general },
+   } = useFormikContext<LivestreamFormValues>()
+   const { data: universitiesByCountry } = useUniversityCountries()
+   const { data: allFieldsOfStudy } = useFieldsOfStudy()
+   const { data: allLevelsOfStudy } = useLevelsOfStudy()
+
+   const filterUniversitiesBySelectedContries = useCallback(
+      (selectedCountries: GroupOption[]) => {
+         const selectedCountriesIds =
+            selectedCountries?.map((country) => country.id) || []
+         return getUniversityOptions(
+            universitiesByCountry,
+            selectedCountriesIds
+         )
+      },
+      [universitiesByCountry]
+   )
+
+   return (
+      <>
+         <FormSectionHeader
+            title="Target Students"
+            subtitle="Select target audience for this live stream"
+            divider
+         />
+         <MultiChipSelect
+            id="general.targetCountries"
+            options={CompanyCountryValues}
+            value={general.targetCountries}
+            limit={GROUP_CONSTANTS.MAX_TARGET_COUNTRY_COUNT}
+            multiple
+            disableCloseOnSelect
+            textFieldProps={{
+               label: "By Country",
+               placeholder: "Select Country",
+            }}
+         />
+         {general.targetCountries.length === 0 ? (
+            <BrandedTextField
+               disabled
+               label="By University"
+               value="Please first select your target countries"
+            />
+         ) : (
+            <MultiChipSelect
+               id="targetUniversities"
+               options={filterUniversitiesBySelectedContries(
+                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                  general.targetCountries as any
+               )}
+               value={general.targetUniversities}
+               groupBy={groupByUniversities}
+               limit={GROUP_CONSTANTS.MAX_TARGET_UNIVERSITY_COUNT}
+               multiple
+               disableCloseOnSelect
+               textFieldProps={{
+                  label: "By University",
+                  placeholder: "Select University",
+               }}
+            />
+         )}
+         <MultiChipSelect
+            id="general.targetFieldsOfStudy"
+            options={allFieldsOfStudy}
+            value={general.targetFieldsOfStudy}
+            limit={GROUP_CONSTANTS.MAX_TARGET_FIELD_OF_STUDY_COUNT}
+            multiple
+            disableCloseOnSelect
+            textFieldProps={{
+               label: "By Field Of Study",
+               placeholder: "Select Fields of Study",
+               required: true,
+            }}
+         />
+         <MultiChipSelect
+            id="general.targetLevelsOfStudy"
+            options={allLevelsOfStudy}
+            value={general.targetLevelsOfStudy}
+            multiple
+            disableCloseOnSelect
+            textFieldProps={{
+               label: "By Level Of Study",
+               placeholder: "Select Levels of Study",
+               required: true,
+            }}
+         />
+      </>
+   )
+}
+
+export default AudienceTargeting
