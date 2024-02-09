fffb2cac62b6f3f92d6ddf8ea209763449804d9e
Walter Gonçalves <152179257+wgoncalvescf@users.noreply.github.com>
2024-02-09 11:00:11 +0000

    Fixed bug for live events aligment (#983)
    ### Motivation
    
    On the /portal page, for live Events, there is a content alignment bug, only for these type of events. More details on [linear issue]([url](https://linear.app/careerfairy/issue/CF-770/live-events-alignment-bug)).
    
    **Update**
    For the company page livestreams, some are showing incorrect widths for the livestreams, but only for specific companies like Hitachi Energy.
    
    ### Changes
    
    -  Adjust style for live events only
    - Adjusted cards styling for company page
    
    ### Browser Testing
    
    -  [x] Chrome
    -  [x] Firefox
    -  [x] Safari
    -  [x] Edge
    
    ### Different Resolutions Testing
    
    -  [x] Large Screens
    -  [x] Medium Screens
    -  [x] Small Screens

--- apps/web/components/views/common/stream-cards/EventPreviewCard.tsx
+++ apps/web/components/views/common/stream-cards/EventPreviewCard.tsx
@@ -139,7 +139,6 @@ const styles = sxStyles({
       },
    },
    cardIsLive: {
-      marginY: 1.5,
       background:
          "linear-gradient(white, white) padding-box, linear-gradient(180deg, #e9911d, #dc2743 50%, #e9911d) border-box",
       backgroundRepeat: "no-repeat",
@@ -147,6 +146,7 @@ const styles = sxStyles({
       backgroundPosition: "0 0, 0 100%",
       border: "4px solid transparent",
       animation: `${gradientAnimation} 1s infinite alternate`,
+      maxHeight: 347,
    },
    companyAvatar: {
       padding: 1,
