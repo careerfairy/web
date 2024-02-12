0b707136b9f0c5247d6f69875fa0c8300cabc328
habibkadiri <kadirit@hotmail.com>
2024-02-12 18:23:09 +0100

    Refactor stream type to StreamUser

--- apps/web/components/views/streaming-page/components/StreamingGrid/useStreams.ts
+++ apps/web/components/views/streaming-page/components/StreamingGrid/useStreams.ts
@@ -1,26 +1,26 @@
 import { useRemoteUsers } from "agora-rtc-react"
 import { useMemo } from "react"
 import { useLocalTracks } from "../../context"
-import { RemoteUser, Stream } from "../../types"
+import { RemoteUser, StreamUser } from "../../types"
 
 /**
  * Combines local and remote streamers into a single array.
  * This hook gathers all streamers without sorting them, preparing the data for further processing or display.
  *
- * @returns {Array<Stream>} An array of combined local and remote users.
+ * @returns {Array<StreamUser>} An array of combined local and remote users.
  */
-export const useStreams = (): Stream[] => {
+export const useStreams = (): StreamUser[] => {
    const remoteStreamers = useRemoteUsers()
    const { localUser } = useLocalTracks()
 
    return useMemo(() => {
-      // Map remoteStreamers to include the 'type' property
-      const mappedRemoteStreamers = remoteStreamers.map<RemoteUser>((user) => ({
-         user,
-         type: "remote" as const,
-      }))
-
-      const combinedStreamers: Stream[] = mappedRemoteStreamers
+      // Start by mapping remoteStreamers to include the 'type' property
+      const combinedStreamers: StreamUser[] = remoteStreamers.map<RemoteUser>(
+         (user) => ({
+            user,
+            type: "remote" as const,
+         })
+      )
 
       if (localUser) {
          combinedStreamers.push(localUser)
