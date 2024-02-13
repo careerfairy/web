b2d3c635fb7a3aa485932a85a1a068859322d905
Maximilian Voss <maximilian@careerfairy.io>
2024-02-13 14:13:53 +0100

    new address for dp notice

--- apps/web/pages/data-protection/DE/index.tsx
+++ apps/web/pages/data-protection/DE/index.tsx
@@ -187,7 +187,7 @@ const TextContent = () => {
                </span>
                <b>
                   <span>
-                     CareerFairy AG, Technoparkstrasse 1, 8005 Z&uuml;rich,
+                     CareerFairy AG, Hardturmstrasse 253, 8005 Z&uuml;rich,
                      Switzerland{" "}
                   </span>
                </b>
