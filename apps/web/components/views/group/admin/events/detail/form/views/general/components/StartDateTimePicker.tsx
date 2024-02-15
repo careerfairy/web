5bd635a0ca92bd7c30ab56ec8c997dbc88aab808
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-02-15 07:43:16 +0000

    Minor improvement

--- apps/web/components/views/group/admin/events/detail/form/views/general/components/StartDateTimePicker.tsx
+++ apps/web/components/views/group/admin/events/detail/form/views/general/components/StartDateTimePicker.tsx
@@ -6,73 +6,69 @@ import { renderMultiSectionDigitalClockTimeView } from "@mui/x-date-pickers"
 import { FormBrandedTextField } from "components/views/common/inputs/BrandedTextField"
 
 const styles = sxStyles({
-   datePickerDesktop: (theme) => {
-      return {
-         ".MuiButton-text": {
-            color: `${theme.palette.secondary.main} !important`,
+   datePickerDesktop: (theme) => ({
+      ".MuiButton-text": {
+         color: `${theme.palette.secondary.main} !important`,
+      },
+      ".MuiButton-text:hover, .MuiPickersDay-root:hover, .MuiPickersDay-root:focus":
+         {
+            backgroundColor: "rgb(137 42 186 / 4%) !important",
          },
-         ".MuiButton-text:hover, .MuiPickersDay-root:hover, .MuiPickersDay-root:focus":
-            {
-               backgroundColor: "rgb(137 42 186 / 4%) !important",
-            },
+      ".Mui-selected": {
+         backgroundColor: `${theme.palette.secondary.main} !important`,
+      },
+      ".Mui-selected:hover": {
+         backgroundColor: `${theme.palette.secondary.dark} !important`,
+      },
+      ".Mui-selected:focus": {
+         backgroundColor: `${theme.palette.secondary.main} !important`,
+      },
+      ".MuiDigitalClock-item:hover": {
+         backgroundColor: "rgb(137 42 186 / 4%) !important",
+      },
+   }),
+   datePickerMobile: (theme) => ({
+      "&.MuiPickersLayout-root": {
+         display: "block",
+      },
+      ".MuiTab-root.Mui-selected": {
+         color: `${theme.palette.secondary.main}`,
+      },
+      ".MuiTabs-indicator": {
+         backgroundColor: `${theme.palette.secondary.main} !important`,
+      },
+      ".MuiPickersDay-root:hover, .MuiPickersDay-root:focus": {
+         backgroundColor: "rgb(137 42 186 / 4%) !important",
+      },
+      ".MuiPickersDay-root.Mui-selected, .MuiPickersYear-yearButton.Mui-selected":
+         {
+            backgroundColor: `${theme.palette.secondary.main} !important`,
+         },
+      ".MuiDialogActions-root": {
+         ".MuiButton-text": {
+            color: `${theme.palette.secondary.main}`,
+         },
+      },
+      ".MuiMultiSectionDigitalClock-root": {
+         justifyContent: "center",
          ".Mui-selected": {
             backgroundColor: `${theme.palette.secondary.main} !important`,
          },
-         ".Mui-selected:hover": {
-            backgroundColor: `${theme.palette.secondary.dark} !important`,
+         ".MuiMenuItem-root": {
+            fontSize: "1.1rem",
+            fontWeight: 600,
          },
-         ".Mui-selected:focus": {
+         ".Mui-disabled": {
+            color: `${theme.palette.secondary.light}`,
+         },
+      },
+      ".MuiClock-root": {
+         ".MuiClock-pin, .MuiClockPointer-root, .MuiClockPointer-thumb": {
+            borderColor: `${theme.palette.secondary.main} !important`,
             backgroundColor: `${theme.palette.secondary.main} !important`,
          },
-         ".MuiDigitalClock-item:hover": {
-            backgroundColor: "rgb(137 42 186 / 4%) !important",
-         },
-      }
-   },
-   datePickerMobile: (theme) => {
-      return {
-         "&.MuiPickersLayout-root": {
-            display: "block",
-         },
-         ".MuiTab-root.Mui-selected": {
-            color: `${theme.palette.secondary.main}`,
-         },
-         ".MuiTabs-indicator": {
-            backgroundColor: `${theme.palette.secondary.main} !important`,
-         },
-         ".MuiPickersDay-root:hover, .MuiPickersDay-root:focus": {
-            backgroundColor: "rgb(137 42 186 / 4%) !important",
-         },
-         ".MuiPickersDay-root.Mui-selected, .MuiPickersYear-yearButton.Mui-selected":
-            {
-               backgroundColor: `${theme.palette.secondary.main} !important`,
-            },
-         ".MuiDialogActions-root": {
-            ".MuiButton-text": {
-               color: `${theme.palette.secondary.main}`,
-            },
-         },
-         ".MuiMultiSectionDigitalClock-root": {
-            justifyContent: "center",
-            ".Mui-selected": {
-               backgroundColor: `${theme.palette.secondary.main} !important`,
-            },
-            ".MuiMenuItem-root": {
-               fontSize: "1.1rem",
-               fontWeight: 600,
-            },
-            ".Mui-disabled": {
-               color: `${theme.palette.secondary.light}`,
-            },
-         },
-         ".MuiClock-root": {
-            ".MuiClock-pin, .MuiClockPointer-root, .MuiClockPointer-thumb": {
-               borderColor: `${theme.palette.secondary.main} !important`,
-               backgroundColor: `${theme.palette.secondary.main} !important`,
-            },
-         },
-      }
-   },
+      },
+   }),
    icon: {
       svg: {
          color: (theme) => theme.palette.secondary.main,
