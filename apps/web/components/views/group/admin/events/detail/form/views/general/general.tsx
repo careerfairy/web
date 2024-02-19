137ba9627476f7632a50f6ebb98bffc9fb7e40b5
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-02-19 11:30:35 +0000

    Fixes base layout abstraction

--- apps/web/components/views/group/admin/events/detail/form/views/general/general.tsx
+++ apps/web/components/views/group/admin/events/detail/form/views/general/general.tsx
@@ -1,27 +1,16 @@
-import { Form } from "formik"
-import { Stack } from "@mui/material"
 import Categories from "./Categories"
 import ReasonsToJoin from "./ReasonsToJoin"
-import { sxStyles } from "types/commonTypes"
 import GeneralSettings from "./GeneralSettings"
 import AudienceTargeting from "./AudienceTargeting"
 
-const styles = sxStyles({
-   root: {
-      padding: "24px",
-   },
-})
-
 const LivestreamFormGeneralStep = () => {
    return (
-      <Form>
-         <Stack sx={styles.root} rowGap={2}>
-            <GeneralSettings />
-            <ReasonsToJoin />
-            <Categories />
-            <AudienceTargeting />
-         </Stack>
-      </Form>
+      <>
+         <GeneralSettings />
+         <ReasonsToJoin />
+         <Categories />
+         <AudienceTargeting />
+      </>
    )
 }
 
