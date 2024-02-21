71330e9362b3a0f5a1093cadf637ec5031a92ac4
habibkadiri <kadirit@hotmail.com>
2024-02-21 11:47:55 +0100

    chore: rename and relocate files

--- apps/web/components/views/streaming-page/components/StreamingGrid/StreamsGrid/index.tsx
+++ apps/web/components/views/streaming-page/components/StreamingGrid/StreamsGrid/index.tsx
@@ -1,16 +1,14 @@
 import { Box } from "@mui/material"
-import { useMemo } from "react"
 import { sxStyles } from "types/commonTypes"
 import { LocalUserStream, LocalScreenStream } from "../../streaming/LocalStream"
 import { GridCarousel } from "../GridCarousel"
 import { LayoutGrid } from "../LayoutGrid"
 import { useSortedStreams } from "../useSortedStreams"
-import { getPaginatedGridLayout } from "../util"
-import { useGalleryLayout } from "./useGalleryLayout"
 import { RemoteStreamer } from "../../streaming"
 import { TrackBoundary } from "agora-rtc-react"
-import { useStreams } from "../useStreams"
 import { UserStreamProvider } from "components/views/streaming-page/context/UserStream"
+import { UserStream } from "components/views/streaming-page/types"
+import { Layout } from "../types"
 
 const styles = sxStyles({
    root: {
@@ -20,19 +18,12 @@ const styles = sxStyles({
    },
 })
 
-export const GalleryLayout = () => {
-   const streams = useStreams()
-   const layout = useGalleryLayout(streams.length)
+type Props = {
+   gridPages: UserStream[][]
+   layout: Layout
+}
 
-   const pageSize = layout.rows * layout.columns
-
-   const sortedStreams = useSortedStreams(streams, pageSize)
-
-   const gridPages = useMemo(
-      () => getPaginatedGridLayout(sortedStreams, layout),
-      [sortedStreams, layout]
-   )
-
+export const StreamsGrid = ({ gridPages, layout }: Props) => {
    return (
       <TrackBoundary>
          <Box sx={styles.root}>
--- apps/web/components/views/streaming-page/components/StreamingGrid/index.tsx
+++ apps/web/components/views/streaming-page/components/StreamingGrid/index.tsx
@@ -1,19 +1,59 @@
 import { sxStyles } from "types/commonTypes"
-import { Box } from "@mui/material"
-import React from "react"
-import { GalleryLayout } from "./GalleryLayout"
+import { Box, Stack } from "@mui/material"
+import React, { useMemo } from "react"
+import { StreamsGrid } from "./StreamsGrid"
+import { useIsSpotlightMode } from "store/selectors/streamingAppSelectors"
+import { useStreamIsLandscape } from "components/custom-hook/streaming"
+import { useStreams } from "./useStreams"
+import { useGalleryLayout } from "./StreamsGrid/useGalleryLayout"
+import { useSortedStreams } from "./useSortedStreams"
+import { getPaginatedGridLayout } from "./util"
 
 const styles = sxStyles({
    root: {
       flex: 1,
       display: "flex",
    },
+   spotlight: {
+      height: 0,
+      // width: "100%",
+      border: "1px solid red",
+      transition: (theme) =>
+         theme.transitions.create(["height", "width"], {
+            duration: theme.transitions.duration.complex,
+         }),
+   },
+   spotlightActive: {
+      height: "50%",
+   },
 })
 
 export const StreamingGrid = () => {
+   const isSpotlightMode = useIsSpotlightMode()
+   const isLandscape = useStreamIsLandscape()
+   console.log(
+      "🚀 ~ file: index.tsx:16 ~ StreamingGrid ~ isSpotlightMode:",
+      isSpotlightMode
+   )
+
+   const streams = useStreams()
+   const layout = useGalleryLayout(streams.length)
+
+   const pageSize = layout.rows * layout.columns
+
+   const sortedStreams = useSortedStreams(streams, pageSize)
+
+   const gridPages = useMemo(
+      () => getPaginatedGridLayout(sortedStreams, layout),
+      [sortedStreams, layout]
+   )
+
    return (
-      <Box sx={styles.root}>
-         <GalleryLayout />
-      </Box>
+      <Stack direction={isLandscape ? "row" : "column"} sx={styles.root}>
+         <StreamsGrid gridPages={gridPages} layout={layout} />
+         <Box
+            sx={[styles.spotlight, isSpotlightMode && styles.spotlightActive]}
+         />
+      </Stack>
    )
 }
