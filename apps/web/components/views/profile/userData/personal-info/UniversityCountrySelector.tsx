4ccd86470a68cd77e009818f3e5a6c87558da392
habibkadiri <kadirit@hotmail.com>
2024-02-29 14:30:53 +0100

    Simplify personal info form with react-hook-form
    Refactored the 'PersonalInfo' component to remove redundant logic and unnecessary useState usage, simplifying the form to utilize controlled components throughout. Removed old form state management that used Formik and replaced it with react-hook-form and localized controlled components. The update improves form maintainability, reduces complexity, and prepares it for future enhancements.

---
+++ apps/web/components/views/profile/userData/personal-info/UniversityCountrySelector.tsx
@@ -0,0 +1,41 @@
+import { universityCountriesMap } from "components/util/constants/universityCountries"
+import { ControlledBrandedAutoComplete } from "components/views/common/inputs/ControlledBrandedAutoComplete"
+import { useFormContext } from "react-hook-form"
+
+type Props = {
+   name: string
+   universityFieldName: string
+}
+
+const countryKeys = Object.keys(universityCountriesMap)
+
+export const UniversityCountrySelector = ({
+   name,
+   universityFieldName,
+}: Props) => {
+   const {
+      setValue,
+      formState: { isSubmitting },
+   } = useFormContext()
+
+   return (
+      <ControlledBrandedAutoComplete
+         label="Select Country of University"
+         name={name}
+         options={countryKeys}
+         autocompleteProps={{
+            id: "universityCountryCode",
+            disabled: isSubmitting,
+            disableClearable: true,
+            autoHighlight: true,
+            getOptionLabel: (option) => universityCountriesMap[option] || "",
+            isOptionEqualToValue: (option, value) => option === value,
+            onChange: () => {
+               setValue(universityFieldName, null)
+            },
+            // @ts-ignore
+            ["data-testid"]: "university-country-selector",
+         }}
+      />
+   )
+}
---
+++ apps/web/components/views/profile/userData/personal-info/UniversitySelector.tsx
@@ -0,0 +1,52 @@
+import { useFormContext, useWatch } from "react-hook-form"
+import { useMemo } from "react"
+import useUniversitiesByCountryCodes from "components/custom-hook/useUniversities"
+import { ControlledBrandedAutoComplete } from "components/views/common/inputs/ControlledBrandedAutoComplete"
+import { OTHER_UNIVERSITY_OPTION, filterWithOther } from "./util"
+
+type Props = {
+   name: string
+   countryCodesName: string
+}
+
+export const UniversitySelector = ({ name, countryCodesName }: Props) => {
+   const {
+      formState: { isSubmitting },
+   } = useFormContext()
+
+   const selectedCountryCode = useWatch({
+      name: countryCodesName,
+   })
+
+   const countryCodes = useMemo(() => {
+      return [selectedCountryCode]
+   }, [selectedCountryCode])
+
+   const universities = useUniversitiesByCountryCodes(countryCodes)
+
+   const options = useMemo(() => {
+      const unies =
+         universities?.map((university) => ({
+            id: university.id,
+            value: university.name,
+         })) || []
+
+      return [OTHER_UNIVERSITY_OPTION, ...unies]
+   }, [universities])
+
+   return (
+      <ControlledBrandedAutoComplete
+         label="Select University"
+         name={name}
+         options={options}
+         autocompleteProps={{
+            id: "universityCountryCode",
+            disabled: isSubmitting,
+            autoHighlight: true,
+            // @ts-ignore
+            ["data-testid"]: "university-country-selector",
+            filterOptions: filterWithOther,
+         }}
+      />
+   )
+}
