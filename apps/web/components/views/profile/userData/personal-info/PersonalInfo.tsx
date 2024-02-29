3a99b157d222455ad51bfc51cc57ecef2013718a
habibkadiri <kadirit@hotmail.com>
2024-02-29 14:39:15 +0100

    fix: form bug

--- apps/web/components/views/profile/userData/personal-info/PersonalInfo.tsx
+++ apps/web/components/views/profile/userData/personal-info/PersonalInfo.tsx
@@ -218,9 +218,9 @@ const SubmitButton = () => {
                code: data.university.id,
                name: data.university.value,
                // If the user selects the same university, we want to keep the questions
-               ...(data.university.id === userData.university.code
+               ...(data.university.id === userData.university?.code
                   ? {
-                       questions: userData.university.questions,
+                       questions: userData.university?.questions || null,
                     }
                   : {}),
             },
