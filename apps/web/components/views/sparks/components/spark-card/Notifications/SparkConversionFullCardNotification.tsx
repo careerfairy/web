a6a7f74e16b7874d56cd5e4cc5ce40f83e2c0ed0
goncaloarnauth <goncaloarnauth@outlook.com>
2024-03-04 18:42:55 +0000

    feature/cf-813-conversion-card-on-sparks-feed

---
+++ apps/web/components/views/sparks/components/spark-card/Notifications/SparkConversionFullCardNotification.tsx
@@ -0,0 +1,165 @@
+import { Box, Typography, Button, Stack } from "@mui/material"
+import { sxStyles } from "types/commonTypes"
+import {
+   Star as StarIcon,
+   Users as UsersIcon,
+   TrendingUp as TrendingUpIcon,
+} from "react-feather"
+import { swipeToSparkByIndex } from "store/reducers/sparksFeedReducer"
+import { useRouter } from "next/router"
+import { useDispatch, useSelector } from "react-redux"
+import { currentSparkIndexSelector } from "store/selectors/sparksFeedSelectors"
+
+const styles = sxStyles({
+   header: {
+      mt: 9,
+      textAlign: "center",
+
+      "@media (max-height: 850px)": {
+         mt: 3,
+      },
+   },
+   title: {
+      fontSize: "64px !important",
+      fontWeight: "bold",
+      lineHeight: "66px",
+
+      "@media (max-height: 850px)": {
+         fontSize: "32px !important",
+         lineHeight: "34px",
+      },
+   },
+   content: {
+      display: "flex",
+      flexDirection: "column",
+      mx: 5,
+      mt: "31px",
+   },
+   infoCard: {
+      display: "flex",
+      background: "white",
+      borderRadius: "8px",
+      height: "96px",
+      alignItems: "center",
+      px: 2,
+
+      "@media (max-height: 700px)": {
+         height: { md: "unset" },
+      },
+   },
+   icon: {
+      mr: 1,
+
+      "& svg": {
+         color: (theme) => theme.palette.primary.main,
+
+         "@media (max-height: 700px)": {
+            height: { md: "20px" },
+            width: { md: "20px" },
+         },
+      },
+   },
+   infoText: {
+      "@media (max-height: 700px)": {
+         fontSize: "12px !important",
+      },
+   },
+   actions: {
+      position: "absolute",
+      left: 0,
+      bottom: "10%",
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+
+      "@media (max-height: 600px)": {
+         bottom: 2,
+         position: "unset",
+         mt: 6,
+      },
+   },
+})
+
+const SparkConversionFullCardNotification = () => {
+   const dispatch = useDispatch()
+   const { push } = useRouter()
+   const currentSparkIndex = useSelector(currentSparkIndexSelector)
+
+   const handleSignUp = () => {
+      void push("/signup")
+   }
+
+   const handleSwipeToNext = () => {
+      dispatch(swipeToSparkByIndex(currentSparkIndex + 1))
+   }
+
+   return (
+      <Box>
+         <Box sx={styles.header}>
+            <Typography variant="brandedH1" sx={styles.title}>
+               <Box component="span" color="primary.main">
+                  Ignite
+               </Box>{" "}
+               your career
+            </Typography>
+         </Box>
+
+         <Stack sx={styles.content} spacing={1}>
+            <Box sx={styles.infoCard}>
+               <Box sx={styles.icon}>
+                  <StarIcon size="42" />
+               </Box>
+
+               <Typography
+                  variant="brandedBody"
+                  color="text.primary"
+                  sx={styles.infoText}
+               >
+                  Get personalized career recommendations
+               </Typography>
+            </Box>
+
+            <Box sx={styles.infoCard}>
+               <Box sx={styles.icon}>
+                  <UsersIcon size="42" />
+               </Box>
+
+               <Typography
+                  variant="brandedBody"
+                  color="text.primary"
+                  sx={styles.infoText}
+               >
+                  Meet your future colleagues in our live streams
+               </Typography>
+            </Box>
+
+            <Box sx={styles.infoCard}>
+               <Box sx={styles.icon}>
+                  <TrendingUpIcon size="42" />
+               </Box>
+
+               <Typography
+                  variant="brandedBody"
+                  color="text.primary"
+                  sx={styles.infoText}
+               >
+                  Increase your chances to get your dream job by 80%
+               </Typography>
+            </Box>
+         </Stack>
+
+         <Box sx={styles.actions}>
+            <Button color="primary" variant="contained" onClick={handleSignUp}>
+               Sign up now
+            </Button>
+
+            <Button color="info" variant="text" onClick={handleSwipeToNext}>
+               Watch more Sparks
+            </Button>
+         </Box>
+      </Box>
+   )
+}
+
+export default SparkConversionFullCardNotification
