be8894a21ab5368abb9a3fe047e24eab81732551
habibkadiri <kadirit@hotmail.com>
2024-03-06 11:38:26 +0100

    feat: add max width support to grid item

--- apps/web/components/views/streaming-page/components/StreamingGrid/gallery/Gallery.tsx
+++ apps/web/components/views/streaming-page/components/StreamingGrid/gallery/Gallery.tsx
@@ -82,6 +82,7 @@ export const Gallery = ({ streams, spacing }: Props) => {
                         <LayoutGrid.Item
                            key={stream.user.uid}
                            layoutColumns={layout.columns}
+                           maxWidth={"100px !important"}
                         >
                            <GradientProvider
                               {...calculateGradientControl({
@@ -104,3 +105,12 @@ export const Gallery = ({ streams, spacing }: Props) => {
       </Box>
    )
 }
+
+// const calculateGridItemMaxWidth: CalculateGridItemMaxWidth = (
+//    maxPageColumns: number,
+//    maxPageRows: number,
+//    numberOfStreamsInPage: number
+// ) => {
+//    const isSingleRowMode = maxPageRows === 1
+//
+// }
