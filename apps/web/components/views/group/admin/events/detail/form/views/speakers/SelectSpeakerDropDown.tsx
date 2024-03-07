230b1d3bc5b42746f987dbc596004b9f2a969646
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-03-07 18:42:06 +0000

    Adds the speaker drop down selector

---
+++ apps/web/components/views/group/admin/events/detail/form/views/speakers/SelectSpeakerDropDown.tsx
@@ -0,0 +1,192 @@
+import React, { useCallback, useMemo } from "react"
+import {
+   Avatar,
+   Divider,
+   ListItemIcon,
+   ListItemText,
+   MenuItem,
+   Stack,
+   Typography,
+} from "@mui/material"
+import { sxStyles } from "types/commonTypes"
+import AddRoundedIcon from "@mui/icons-material/AddRounded"
+import { useLivestreamFormValues } from "../../useLivestreamFormValues"
+import { UpdateCreatorData } from "@careerfairy/shared-lib/groups/creators"
+import BaseStyles from "components/views/admin/company-information/BaseStyles"
+import { FormBrandedAutocomplete } from "components/views/common/inputs/BrandedAutocomplete"
+import CreatorAvatar from "components/views/sparks/components/CreatorAvatar"
+
+const styles = sxStyles({
+   menuItem: {
+      p: 2,
+   },
+   listIcon: {
+      mr: 1,
+   },
+   addNewAvatar: {
+      bgcolor: "#EFEFEF",
+      color: "#9E9E9E",
+      "& svg": {
+         width: 24,
+         height: 24,
+      },
+   },
+   avatar: {
+      width: 40,
+      height: 40,
+   },
+   input: {
+      ".MuiInputBase-root": {
+         paddingX: "12px",
+      },
+      input: {
+         padding: "0 !important",
+      },
+   },
+   optionElementContainer: {
+      width: { xs: "85%", md: "100%" },
+      padding: "16px",
+   },
+   optionTitle: {
+      fontSize: { xs: "14px", md: "16px" },
+      width: "600",
+      fontWeight: "bold",
+      marginBottom: 1,
+   },
+   optionInfo: {
+      fontSize: { xs: "12px", md: "14px" },
+   },
+   ellipsis: {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+   },
+})
+
+type Props = {
+   fieldId: string
+   label: string
+   placeholder: string
+   options: Partial<UpdateCreatorData>[]
+   handleCreateNew: () => void
+   values?: Partial<UpdateCreatorData>[]
+}
+
+const SelectSpeakersDropDown = ({
+   fieldId,
+   label,
+   placeholder,
+   options,
+   handleCreateNew,
+   values,
+}: Props) => {
+   const { setFieldValue, isSubmitting } = useLivestreamFormValues()
+
+   const renderCreateNewCreatorOption = useMemo(
+      () => (
+         <>
+            <MenuItem sx={styles.menuItem} onMouseDown={handleCreateNew}>
+               <ListItemIcon sx={styles.listIcon}>
+                  <Avatar sx={styles.addNewAvatar}>
+                     <AddRoundedIcon />
+                  </Avatar>
+               </ListItemIcon>
+               <ListItemText primary={"Create a new contributor"} />
+            </MenuItem>
+            {options?.length ? <Divider /> : null}
+         </>
+      ),
+      [handleCreateNew, options?.length]
+   )
+
+   const onChangeHandler = async (
+      _,
+      selectedOptions: Partial<UpdateCreatorData>[]
+   ) => {
+      await setFieldValue(fieldId, selectedOptions)
+   }
+
+   const getOptionElement = useCallback(
+      (speaker: Partial<UpdateCreatorData>) => (
+         <Stack direction="row" padding="16px" gap="8px" width="100%">
+            <CreatorAvatar
+               creator={{
+                  firstName: speaker.firstName,
+                  lastName: speaker.lastName,
+                  avatarUrl: speaker.avatar,
+               }}
+               size={48}
+            />
+            <Stack direction="column" alignContent="center">
+               <Typography fontSize="16px" lineHeight="24px">
+                  {`${speaker.firstName} ${speaker.lastName}`}
+               </Typography>
+               {Boolean(speaker.position || speaker.email) && (
+                  <Stack spacing={2} direction={"row"}>
+                     {Boolean(speaker.position) && (
+                        <Typography
+                           fontSize="14px"
+                           fontWeight={300}
+                           lineHeight="24px"
+                        >
+                           {speaker.position}
+                        </Typography>
+                     )}
+
+                     {Boolean(speaker.position && speaker.email) && (
+                        <Divider orientation="vertical" flexItem />
+                     )}
+
+                     {Boolean(speaker.email) && (
+                        <Typography
+                           fontSize="14px"
+                           fontWeight={300}
+                           lineHeight="24px"
+                           sx={[styles.optionInfo, styles.ellipsis]}
+                        >
+                           {speaker.email}
+                        </Typography>
+                     )}
+                  </Stack>
+               )}
+            </Stack>
+         </Stack>
+      ),
+      []
+   )
+
+   return (
+      <FormBrandedAutocomplete
+         name={fieldId}
+         value={values}
+         options={options}
+         isOptionEqualToValue={isOptionEqualToValue}
+         getOptionLabel={getLabelFn}
+         sx={BaseStyles.chipInput}
+         textFieldProps={{
+            name: fieldId,
+            label: label,
+            fullWidth: true,
+            placeholder: placeholder,
+            sx: styles.input,
+         }}
+         onChange={onChangeHandler}
+         multiple
+         disableCloseOnSelect
+         initialOptionSection={renderCreateNewCreatorOption}
+         getOptionElement={getOptionElement}
+         disabled={isSubmitting}
+         limit={5}
+      />
+   )
+}
+
+const getLabelFn = (value: Partial<UpdateCreatorData>) => {
+   return `${value.firstName} ${value.lastName}`
+}
+
+const isOptionEqualToValue = (
+   option: Partial<UpdateCreatorData>,
+   value: Partial<UpdateCreatorData>
+): boolean => option.id === value.id
+
+export default SelectSpeakersDropDown
