ab5e9ee036bfa373d00643a48ef3231e8c6d19f6
goncaloarnauth <goncaloarnauth@outlook.com>
2024-03-07 11:31:54 +0000

    add card shadow and background

--- apps/web/components/views/sparks/components/spark-card/Notifications/SparkConversionFullCardNotification.tsx
+++ apps/web/components/views/sparks/components/spark-card/Notifications/SparkConversionFullCardNotification.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/router"
 import { useDispatch, useSelector } from "react-redux"
 import { currentSparkIndexSelector } from "store/selectors/sparksFeedSelectors"
 import Link from "next/link"
+import Image from "next/image"
 
 const styles = sxStyles({
    header: {
@@ -47,7 +48,7 @@ const styles = sxStyles({
       "@media (max-height: 700px)": {
          height: { md: "unset" },
       },
-      "@media (max-height: 550px)": {
+      "@media (max-height: 560px)": {
          height: "unset",
       },
    },
@@ -62,7 +63,7 @@ const styles = sxStyles({
             width: { md: "24px" },
          },
 
-         "@media (max-height: 550px)": {
+         "@media (max-height: 560px)": {
             height: "24px",
             width: "24px",
          },
@@ -76,7 +77,7 @@ const styles = sxStyles({
    actions: {
       position: "absolute",
       left: 0,
-      bottom: "10%",
+      bottom: "6%",
       width: "100%",
       display: "flex",
       flexDirection: "column",
@@ -91,6 +92,24 @@ const styles = sxStyles({
          position: "unset",
       },
    },
+   shadow: {
+      width: "100%",
+      height: "27px",
+      flexShrink: 0,
+      borderRadius: "406px",
+      background: "rgba(0, 0, 0, 0.20)",
+      filter: "blur(20px)",
+   },
+   background: {
+      zIndex: -1,
+
+      "& img": {
+         inset: "0 0 auto auto !important",
+         height: "unset !important",
+         width: "unset !important",
+         maxWidth: "80%",
+      },
+   },
 })
 
 const SparkConversionFullCardNotification = () => {
@@ -103,75 +122,87 @@ const SparkConversionFullCardNotification = () => {
    }
 
    return (
-      <Box>
-         <Box sx={styles.header}>
-            <Typography variant="brandedH1" sx={styles.title}>
-               <Box component="span" color="primary.main">
-                  Ignite
-               </Box>{" "}
-               your career
-            </Typography>
+      <>
+         <Box sx={styles.background}>
+            <Image
+               fill
+               src={"/career-text-background.svg"}
+               alt="career-text-background"
+            />
          </Box>
 
-         <Stack sx={styles.content} spacing={1}>
-            <Box sx={styles.infoCard}>
-               <Box sx={styles.icon}>
-                  <StarIcon size="42" />
-               </Box>
-
-               <Typography
-                  variant="brandedBody"
-                  color="text.primary"
-                  sx={styles.infoText}
-               >
-                  Get personalized career recommendations
+         <Box>
+            <Box sx={styles.header}>
+               <Typography variant="brandedH1" sx={styles.title}>
+                  <Box component="span" color="primary.main">
+                     Ignite
+                  </Box>{" "}
+                  your career
                </Typography>
             </Box>
 
-            <Box sx={styles.infoCard}>
-               <Box sx={styles.icon}>
-                  <UsersIcon size="42" />
+            <Stack sx={styles.content} spacing={1}>
+               <Box sx={styles.infoCard}>
+                  <Box sx={styles.icon}>
+                     <StarIcon size="42" />
+                  </Box>
+
+                  <Typography
+                     variant="brandedBody"
+                     color="text.primary"
+                     sx={styles.infoText}
+                  >
+                     Get personalized career recommendations
+                  </Typography>
                </Box>
 
-               <Typography
-                  variant="brandedBody"
-                  color="text.primary"
-                  sx={styles.infoText}
-               >
-                  Meet your future colleagues in our live streams
-               </Typography>
-            </Box>
+               <Box sx={styles.infoCard}>
+                  <Box sx={styles.icon}>
+                     <UsersIcon size="42" />
+                  </Box>
 
-            <Box sx={styles.infoCard}>
-               <Box sx={styles.icon}>
-                  <TrendingUpIcon size="42" />
+                  <Typography
+                     variant="brandedBody"
+                     color="text.primary"
+                     sx={styles.infoText}
+                  >
+                     Meet your future colleagues in our live streams
+                  </Typography>
                </Box>
 
-               <Typography
-                  variant="brandedBody"
-                  color="text.primary"
-                  sx={styles.infoText}
+               <Box sx={styles.infoCard}>
+                  <Box sx={styles.icon}>
+                     <TrendingUpIcon size="42" />
+                  </Box>
+
+                  <Typography
+                     variant="brandedBody"
+                     color="text.primary"
+                     sx={styles.infoText}
+                  >
+                     Increase your chances to get your dream job by 80%
+                  </Typography>
+               </Box>
+            </Stack>
+
+            <Box sx={styles.actions}>
+               <Button
+                  color="primary"
+                  variant="contained"
+                  component={Link}
+                  href={`/login?absolutePath=${asPath}`}
                >
-                  Increase your chances to get your dream job by 80%
-               </Typography>
+                  Sign up now
+               </Button>
+
+               <Button color="info" variant="text" onClick={handleSwipeToNext}>
+                  Watch more Sparks
+               </Button>
+
+               <Box sx={styles.shadow} />
             </Box>
-         </Stack>
-
-         <Box sx={styles.actions}>
-            <Button
-               color="primary"
-               variant="contained"
-               component={Link}
-               href={`/login?absolutePath=${asPath}`}
-            >
-               Sign up now
-            </Button>
-
-            <Button color="info" variant="text" onClick={handleSwipeToNext}>
-               Watch more Sparks
-            </Button>
          </Box>
-      </Box>
+      </>
    )
 }
 
