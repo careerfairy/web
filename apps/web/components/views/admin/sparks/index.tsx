f5c089e3f819346a53c6d2a3160a7957752f85ec
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-03-14 12:33:00 +0000

    Base functionality done, full process implemented, only UI changes left

--- apps/web/components/views/admin/sparks/index.tsx
+++ apps/web/components/views/admin/sparks/index.tsx
@@ -6,9 +6,7 @@ import EmptySparksView from "./empty-sparks-view/EmptySparksView"
 import GeneralSparksView from "./general-sparks-view/GeneralSparksView"
 import SparksDialog from "./sparks-dialog/SparksDialog"
 import SparksOnboardingDialog from "./sparks-onboarding-dialog/SparksOnboardingDialog"
-import useGroupPlanIsValid from "components/custom-hook/group/useGroupPlanIsValid"
 import UpgradePlanBanner from "components/views/checkout/forms/UpgradePlanBanner"
-import ConditionalWrapper from "components/util/ConditionalWrapper"
 import { SPARKS_1_YEAR_SUBSCRIPTION_CONFIG } from "data/stripe/stripe"
 
 const Sparks = () => {
@@ -19,18 +17,20 @@ const Sparks = () => {
    )
 }
 
+const BANNER_TITLE = "Reignite your Sparks and keep the momentum going!"
+const BANNER_DESCRIPTION =
+   "Your Sparks trial has ended, and they're no longer visible to the CareerFairy talent community. But the magic doesn't have to stop! Upgrade now and reignite the spark to continue engaging all year round with your target audience, access in-depth analytics and showcase your job opportunities in an innovative way. Don't let the momentum you built fade, upgrade now and reignite the spark!"
 const SparksComponent = () => {
    const { group } = useGroup()
-   const planStatus = useGroupPlanIsValid(group.groupId, ["trial"])
    const groupHasSparks = useGroupHasSparks(group.id)
 
    return (
       <Fragment>
-         <ConditionalWrapper condition={!planStatus.valid}>
-            <UpgradePlanBanner
-               productConfig={SPARKS_1_YEAR_SUBSCRIPTION_CONFIG}
-            />
-         </ConditionalWrapper>
+         <UpgradePlanBanner
+            productConfig={SPARKS_1_YEAR_SUBSCRIPTION_CONFIG}
+            title={BANNER_TITLE}
+            description={BANNER_DESCRIPTION}
+         />
          {groupHasSparks ? <GeneralSparksView /> : <EmptySparksView />}
          <SparksDialog />
          <SparksOnboardingDialog />
