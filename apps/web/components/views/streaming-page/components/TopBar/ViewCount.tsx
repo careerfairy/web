fc3fd1fa80c8fa9465bff4577ddcc1e67188889c
habibkadiri <kadirit@hotmail.com>
2024-03-14 09:51:37 +0000

    feat: reduce files changed

--- apps/web/components/views/streaming-page/components/TopBar/ViewCount.tsx
+++ apps/web/components/views/streaming-page/components/TopBar/ViewCount.tsx
@@ -3,7 +3,6 @@ import { sxStyles } from "types/commonTypes"
 
 import { Eye } from "react-feather"
 import React from "react"
-import { useCurrentViewCount } from "store/selectors/streamingAppSelectors"
 
 const styles = sxStyles({
    root: {
@@ -12,15 +11,13 @@ const styles = sxStyles({
 })
 
 export const ViewCount = () => {
-   const viewCount = useCurrentViewCount()
-
    return (
       <ResponsiveStreamButton
          sx={styles.root}
          variant="outlined"
          startIcon={<Eye />}
       >
-         {viewCount}
+         128
       </ResponsiveStreamButton>
    )
 }
