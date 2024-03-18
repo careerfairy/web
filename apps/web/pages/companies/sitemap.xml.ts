dd7d3be5d549817d2e4f1a090ed808b593154db4
goncaloarnauth <goncaloarnauth@outlook.com>
2024-03-18 15:04:34 +0000

    add dynamic baseUrls to sitemap

--- apps/web/pages/companies/sitemap.xml.ts
+++ apps/web/pages/companies/sitemap.xml.ts
@@ -1,20 +1,21 @@
 import { Group } from "@careerfairy/shared-lib/groups"
 import { mapGroupsToSiteMap, siteMapXmlWrapper } from "components/util/Sitemap"
+import { getServerSideBaseUrl } from "components/util/url"
 import { groupRepo } from "data/RepositoryInstances"
 import { GetServerSideProps } from "next"
 
 const CompaniesSitemap = () => {
    return null
 }
-const generateSiteMap = async () => {
+const generateSiteMap = async (basePath: string) => {
    const results: Group[] = await groupRepo.getAllPublicProfileGroups()
 
-   return siteMapXmlWrapper(mapGroupsToSiteMap("/company", results))
+   return siteMapXmlWrapper(mapGroupsToSiteMap(`${basePath}/company`, results))
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+export const getServerSideProps: GetServerSideProps = async ({ res, req }) => {
    // Generate the XML sitemap
-   const sitemap = await generateSiteMap()
+   const sitemap = await generateSiteMap(getServerSideBaseUrl(req))
 
    res.setHeader("Content-Type", "text/xml")
    // Send the XML to the browser
--- apps/web/pages/sitemap.xml.ts
+++ apps/web/pages/sitemap.xml.ts
@@ -1,37 +1,39 @@
-import { getBaseUrl } from "components/helperFunctions/HelperFunctions"
+import { getServerSideBaseUrl } from "components/util/url"
 import { GetServerSideProps } from "next"
 
 const SitemapIndex = () => {
    return null
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+export const getServerSideProps: GetServerSideProps = async ({ res, req }) => {
+   const basePath = getServerSideBaseUrl(req)
+
    // Generate the XML sitemap index
    const sitemapIndex = `<?xml version='1.0' encoding='UTF-8'?>
     <sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
              xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
              xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
        <sitemap>
-            <loc>${getBaseUrl()}/portal/sitemap.xml</loc>
+            <loc>${basePath}/portal/sitemap.xml</loc>
             <changefreq>daily</changefreq>
        </sitemap>
     
        <sitemap>
-            <loc>${getBaseUrl()}/companies/sitemap.xml</loc>
+            <loc>${basePath}/companies/sitemap.xml</loc>
        </sitemap>
     
        <sitemap>
-            <loc>${getBaseUrl()}/next-livestreams/sitemap.xml</loc>
+            <loc>${basePath}/next-livestreams/sitemap.xml</loc>
             <changefreq>daily</changefreq>
        </sitemap>
        
        <sitemap>
-            <loc>${getBaseUrl()}/past-livestreams/sitemap.xml</loc>
+            <loc>${basePath}/past-livestreams/sitemap.xml</loc>
             <changefreq>daily</changefreq>
        </sitemap>
        
        <sitemap>
-            <loc>${getBaseUrl()}/landing_sitemap.xml</loc>
+            <loc>${basePath}/landing_sitemap.xml</loc>
        </sitemap>    
     </sitemapindex>`
 
