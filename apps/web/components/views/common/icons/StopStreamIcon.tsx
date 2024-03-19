24adb5903580e8df5eb26f83e839bcd204324157
habibkadiri <kadirit@hotmail.com>
2024-03-19 19:20:06 +0100

    feat: add non-responsive typography variants & stream start/stop confirmation dialog

---
+++ apps/web/components/views/common/icons/StopStreamIcon.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon"
+
+export const StopStreamIcon = (props: SvgIconProps) => {
+   return (
+      <SvgIcon viewBox="0 0 64 64" {...props}>
+         <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="64"
+            height="64"
+            viewBox="0 0 64 64"
+            fill="none"
+         >
+            <path
+               d="M31.9997 58.6667C46.7273 58.6667 58.6663 46.7276 58.6663 32C58.6663 17.2724 46.7273 5.33337 31.9997 5.33337C17.2721 5.33337 5.33301 17.2724 5.33301 32C5.33301 46.7276 17.2721 58.6667 31.9997 58.6667Z"
+               fill="#FFE8E8"
+               stroke="#FF1616"
+               stroke-width="4"
+               stroke-linecap="round"
+               stroke-linejoin="round"
+            />
+            <path
+               d="M40 24H24V40H40V24Z"
+               fill="#FF1616"
+               stroke="#FF1616"
+               stroke-width="4"
+               stroke-linecap="round"
+               stroke-linejoin="round"
+            />
+         </svg>
+      </SvgIcon>
+   )
+}
--- apps/web/components/views/streaming-page/components/TopBar/ToggleStartLiveStreamButton.tsx
+++ apps/web/components/views/streaming-page/components/TopBar/ToggleStartLiveStreamButton.tsx
@@ -1,26 +1,38 @@
+import { StartStreamIcon, StopStreamIcon } from "components/views/common/icons"
 import { ResponsiveStreamButton } from "../Buttons"
 import { useStreamIsMobile } from "components/custom-hook/streaming"
+import ConfirmationDialog from "materialUI/GlobalModals/ConfirmationDialog"
 import { useState } from "react"
+import { useHasStarted } from "store/selectors/streamingAppSelectors"
+import { useToggleStartLivestream } from "components/custom-hook/streaming/useToggleStartLivestream"
+import { useStreamingContext } from "../../context"
 
-// type ConfirmationDialogAction = "start" | "stop" | null
+type ConfirmationDialogAction = "start" | "stop" | null
 
 export const ToggleStartLiveStreamButton = () => {
-   const [hasStartedLivestream, setHasStartedLivestream] =
-      useState<boolean>(false)
+   const hasStarted = useHasStarted()
+   const { livestreamId } = useStreamingContext()
 
-   // const [confirmationDialogAction, setConfirmationDialogAction] =
-   //    useState<ConfirmationDialogAction>(null)
+   const [confirmationDialogAction, setConfirmationDialogAction] =
+      useState<ConfirmationDialogAction>(null)
 
    const isMobile = useStreamIsMobile(390)
+
+   const shouldStop = confirmationDialogAction === "stop"
+
+   const { trigger: toggleStartLivestream, isMutating } =
+      useToggleStartLivestream(livestreamId, !shouldStop)
 
    return (
       <>
          <ResponsiveStreamButton
-            onClick={() => setHasStartedLivestream(!hasStartedLivestream)}
-            color={hasStartedLivestream ? "error" : "primary"}
+            onClick={() =>
+               setConfirmationDialogAction(hasStarted ? "stop" : "start")
+            }
+            color={hasStarted ? "error" : "primary"}
             variant="contained"
          >
-            {hasStartedLivestream
+            {hasStarted
                ? isMobile
                   ? "Stop"
                   : "End live stream"
@@ -28,6 +40,35 @@ export const ToggleStartLiveStreamButton = () => {
                ? "Start"
                : "Start live stream"}
          </ResponsiveStreamButton>
+         {Boolean(confirmationDialogAction) && (
+            <ConfirmationDialog
+               open={Boolean(confirmationDialogAction)}
+               title={shouldStop ? "End live stream" : "Start live stream"}
+               description={
+                  shouldStop
+                     ? "Are you sure you want to end your live stream? Your viewers will no longer be able to see or hear you."
+                     : "Are you sure you want to start your live stream? Once you go live, your viewers will be able to see and hear you."
+               }
+               icon={shouldStop ? <StopStreamIcon /> : <StartStreamIcon />}
+               secondaryAction={{
+                  text: "Cancel",
+                  color: "grey",
+                  callback: () => setConfirmationDialogAction(null),
+                  variant: "outlined",
+                  loading: isMutating,
+               }}
+               primaryAction={{
+                  text: shouldStop ? "End live stream" : "Start live stream",
+                  color: shouldStop ? "error" : "primary",
+                  callback: async () => {
+                     await toggleStartLivestream()
+                     setConfirmationDialogAction(null)
+                  },
+                  variant: "contained",
+                  loading: isMutating,
+               }}
+            />
+         )}
       </>
    )
 }
