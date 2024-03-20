f693f0cb787eec9f7a913b4885b205418c27cd4f
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-03-20 10:15:33 +0000

    Refactors speaker option to pure component

--- apps/web/components/views/group/admin/events/detail/form/views/speakers/SelectSpeakerDropDown.tsx
+++ apps/web/components/views/group/admin/events/detail/form/views/speakers/SelectSpeakerDropDown.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from "react"
+import AddRoundedIcon from "@mui/icons-material/AddRounded"
 import {
    Avatar,
    Divider,
@@ -8,13 +8,13 @@ import {
    Stack,
    Typography,
 } from "@mui/material"
-import { sxStyles } from "types/commonTypes"
-import { LivestreamCreator } from "../questions/commons"
-import AddRoundedIcon from "@mui/icons-material/AddRounded"
-import { useLivestreamFormValues } from "../../useLivestreamFormValues"
-import CreatorAvatar from "components/views/sparks/components/CreatorAvatar"
 import BaseStyles from "components/views/admin/company-information/BaseStyles"
 import { FormBrandedAutocomplete } from "components/views/common/inputs/BrandedAutocomplete"
+import CreatorAvatar from "components/views/sparks/components/CreatorAvatar"
+import { useMemo } from "react"
+import { sxStyles } from "types/commonTypes"
+import { useLivestreamFormValues } from "../../useLivestreamFormValues"
+import { LivestreamCreator } from "../questions/commons"
 
 const styles = sxStyles({
    menuItem: {
@@ -102,55 +102,6 @@ const SelectSpeakersDropDown = ({
       await setFieldValue(fieldId, selectedOptions)
    }
 
-   const getOptionElement = useCallback(
-      (speaker: LivestreamCreator) => (
-         <Stack direction="row" padding="16px" gap="8px" width="100%">
-            <CreatorAvatar
-               creator={{
-                  firstName: speaker.firstName,
-                  lastName: speaker.lastName,
-                  avatarUrl: speaker.avatarUrl,
-               }}
-               size={48}
-            />
-            <Stack direction="column" alignContent="center">
-               <Typography fontSize="16px" lineHeight="24px">
-                  {`${speaker.firstName} ${speaker.lastName}`}
-               </Typography>
-               {Boolean(speaker.position || speaker.email) && (
-                  <Stack spacing={2} direction={"row"}>
-                     {Boolean(speaker.position) && (
-                        <Typography
-                           fontSize="14px"
-                           fontWeight={300}
-                           lineHeight="24px"
-                        >
-                           {speaker.position}
-                        </Typography>
-                     )}
-
-                     {Boolean(speaker.position && speaker.email) && (
-                        <Divider orientation="vertical" flexItem />
-                     )}
-
-                     {Boolean(speaker.email) && (
-                        <Typography
-                           fontSize="14px"
-                           fontWeight={300}
-                           lineHeight="24px"
-                           sx={[styles.optionInfo, styles.ellipsis]}
-                        >
-                           {speaker.email}
-                        </Typography>
-                     )}
-                  </Stack>
-               )}
-            </Stack>
-         </Stack>
-      ),
-      []
-   )
-
    return (
       <FormBrandedAutocomplete
          name={fieldId}
@@ -176,6 +127,52 @@ const SelectSpeakersDropDown = ({
       />
    )
 }
+
+const getOptionElement = (speaker: LivestreamCreator) => (
+   <Stack direction="row" padding="16px" gap="8px" width="100%">
+      <CreatorAvatar
+         creator={{
+            firstName: speaker.firstName,
+            lastName: speaker.lastName,
+            avatarUrl: speaker.avatarUrl,
+         }}
+         size={48}
+      />
+      <Stack direction="column" alignContent="center">
+         <Typography fontSize="16px" lineHeight="24px">
+            {`${speaker.firstName} ${speaker.lastName}`}
+         </Typography>
+         {Boolean(speaker.position || speaker.email) && (
+            <Stack spacing={2} direction={"row"}>
+               {Boolean(speaker.position) && (
+                  <Typography
+                     fontSize="14px"
+                     fontWeight={300}
+                     lineHeight="24px"
+                  >
+                     {speaker.position}
+                  </Typography>
+               )}
+
+               {Boolean(speaker.position && speaker.email) && (
+                  <Divider orientation="vertical" flexItem />
+               )}
+
+               {Boolean(speaker.email) && (
+                  <Typography
+                     fontSize="14px"
+                     fontWeight={300}
+                     lineHeight="24px"
+                     sx={[styles.optionInfo, styles.ellipsis]}
+                  >
+                     {speaker.email}
+                  </Typography>
+               )}
+            </Stack>
+         )}
+      </Stack>
+   </Stack>
+)
 
 const getLabelFn = (value: LivestreamCreator) => {
    return `${value.firstName} ${value.lastName}`
