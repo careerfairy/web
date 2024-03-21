2d1f840e79df312de06d070c7b863d06f7d0d96b
habibkadiri <kadirit@hotmail.com>
2024-03-21 14:15:09 +0100

    feat: fix side panel overflow bug on firefox

--- apps/web/components/views/streaming-page/components/SidePanel/index.tsx
+++ apps/web/components/views/streaming-page/components/SidePanel/index.tsx
@@ -1,5 +1,5 @@
 import { sxStyles } from "types/commonTypes"
-import { Box, SwipeableDrawer } from "@mui/material"
+import { Box, SwipeableDrawer, Collapse } from "@mui/material"
 import { useAppDispatch, useAppSelector } from "components/custom-hook/store"
 import { ActiveView, toggleSidePanel } from "store/reducers/streamingAppReducer"
 import { sidePanelSelector } from "store/selectors/streamingAppSelectors"
@@ -18,15 +18,13 @@ const drawerWidth = 328
 const styles = sxStyles({
    root: {
       display: "flex",
-      transition: (theme) => theme.transitions.create(["width", "opacity"]),
+      width: drawerWidth,
+      height: "100%",
+      opacity: 0,
+      transition: (theme) => theme.transitions.create("opacity"),
    },
    open: {
-      width: drawerWidth,
       opacity: 1,
-   },
-   closed: {
-      width: 0,
-      opacity: 0,
    },
    paper: {
       borderRadius: "12px 12px 0 0",
@@ -83,8 +81,8 @@ export const SidePanel = () => {
    }
 
    return (
-      <Box sx={[styles.root, isOpen ? styles.open : styles.closed]}>
-         {content}
-      </Box>
+      <Collapse in={isOpen} orientation="horizontal">
+         <Box sx={[styles.root, isOpen && styles.open]}>{content}</Box>
+      </Collapse>
    )
 }
