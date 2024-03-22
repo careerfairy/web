b161e9d29fd2d8cd202b6b189b07de25a061a50c
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-03-22 17:24:48 +0000

    Update CreateSparksButton for including max upload

--- apps/web/components/views/admin/sparks/components/CreateSparkButton.tsx
+++ apps/web/components/views/admin/sparks/components/CreateSparkButton.tsx
@@ -6,6 +6,8 @@ import { combineStyles, sxStyles } from "types/commonTypes"
 import { useGroup } from "layouts/GroupDashboardLayout"
 import useGroupPlanIsValid from "components/custom-hook/group/useGroupPlanIsValid"
 import { GroupPlanTypes } from "@careerfairy/shared-lib/groups"
+import useGroupSparks from "components/custom-hook/spark/useGroupSparks"
+import { PLAN_CONSTANTS } from "@careerfairy/shared-lib/groups/planConstants"
 
 const styles = sxStyles({
    root: {
@@ -15,21 +17,32 @@ const styles = sxStyles({
 
 const CreateSparkButton: FC<ButtonProps> = ({ sx, children, ...props }) => {
    const dispatch = useDispatch()
-   const group = useGroup()
-   const planStatus = useGroupPlanIsValid(group.group.groupId, [
+   const { group, groupPresenter } = useGroup()
+
+   const maxPublicSparks = groupPresenter.getMaxPublicSparks()
+
+   const { data: publicSparks } = useGroupSparks(group.groupId, {
+      isPublished: true,
+      limit: maxPublicSparks,
+   })
+   const planStatus = useGroupPlanIsValid(group.groupId, [
       GroupPlanTypes.Trial,
       GroupPlanTypes.Tier1,
       GroupPlanTypes.Advanced,
       GroupPlanTypes.Premium,
    ])
 
+   const disableUploadSparks =
+      !planStatus.valid ||
+      publicSparks.length >=
+         PLAN_CONSTANTS[group.plan.type].sparks.MAX_PUBLIC_SPARKS
    const handleOpen = useCallback(() => {
       dispatch(openSparkDialog(null))
    }, [dispatch])
 
    return (
       <Button
-         disabled={!planStatus.valid}
+         disabled={disableUploadSparks}
          onClick={handleOpen}
          color="secondary"
          sx={combineStyles(styles.root, sx)}
