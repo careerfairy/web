598d552e2cfa788eef53634b21eb336eb5d9a9e8
goncaloarnauth <goncaloarnauth@outlook.com>
2024-03-22 13:58:45 +0000

    add logic to mobile to support auto-play

--- apps/web/components/views/sparks/components/spark-card/SparkCarouselCard.tsx
+++ apps/web/components/views/sparks/components/spark-card/SparkCarouselCard.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react"
+import { FC, useEffect, useRef, useState } from "react"
 import { Spark } from "@careerfairy/shared-lib/sparks/sparks"
 import Box from "@mui/material/Box"
 import { sxStyles } from "types/commonTypes"
@@ -8,6 +8,8 @@ import SparkQuestion from "./SparkQuestion"
 import { Stack } from "@mui/material"
 import SparkCarouselCardContainer from "./SparkCarouselCardContainer"
 import { SparkPresenter } from "@careerfairy/shared-lib/sparks/SparkPresenter"
+import useIsMobile from "components/custom-hook/useIsMobile"
+import { debounce } from "lodash"
 
 const cardPadding = 2
 
@@ -28,6 +30,43 @@ type Props = {
 const SparkCarouselCard: FC<Props> = ({ spark, onClick, preview = false }) => {
    const sparkPresenter = SparkPresenter.createFromFirebaseObject(spark)
    const [isHovered, setIsHovered] = useState(false)
+   // console.log("🚀 ~ isHovered:", isHovered)
+   const containerRef = useRef<HTMLDivElement>(null)
+   const isMobile = useIsMobile()
+
+   useEffect(() => {
+      const currentContainerRef = containerRef.current
+
+      const observable = (entries) => {
+         const entry = entries[0]
+
+         if (entry.isIntersecting && entry.intersectionRatio === 1) {
+            // console.log("START -> ", entry)
+            setIsHovered(true)
+         } else {
+            // console.log("STOP -> ", entry)
+            setIsHovered(false)
+         }
+      }
+
+      const debouncedObservable = debounce(observable, 300)
+
+      if (isMobile) {
+         const observer = new IntersectionObserver(debouncedObservable, {
+            threshold: 1.0,
+         })
+
+         if (currentContainerRef) {
+            observer.observe(currentContainerRef)
+         }
+
+         return () => {
+            if (currentContainerRef) {
+               observer.unobserve(currentContainerRef)
+            }
+         }
+      }
+   }, [isMobile])
 
    return (
       <SparkCarouselCardContainer
@@ -36,9 +75,10 @@ const SparkCarouselCard: FC<Props> = ({ spark, onClick, preview = false }) => {
             url: sparkPresenter.getTransformedVideoUrl(),
             preview: preview,
          }}
-         onMouseEnter={() => setIsHovered(true)}
-         onMouseLeave={() => setIsHovered(false)}
+         onMouseEnter={isMobile ? null : () => setIsHovered(true)}
+         onMouseLeave={isMobile ? null : () => setIsHovered(false)}
          isHovered={isHovered}
+         containerRef={containerRef}
       >
          <Box px={cardPadding} pt={cardPadding}>
             <SparkHeader showAdminOptions={false} spark={spark} />
