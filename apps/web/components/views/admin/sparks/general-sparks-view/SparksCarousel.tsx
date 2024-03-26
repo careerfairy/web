24e041bb14f6842fdb1118aaabc0a821adc0b3d9
goncaloarnauth <goncaloarnauth@outlook.com>
2024-03-26 12:57:23 +0000

    move logic to parent component

--- apps/web/components/views/admin/sparks/general-sparks-view/SparksCarousel.tsx
+++ apps/web/components/views/admin/sparks/general-sparks-view/SparksCarousel.tsx
@@ -1,11 +1,15 @@
-import React, { ReactNode } from "react"
+import React, { ReactNode, useEffect, useRef, useState } from "react"
 import { Spark } from "@careerfairy/shared-lib/sparks/sparks"
 import Box from "@mui/material/Box"
 import SparkCarouselCardForAdmin from "components/views/sparks/components/spark-card/SparkCarouselCardForAdmin"
-import SparkCarouselCard from "components/views/sparks/components/spark-card/SparkCarouselCard"
+import SparkCarouselCard, {
+   AUTO_PLAY_TIME,
+} from "components/views/sparks/components/spark-card/SparkCarouselCard"
 import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react"
 import { sxStyles } from "types/commonTypes"
 import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures"
+import useIsMobile from "components/custom-hook/useIsMobile"
+import { debounce } from "lodash"
 
 const slideSpacing = 21
 const desktopSlideWidth = 306 + slideSpacing
@@ -73,23 +77,32 @@ const SparksCarousel = React.forwardRef<ChildRefType, PropType>(
             <Box sx={styles.container}>
                {sparks?.length
                   ? sparks.map((spark) => (
-                       <Box key={spark.id} sx={styles.slide}>
-                          {isAdmin ? (
-                             <SparkCarouselCardForAdmin
-                                onClick={() => onSparkClick(spark)}
-                                spark={spark}
-                             />
-                          ) : (
-                             <SparkCarouselCard
-                                onClick={() => onSparkClick(spark)}
-                                spark={spark}
-                                onGoNext={() => emblaApi.scrollNext()}
-                                isLastPosition={
-                                   spark.id === sparks[sparks.length - 1].id
-                                }
-                             />
-                          )}
-                       </Box>
+                       <Content
+                          key={spark.id}
+                          sparks={sparks}
+                          currentSpark={spark}
+                          isAdmin={isAdmin}
+                          handleClick={() => onSparkClick(spark)}
+                          handleGoNext={() => emblaApi.scrollNext()}
+                       />
+                       //   <Box key={spark.id} sx={styles.slide}>
+
+                       //      {isAdmin ? (
+                       //         <SparkCarouselCardForAdmin
+                       //            onClick={() => onSparkClick(spark)}
+                       //            spark={spark}
+                       //         />
+                       //      ) : (
+                       //         <SparkCarouselCard
+                       //            onClick={() => onSparkClick(spark)}
+                       //            spark={spark}
+                       //            onGoNext={() => emblaApi.scrollNext()}
+                       //            isLastPosition={
+                       //               spark.id === sparks[sparks.length - 1].id
+                       //            }
+                       //         />
+                       //      )}
+                       //   </Box>
                     ))
                   : children?.map((child, i) => (
                        <Box key={i} sx={styles.slide}>
@@ -109,3 +122,101 @@ const SparksCarousel = React.forwardRef<ChildRefType, PropType>(
 SparksCarousel.displayName = "SparksCarousel"
 
 export default SparksCarousel
+
+const Content = ({
+   sparks,
+   currentSpark,
+   handleClick,
+   isAdmin,
+   handleGoNext,
+}) => {
+   const [autoPlaying, setAutoPlaying] = useState(false)
+   const containerRef = useRef<HTMLDivElement>(null)
+   const isMobile = useIsMobile()
+
+   const isLastPosition = currentSpark.id === sparks[sparks.length - 1].id
+
+   // Set up intersection observer to handle auto-playing
+   useEffect(() => {
+      const currentContainerRef = containerRef.current
+      let timeout
+
+      const observable = (entries) => {
+         const entry = entries[0]
+
+         if (entry && entry.intersectionRatio > 0.9) {
+            timeout = setTimeout(() => {
+               setAutoPlaying(true)
+            }, 1000)
+         } else {
+            setAutoPlaying(false)
+            clearTimeout(timeout)
+         }
+      }
+
+      const debouncedObservable = debounce(observable, 300)
+
+      const observer = new IntersectionObserver(debouncedObservable, {
+         threshold: 0.9,
+      })
+
+      if (isMobile && containerRef.current) {
+         observer.observe(containerRef.current)
+      }
+
+      return () => {
+         // if (currentContainerRef) {
+         observer.unobserve(currentContainerRef)
+         // }
+         clearTimeout(timeout)
+      }
+   }, [isMobile])
+
+   // Set up auto-playing timeout for mobile experience
+   useEffect(() => {
+      let timeout
+
+      if (autoPlaying && isMobile) {
+         timeout = setTimeout(() => {
+            if (isLastPosition) {
+               // if it's the last spark on the carousel and already auto-played we should set the autoPlaying to false and do nothing
+               setAutoPlaying(false)
+            } else {
+               // After auto-play we should transition to the next spark
+               handleGoNext && handleGoNext()
+            }
+         }, AUTO_PLAY_TIME)
+      }
+
+      return () => {
+         clearTimeout(timeout)
+      }
+   }, [autoPlaying, handleGoNext, isLastPosition, isMobile])
+
+   return (
+      <Box
+         key={currentSpark.id}
+         sx={styles.slide}
+         ref={containerRef}
+         onMouseEnter={isMobile && isAdmin ? null : () => setAutoPlaying(true)}
+         onMouseLeave={isMobile && isAdmin ? null : () => setAutoPlaying(false)}
+      >
+         {isAdmin ? (
+            <SparkCarouselCardForAdmin
+               onClick={handleClick}
+               spark={currentSpark}
+            />
+         ) : (
+            <SparkCarouselCard
+               onClick={handleClick}
+               spark={currentSpark}
+               autoPlaying={autoPlaying}
+               // onGoNext={handleGoNext}
+               // isLastPosition={
+               //    currentSpark.id === sparks[sparks.length - 1].id
+               // }
+            />
+         )}
+      </Box>
+   )
+}
--- apps/web/components/views/sparks/components/spark-card/SparkCarouselCard.tsx
+++ apps/web/components/views/sparks/components/spark-card/SparkCarouselCard.tsx
@@ -1,4 +1,7 @@
-import { FC, useEffect, useRef, useState } from "react"
+import {
+   FC,
+   // , useEffect, useRef, useState
+} from "react"
 import { Spark } from "@careerfairy/shared-lib/sparks/sparks"
 import Box from "@mui/material/Box"
 import { sxStyles } from "types/commonTypes"
@@ -8,8 +11,8 @@ import SparkQuestion from "./SparkQuestion"
 import { Stack } from "@mui/material"
 import SparkCarouselCardContainer from "./SparkCarouselCardContainer"
 import { SparkPresenter } from "@careerfairy/shared-lib/sparks/SparkPresenter"
-import useIsMobile from "components/custom-hook/useIsMobile"
-import { debounce } from "lodash"
+// import useIsMobile from "components/custom-hook/useIsMobile"
+// import { debounce } from "lodash"
 
 const cardPadding = 2
 export const AUTO_PLAY_TIME = 5000
@@ -28,76 +31,78 @@ type Props = {
    onClick?: () => void
    onGoNext?: () => void
    isLastPosition?: boolean
+   autoPlaying?: boolean
 }
 
 const SparkCarouselCard: FC<Props> = ({
    spark,
    onClick,
    preview = false,
-   onGoNext,
-   isLastPosition,
+   autoPlaying,
+   // onGoNext,
+   // isLastPosition,
 }) => {
    const sparkPresenter = SparkPresenter.createFromFirebaseObject(spark)
-   const [autoPlaying, setAutoPlaying] = useState(false)
-   const containerRef = useRef<HTMLDivElement>(null)
-   const isMobile = useIsMobile()
+   // const [autoPlaying, setAutoPlaying] = useState(false)
+   // const containerRef = useRef<HTMLDivElement>(null)
+   // const isMobile = useIsMobile()
 
-   // Set up intersection observer to handle auto-playing
-   useEffect(() => {
-      const currentContainerRef = containerRef.current
-      let timeout
+   // // Set up intersection observer to handle auto-playing
+   // useEffect(() => {
+   //    const currentContainerRef = containerRef.current
+   //    let timeout
 
-      const observable = (entries) => {
-         const entry = entries[0]
+   //    const observable = (entries) => {
+   //       const entry = entries[0]
 
-         if (entry && entry.intersectionRatio > 0.9) {
-            timeout = setTimeout(() => {
-               setAutoPlaying(true)
-            }, 1000)
-         } else {
-            setAutoPlaying(false)
-            clearTimeout(timeout)
-         }
-      }
+   //       if (entry && entry.intersectionRatio > 0.9) {
+   //          timeout = setTimeout(() => {
+   //             setAutoPlaying(true)
+   //          }, 1000)
+   //       } else {
+   //          setAutoPlaying(false)
+   //          clearTimeout(timeout)
+   //       }
+   //    }
 
-      const debouncedObservable = debounce(observable, 300)
+   //    const debouncedObservable = debounce(observable, 300)
 
-      const observer = new IntersectionObserver(debouncedObservable, {
-         threshold: 0.9,
-      })
+   //    const observer = new IntersectionObserver(debouncedObservable, {
+   //       threshold: 0.9,
+   //    })
 
-      if (isMobile && containerRef.current) {
-         observer.observe(containerRef.current)
-      }
+   //    if (isMobile && containerRef.current) {
+   //       observer.observe(containerRef.current)
+   //    }
 
-      return () => {
-         // if (currentContainerRef) {
-         observer.unobserve(currentContainerRef)
-         // }
-         clearTimeout(timeout)
-      }
-   }, [isMobile])
+   //    return () => {
+   //       // if (currentContainerRef) {
+   //       observer.unobserve(currentContainerRef)
+   //       // }
+   //       clearTimeout(timeout)
+   //    }
+   // }, [isMobile])
 
-   // Set up auto-playing timeout for mobile experience
-   useEffect(() => {
-      let timeout
+   // // Set up auto-playing timeout for mobile experience
+   // useEffect(() => {
+   //    let timeout
 
-      if (autoPlaying && isMobile) {
-         timeout = setTimeout(() => {
-            if (isLastPosition) {
-               // if it's the last spark on the carousel and already auto-played we should set the autoPlaying to false and do nothing
-               setAutoPlaying(false)
-            } else {
-               // After auto-play we should transition to the next spark
-               onGoNext && onGoNext()
-            }
-         }, AUTO_PLAY_TIME)
-      }
+   //    if (autoPlaying && isMobile) {
+   //       timeout = setTimeout(() => {
+   //          if (isLastPosition) {
+   //             // if it's the last spark on the carousel and already auto-played we should set the autoPlaying to false and do nothing
+   //             setAutoPlaying(false)
+   //          } else {
+   //             // After auto-play we should transition to the next spark
+   //             onGoNext && onGoNext()
+   //          }
+   //       }, AUTO_PLAY_TIME)
+   //    }
 
-      return () => {
-         clearTimeout(timeout)
-      }
-   }, [autoPlaying, isLastPosition, isMobile, onGoNext])
+   //    return () => {
+   //       clearTimeout(timeout)
+   //    }
+   // }, [autoPlaying, isLastPosition, isMobile, onGoNext])
 
    return (
       <SparkCarouselCardContainer
@@ -106,10 +111,10 @@ const SparkCarouselCard: FC<Props> = ({
             url: sparkPresenter.getTransformedVideoUrl(),
             preview: preview,
          }}
-         onMouseEnter={isMobile ? null : () => setAutoPlaying(true)}
-         onMouseLeave={isMobile ? null : () => setAutoPlaying(false)}
+         // onMouseEnter={isMobile ? null : () => setAutoPlaying(true)}
+         // onMouseLeave={isMobile ? null : () => setAutoPlaying(false)}
          autoPlaying={autoPlaying}
-         containerRef={containerRef}
+         // containerRef={containerRef}
       >
          <Box px={cardPadding} pt={cardPadding}>
             <SparkHeader showAdminOptions={false} spark={spark} />
