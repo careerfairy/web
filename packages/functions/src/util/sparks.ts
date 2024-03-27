a7e797cf0c04dec002dc392921526e2aeb31e113
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-03-27 10:16:28 +0000

    rolled back feature

--- packages/functions/src/util/sparks.ts
+++ packages/functions/src/util/sparks.ts
@@ -5,7 +5,6 @@ import { groupRepo, sparkRepo } from "../api/repositories"
 import { Creator } from "@careerfairy/shared-lib/groups/creators"
 import functions = require("firebase-functions")
 import { GroupPresenter } from "@careerfairy/shared-lib/groups/GroupPresenter"
-import { DateTime } from "luxon"
 
 /**
  * Adds the current timestamp to the "addedToFeedAt" field of a spark.
@@ -50,13 +49,6 @@ export const validateGroupSparks = async (group: Group) => {
          return
       }
 
-      // If the groups current plan has expired no need for the other validations and set publicSparks = false
-      if (hasGroupPlanExpired(group)) {
-         // only update if needed
-         if (publicSparks) {
-            return groupRepo.updatePublicSparks(groupId, false)
-         }
-      }
       // get all the creators from a group
       const creators: Creator[] = await groupRepo.getCreators(groupId)
 
@@ -130,14 +122,4 @@ export const validateGroupSparks = async (group: Group) => {
    } catch (error) {
       return functions.logger.error("Error during Spark validation", { error })
    }
-}
-
-function hasGroupPlanExpired(group: Group): boolean {
-   if (group.plan) {
-      const now = DateTime.now().toJSDate()
-      if (group.plan.expiresAt.toDate() < now) {
-         return true
-      }
-   }
-   return false
 }
