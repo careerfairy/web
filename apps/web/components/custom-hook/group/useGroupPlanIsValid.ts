3eac8f35ca64c3ec2bd34048dfe3571f81748908
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-03-28 18:26:10 +0000

    Updated optional types

--- apps/web/components/custom-hook/group/useGroupPlanIsValid.ts
+++ apps/web/components/custom-hook/group/useGroupPlanIsValid.ts
@@ -1,17 +1,25 @@
+import { GroupPlanType } from "@careerfairy/shared-lib/groups"
 import useGroupsByIds from "../useGroupsByIds"
 import { GroupPresenter } from "@careerfairy/shared-lib/groups/GroupPresenter"
 type Result = {
    valid: boolean
 }
 
-const useGroupPlanIsValid = (groupId: string): Result => {
+const useGroupPlanIsValid = (
+   groupId: string,
+   types?: GroupPlanType[]
+): Result => {
    const { data: group } = useGroupsByIds([groupId])
 
    let valid
    if (!group.length) valid = false
    const groupPresenter = GroupPresenter.createFromDocument(group.at(0))
+
    valid = !groupPresenter.hasPlanExpired()
-   return { valid }
+   if (!types?.length) return { valid }
+   const hasType = types.includes(groupPresenter.plan?.type)
+
+   return { valid: valid && hasType }
 }
 
 export default useGroupPlanIsValid
