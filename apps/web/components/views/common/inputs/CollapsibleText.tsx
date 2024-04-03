b5107c535952c787c77e7123e75405f4a9cb02de
matilderamosCF <matilde.ramos@careerfairy.io>
2024-04-03 18:10:31 +0100

    hotfix/cf-850-add-rich-text-to-job-description

--- apps/web/components/views/common/inputs/CollapsibleText.tsx
+++ apps/web/components/views/common/inputs/CollapsibleText.tsx
@@ -2,12 +2,11 @@ import { Box, Button, Collapse } from "@mui/material"
 import SanitizedHTML from "../../../util/SanitizedHTML"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import ExpandLessIcon from "@mui/icons-material/ExpandLess"
-import React, { useCallback, useMemo, useState } from "react"
+import React, { useCallback, useEffect, useRef, useState } from "react"
 import { sxStyles } from "../../../../types/commonTypes"
 import { SxProps } from "@mui/system"
 import { Theme } from "@mui/material/styles"
-
-const MAX_LENGTH_TO_SHOW_COLLAPSE_BUTTONS = 290
+import { isHeightOverflowing } from "util/LayoutUtil"
 
 const styles = sxStyles({
    text: {
@@ -26,22 +25,17 @@ const styles = sxStyles({
 type Props = {
    text: string
    textStyle?: SxProps<Theme>
-   maxLength?: number
    collapsedSize?: number | string
 }
 
-const CollapsibleText = ({
-   text,
-   textStyle,
-   maxLength,
-   collapsedSize,
-}: Props) => {
+const CollapsibleText = ({ text, textStyle, collapsedSize }: Props) => {
    const [isTextCollapsed, setIsTextCollapsed] = useState(true)
+   const [isTextOverflowing, setIsTextOverflowing] = useState(false)
+   const textRef = useRef(null)
 
-   const showCollapse = useMemo(
-      () => text.length >= (maxLength || MAX_LENGTH_TO_SHOW_COLLAPSE_BUTTONS),
-      [maxLength, text.length]
-   )
+   useEffect(() => {
+      setIsTextOverflowing(isHeightOverflowing(textRef?.current))
+   }, [textRef])
 
    const handleClick = useCallback(() => {
       setIsTextCollapsed((prev) => !prev)
@@ -49,13 +43,17 @@ const CollapsibleText = ({
 
    return (
       <Box>
-         <Collapse in={!isTextCollapsed} collapsedSize={collapsedSize || 75}>
+         <Collapse
+            in={!isTextCollapsed}
+            ref={textRef}
+            collapsedSize={collapsedSize || 75}
+         >
             <SanitizedHTML
                sx={textStyle ? textStyle : styles.text}
                htmlString={text}
             />
          </Collapse>
-         {showCollapse ? (
+         {isTextOverflowing ? (
             <Box display="flex" justifyContent="start">
                {isTextCollapsed ? (
                   <Button
