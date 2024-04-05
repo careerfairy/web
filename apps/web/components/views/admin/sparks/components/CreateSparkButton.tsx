1f5317331d4d0326e4271346e326f4b96a43be29
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-04-05 10:21:48 +0100

    Fixed createSparkButton

--- apps/web/components/views/admin/sparks/components/CreateSparkButton.tsx
+++ apps/web/components/views/admin/sparks/components/CreateSparkButton.tsx
@@ -6,6 +6,7 @@ import { combineStyles, sxStyles } from "types/commonTypes"
 import { useGroup } from "layouts/GroupDashboardLayout"
 import useGroupPlanIsValid from "components/custom-hook/group/useGroupPlanIsValid"
 import useGroupSparks from "components/custom-hook/spark/useGroupSparks"
+import { GroupPlanTypes } from "@careerfairy/shared-lib/groups"
 
 const styles = sxStyles({
    root: {
@@ -23,7 +24,12 @@ const CreateSparkButton: FC<ButtonProps> = ({ sx, children, ...props }) => {
       isPublished: true,
       limit: maxPublicSparks,
    })
-   const planStatus = useGroupPlanIsValid(group.groupId)
+   const planStatus = useGroupPlanIsValid(group.groupId, [
+      GroupPlanTypes.Trial,
+      GroupPlanTypes.Tier1,
+      GroupPlanTypes.Tier2,
+      GroupPlanTypes.Tier3,
+   ])
 
    const maxSparksReached = groupPresenter.hasReachedMaxSparks(
       publicSparks.length
