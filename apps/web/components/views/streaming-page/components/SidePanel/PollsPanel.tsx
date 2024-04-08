03824c0de23b47a49f9c03e35790865461a00adc
habibkadiri <kadirit@hotmail.com>
2024-04-08 13:28:03 +0200

    init

--- apps/web/components/views/streaming-page/components/SidePanel/PollsPanel.tsx
+++ apps/web/components/views/streaming-page/components/SidePanel/PollsPanel.tsx
@@ -1,10 +1,32 @@
 import React from "react"
 import { SidePanelView } from "./SidePanelView"
 import { PollIcon } from "components/views/common/icons"
+import { sxStyles } from "types/commonTypes"
+import { useStreamingContext } from "../../context"
+import { PollCreationButton } from "../polls/PollCreationButton"
 
+const styles = sxStyles({
+   contentWrapperViewer: {
+      px: 2,
+      py: 1.5,
+   },
+   contentWrapperHost: {
+      p: 1.5,
+   },
+})
 export const PollsPanel = () => {
+   const { isHost } = useStreamingContext()
+
    return (
-      <SidePanelView id="polls-panel" title="Polls" icon={<PollIcon />}>
+      <SidePanelView
+         contentWrapperStyles={
+            isHost ? styles.contentWrapperHost : styles.contentWrapperViewer
+         }
+         id="polls-panel"
+         title="Polls"
+         icon={<PollIcon />}
+      >
+         {isHost ? <PollCreationButton /> : null}
          content
       </SidePanelView>
    )
---
+++ apps/web/components/views/streaming-page/components/polls/PollCreationButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material"
+
+export const PollCreationButton = () => {
+   return (
+      <Button color="primary" variant="contained" fullWidth>
+         Create new poll
+      </Button>
+   )
+}
