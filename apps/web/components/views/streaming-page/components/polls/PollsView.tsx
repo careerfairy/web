9cc1ccc090fdb0bf48522effccac27ec9c9e4147
habibkadiri <kadirit@hotmail.com>
2024-04-09 12:09:13 +0200

    feat(polls): prioritize display of current polls in PollsView
    Implemented sorting mechanism using useMemo to ensure current/ongoing polls are shown at the top of the list, enhancing visibility for users in live-streaming sessions.

--- apps/web/components/views/streaming-page/components/polls/PollsView.tsx
+++ apps/web/components/views/streaming-page/components/polls/PollsView.tsx
@@ -1,7 +1,7 @@
 import { Box, Collapse, Stack } from "@mui/material"
 import { SuspenseWithBoundary } from "components/ErrorBoundary"
 import { useLivestreamPolls } from "components/custom-hook/streaming/useLivestreamPolls"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { TransitionGroup } from "react-transition-group"
 import { useStreamingContext } from "../../context"
 import { PollCard } from "./PollCard"
@@ -20,6 +20,13 @@ const Content = () => {
    const { livestreamId, isHost } = useStreamingContext()
    const { data: polls } = useLivestreamPolls(livestreamId)
 
+   // We always want to show the current/ongoing poll(s) first
+   const orderedPolls = useMemo(() => {
+      const currentPolls = polls.filter((poll) => poll.state === "current")
+      const otherPolls = polls.filter((poll) => poll.state !== "current")
+      return [...currentPolls, ...otherPolls]
+   }, [polls])
+
    const [isCreatePollFormOpen, setIsCreatePollFormOpen] = useState(false)
 
    const hasPolls = polls.length > 0
@@ -34,7 +41,7 @@ const Content = () => {
             />
          ) : null}
          <Stack spacing={1} component={TransitionGroup}>
-            {polls.map((poll) => (
+            {orderedPolls.map((poll) => (
                <Collapse key={poll.id}>
                   <Box>
                      <SuspenseWithBoundary fallback={<PollCardSkeleton />}>
