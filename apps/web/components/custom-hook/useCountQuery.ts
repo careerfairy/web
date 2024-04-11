253818167e4e214e5caba31d914a3526a9ec541a
habibkadiri <kadirit@hotmail.com>
2024-04-11 14:35:14 +0200

    revert(polls): revert useCount query and add new useSWRCountQuery to not break 2e2 tests

--- apps/web/components/custom-hook/useCountQuery.ts
+++ apps/web/components/custom-hook/useCountQuery.ts
@@ -1,77 +1,50 @@
 import { getCountFromServer, Query } from "@firebase/firestore"
-import { queryEqual } from "firebase/firestore"
-import { ReactFireGlobals } from "reactfire"
-import useSWR, { SWRConfiguration } from "swr"
+import { useEffect, useMemo, useState } from "react"
 import { errorLogAndNotify } from "util/CommonUtil"
-
-const fetchCount = async (q: Query) => {
-   try {
-      const response = await getCountFromServer(q)
-      return response.data().count
-   } catch (error) {
-      errorLogAndNotify(error)
-      throw error
-   }
-}
-
-/**
- * Global cache for Firestore queries to ensure unique identifiers for SWR keys.
- * This cache stores references to Firestore queries that have been made,
- * allowing for efficient re-use and deduplication of queries across the application.
- *
- * TODO: Move this to where the reactfire providers are initialized.
- */
-const cachedQueries: Array<Query> =
-   (globalThis as unknown as ReactFireGlobals)._reactFireFirestoreQueryCache ||
-   []
-
-// TODO: Move this to where the reactfire providers are initialized.
-function getUniqueIdForFirestoreQuery(query: Query) {
-   const index = cachedQueries.findIndex((cachedQuery) =>
-      queryEqual(cachedQuery, query)
-   )
-   if (index > -1) {
-      return index
-   }
-
-   return cachedQueries.push(query) - 1
-}
 
 export type CountQuery = {
    loading: boolean
    count: number | null
    error: Error | undefined
-   refetch: () => void
 }
-
 /**
- * Retrieves the count of documents matching a query.
+ * Count documents in a query
  *
- * Leverages Firestore's getCountFromServer() alongside SWR for optimized data retrieval.
- * Executes an aggregation query, potentially reducing costs to 1/1000th of a standard read operation.
- * For further details on aggregation queries and their cost benefits, visit https://firebase.google.com/docs/firestore/pricing#aggregation_queries
+ * Uses the new fistore getCountFromServer() feature
  */
-const useCountQuery = (
-   q: Query,
-   options?: SWRConfiguration<number>
-): CountQuery => {
-   const mergedOptions: SWRConfiguration<number> = {
-      revalidateOnFocus: false,
-      ...options,
-   }
+const useCountQuery = (q: Query): CountQuery => {
+   const [count, setCount] = useState<number | null>(null)
+   const [error, setError] = useState<Error | undefined>(undefined)
 
-   const { data, error, isLoading, mutate } = useSWR(
-      q ? ["countQuery", getUniqueIdForFirestoreQuery(q)] : null,
-      () => fetchCount(q),
-      mergedOptions
-   )
+   useEffect(() => {
+      if (!q) {
+         return
+      }
+      let mounted = true
 
-   return {
-      loading: isLoading,
-      count: data,
-      error,
-      refetch: mutate,
-   }
+      getCountFromServer(q)
+         .then((r) => {
+            if (mounted) {
+               setCount(r.data().count)
+            }
+         })
+         .catch((e) => {
+            errorLogAndNotify(e)
+            setError(e)
+         })
+
+      return () => {
+         mounted = false
+      }
+   }, [q])
+
+   return useMemo(() => {
+      return {
+         loading: count === null,
+         count,
+         error,
+      }
+   }, [count, error])
 }
 
 export default useCountQuery
