fb46e6e9deb033a704320092048b4135a175b414
Walter Gonçalves <152179257+wgoncalvescf@users.noreply.github.com>
2024-04-15 10:58:42 +0100

    Trial Project #4: Checkout Confirmation Dialog (#1053)
    ### Motivation
    
    With Stripe integration complete, its necessary to display additional information to customers upon redirect from Stripe checkout, displaying information whether the transaction was successful or not.
    
    ### Changes
    
    -  Created a new dialog for showing Payment status information after redirect
    - Created a new cloud function **fetchStripeSessionStatus** for usage within new custom hook **useStripeSessionStatus**, which retrieves the status of a transaction from Stripe via the API
    
    ### Functions to deploy
    - [x] fetchStripeSessionStatus
    ### Testing
    
    While in admin mode for any company with an expiring plan, purchase a new plan and after successful checkout the new confirmation dialog should be displayed upon refresh.
    
    
    1. Navigate while in admin mode to a company with an expiring plan.
         - If no companies have an expiring plan, the expiresAt of a plan can be updated via Firefoo
    2. Initiate a plan subscription process by clicking the button on `/group/{group_id}/admin/sparks` page.
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/S0WylOadBGaro8NXGBwS/df81955a-38b0-43ee-a16e-c34ac6e11efc.png)
    3. Select a plan
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/S0WylOadBGaro8NXGBwS/88de7c39-bac8-4811-8e10-331cd110ca3f.png)
    4. Use test data for payment, test card: 4242 4242 4242 4242
    5. Upon payment confirmation there should be a refresh and the confirmation dialog should be appear and can be dismissed via button click
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/S0WylOadBGaro8NXGBwS/d034c53b-98c5-4222-9a37-b79b10d89a27.png)
    
    **Test url for dialog with valid session id**
    `http://localhost:3000/group/yrUCEdMPNc6vz2mMXkx1/admin/sparks?stripe_session_id=cs_test_a1MX0O7gGNxsrOwBRqnO3MNEUAZeOVrKkCmNmN7l0uekoVQ7EIHmwcnXsd`
    
    
    ### Browser Testing
    
    <div class='graphite__hidden'>
              <div>🎥 Video uploaded on Graphite:</div>
                <a href="https://app.graphite.dev/media/video/S0WylOadBGaro8NXGBwS/5a0de448-e1a3-4acb-bc9b-1d52694ec8cb.mp4">
                  <img src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/S0WylOadBGaro8NXGBwS/5a0de448-e1a3-4acb-bc9b-1d52694ec8cb.mp4">
                </a>
              </div>
    <video src="https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/S0WylOadBGaro8NXGBwS/5a0de448-e1a3-4acb-bc9b-1d52694ec8cb.mp4">Gravação do ecrã 2024-03-28, às 10.mp4</video>
    
    -  [ ] Chrome
    -  [ ] Firefox
    -  [ ] Safari
    -  [ ] Edge
    
    ### Different Resolutions Testing
    
    -  [ ] Large Screens
    -  [ ] Medium Screens
    -  [ ] Small Screens

---
+++ apps/web/components/views/checkout/views/PlanActivationConfirmationDialog.tsx
@@ -0,0 +1,165 @@
+import { Box, Button, Dialog, Stack, Typography } from "@mui/material"
+import useStripeSessionStatus from "components/custom-hook/stripe/useStripeSessionStatus"
+import useIsMobile from "components/custom-hook/useIsMobile"
+import { getBaseUrl } from "components/helperFunctions/HelperFunctions"
+import ConditionalWrapper from "components/util/ConditionalWrapper"
+import { SlideUpTransition } from "components/views/common/transitions"
+import { useRouter } from "next/router"
+import { FC, useState } from "react"
+import { sxStyles } from "types/commonTypes"
+
+const styles = sxStyles({
+   dialogPaperMobile: {
+      minHeight: "100%",
+      minWidth: "100%",
+      justifyContent: "center",
+      borderBottomLeftRadius: "0px",
+      borderBottomRightRadius: "0px",
+   },
+   dialogPaperDesktop: {
+      p: "28px",
+      justifyContent: "center",
+   },
+   container: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "end",
+   },
+   containerMobile: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "end",
+      mt: "220px",
+   },
+   dialogContentWrapper: {
+      p: "15px",
+   },
+   messageTitle: {
+      maxWidth: "350px",
+      fontWeight: 700,
+      textAlign: "center",
+   },
+   messageDescription: {
+      maxWidth: "450px",
+      fontWeight: 400,
+      textAlign: "center",
+      color: (theme) => theme.palette.neutral[400],
+   },
+   imageWrapper: {
+      backgroundImage: "url('/Party-popper1.png')",
+      width: "128.229px",
+      height: "132px",
+   },
+   closeButton: {
+      minWidth: "95%",
+      borderRadius: "20px",
+   },
+})
+
+const PlanActivationConfirmationDialog = () => {
+   const { query } = useRouter()
+
+   const stripeSessionId = query.stripe_session_id as string
+
+   return (
+      <ConditionalWrapper condition={Boolean(stripeSessionId)}>
+         <PaymentCompleteComponent sessionId={stripeSessionId} />
+      </ConditionalWrapper>
+   )
+}
+
+type PaymentCompleteComponentProps = {
+   sessionId: string
+}
+
+const PaymentCompleteComponent: FC<PaymentCompleteComponentProps> = ({
+   sessionId,
+}) => {
+   const { asPath, replace } = useRouter()
+   const [isOpen, setIsOpen] = useState(true)
+   const isMobile = useIsMobile()
+
+   const { data: sessionStatus } = useStripeSessionStatus(sessionId)
+
+   const closeDialog = () => {
+      setIsOpen(false)
+      replace(replaceUrl)
+   }
+
+   const showSuccess =
+      Boolean(sessionStatus) &&
+      sessionStatus.status == "complete" &&
+      sessionStatus.paymentStatus == "paid"
+
+   // Replacing only this query string to prevent breaking other properties if needed
+   const url = new URL(asPath, getBaseUrl())
+   url.searchParams.delete("stripe_session_id")
+
+   const replaceUrl = url.pathname + url.search
+
+   return (
+      <Dialog
+         sx={isMobile ? styles.containerMobile : styles.container}
+         scroll="paper"
+         open={isOpen}
+         maxWidth={"md"}
+         PaperProps={{
+            sx: isMobile ? styles.dialogPaperMobile : styles.dialogPaperDesktop,
+         }}
+         TransitionComponent={SlideUpTransition}
+      >
+         <ConditionalWrapper
+            condition={showSuccess}
+            fallback={<PaymentFailureComponent />}
+         >
+            <PaymentSuccessComponent handleClose={closeDialog} />
+         </ConditionalWrapper>
+      </Dialog>
+   )
+}
+
+const PaymentFailureComponent = () => {
+   return <>error: todo view</>
+}
+
+type PaymentSuccessComponentProps = {
+   handleClose: () => void
+}
+
+const PaymentSuccessComponent: FC<PaymentSuccessComponentProps> = ({
+   handleClose,
+}) => {
+   const { query } = useRouter()
+
+   const planName = query.planName as string
+   return (
+      <Stack
+         direction={"column"}
+         alignItems={"center"}
+         sx={styles.dialogContentWrapper}
+         spacing={2}
+      >
+         <Stack alignItems={"center"} spacing={1}>
+            <Box sx={styles.imageWrapper} />
+            <Typography variant="brandedH3" sx={styles.messageTitle}>
+               Your Sparks {planName} plan is now active!
+            </Typography>
+            <Typography variant="brandedBody" sx={styles.messageDescription}>
+               Discover the latest trends thanks to comprehensive Sparks
+               analytics and engage even more your target audience by publishing
+               additional Sparks.
+            </Typography>
+         </Stack>
+
+         <Button
+            sx={styles.closeButton}
+            color="secondary"
+            variant="contained"
+            onClick={handleClose}
+         >
+            Start using your plan
+         </Button>
+      </Stack>
+   )
+}
+export default PlanActivationConfirmationDialog
--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -124,6 +124,7 @@ exports.manualSparkReleaseEmail = newsletter.manualTemplatedEmail
 exports.stripeWebHook = stripe.stripeWebHook
 exports.fetchStripeCustomerSession = stripe.fetchStripeCustomerSession
 exports.fetchStripePrice = stripe.fetchStripePrice
+exports.fetchStripeSessionStatus = stripe.fetchStripeSessionStatus
 
 // Livestreams
 exports.setFirstCommentOfQuestionOnCreate =
