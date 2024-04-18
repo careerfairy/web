8e2c2a710773c21e84d618c7580ebb5ac273a1ff
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-04-18 20:30:29 +0100

    feat(PublishButton): adds publish button component

---
+++ apps/web/components/views/group/admin/events/detail/PublishButton.tsx
@@ -0,0 +1,85 @@
+import { sxStyles } from "@careerfairy/shared-ui"
+import { Button, SwipeableDrawer } from "@mui/material"
+import useDialogStateHandler from "components/custom-hook/useDialogStateHandler"
+import useIsMobile from "components/custom-hook/useIsMobile"
+import { useCallback, useState } from "react"
+import { useLivestreamCreationContext } from "./LivestreamCreationContext"
+import { PublishConfirmation } from "./PublishConfirmation"
+import { useLivestreamFormValues } from "./form/useLivestreamFormValues"
+import BrandedDialog from "./form/views/questions/components/BrandedDialog"
+
+const styles = sxStyles({
+   drawer: {
+      ".MuiPaper-root": {
+         borderTopLeftRadius: 12,
+         borderTopRightRadius: 12,
+      },
+   },
+   dialogPaper: {
+      width: "414px",
+      height: "300px",
+   },
+   button: {
+      display: "flex",
+      height: "40px",
+      padding: "12px 24px !important",
+   },
+})
+
+export const PublishButton = () => {
+   const isMobile = useIsMobile()
+   const { isValid } = useLivestreamFormValues()
+   const { livestream } = useLivestreamCreationContext()
+   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+   const [isDialogOpen, handleOpenDialog, handleCloseDialog] =
+      useDialogStateHandler()
+
+   const handlePublishButtonClick = useCallback(() => {
+      if (isMobile) {
+         setIsDrawerOpen(true)
+      } else {
+         handleOpenDialog()
+      }
+   }, [isMobile, handleOpenDialog])
+
+   if (!livestream.isDraft) {
+      return null
+   }
+
+   return (
+      <>
+         <Button
+            variant="contained"
+            color="secondary"
+            sx={styles.button}
+            disabled={!isValid}
+            onClick={handlePublishButtonClick}
+         >
+            Publish
+         </Button>
+         {isMobile ? (
+            <SwipeableDrawer
+               anchor="bottom"
+               onClose={() => setIsDrawerOpen(false)}
+               onOpen={() => null}
+               open={isDrawerOpen}
+               sx={styles.drawer}
+            >
+               <PublishConfirmation
+                  handleCancelClick={() => {
+                     setIsDrawerOpen(false)
+                  }}
+               />
+            </SwipeableDrawer>
+         ) : (
+            <BrandedDialog
+               isDialogOpen={isDialogOpen}
+               handleCloseDialog={handleCloseDialog}
+               paperSx={styles.dialogPaper}
+            >
+               <PublishConfirmation handleCancelClick={handleCloseDialog} />
+            </BrandedDialog>
+         )}
+      </>
+   )
+}
