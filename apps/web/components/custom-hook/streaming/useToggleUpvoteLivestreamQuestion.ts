9421d8ecb508743071faf40ed0246a09f2e6f742
habibkadiri <kadirit@hotmail.com>
2024-04-19 10:05:59 +0200

    feat(livestream): Implement upvote toggling for livestream questions
    Introduced a custom hook `useToggleUpvoteLivestreamQuestion` to handle upvote state changes with SWR mutation handling and error notification support. Streamlined upvote functionality by integrating `ToggleUpvoteButton` across the streaming page components, passing question identifiers as props. Updated `CommentInput` to use new streaming context, enabling comments on specific questions and improving input attributes. Enhanced `LivestreamService` to account for optional deprecated identifiers during upvote toggling transactions. Added `streamRef` context to provide a reference to the current livestream document. Adjusted related components to utilize new hook, improving separation of concerns and reusability within the codebase.

---
+++ apps/web/components/custom-hook/streaming/useToggleUpvoteLivestreamQuestion.ts
@@ -0,0 +1,45 @@
+import { LivestreamEvent } from "@careerfairy/shared-lib/livestreams"
+import { livestreamService } from "data/firebase/LivestreamService"
+import { DocumentReference } from "firebase/firestore"
+import useSWRMutation from "swr/mutation"
+import useSnackbarNotifications from "../useSnackbarNotifications"
+
+const getKey = (livestreamId: string, questionId: string) => {
+   if (!questionId || !livestreamId) {
+      return null
+   }
+   return `toggle-upvote-question-${livestreamId}-${questionId}`
+}
+
+/**
+ * Custom hook for toggling the upvote on a specific livestream question.
+ *
+ * @param  livestreamId - The ID of the livestream.
+ * @param  questionId - The ID of the question to toggle upvote.
+ * @param  livestreamToken - The token for authenticating the livestream action.
+ * @returns An object containing the mutation function to toggle upvote on a question and its related SWR mutation state.
+ */
+export const useToggleUpvoteLivestreamQuestion = (
+   streamRef: DocumentReference<LivestreamEvent>,
+   questionId: string,
+   args: { email: string; uid: string; deprecatedSessionUuid?: string }
+) => {
+   const { errorNotification } = useSnackbarNotifications()
+
+   const fetcher = () =>
+      livestreamService.toggleUpvoteQuestion(streamRef, questionId, args)
+
+   return useSWRMutation(getKey(streamRef.id, questionId), fetcher, {
+      onError: (error, key) => {
+         errorNotification(
+            error,
+            "Failed to toggle upvote on question in livestream",
+            {
+               key,
+               questionId,
+               streamRef,
+            }
+         )
+      },
+   })
+}
