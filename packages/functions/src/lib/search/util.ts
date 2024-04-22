c06dccc77b6baf8e42233123ad808425927596f9
habibkadiri <kadirit@hotmail.com>
2024-04-22 17:42:27 +0200

    chore(search): revert env changes
    chore(search): streamline Algolia setup and usage across environments
    
    Simplified Algolia integration by centralizing configuration and adopting consistent environment variable setup. Environment-specific index naming is now handled via utility functions to avoid manual setting adjustments for different deployment stages. Updated documentation to reflect the new setup process.

--- packages/functions/src/lib/search/util.ts
+++ packages/functions/src/lib/search/util.ts
@@ -2,6 +2,7 @@ import { logger } from "firebase-functions"
 import { algoliaClient } from "../../api/algolia"
 import { SearchIndex } from "algoliasearch"
 import { IndexSettings } from "./searchIndexGenerator"
+import { getEnvPrefix } from "../../util"
 
 export const logCreateIndex = (id: string, data: object) => {
    logger.info(`Creating new Algolia index for document ${id}`, data)
@@ -15,8 +16,13 @@ export const logDeleteIndex = (id: string) => {
    logger.info(`Deleting existing Algolia index for document ${id}`)
 }
 
+// Helper function to append the environment prefix to the index name so Type
+export const appendEnvPrefix = <T extends string>(value: T) => {
+   return `${value}${getEnvPrefix()}`
+}
+
 export const initAlgoliaIndex = (indexName: string) => {
-   return algoliaClient.initIndex(indexName)
+   return algoliaClient.initIndex(appendEnvPrefix(indexName))
 }
 
 type ReplicaEntry = `${string}_${string}_${"asc" | "desc"}`
@@ -54,16 +60,15 @@ export const configureSettings = async (
    settings: IndexSettings,
    index: SearchIndex
 ) => {
-   await index.setSettings(settings)
-
-   const env = {
-      appId: process.env.ALGOLIA_APP_ID,
-      apiKey: process.env.ALGOLIA_API_KEY,
+   const newSettings = {
+      ...settings,
+      replicas:
+         (settings.replicas?.map(appendEnvPrefix) as ReplicaEntry[]) || [],
    }
 
-   console.log("🚀 ~ HABIB-LOG env:", env)
+   await index.setSettings(newSettings)
 
    for (const entry of settings.replicas) {
-      await configureReplica(entry, settings)
+      await configureReplica(entry, newSettings)
    }
 }
