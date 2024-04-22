e53dcba4690f786c1437e7dd79c9a7c615eb28eb
habibkadiri <kadirit@hotmail.com>
2024-04-22 15:51:25 +0200

    feat(algolia): configure test environment with hardcoded credentials
    Introduce conditional assignment of Algolia credentials to support testing environments, aligning with ongoing test environment setup (ref: PR:https://github.com/careerfairy/web/pull/915).

--- packages/functions/src/api/algolia.ts
+++ packages/functions/src/api/algolia.ts
@@ -1,6 +1,16 @@
 import algoliasearch from "algoliasearch"
+import { isTestEnvironment } from "src/util"
 
-export const algoliaClient = algoliasearch(
-   process.env.ALGOLIA_APP_ID,
-   process.env.ALGOLIA_API_KEY
-)
+let appId: string
+let apiKey: string
+
+// TODO: Relocate this variable to a test env file as part of PR: https://github.com/careerfairy/web/pull/915
+if (isTestEnvironment()) {
+   appId = "W1YCBYSEZ3"
+   apiKey = "799a8ac590c7132b16979367215eb8c3"
+} else {
+   appId = process.env.ALGOLIA_APP_ID
+   apiKey = process.env.ALGOLIA_API_KEY
+}
+
+export const algoliaClient = algoliasearch(appId, apiKey)
