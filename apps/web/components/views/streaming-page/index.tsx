48813ba8c94355f3350c9364f6d5a7bb13c116a9
matilderamosCF <matilde.ramos@careerfairy.io>
2024-04-23 11:49:23 +0100

    feature/cf-875-disconnected-state-ui

--- apps/web/components/views/streaming-page/index.tsx
+++ apps/web/components/views/streaming-page/index.tsx
@@ -1,15 +1,15 @@
+import { CircularProgress } from "@mui/material"
 import { SuspenseWithBoundary } from "components/ErrorBoundary"
 import { useLivestreamData } from "components/custom-hook/streaming/useLivestreamData"
 import { useConditionalRedirect } from "components/custom-hook/useConditionalRedirect"
 import { appendCurrentQueryParams } from "components/util/url"
 import { Fragment, useMemo } from "react"
-import { CircularProgress } from "@mui/material"
 import { getAgoraUserId } from "./util"
 
 import { useAuth } from "HOCs/AuthProvider"
-import { useRouter } from "next/router"
 import ConditionalWrapper from "components/util/ConditionalWrapper"
 import dynamic from "next/dynamic"
+import { useRouter } from "next/router"
 import { LivestreamStateTrackers } from "./components/streaming/LivestreamStateTrackers"
 import { WaitingRoom } from "./components/viewer/WaitingRoom"
 
@@ -17,6 +17,14 @@ const SessionConflictModal = dynamic(
    () =>
       import("./components/SessionConflictModal").then(
          (mod) => mod.SessionConflictModal
+      ),
+   { ssr: false }
+)
+
+const SessionDisconnectedModal = dynamic(
+   () =>
+      import("./components/SessionDisconnectedModal").then(
+         (mod) => mod.SessionDisconnectedModal
       ),
    { ssr: false }
 )
@@ -204,6 +212,7 @@ const Component = ({ isHost }: Props) => {
                         {isHost ? null : <ViewerTrackers />}
                         {isHost ? null : <OngoingPollTracker />}
                         <SessionConflictModal />
+                        <SessionDisconnectedModal />
                      </RTMSignalingProvider>
                   </StreamingProvider>
                </UserClientProvider>
