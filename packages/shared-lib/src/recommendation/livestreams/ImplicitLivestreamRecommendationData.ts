baa8afe360d63840839fe9b7a21e03602574f8d8
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-04-29 02:18:43 +0200

    Updated additional code

--- packages/shared-lib/src/recommendation/livestreams/ImplicitLivestreamRecommendationData.ts
+++ packages/shared-lib/src/recommendation/livestreams/ImplicitLivestreamRecommendationData.ts
@@ -1,9 +1,9 @@
-import { CustomJob } from "../../customJobs/customJobs"
+import { CustomJobApplicant } from "../../customJobs/customJobs"
 import { LivestreamEvent } from "../../livestreams"
 import { Spark } from "../../sparks/sparks"
 
 export type ImplicitLivestreamRecommendationData = {
    watchedLivestreams: LivestreamEvent[] // most recently watched live streams (both live and recordings)
    watchedSparks: Spark[] // most recently watched Sparks
-   appliedJobs: CustomJob[] // most recent job applications
+   appliedJobs: CustomJobApplicant[] // most recent job applications
 }
