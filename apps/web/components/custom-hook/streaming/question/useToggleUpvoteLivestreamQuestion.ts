ca44ffd87ed1e1c63dd75946c295b773010c32f6
habibkadiri <kadirit@hotmail.com>
2024-05-03 10:56:07 +0200

    refactor(custom-hooks): standardize 'live stream' terminology and clean up UserDetailsSkeleton
    Standardized the term 'live stream' across various custom hooks, ensuring consistency and better readability. Simplified the UserDetailsSkeleton component by removing unnecessary styling and using MUI props directly.

--- apps/web/components/custom-hook/streaming/question/useToggleUpvoteLivestreamQuestion.ts
+++ apps/web/components/custom-hook/streaming/question/useToggleUpvoteLivestreamQuestion.ts
@@ -12,11 +12,11 @@ const getKey = (livestreamId: string, questionId: string) => {
 }
 
 /**
- * Custom hook for toggling the upvote on a specific livestream question.
+ * Custom hook for toggling the upvote on a specific live stream question.
  *
- * @param  livestreamId - The ID of the livestream.
+ * @param  livestreamId - The ID of the live stream.
  * @param  questionId - The ID of the question to toggle upvote.
- * @param  livestreamToken - The token for authenticating the livestream action.
+ * @param  livestreamToken - The token for authenticating the live stream action.
  * @returns An object containing the mutation function to toggle upvote on a question and its related SWR mutation state.
  */
 export const useToggleUpvoteLivestreamQuestion = (
@@ -33,7 +33,7 @@ export const useToggleUpvoteLivestreamQuestion = (
       onError: (error, key) => {
          errorNotification(
             error,
-            "Failed to toggle upvote on question in livestream",
+            "Failed to toggle upvote on question in live stream",
             {
                key,
                questionId,
