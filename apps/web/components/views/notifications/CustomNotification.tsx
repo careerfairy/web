f88a5acc5830238e98240486fb774d4a1ad6670c
habibkadiri <kadirit@hotmail.com>
2024-05-07 15:05:18 +0200

    fix(hook, notifications): Refactor useHandleHandRaise and standardize notification components
    - Simplify the function signature of useHandleHandRaise to accept an object, enhancing readability and maintainability.
    - Amend logic to only trigger hand raise state when transitioning from not publishing to publishing state, avoiding redundant calls.
    - Streamline imports and adjust component styling in CustomNotification for better visual consistency.
    - Rename message prop to content in notification components to better reflect its role.
    - Implement function component refactoring for notification actions, increasing modularity and readability.
    - Introduce new 'HandRaiseRequestedNotification' and refactor notification tracking for hand raise state changes, enhancing user feedback and interaction.
    - Update snackbarActions to use the correct prop 'content' over 'message' aligning with component changes.

--- apps/web/components/views/notifications/CustomNotification.tsx
+++ apps/web/components/views/notifications/CustomNotification.tsx
@@ -1,13 +1,12 @@
-import { forwardRef, ReactNode, useCallback } from "react"
-import { SnackbarContent, useSnackbar, VariantType } from "notistack"
-import Typography from "@mui/material/Typography"
-import Card from "@mui/material/Card"
-import CardActions from "@mui/material/CardActions"
-import IconButton from "@mui/material/IconButton"
 import CloseIcon from "@mui/icons-material/Close"
-import { sxStyles } from "../../../types/commonTypes"
 import { Box } from "@mui/material"
+import Card from "@mui/material/Card"
+import IconButton from "@mui/material/IconButton"
 import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+import { SnackbarContent, useSnackbar, VariantType } from "notistack"
+import { forwardRef, ReactNode, useCallback } from "react"
+import { sxStyles } from "../../../types/commonTypes"
 
 const styles = sxStyles({
    root: {
@@ -18,23 +17,26 @@ const styles = sxStyles({
    },
    card: {
       width: "100%",
-      p: 1,
+      p: 2,
    },
    actionRoot: {
       padding: [1, 1, 1, 2],
       justifyContent: "space-between",
    },
    icons: {
-      marginLeft: "auto",
-      alignSelf: "flex-start",
+      position: "absolute",
+      right: 0,
+      top: 0,
+      p: 2,
    },
    expand: {
-      padding: [1, 1],
+      p: 1,
+      m: -1,
    },
 })
 
 interface ReportCompleteProps {
-   message: string | ReactNode
+   content: string | ReactNode
    title: string | ReactNode
    id: string | number
    variant?: VariantType
@@ -51,30 +53,29 @@ const CustomNotification = forwardRef<HTMLDivElement, ReportCompleteProps>(
       return (
          <Box component={SnackbarContent} ref={ref} sx={styles.root}>
             <Card sx={styles.card}>
-               <CardActions sx={styles.actionRoot}>
-                  <Stack spacing={1.5}>
-                     {props.title ? (
-                        <Typography
-                           fontSize="1.42rem"
-                           fontWeight={700}
-                           variant="h4"
-                           color={getColor(variant)}
-                        >
-                           {props.title}
-                        </Typography>
-                     ) : null}
-                     <Typography variant="body1">{props.message}</Typography>
-                  </Stack>
-                  <Box sx={styles.icons}>
-                     <IconButton
-                        size="small"
-                        sx={styles.expand}
-                        onClick={handleDismiss}
+               <Stack spacing={1.5}>
+                  {props.title ? (
+                     <Typography
+                        fontWeight={700}
+                        variant="brandedH5"
+                        color={getColor(variant)}
                      >
-                        <CloseIcon fontSize="small" />
-                     </IconButton>
-                  </Box>
-               </CardActions>
+                        {props.title}
+                     </Typography>
+                  ) : null}
+                  {Boolean(props.content) && (
+                     <Typography variant="small">{props.content}</Typography>
+                  )}
+               </Stack>
+               <Box sx={styles.icons}>
+                  <IconButton
+                     size="small"
+                     sx={styles.expand}
+                     onClick={handleDismiss}
+                  >
+                     <CloseIcon fontSize="small" />
+                  </IconButton>
+               </Box>
             </Card>
          </Box>
       )
@@ -84,7 +85,7 @@ const CustomNotification = forwardRef<HTMLDivElement, ReportCompleteProps>(
 const getColor = (variant: VariantType) => {
    switch (variant) {
       case "success":
-         return "success.main"
+         return "primary.main"
       case "error":
          return "error.main"
       case "warning":
