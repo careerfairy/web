448a1afdbaaa9b38fef8c9357db62212b4a26a90
habibkadiri <kadirit@hotmail.com>
2024-05-08 14:25:11 +0200

    feat(streaming): Add notification badges and improve UX for hand raises
    Enhance the streaming page's user experience by adding notification badges to the `ActionsSpeedDial` component, which display the number of hand raise notifications. Refactor `UserDetails` to accept a dynamic text color, and update the hand raise UI in `HandRaiseCard` to reflect different states such as connecting and connected, with appropriate background colors. Additionally, introduce live updates for the time elapsed since a hand raise request was made.

--- apps/web/components/views/streaming-page/components/hand-raise/HandRaiseCard.tsx
+++ apps/web/components/views/streaming-page/components/hand-raise/HandRaiseCard.tsx
@@ -6,8 +6,12 @@ import {
 import { LoadingButton } from "@mui/lab"
 import { Stack, Typography } from "@mui/material"
 import { useUpdateUserHandRaiseState } from "components/custom-hook/streaming/hand-raise/useUpdateUserHandRaiseState"
+import { useEffect, useState } from "react"
 import { sxStyles } from "types/commonTypes"
+import DateUtil from "util/DateUtil"
 import { useStreamingContext } from "../../context"
+import { UserType } from "../../util"
+import { UserDetails } from "../UserDetails"
 
 const styles = sxStyles({
    root: {
@@ -15,6 +19,18 @@ const styles = sxStyles({
       p: 2,
       border: "1px solid",
       borderColor: "divider",
+      backgroundColor: "transparent",
+      transition: (theme) => theme.transitions.create("background-color"),
+   },
+   greenBackground: {
+      backgroundColor: "primary.main",
+      color: "white",
+   },
+   removeButton: {
+      backgroundColor: (theme) => theme.brand.white[50],
+      "&:hover": {
+         backgroundColor: (theme) => theme.brand.white[100],
+      },
    },
 })
 
@@ -30,9 +46,42 @@ export const HandRaiseCard = ({ handRaise }: Props) => {
       isMutating: isUpdatingUserHandRaiseState,
    } = useUpdateUserHandRaiseState(livestreamId)
 
+   const timeSinceHandRaise = useTimeSinceHandRaise(handRaise)
+
+   const isGreenBackground =
+      handRaise.state === HandRaiseState.connecting ||
+      handRaise.state === HandRaiseState.connected
+
    return (
-      <Stack sx={styles.root} spacing={1} p={1} key={handRaise.id}>
-         <Typography>{handRaise.name}</Typography>
+      <Stack
+         sx={[styles.root, isGreenBackground && styles.greenBackground]}
+         spacing={3}
+         p={1}
+         key={handRaise.id}
+      >
+         <Stack spacing={1.5}>
+            {handRaise.state === HandRaiseState.requested && (
+               <Typography color="primary" variant="small">
+                  Raised hands {timeSinceHandRaise}
+               </Typography>
+            )}
+            {handRaise.state === HandRaiseState.invited && (
+               <Typography color="primary" variant="small">
+                  Invited to panel
+               </Typography>
+            )}
+            {handRaise.state === HandRaiseState.connecting && (
+               <Typography variant="small">Connecting to the panel</Typography>
+            )}
+            {handRaise.state === HandRaiseState.connected && (
+               <Typography variant="small">Currently participating</Typography>
+            )}
+            <UserDetails
+               displayName={handRaise.name}
+               userType={UserType.Viewer}
+               color={isGreenBackground ? "white" : undefined}
+            />
+         </Stack>
          {isUserCanJoinPanel(handRaise) ? (
             <LoadingButton
                color="error"
@@ -44,6 +93,7 @@ export const HandRaiseCard = ({ handRaise }: Props) => {
                   })
                }
                loading={isUpdatingUserHandRaiseState}
+               sx={styles.removeButton}
             >
                Remove participant
             </LoadingButton>
@@ -71,14 +121,38 @@ export const HandRaiseCard = ({ handRaise }: Props) => {
                         handRaiseId: handRaise.id,
                      })
                   }
+                  disabled={handRaise.state === HandRaiseState.invited}
                   loading={isUpdatingUserHandRaiseState}
                   color="primary"
                   variant="contained"
                >
-                  Approve
+                  {handRaise.state === HandRaiseState.invited
+                     ? "Invited"
+                     : "Approve"}
                </LoadingButton>
             </Stack>
          )}
       </Stack>
    )
+}
+
+const useTimeSinceHandRaise = (entry: HandRaise) => {
+   const [timeSince, setTimeSince] = useState(() =>
+      DateUtil.getTimeAgo(entry.timeStamp.toDate())
+   )
+
+   useEffect(() => {
+      // Function to periodically refresh the "time since" string
+      const refreshTimeSince = () => {
+         setTimeSince(DateUtil.getTimeAgo(entry.timeStamp.toDate()))
+      }
+
+      // Refresh the "time since" every 15 seconds
+      const interval = setInterval(refreshTimeSince, 15000)
+
+      // Clear the interval when the component unmounts
+      return () => clearInterval(interval)
+   }, [entry.timeStamp])
+
+   return timeSince
 }
