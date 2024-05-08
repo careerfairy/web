bd3e38081c6e4f27ffe024266ae46aaae622394d
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-05-08 09:33:22 +0100

    feat(MultiChipSelect): adds support for "select all"

--- apps/web/components/views/group/admin/events/detail/form/views/general/components/MultiChipSelect.tsx
+++ apps/web/components/views/group/admin/events/detail/form/views/general/components/MultiChipSelect.tsx
@@ -1,12 +1,26 @@
-import { AutocompleteProps } from "@mui/material"
+import {
+   AutocompleteInputChangeReason,
+   AutocompleteProps,
+   ListItemText,
+   MenuItem,
+} from "@mui/material"
 import BaseStyles from "components/views/admin/company-information/BaseStyles"
 import {
    BrandedAutocompleteProps,
    FormBrandedAutocomplete,
 } from "components/views/common/inputs/BrandedAutocomplete"
+import BrandedCheckbox from "components/views/common/inputs/BrandedCheckbox"
 import { BrandedTextFieldProps } from "components/views/common/inputs/BrandedTextField"
 import { useFormikContext } from "formik"
+import {
+   SyntheticEvent,
+   useCallback,
+   useEffect,
+   useMemo,
+   useState,
+} from "react"
 import { sxStyles } from "types/commonTypes"
+import { SELECT_ALL_ID } from "../../../commons"
 
 const styles = sxStyles({
    input: {
@@ -17,23 +31,43 @@ const styles = sxStyles({
          padding: "0 !important",
       },
    },
+   selectAll: {
+      menuItem: {
+         "&[aria-selected='true']": {
+            backgroundColor: "#FAFAFA !important",
+         },
+      },
+      text: {
+         padding: "16px",
+      },
+   },
 })
 
 type MultiChipSelectProps = {
    keyOptionIndexer?: string
    textFieldProps?: BrandedTextFieldProps
+   selectAllFieldLabel?: string
 } & BrandedAutocompleteProps &
    Pick<AutocompleteProps<unknown, boolean, boolean, boolean>, "renderTags">
 
 const MultiChipSelect = ({
    keyOptionIndexer,
    textFieldProps,
+   selectAllFieldLabel,
    ...props
 }: MultiChipSelectProps) => {
-   const { id, value, multiple } = props
+   const { id, value, options, multiple } = props
 
    const { label, required, placeholder } = textFieldProps
    const { validateField, setFieldValue } = useFormikContext()
+   const [selectedAll, setSelectedAll] = useState(false)
+
+   const selectAllOption = useMemo(() => {
+      return {
+         id: SELECT_ALL_ID,
+         name: selectAllFieldLabel,
+      }
+   }, [selectAllFieldLabel])
 
    const showPlaceholder = !multiple || value?.length === 0
 
@@ -43,12 +77,76 @@ const MultiChipSelect = ({
    }
 
    const onChangeHandler = async (_, selectedOption) => {
-      const newValue = keyOptionIndexer
-         ? selectedOption?.[keyOptionIndexer]
-         : selectedOption
-      await setFieldValue(id, newValue)
+      if (
+         Array.isArray(selectedOption) &&
+         selectedOption.some((option) => option.id === selectAllOption.id)
+      ) {
+         setSelectedAll(false)
+         const filteredOptions = selectedOption.filter(
+            (option) => option.id !== selectAllOption.id
+         )
+         const newValue = keyOptionIndexer
+            ? filteredOptions.map((option) => option[keyOptionIndexer])
+            : filteredOptions
+         await setFieldValue(id, newValue)
+      } else {
+         const newValue = keyOptionIndexer
+            ? selectedOption?.[keyOptionIndexer]
+            : selectedOption
+         await setFieldValue(id, newValue)
+      }
       await validateField(id)
    }
+
+   const onChangeSelectAll = useCallback(async () => {
+      setSelectedAll((currentSelectedAll) => {
+         const newSelectedAll = !currentSelectedAll
+         const newValue = newSelectedAll ? [selectAllOption] : []
+         setFieldValue(id, newValue)
+         return newSelectedAll
+      })
+   }, [id, selectAllOption, setFieldValue])
+
+   const onInputChangeHandler = useCallback(
+      (
+         event: SyntheticEvent,
+         value: string,
+         reason: AutocompleteInputChangeReason
+      ) => {
+         if (reason === "clear") {
+            setSelectedAll(false)
+         }
+      },
+      []
+   )
+
+   const selectAllOptionComponent = useMemo(
+      () =>
+         selectAllFieldLabel ? (
+            <MenuItem
+               key={`${id}-select-all`}
+               sx={styles.selectAll.menuItem}
+               onMouseDown={onChangeSelectAll}
+            >
+               <ListItemText
+                  key={`${id}-select-all-text`}
+                  primary={selectAllFieldLabel}
+                  sx={styles.selectAll.text}
+               />
+               <BrandedCheckbox checked={selectedAll} />
+            </MenuItem>
+         ) : null,
+      [id, onChangeSelectAll, selectAllFieldLabel, selectedAll]
+   )
+
+   useEffect(() => {
+      if (!selectAllFieldLabel) return
+
+      if (value.length > 0 && value.length == options.length) {
+         setFieldValue(id, [selectAllOption])
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [options, setFieldValue, value])
 
    return (
       <FormBrandedAutocomplete
@@ -65,6 +163,8 @@ const MultiChipSelect = ({
             sx: styles.input,
          }}
          onChange={onChangeHandler}
+         initialOptionSection={selectAllOptionComponent}
+         onInputChange={onInputChangeHandler}
          {...props}
       />
    )
