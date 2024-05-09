3ec976068ac8bf06e151dda34f87a86cda9acdde
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-05-09 17:04:57 +0100

    Beef: Thomas x Lindner

--- packages/functions/src/lib/ManualTemplatedEmailService.ts
+++ packages/functions/src/lib/ManualTemplatedEmailService.ts
@@ -1,10 +1,8 @@
 import { convertDocArrayToDict } from "@careerfairy/shared-lib/BaseFirebaseRepository"
 import { UserData } from "@careerfairy/shared-lib/users"
 import { Logger } from "@careerfairy/shared-lib/utils/types"
-import { IUserFunctionsRepository } from "./UserFunctionsRepository"
 import { ManualTemplatedEmailBuilder } from "./ManualTemplatedEmailBuilder"
-import { ILivestreamFunctionsRepository } from "./LivestreamFunctionsRepository"
-import { removeDuplicates } from "@careerfairy/shared-lib/utils"
+import { IUserFunctionsRepository } from "./UserFunctionsRepository"
 
 /**
  * Gathers all the required data to build the release email
@@ -17,66 +15,30 @@ export class ManualTemplatedEmailService {
 
    constructor(
       private readonly userRepo: IUserFunctionsRepository,
-      private readonly livestreamRepo: ILivestreamFunctionsRepository,
+      // private readonly livestreamRepo: ILivestreamFunctionsRepository,
       private readonly emailBuilder: ManualTemplatedEmailBuilder,
       private readonly logger: Logger
    ) {}
 
    /**
-    * Fetches the required data for generating the email
+    * Fetches the required data for generating the email.
     */
    async fetchRequiredData(overrideUsers: string[]) {
       // start fetching
-      const ABB_LIVESTREAM_ID = "WWV4LsLI1Ec0dQjiiUyn" // NESTLE Testing livestream -> "Bdx3RrkeYI2XFqEm6j7L"
-      const ABB_GROUP_ID = "lgHmyR0XipDBcYm0UPtH" // NESTLE Testing group -> "CDsb5v08tGhFikXvGDw4"
 
-      const fetchedUsers = await this.userRepo.getSubscribedUsers(overrideUsers)
+      const fetchedUsers =
+         await this.userRepo.getAllSubscribedUsersFromUniversityCountry(
+            overrideUsers,
+            "DE"
+         )
       const subscribedUsers: UserData[] = fetchedUsers ? fetchedUsers : []
 
-      // AAB Livestream
-      const livestreams = await this.livestreamRepo.getLivestreamsByIds([
-         ABB_LIVESTREAM_ID,
-      ])
-
-      const aabPastLivestreams = await this.livestreamRepo.getEventsOfGroup(
-         ABB_GROUP_ID,
-         "past"
-      )
-      const aabUpcomingLivestreams = await this.livestreamRepo.getEventsOfGroup(
-         ABB_GROUP_ID,
-         "upcoming"
-      )
-
-      const aabTalentPool = removeDuplicates(
-         aabPastLivestreams
-            .flatMap((stream) => stream.talentPool)
-            .concat(
-               aabUpcomingLivestreams.flatMap((stream) => stream.talentPool)
-            )
-      )
-
-      if (!livestreams || !livestreams.length) {
-         this.logger.error("Could not retrieve AAB livestream by ID")
-         return this
-      }
-
-      const aabLivestream = livestreams.at(0)
-
-      // Filter: User is in ABB talent pool AND
-      const filteredRegisteredUsers = subscribedUsers.filter((user) => {
-         const isUserRegistered = aabLivestream.registeredUsers.includes(
-            user.id
-         )
-         const isUserInTalentPool = aabTalentPool.includes(user.id)
-         return !isUserRegistered && isUserInTalentPool
-      })
-
       this.subscribedUsers = convertDocArrayToDict(
-         filteredRegisteredUsers as UserData[]
+         subscribedUsers as UserData[]
       )
 
       this.logger.info(
-         "Total Users subscribed to the release email for AAB Talent Pool communication",
+         "Total German student Users subscribed to the resolution email",
          Object.keys(this.subscribedUsers).length
       )
 
@@ -84,7 +46,7 @@ export class ManualTemplatedEmailService {
    }
 
    /**
-    * Sends the Spark Release Email to the subscribed users
+    * Sends the email
     *
     * Possibility of overriding the users to send the release email to
     * for testing purposes
