433956fc8098afa8ec8a66ff5ace83f094b5cd6e
Habib Kadiri <habib@careerfairy.io>
2024-05-13 10:54:04 +0200

    feat(_document): Implement GTM script and default consent mode (#1104)
    Add separate scripts for Google Tag Manager (GTM) and default consent configuration to enhance user privacy control and compliance with data protection regulations.

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -1,15 +1,15 @@
-import * as React from "react"
-import Document, {
-   Html,
-   Head,
-   Main,
-   NextScript,
-   DocumentContext,
-   DocumentInitialProps,
-} from "next/document"
 import createEmotionServer from "@emotion/server/create-instance"
 import { brandedLightTheme, poppins } from "materialUI"
 import createEmotionCache from "materialUI/createEmotionCache"
+import Document, {
+   DocumentContext,
+   DocumentInitialProps,
+   Head,
+   Html,
+   Main,
+   NextScript,
+} from "next/document"
+import * as React from "react"
 import { isEmbedded, isGroupAdminPath, isStreamingPath } from "util/PathUtils"
 
 interface DocumentProps extends DocumentInitialProps {
@@ -21,6 +21,35 @@ interface DocumentProps extends DocumentInitialProps {
 }
 
 export default function MyDocument(props: DocumentProps) {
+   const consentModeScript = `
+   window.dataLayer = window.dataLayer || [];
+   function gtag() { dataLayer.push(arguments); }
+   gtag('consent', 'default', {
+       ad_user_data: 'denied',
+       ad_personalization: 'denied',
+       ad_storage: 'denied',
+       analytics_storage: 'denied',
+       wait_for_update: 2000
+   });
+   gtag('set', 'ads_data_redaction', true);
+`
+
+   const gtmScript = `
+   ;(function(w, d, s, l, i) {
+       w[l] = w[l] || [];
+       w[l].push({
+           'gtm.start': new Date().getTime(),
+           event: 'gtm.js'
+       });
+       var f = d.getElementsByTagName(s)[0],
+           j = d.createElement(s),
+           dl = l != 'dataLayer' ? '&l=' + l : '';
+       j.async = true;
+       j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+       f.parentNode.insertBefore(j, f);
+   })(window, document, 'script', 'dataLayer', 'GTM-P29VCWC')
+`
+
    return (
       <Html lang="en" className={poppins.className}>
          <Head>
@@ -57,25 +86,21 @@ export default function MyDocument(props: DocumentProps) {
 
             {props.shouldRunUsercentrics && props.shouldRunGTM ? (
                // eslint-disable-next-line @next/next/next-script-for-ga
-               <script
-                  id="google-analytics"
-                  type="text/plain"
-                  data-usercentrics="Google Tag Manager"
-                  dangerouslySetInnerHTML={{
-                     __html: `
-;(function (w, d, s, l, i) {
-   w[l] = w[l] || []
-   w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
-   var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l != "dataLayer" ? "&l=" + l : ""
-   j.async = true
-   j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
-   f.parentNode.insertBefore(j, f)
-})(window, document, "script", "dataLayer", "GTM-P29VCWC")
-                  `,
-                  }}
-               ></script>
+               <>
+                  <script
+                     id="google-analytics"
+                     type="text/plain"
+                     data-usercentrics="Google Tag Manager"
+                     dangerouslySetInnerHTML={{
+                        __html: gtmScript,
+                     }}
+                  ></script>
+                  <script
+                     dangerouslySetInnerHTML={{
+                        __html: consentModeScript,
+                     }}
+                  ></script>
+               </>
             ) : null}
             <link
                rel="preload"
