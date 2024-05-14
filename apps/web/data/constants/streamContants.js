6b7d0504ef1cb8012b8f93ca01bd5701d7e9b0b4
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-05-14 10:15:36 +0100

    chore(data): removes legacy live streams constant

--- apps/web/data/constants/streamContants.js
+++ apps/web/data/constants/streamContants.js
@@ -1,4 +1,3 @@
 export const START_DATE_FOR_REPORTED_EVENTS = "April 1, 2020 00:00:00"
-export const FORTY_FIVE_MINUTES_IN_MILLISECONDS = 1000 * 60 * 45
 export const PAST_LIVESTREAMS_NAME = "pastLivestreams"
 export const UPCOMING_LIVESTREAMS_NAME = "upcomingLivestreams"
