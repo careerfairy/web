375f31e3e805f0a1cacb8113d5f3081aea714e4c
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-05-15 11:56:39 +0100

    feat(LivestreamForm): adapts form to handle preview

--- apps/web/components/views/group/admin/events/detail/form/LivestreamForm.tsx
+++ apps/web/components/views/group/admin/events/detail/form/LivestreamForm.tsx
@@ -1,16 +1,29 @@
 import { sxStyles } from "@careerfairy/shared-ui"
-import { Stack } from "@mui/material"
+import { Box, Stack } from "@mui/material"
 import { Form } from "formik"
 import { useLivestreamCreationContext } from "../LivestreamCreationContext"
 import { TAB_VALUES } from "./commons"
 import LivestreamFormGeneralStep from "./views/general"
 import LivestreamFormJobsStep from "./views/jobs"
+import Preview from "./views/preview/Preview"
 import LivestreamFormQuestionsStep from "./views/questions"
 import LivestreamFormSpeakersStep from "./views/speakers"
 
 const styles = sxStyles({
    root: {
+      display: "flex",
+      flexDirection: "row",
+      columnGap: "24px",
+   },
+   tabs: {
+      flexBasis: "61%",
       padding: "24px",
+   },
+   preview: {
+      flexBasis: "39%",
+      marginTop: "10px",
+      backgroundColor: "#F7F8FC",
+      borderTopLeftRadius: "12px",
    },
 })
 
@@ -18,8 +31,8 @@ const LivestreamForm = () => {
    const { tabValue } = useLivestreamCreationContext()
 
    return (
-      <Form>
-         <Stack sx={styles.root} rowGap={2}>
+      <Form style={styles.root}>
+         <Stack sx={styles.tabs} rowGap={2}>
             {tabValue === TAB_VALUES.GENERAL && <LivestreamFormGeneralStep />}
             {tabValue == TAB_VALUES.SPEAKERS && <LivestreamFormSpeakersStep />}
             {tabValue === TAB_VALUES.QUESTIONS && (
@@ -27,6 +40,9 @@ const LivestreamForm = () => {
             )}
             {tabValue === TAB_VALUES.JOBS && <LivestreamFormJobsStep />}
          </Stack>
+         <Box sx={styles.preview}>
+            <Preview />
+         </Box>
       </Form>
    )
 }
