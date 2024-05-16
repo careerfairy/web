bff8fe5d083404035afe0b24d4695089e2232da5
habibkadiri <kadirit@hotmail.com>
2024-05-16 14:21:43 +0200

    feat(streaming): Implement sharing YouTube videos for livestreams
    Replace placeholder functionality with a new form for sharing YouTube links in livestreams. Remove unimplemented alert and refactor to improve both desktop and mobile user experiences. Utilize a new `ShareYoutubeVideoForm` component for consistent video sharing across devices, and integrate backend support for setting and updating YouTube video details in the livestream.

--- apps/web/components/views/streaming-page/components/StreamingGrid/Spotlight/youtube/ShareYoutubeVideoDialog.tsx
+++ apps/web/components/views/streaming-page/components/StreamingGrid/Spotlight/youtube/ShareYoutubeVideoDialog.tsx
@@ -1,25 +1,19 @@
-import { LoadingButton } from "@mui/lab"
-import {
-   Box,
-   Dialog,
-   DialogActions,
-   Stack,
-   SwipeableDrawer,
-   Typography,
-} from "@mui/material"
+import { Dialog, SwipeableDrawer } from "@mui/material"
 import { useAppDispatch } from "components/custom-hook/store"
 import { useStreamIsMobile } from "components/custom-hook/streaming"
-import { useStreamingContext } from "components/views/streaming-page/context"
 import { Youtube } from "react-feather"
 import { setShareYoutubeVideoDialogOpen } from "store/reducers/streamingAppReducer"
 import { useShareYoutubeVideoDialogOpen } from "store/selectors/streamingAppSelectors"
 import { sxStyles } from "types/commonTypes"
+import { SidePanelView } from "../../../SidePanel/SidePanelView"
+import { ShareYoutubeVideoForm } from "./ShareYoutubeVideoForm"
 
 const styles = sxStyles({
    dialog: {
       "& .MuiDialog-paper": {
          minWidth: 589,
          maxWidth: 589,
+         p: 4,
       },
    },
    drawer: {
@@ -29,45 +23,16 @@ const styles = sxStyles({
          maxHeight: "calc(100vh - 40px)",
       },
    },
-   container: {
-      p: { xs: 2, md: 4 },
-      position: "relative",
-   },
-   header: {
-      mb: 4,
-   },
-   actions: {
-      mt: 4,
-      position: "sticky",
-      bottom: 0,
-      "& button": {
-         width: 150,
-      },
-   },
-   actionsMobile: {
-      backgroundColor: "white",
-      borderTop: "1px solid",
-      borderColor: "neutral.50",
-      mx: -2,
-      mb: -2,
-   },
-   icon: {
-      width: 84,
-      height: 84,
-      color: "primary.main",
-   },
-   loader: {
-      m: "auto",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
+   drawerContent: {
+      p: 2,
+      pt: 2.5,
    },
 })
 
 export const ShareYoutubeVideoDialog = () => {
    const isMobile = useStreamIsMobile()
 
-   const settingsMenuOpen = useShareYoutubeVideoDialogOpen()
+   const open = useShareYoutubeVideoDialogOpen()
 
    const dispatch = useAppDispatch()
 
@@ -78,13 +43,20 @@ export const ShareYoutubeVideoDialog = () => {
    if (isMobile) {
       return (
          <SwipeableDrawer
-            open={settingsMenuOpen}
+            open={open}
             sx={styles.drawer}
             anchor="bottom"
             onClose={closeMenu}
             onOpen={closeMenu}
          >
-            <Content isMobile={isMobile} onClose={closeMenu} />
+            <SidePanelView
+               contentWrapperStyles={styles.drawerContent}
+               id="share-youtube-drawer"
+               title="Share video"
+               icon={<Youtube />}
+            >
+               <ShareYoutubeVideoForm isMobile={isMobile} onClose={closeMenu} />
+            </SidePanelView>
          </SwipeableDrawer>
       )
    }
@@ -94,76 +66,10 @@ export const ShareYoutubeVideoDialog = () => {
          maxWidth={false}
          sx={styles.dialog}
          onClose={closeMenu}
-         open={settingsMenuOpen}
+         open={open}
          TransitionProps={{ unmountOnExit: true }}
       >
-         <Content isMobile={isMobile} onClose={closeMenu} />
+         <ShareYoutubeVideoForm isMobile={isMobile} onClose={closeMenu} />
       </Dialog>
-   )
-}
-
-type ContentProps = {
-   onClose: () => void
-   isMobile: boolean
-}
-
-const Content = ({ onClose, isMobile }: ContentProps) => {
-   const { livestreamId } = useStreamingContext()
-   console.log(
-      "🚀 ~ file: ShareYoutubeVideoDialog.tsx:112 ~ Content ~ livestreamId:",
-      livestreamId
-   )
-
-   return (
-      <Box sx={styles.container}>
-         <Stack
-            sx={styles.header}
-            spacing={1}
-            alignItems="center"
-            justifyContent="center"
-         >
-            <Box component={Youtube} sx={styles.icon} />
-            <Stack spacing={0.75} justifyContent="center" alignItems="center">
-               <Typography
-                  variant="desktopBrandedH3"
-                  fontWeight={700}
-                  color="neutral.800"
-               >
-                  Share a video
-               </Typography>
-
-               <Typography
-                  variant="medium"
-                  color="neutral.700"
-                  textAlign="center"
-               >
-                  {`When sharing a video, the player actions (play, pause, etc) will be replayed
-on the viewer's screens as well.`}
-               </Typography>
-            </Stack>
-         </Stack>
-         <Stack
-            component={DialogActions}
-            sx={[styles.actions, isMobile && styles.actionsMobile]}
-            justifyContent={isMobile ? "flex-end" : "center"}
-            direction="row"
-            spacing={1.25}
-         >
-            <LoadingButton onClick={onClose} variant="outlined" color="grey">
-               Cancel
-            </LoadingButton>
-            <LoadingButton
-               variant="contained"
-               color="primary"
-               onClick={
-                  () => alert("Share video")
-                  // share video
-                  // close dialog
-               }
-            >
-               Share video
-            </LoadingButton>
-         </Stack>
-      </Box>
    )
 }
--- packages/shared-lib/src/livestreams/livestreams.ts
+++ packages/shared-lib/src/livestreams/livestreams.ts
@@ -763,3 +763,16 @@ export interface LivestreamPresentation extends Identifiable {
    /** The size of the PDF in bytes */
    fileSize?: number
 }
+
+/**
+ * A YouTube video for a livestream
+ * Document Path: livestreams/{livestreamId}/videos/video
+ */
+export interface LivestreamVideo extends Identifiable {
+   url: string
+   seconds: number
+   state: "playing" | "paused"
+   lastPlayed: firebase.firestore.Timestamp
+   /** The ID of the user that can update the video */
+   updater: string
+}
