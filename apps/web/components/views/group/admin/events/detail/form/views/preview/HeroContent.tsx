78f5f5623390bee7033f1e7a8d50ce250be43a41
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-05-16 19:24:08 +0100

    feat: adds HeroContent component

---
+++ apps/web/components/views/group/admin/events/detail/form/views/preview/HeroContent.tsx
@@ -0,0 +1,76 @@
+import { sxStyles } from "@careerfairy/shared-ui"
+import { Box, Container } from "@mui/material"
+import Image from "next/legacy/image"
+import { ReactNode } from "react"
+import BackAndCloseButton from "./BackAndCloseButton"
+
+const styles = sxStyles({
+   heroContent: {
+      color: "white",
+      justifyContent: "center",
+      position: "relative",
+      display: "flex",
+      flexDirection: "column",
+      pt: 6,
+      pb: 2.25,
+      px: 2.25,
+      borderRadius: 3,
+      overflow: "hidden",
+      "& #background-image": {
+         zIndex: -1,
+      },
+      minHeight: {
+         xs: 439,
+         md: 0,
+      },
+   },
+   heroContentContainer: {
+      zIndex: 1,
+   },
+   backgroundImgOverlay: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      background: "black",
+      opacity: 0.65,
+      zIndex: -1,
+   },
+})
+
+type HeroContentProps = {
+   backgroundImage: string
+   children: ReactNode
+   handleCloseDialog?: () => void
+}
+
+const HeroContent = ({
+   backgroundImage,
+   handleCloseDialog,
+   children,
+}: HeroContentProps) => {
+   return (
+      <Box sx={styles.heroContent}>
+         <Container
+            disableGutters
+            sx={styles.heroContentContainer}
+            maxWidth={"md"}
+         >
+            {children}
+         </Container>
+         {backgroundImage ? (
+            <Image
+               alt={"background image"}
+               src={backgroundImage}
+               layout={"fill"}
+               objectFit={"cover"}
+            />
+         ) : null}
+         <Box sx={styles.backgroundImgOverlay} />
+         <BackAndCloseButton handleCloseDialog={handleCloseDialog} />
+      </Box>
+   )
+}
+
+export default HeroContent
