8741ceaa8e520391833e9a9240f61b273c306690
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-05-16 19:19:23 +0100

    feat: adds Speakers component

---
+++ apps/web/components/views/group/admin/events/detail/form/views/preview/Speakers.tsx
@@ -0,0 +1,151 @@
+import { sxStyles } from "@careerfairy/shared-ui"
+import { Typography } from "@mui/material"
+import Box from "@mui/material/Box"
+import Skeleton from "@mui/material/Skeleton"
+import Stack from "@mui/material/Stack"
+import { speakerPlaceholder } from "components/util/constants"
+import HorizontalScroll from "components/views/common/HorizontalScroll"
+import SectionTitle from "components/views/livestream-dialog/views/livestream-details/main-content/SectionTitle"
+import { NICE_SCROLLBAR_STYLES } from "constants/layout"
+import Image from "next/legacy/image"
+import { FC } from "react"
+import { LivestreamFormSpeaker } from "../../types"
+
+const styles = sxStyles({
+   root: {
+      ...NICE_SCROLLBAR_STYLES,
+   },
+   speakerAvatar: {
+      "& .avatar": {
+         borderRadius: "50%",
+         minWidth: "56px !important",
+         minHeight: "56px !important",
+      },
+   },
+   displayName: {
+      fontSize: "1.14rem",
+   },
+   position: {
+      color: "text.secondary",
+      fontSize: "1rem",
+   },
+   speakersWrapper: {
+      overflowX: "auto",
+      flexWrap: "nowrap",
+      display: "flex",
+      flexDirection: "row",
+      "& > *": {
+         "&:not(:last-child)": {
+            mr: 3,
+         },
+      },
+   },
+})
+
+type SpeakersProps = {
+   speakers?: LivestreamFormSpeaker[]
+}
+
+const Speakers = ({ speakers }: SpeakersProps) => {
+   if (!speakers) {
+      return null
+   }
+
+   return (
+      <Box sx={styles.root}>
+         <SectionTitle>Speakers</SectionTitle>
+         <HorizontalScroll sx={styles.speakersWrapper}>
+            {speakers.map((speaker) => (
+               <SpeakerAvatar key={speaker.id} speaker={speaker} />
+            ))}
+         </HorizontalScroll>
+      </Box>
+   )
+}
+
+type SpeakerAvatarProps = {
+   speaker: LivestreamFormSpeaker
+}
+
+const SpeakerAvatar = ({ speaker }: SpeakerAvatarProps) => {
+   const displayName = `${speaker.firstName ?? ""} ${speaker.lastName ?? ""}`
+
+   return (
+      <Stack spacing={0.75} direction="row" sx={styles.speakerAvatar}>
+         <Box minWidth={56} minHeight={56}>
+            <Image
+               className="avatar"
+               width={56}
+               height={56}
+               src={speaker.avatarUrl}
+               objectFit="cover"
+               alt={displayName}
+            />
+         </Box>
+         <Stack>
+            <Typography
+               sx={styles.displayName}
+               whiteSpace="nowrap"
+               variant="h6"
+            >
+               {displayName}
+            </Typography>
+            <Typography
+               sx={styles.position}
+               whiteSpace="nowrap"
+               variant="body2"
+            >
+               {speaker.position}
+            </Typography>
+         </Stack>
+      </Stack>
+   )
+}
+
+const SpeakerAvatarSkeleton: FC = () => {
+   return (
+      <Stack spacing={0.75} direction="row" sx={styles.speakerAvatar}>
+         <Box minWidth={56} minHeight={56}>
+            <Image
+               className="avatar"
+               width={56}
+               height={56}
+               src={speakerPlaceholder}
+               objectFit="cover"
+               alt={"Speaker Placeholder"}
+            />
+         </Box>
+         <Stack>
+            <Typography
+               sx={styles.displayName}
+               whiteSpace="nowrap"
+               variant="h6"
+            >
+               <Skeleton width={160} />
+            </Typography>
+            <Typography
+               sx={styles.position}
+               whiteSpace="nowrap"
+               variant="body2"
+            >
+               <Skeleton width={100} />
+            </Typography>
+         </Stack>
+      </Stack>
+   )
+}
+
+export const SpeakersSkeleton: FC = () => {
+   return (
+      <Box sx={styles.root}>
+         <SectionTitle>Speakers</SectionTitle>
+         <Stack sx={styles.speakersWrapper} direction="row" spacing={3}>
+            {Array.from({ length: 3 }).map((_, i) => (
+               <SpeakerAvatarSkeleton key={i} />
+            ))}
+         </Stack>
+      </Box>
+   )
+}
+
+export default Speakers
