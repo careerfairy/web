0ea1f5d8f4cc65ad847006f0abd318a3d4b3a847
goncaloarnauth <goncaloarnauth@outlook.com>
2024-05-17 12:42:07 +0100

    to print IP address

--- apps/web/pages/sparks/index.tsx
+++ apps/web/pages/sparks/index.tsx
@@ -11,36 +11,51 @@ export default function Sparks() {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-   const { lookup } = await import("geoip-lite")
-   const ipAddress =
-      context.req.headers["x-forwarded-for"] || context.req.socket.remoteAddress
+   // const { lookup } = await import("geoip-lite")
+   const { req, query } = context
+
+   // default to using req.socket.remoteAddress
+   let ipAddress = req.socket.remoteAddress
+
+   if (req.headers["x-forwarded-for"]) {
+      const forwarded = req.headers["x-forwarded-for"]
+      if (Array.isArray(forwarded)) {
+         ipAddress = forwarded[0].split(",")[0].trim()
+      } else {
+         ipAddress = forwarded.split(",")[0].trim()
+      }
+   }
+
+   // const ipAddress =
+   // context.req.headers["x-forwarded-for"] || context.req.connection.remoteAddress || context.req.socket.remoteAddress
+
+   console.log(
+      "🚀 ~ constgetServerSideProps:GetServerSideProps= ~ ipAddress:",
+      ipAddress
+   )
 
    // Use geoip-lite to get geolocation data based on the IP address
-   const geo = lookup(ipAddress as string)
+   // const geo = lookup(ipAddress as string)
 
-   const anonymousUserCountryCode = geo ? geo.country : null
-   console.log(
-      "🚀 ~ constgetServerSideProps:GetServerSideProps= ~ anonymousUserCountryCode:",
-      anonymousUserCountryCode
-   )
+   // const anonymousUserCountryCode = geo ? geo.country : null
+   // console.log(
+   //    "🚀 ~ constgetServerSideProps:GetServerSideProps= ~ anonymousUserCountryCode:",
+   //    anonymousUserCountryCode
+   // )
 
    const sparks = await sparkService.fetchNextSparks(null, {
       numberOfSparks: 1,
       userId: null,
-      anonymousUserCountryCode,
+      anonymousUserCountryCode: "AL",
    })
-   console.log(
-      "🚀 ~ constgetServerSideProps:GetServerSideProps= ~ sparks length:",
-      sparks?.length
-   )
 
-   const queryParamString = encode(context.query)
+   const queryParamString = encode(query)
 
    if (sparks.length > 0) {
       return {
          redirect: {
             destination: `/sparks/${sparks[0].id}${
-               queryParamString && `?${queryParamString}`
+               queryParamString && `?${queryParamString}&ipAddress=${ipAddress}`
             }`,
             permanent: false,
          },
