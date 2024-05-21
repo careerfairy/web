b172a929deab2edae587ef3914caeb7243e81e89
matilderamosCF <matilde.ramos@careerfairy.io>
2024-05-21 18:21:52 +0100

    send email reminder button

--- apps/web/components/custom-hook/streaming/useUserLivestreamData.ts
+++ apps/web/components/custom-hook/streaming/useUserLivestreamData.ts
@@ -3,16 +3,17 @@ import { ReactFireOptions } from "reactfire"
 import { useFirestoreDocument } from "../utils/useFirestoreDocument"
 
 export const useUserLivestreamData = (
-    livestreamId: string,
-    userId: string,
-    options?: ReactFireOptions
- ) => {
-    return useFirestoreDocument<UserLivestreamData>(
-       "livestreams",
-       [livestreamId, "userLivestreamData", userId],
-       {
-          idField: "id",
-          ...options,
-       }
-    )
- }
+   livestreamId: string,
+   userId: string,
+   options?: ReactFireOptions
+) => {
+   return useFirestoreDocument<UserLivestreamData>(
+      "livestreams",
+      [livestreamId, "userLivestreamData", userId],
+      {
+         idField: "id",
+         suspense: true,
+         ...options,
+      }
+   )
+}
