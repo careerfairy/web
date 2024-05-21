cc95e9e9c095e826154a4b36dfedd5de47e91da6
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-05-21 17:59:34 +0100

    feat: adds commons doc with some constant values used throughout the preview codebase

---
+++ apps/web/components/views/group/admin/events/detail/form/views/preview/commons.ts
@@ -0,0 +1,5 @@
+// Width of the actual Dialog on the portal page
+export const REAL_DIALOG_WIDTH = 915
+export const PREVIEW_COLUMN_WIDTH_PERCENTAGE = ["39%", 0.39] as const
+export const PREVIEW_COLUMN_PADDING_X = 56
+export const PREVIEW_COLUMN_PADDING_Y = 24
