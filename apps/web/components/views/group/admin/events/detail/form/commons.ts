a888f96e2299eb084da65c91f3afe7578857ee69
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-05-23 17:58:49 +0100

    CF-925 Creating new speaker doesn't work (#1128)
    * fix(CreatorFormDialog): handles creation of speaker
    
    * feat: improves linkedIn field placeholder and  validation message
    
    * fix(Speakers): handles persistence of linkedin url

--- apps/web/components/views/group/admin/events/detail/form/commons.ts
+++ apps/web/components/views/group/admin/events/detail/form/commons.ts
@@ -104,6 +104,7 @@ const mapCreatorToSpeaker = (creator: Creator): Speaker => {
       lastName: creator.lastName,
       position: creator.position,
       email: creator.email,
+      linkedInUrl: creator.linkedInUrl,
    }
 }
 
