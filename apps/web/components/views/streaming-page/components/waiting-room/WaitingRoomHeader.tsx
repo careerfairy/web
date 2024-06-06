f7addecf1a772deed417d7fe23fa150fc7a737e2
habibkadiri <kadirit@hotmail.com>
2024-06-06 14:42:59 +0200

    refactor(waiting-room): reorganize waiting room components
    - Moved `CountDown`, `Footer`, `HostDetails`, `StatusTitle`, and `WaitingRoom` components from `viewer/waiting-room` to `waiting-room`.
    - Created `WaitingRoomHeader` component to encapsulate `Header` and `LogoBackButton`.
    - Updated imports and references to the moved components.

---
+++ apps/web/components/views/streaming-page/components/waiting-room/WaitingRoomHeader.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../TopBar/Header"
+import { LogoBackButton } from "../TopBar/LogoBackButton"
+
+export const WaitingRoomHeader = () => {
+   return (
+      <Header>
+         <LogoBackButton />
+      </Header>
+   )
+}
