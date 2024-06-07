b9141d35a086afd82b92fc42d5a1831c342cf3cf
goncaloarnauth <goncaloarnauth@outlook.com>
2024-06-07 19:18:28 +0100

    update functions; update updateCustomJobWithLinkedLivestreams function

--- packages/shared-lib/src/customJobs/CustomJobRepository.ts
+++ packages/shared-lib/src/customJobs/CustomJobRepository.ts
@@ -104,7 +104,7 @@ export interface ICustomJobRepository {
    syncExpiredCustomJobs(): Promise<void>
 
    /**
-    * Get all group custom jobs by group IDs
+    * Get all group custom jobs
     * @param groupId
     */
    getCustomJobsByGroupId(groupId: string): Promise<CustomJob[]>
