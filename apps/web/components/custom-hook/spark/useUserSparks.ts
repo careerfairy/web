31fda0b0908f898b4a6866489f843bba286c4c18
habibkadiri <kadirit@hotmail.com>
2024-06-11 14:05:10 +0200

    feat: add user sparks fetching and carousel components
    - Implemented `useUserSparks` custom hook for fetching user sparks.
    - Enhanced `GenericCarousel` with context and custom hook for EmblaCarousel.
    - Created `Sparks` component for end-of-stream section with responsive carousel.

---
+++ apps/web/components/custom-hook/spark/useUserSparks.ts
@@ -0,0 +1,43 @@
+import { useAuth } from "HOCs/AuthProvider"
+import { sparkService } from "data/firebase/SparksService"
+import useSWR from "swr"
+import { errorLogAndNotify } from "util/CommonUtil"
+import { reducedRemoteCallsOptions } from "../utils/useFunctionsSWRFetcher"
+
+const getKey = (userId: string) =>
+   userId ? `userSparks-${userId}` : "userSparks"
+
+/**
+ * Custom hook to fetch user sparks.
+ * Utilizes SWR for data fetching and caching.
+ *
+ * @returns SWR response object containing user sparks data.
+ */
+export const useUserSparks = () => {
+   const { authenticatedUser } = useAuth()
+
+   const key = authenticatedUser.isLoaded
+      ? getKey(authenticatedUser.email)
+      : null
+
+   return useSWR(
+      key,
+      async () => {
+         const { sparks } = await sparkService.fetchFeed({
+            numberOfSparks: 10,
+            userId: authenticatedUser.email || null,
+         })
+
+         return sparks
+      },
+      {
+         ...reducedRemoteCallsOptions,
+         onError: (error, key) => {
+            errorLogAndNotify(error, {
+               message: "Error fetching user sparks",
+               key,
+            })
+         },
+      }
+   )
+}
