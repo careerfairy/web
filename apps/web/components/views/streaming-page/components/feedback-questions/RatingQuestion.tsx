d4c7d04e7f17338074fc510f423cd6f947f522d6
matilderamosCF <matilde.ramos@careerfairy.io>
2024-06-12 11:37:49 +0100

    log cleanup

--- apps/web/components/views/streaming-page/components/feedback-questions/RatingQuestion.tsx
+++ apps/web/components/views/streaming-page/components/feedback-questions/RatingQuestion.tsx
@@ -1,4 +1,4 @@
-import { Rating, Typography, styled } from "@mui/material"
+import { Rating, Typography } from "@mui/material"
 import Box from "@mui/material/Box"
 import { IconContainerProps, RatingProps } from "@mui/material/Rating"
 import { sxStyles } from "types/commonTypes"
@@ -16,6 +16,36 @@ const styles = sxStyles({
          alignSelf: "stretch",
          gap: "12px",
       },
+      "& .MuiRating-icon": {
+         display: "flex",
+         flexDirection: "column",
+         justifyContent: "center",
+         alignItems: "center",
+         flex: "1 0 0",
+         borderRadius: "4px",
+         padding: "14px",
+         alignSelf: "stretch",
+         fontSize: "14px",
+         lineHeight: "150%" /* 21px */,
+         color: (theme) => theme.palette.neutral[600],
+         background: (theme) => theme.brand.black[400],
+      },
+      "& .MuiRating-iconHover": {
+         color: "white",
+         background: (theme) => theme.palette.primary.main,
+         transform: "none",
+         transition: "none",
+      },
+      "& .MuiRating-label": {
+         display: "none",
+      },
+      "& label": {
+         display: "flex",
+         flexDirection: "column",
+         justifyContent: "center",
+         alignItems: "center",
+         flex: "1 0 0",
+      },
    },
    subText: {
       display: "flex",
@@ -27,39 +57,6 @@ const styles = sxStyles({
    },
 })
 
-const StyledRating = styled(Rating)(({ theme }) => ({
-   "& .MuiRating-icon": {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      flex: "1 0 0",
-      borderRadius: "4px",
-      padding: "14px",
-      alignSelf: "stretch",
-      fontSize: "14px",
-      lineHeight: "150%" /* 21px */,
-      color: theme.palette.neutral[600],
-      background: theme.brand.black[400],
-   },
-   "& .MuiRating-iconHover": {
-      color: "white",
-      background: theme.palette.primary.main,
-      transform: "none",
-      transition: "none",
-   },
-   "& .MuiRating-label": {
-      display: "none",
-   },
-   "& label": {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      flex: "1 0 0",
-   },
-}))
-
 const RatingQuestion = ({
    readOnly,
    onChange,
@@ -69,7 +66,7 @@ const RatingQuestion = ({
 }: RatingProps) => {
    return (
       <Box sx={styles.ratingWrapper}>
-         <StyledRating
+         <Rating
             name={name}
             value={Number(value)}
             size="large"
@@ -77,7 +74,6 @@ const RatingQuestion = ({
             max={5}
             onChange={onChange}
             IconContainerComponent={ButtonContainer}
-            onChangeActive={(event, value) => console.log("active ", value)}
             {...rest}
          />
          <Box sx={styles.subText}>
