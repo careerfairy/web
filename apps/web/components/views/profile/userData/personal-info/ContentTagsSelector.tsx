6ac4eeef481657c25db51395fad9013f77d7521f
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-06-18 23:47:15 +0100

    Refactored inputs TODO: bug when editing missing field on user profile

---
+++ apps/web/components/views/profile/userData/personal-info/ContentTagsSelector.tsx
@@ -0,0 +1,39 @@
+import { OptionGroup } from "@careerfairy/shared-lib/commonTypes"
+import { Stack } from "@mui/material"
+import Selector from "components/views/signup/userInformation/tags/Selector"
+import { userRepo } from "data/RepositoryInstances"
+import { useCallback } from "react"
+import { errorLogAndNotify } from "util/CommonUtil"
+import { mapOptions } from "../../utils"
+
+type ContentTagsSelectorProps = {
+   tags: OptionGroup[]
+   field: "businessFunctionsTagIds" | "contentTopicsTagIds"
+}
+
+export const ContentTagsSelector = (props: ContentTagsSelectorProps) => {
+   const handleSelectedInterestsChange = useCallback(
+      async (userEmail: string, name: string, selectedTags: OptionGroup[]) => {
+         try {
+            return await userRepo.updateAdditionalInformation(userEmail, {
+               [name]: mapOptions(selectedTags),
+            })
+         } catch (error) {
+            errorLogAndNotify(error)
+         }
+      },
+      []
+   )
+
+   return (
+      <>
+         <Stack spacing={1}>
+            <Selector
+               handleInterestTagChange={handleSelectedInterestsChange}
+               tags={props.tags}
+               field={props.field}
+            />
+         </Stack>
+      </>
+   )
+}
