2b6fd89f45db3660be8c87f8ca39b92d2bcc8f06
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-06-18 23:47:15 +0100

    Refactored inputs TODO: bug when editing missing field on user profile

--- apps/web/components/views/signup/steps/InterestsInformation.tsx
+++ apps/web/components/views/signup/steps/InterestsInformation.tsx
@@ -2,37 +2,31 @@ import {
    BusinessFunctionsTagValues,
    ContentTopicsTagValues,
 } from "@careerfairy/shared-lib/constants/tags"
-import { OptionGroup } from "@careerfairy/shared-lib/dist/commonTypes"
+import { sxStyles } from "@careerfairy/shared-ui"
 import { Stack } from "@mui/material"
 import ConditionalWrapper from "components/util/ConditionalWrapper"
-import { userRepo } from "data/RepositoryInstances"
-import { useCallback } from "react"
-import { errorLogAndNotify } from "util/CommonUtil"
 import { useAuth } from "../../../../HOCs/AuthProvider"
-import TagsSelector from "../userInformation/tags/TagsSelector"
-import { mapOptions } from "../utils"
+import { TagsSelector } from "../userInformation/tags/TagsSelector"
 
+const BUSINESS_FUNCTIONS_INTERESTS_LABEL =
+   "What kind of work are you excited about?"
+
+const CONTENT_TOPIC_INTERESTS_LABEL =
+   "Choose the topics that spark your curiosity!"
+
+const styles = sxStyles({
+   label: {
+      color: (theme) => theme.palette.neutral[900],
+      fontFamily: "Poppins",
+      fontSize: "11.2px",
+      fontStyle: "normal",
+      fontWeight: "700",
+      lineHeight: "14.94px",
+      textTransform: "uppercase",
+   },
+})
 const InterestsInformation = () => {
    const { userData } = useAuth()
-
-   const handleSelectedInterestsChange = useCallback(
-      async (userEmail: string, name: string, selectedTags: OptionGroup[]) => {
-         try {
-            return await userRepo.updateAdditionalInformation(userEmail, {
-               [name]: mapOptions(selectedTags),
-            })
-         } catch (error) {
-            errorLogAndNotify(error)
-         }
-      },
-      []
-   )
-
-   const BUSINESS_FUNCTIONS_INTERESTS_LABEL =
-      "What kind of work are you excited about?"
-
-   const CONTENT_TOPIC_INTERESTS_LABEL =
-      "Choose the topics that spark your curiosity!"
 
    return (
       <ConditionalWrapper condition={Boolean(userData)}>
@@ -43,16 +37,16 @@ const InterestsInformation = () => {
             data-testid={"registration-interests-information-step"}
          >
             <TagsSelector
-               handleInterestTagChange={handleSelectedInterestsChange}
                tags={ContentTopicsTagValues}
                field="contentTopicsTagIds"
                label={CONTENT_TOPIC_INTERESTS_LABEL}
+               labelSx={styles.label}
             />
             <TagsSelector
-               handleInterestTagChange={handleSelectedInterestsChange}
                tags={BusinessFunctionsTagValues}
                field="businessFunctionsTagIds"
                label={BUSINESS_FUNCTIONS_INTERESTS_LABEL}
+               labelSx={styles.label}
             />
          </Stack>
       </ConditionalWrapper>
