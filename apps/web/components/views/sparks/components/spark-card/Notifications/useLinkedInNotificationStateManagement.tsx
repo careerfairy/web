29a4b199214556375cc04f198642e62b71bc6d67
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-06-19 16:55:08 +0100

    feat(useLinkedInNotificationsStateManagement): adapts to redux state

--- apps/web/components/views/sparks/components/spark-card/Notifications/useLinkedInNotificationStateManagement.tsx
+++ apps/web/components/views/sparks/components/spark-card/Notifications/useLinkedInNotificationStateManagement.tsx
@@ -1,24 +1,43 @@
 import { SPARK_CONSTANTS } from "@careerfairy/shared-lib/sparks/constants"
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { setShouldShowLinkedInPopUpNotification } from "store/reducers/sparksFeedReducer"
+import { shouldShowLinkedInPopUpNotificationSelector } from "store/selectors/sparksFeedSelectors"
 
 export const useLinkedInNotificationStateManagement = () => {
-   const [shouldShowLinkedInCTA, setShouldShowLinkedInCTA] = useState(false)
+   const dispatch = useDispatch()
+   const shouldShowLinkedInCTA = useSelector(
+      shouldShowLinkedInPopUpNotificationSelector
+   )
+   const [prevPercentagePlayed, setPrevPercentagePlayed] = useState(0.0)
 
    const onSparkPercentagePlayed = useCallback(
       (percentagePlayed: number) => {
+         if (shouldShowLinkedInCTA && prevPercentagePlayed > percentagePlayed) {
+            setPrevPercentagePlayed(0.0)
+            dispatch(setShouldShowLinkedInPopUpNotification(false))
+            return
+         }
+
          if (
             !shouldShowLinkedInCTA &&
             percentagePlayed >=
                SPARK_CONSTANTS.PLAYED_PERCENTAGE_TO_SHOW_LINKEDIN_NOTIFICATION
          ) {
-            setShouldShowLinkedInCTA(true)
+            dispatch(setShouldShowLinkedInPopUpNotification(true))
          }
+         setPrevPercentagePlayed(percentagePlayed)
       },
-      [shouldShowLinkedInCTA]
+      [dispatch, prevPercentagePlayed, shouldShowLinkedInCTA]
    )
 
-   return {
-      shouldShowLinkedInCTA,
-      onSparkPercentagePlayed,
-   }
+   useEffect(() => {
+      return () => {
+         dispatch(setShouldShowLinkedInPopUpNotification(false))
+         setPrevPercentagePlayed(0.0)
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [])
+
+   return { onSparkPercentagePlayed }
 }
