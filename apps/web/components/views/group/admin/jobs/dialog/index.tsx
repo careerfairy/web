b4a99e253125d6def2cc536b5f9b0074afd78241
goncaloarnauth <goncaloarnauth@outlook.com>
2024-06-19 18:55:57 +0100

    update reset form logic

--- apps/web/components/views/group/admin/jobs/dialog/index.tsx
+++ apps/web/components/views/group/admin/jobs/dialog/index.tsx
@@ -5,6 +5,7 @@ import {
 import { CircularProgress } from "@mui/material"
 import JobFetchWrapper from "HOCs/job/JobFetchWrapper"
 import { SuspenseWithBoundary } from "components/ErrorBoundary"
+import useFeatureFlags from "components/custom-hook/useFeatureFlags"
 import dynamic from "next/dynamic"
 import { useCallback, useMemo, useRef } from "react"
 import { useDispatch, useSelector } from "react-redux"
