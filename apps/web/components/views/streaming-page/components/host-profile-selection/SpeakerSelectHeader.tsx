8baf172342a273c641e47e4a654f60d613a619a4
habibkadiri <kadirit@hotmail.com>
2024-07-02 16:47:40 +0200

    refactor(streaming-page): simplify SpeakerSelectHeader component
    Removed unused imports and simplified the SpeakerSelectHeader component by removing conditional rendering and unnecessary styles.

--- apps/web/components/views/streaming-page/components/host-profile-selection/SpeakerSelectHeader.tsx
+++ apps/web/components/views/streaming-page/components/host-profile-selection/SpeakerSelectHeader.tsx
@@ -1,41 +1,10 @@
-import { IconButton, Stack } from "@mui/material"
-import { useStreamIsMobile } from "components/custom-hook/streaming"
-import Link from "components/views/common/Link"
-import { X } from "react-feather"
-import { sxStyles } from "types/commonTypes"
 import { Header } from "../TopBar/Header"
 import { LogoBackButton } from "../TopBar/LogoBackButton"
-import { PlatformStreamLogo } from "../TopBar/PlatformStreamLogo"
-
-const styles = sxStyles({
-   closeButton: {
-      p: 0.5,
-      m: -0.5,
-   },
-   root: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "100%",
-   },
-})
 
 export const SpeakerSelectHeader = () => {
-   const streamIsMobile = useStreamIsMobile()
-
    return (
       <Header>
-         <Stack sx={styles.root}>
-            {streamIsMobile ? <PlatformStreamLogo /> : <LogoBackButton />}
-            <IconButton
-               component={Link}
-               noLinkStyle
-               href={"/portal"}
-               sx={styles.closeButton}
-            >
-               <X />
-            </IconButton>
-         </Stack>
+         <LogoBackButton />
       </Header>
    )
 }
