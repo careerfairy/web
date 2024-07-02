8e0e773fc4de0ed5635c519376b4831f132a27e2
habibkadiri <kadirit@hotmail.com>
2024-07-02 14:37:40 +0200

    feat(migrations): add progress bar and filter creators by email in migrateCreatorIds
    - Added `cli-progress` to show progress bar during migration.
    - Filtered creators to only include those with IDs containing "@".
    - Updated creator ID assignment and bulk writing logic.
    - Enhanced logging and progress tracking.

--- packages/firebase-scripts/src/migrations/creators/migrateCreatorIds/index.ts
+++ packages/firebase-scripts/src/migrations/creators/migrateCreatorIds/index.ts
@@ -1,18 +1,25 @@
 import { pickPublicDataFromCreator } from "@careerfairy/shared-lib/dist/groups/creators"
 import { LivestreamEvent } from "@careerfairy/shared-lib/src/livestreams"
 import { Spark } from "@careerfairy/shared-lib/src/sparks/sparks"
+import * as cliProgress from "cli-progress"
 import Counter from "../../../lib/Counter"
 import { firestore } from "../../../lib/firebase"
 import { groupRepo, livestreamRepo } from "../../../repositories"
 import { logAction } from "../../../util/logger"
-import { throwMigrationError } from "../../../util/misc"
+import { getCLIBarOptions, throwMigrationError } from "../../../util/misc"
 
 const counter = new Counter()
+const progressBar = new cliProgress.SingleBar(
+   {
+      clearOnComplete: false,
+      hideCursor: true,
+      ...getCLIBarOptions("Writing batch", "Writes"),
+   },
+   cliProgress.Presets.shades_grey
+)
 
 export async function run() {
    try {
-      Counter.log("Fetching all sparks and creators")
-
       const [creators, sparks, livestreams] = await logAction(
          () =>
             Promise.all([
@@ -31,9 +38,16 @@ export async function run() {
          creators.length + sparks.length + livestreams.length
       )
 
-      const bulkWriter = firestore.bulkWriter()
+      const creatorsWithIdsUsingEmails = creators.filter((creator) =>
+         creator.id.includes("@")
+      )
 
-      for (const creator of creators) {
+      const bulkWriter = firestore.bulkWriter()
+      progressBar.start(creatorsWithIdsUsingEmails.length, 0)
+
+      let index = 0
+
+      for (const creator of creatorsWithIdsUsingEmails) {
          // Generate a new Firestore ID
          const newId = firestore
             .collection("careerCenterData")
@@ -41,15 +55,15 @@ export async function run() {
             .collection("creators")
             .doc().id
 
-         // Update the creator's ID
-         creator.id = newId
-
          // Update the creator document with the new ID
          const newCreatorRef = firestore
             .collection("careerCenterData")
             .doc(creator.groupId)
             .collection("creators")
             .doc(newId)
+
+         // Update the creator's ID
+         creator.id = newId
 
          bulkWriter.set(newCreatorRef, creator)
          counter.writeIncrement()
@@ -90,7 +104,7 @@ export async function run() {
 
          // Update the creator's ID in all livestreams that reference this creator
          for (const livestream of livestreams) {
-            if (livestream.creatorsIds.includes(creator.email)) {
+            if (livestream.creatorsIds?.includes(creator.email)) {
                const livestreamRef = firestore
                   .collection("livestreams")
                   .doc(livestream.id)
@@ -115,10 +129,14 @@ export async function run() {
                counter.addToCustomCount("livestreamCreatorIdsUpdated", 1)
             }
          }
+
+         index++
+         progressBar.update(index)
       }
 
       // Commit the bulkWriter
       await bulkWriter.close()
+      progressBar.stop()
       Counter.log("BulkWriter committed")
    } catch (error) {
       console.error(error)
