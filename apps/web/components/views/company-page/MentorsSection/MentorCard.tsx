8ce7d6b0a2bd9759712ca7fa7e63277d16d8750a
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-07-03 11:12:13 +0100

    feature(MentorCard): improves mentor card so it complies with specification

--- apps/web/components/views/company-page/MentorsSection/MentorCard.tsx
+++ apps/web/components/views/company-page/MentorsSection/MentorCard.tsx
@@ -1,31 +1,84 @@
-import { Creator } from "@careerfairy/shared-lib/groups/creators"
-import { Stack, Typography } from "@mui/material"
+import { PublicCreator } from "@careerfairy/shared-lib/groups/creators"
+import { Box, IconButton, Typography, useTheme } from "@mui/material"
 import CircularLogo from "components/views/common/logos/CircularLogo"
-import { Box, Edit2 } from "react-feather"
+import { Edit2 } from "react-feather"
+import { sxStyles } from "types/commonTypes"
+
+const styles = sxStyles({
+   container: (theme) => ({
+      position: "relative",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      background: `${theme.brand.white[100]}`,
+      borderRadius: "10px",
+      border: `1px solid ${theme.palette.secondary.light}`,
+      boxShadow: "0px 0px 8px 0px #1414140F",
+      minWidth: "214px",
+      height: "194px",
+      padding: "16px",
+      gap: 1,
+      userSelect: "none",
+      cursor: "pointer",
+   }),
+   creator: {
+      name: {
+         width: "100%",
+         fontSize: "20px",
+         fontWeight: 600,
+         lineHeight: "30px",
+         textAlign: "center",
+         textOverflow: "ellipsis",
+         overflow: "hidden",
+         whiteSpace: "nowrap",
+      },
+      position: {
+         fontSize: "14px",
+         fontWeight: 400,
+         lineHeight: "20px",
+         textAlign: "center",
+         color: "neutral.400",
+      },
+   },
+   edit: {
+      position: "absolute",
+      right: 4,
+      top: 4,
+   },
+})
 
 type MentorCardProps = {
-   creator: Creator
+   creator: PublicCreator
    key?: string
    isEditMode?: boolean
 }
 
 const MentorCard = ({ creator, key, isEditMode }: MentorCardProps) => {
    const creatorName = `${creator.firstName} ${creator.lastName}`
+   const theme = useTheme()
+
+   const handleEdit = () => {
+      alert("Will open edit dialog")
+   }
 
    return (
-      <Stack key={key}>
+      <Box key={key} sx={styles.container}>
          {Boolean(isEditMode) && (
-            <Box>
-               <Edit2 />
-            </Box>
+            <IconButton sx={styles.edit} onClick={handleEdit}>
+               <Edit2 size={20} color={theme.palette.neutral[700]} />
+            </IconButton>
          )}
          <CircularLogo
+            size={80}
             src={creator.avatarUrl}
             alt={`Picture of creator ${creatorName}`}
+            objectFit="cover"
          />
-         <Typography variant="h4">{creatorName}</Typography>
-         <Typography variant="body1">{creator.position}</Typography>
-      </Stack>
+         <Typography sx={styles.creator.name}>{creatorName}</Typography>
+         <Typography sx={styles.creator.position}>
+            {creator.position}
+         </Typography>
+      </Box>
    )
 }
 
