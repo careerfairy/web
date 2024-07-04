6cb8266c499e9a1f7727ee526314098591f049c2
Habib Kadiri <habib@careerfairy.io>
2024-07-04 17:02:02 +0200

    Hotfix: resolve clsx import issue and update tsconfig moduleResolution (#1158)
    ### Motivation
    
    This change resolves the clsx import issue (https://github.com/lukeed/clsx/issues/92) when running our Dockerfile.test. The tsconfig moduleResolution was updated to "bundler" as recommended for TypeScript 5 projects. This change caused several new TypeScript errors, which have been resolved in this commit. The Dockerfile.test now works as expected, allowing us to reproduce the CI locally.
    
    ### Changes
    
    - Updated tsconfig.json to use "bundler" for moduleResolution.
    - Fixed TypeScript import errors across multiple files.
    - Updated dependencies in package.json and package-lock.json.
    - Added default values for props in MultiListSelect component.
    - Refactored async functions in useInfiniteScrollGet hook.
    - Corrected import paths and types in various files.
    
    ### Browser Testing
    
    -  [ ] Chrome
    -  [ ] Firefox
    -  [ ] Safari
    -  [ ] Edge
    
    ### Different Resolutions Testing
    
    -  [ ] Large Screens
    -  [ ] Medium Screens
    -  [ ] Small Screens
    
    ### Additional Checks
    
    -  [ ] Double-check the date-time pickers on the app as the library has been updated to the latest version.

--- apps/web/data/algolia/AlgoliaRepository.ts
+++ apps/web/data/algolia/AlgoliaRepository.ts
@@ -96,6 +96,11 @@ const handleSearch = <AlgoliaResponseType>(
    const isTest = isTestEnvironment()
    const workflowId = getWorkflowId()
 
+   if (isTest) {
+      // This console log is required to ensure the compiler outputs the workflow ID for the CI
+      console.log(`🚀 - Workflow ID: ${workflowId}`)
+   }
+
    return index.search<AlgoliaResponseType>(query, {
       filters: (isTest ? `workflowId:${workflowId} AND ` : "") + filters,
       page,
