396d53465936c73e916337ed70d9349a47c00bf1
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-07-05 17:14:28 +0100

    refactor(BackButton): isolates component

---
+++ apps/web/components/views/mentor-page/BackButton.tsx
@@ -0,0 +1,29 @@
+import { sxStyles } from "@careerfairy/shared-ui"
+import { Button } from "@mui/material"
+import { useRouter } from "next/router"
+import { ChevronLeft } from "react-feather"
+
+const styles = sxStyles({
+   backButton: {
+      position: "absolute",
+      top: 8,
+      left: 18,
+      padding: "8px",
+   },
+})
+
+export const BackButton = () => {
+   const router = useRouter()
+
+   return (
+      <Button
+         variant="text"
+         color="grey"
+         startIcon={<ChevronLeft />}
+         onClick={() => router.back()}
+         sx={styles.backButton}
+      >
+         Back
+      </Button>
+   )
+}
