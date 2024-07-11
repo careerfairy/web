7b7a4bd8c45d978ab399d76a11ddc27b918a831a
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-07-11 15:24:46 +0100

    refactor(ContentCarousel): extends component with styling capabilities and dep injection

--- apps/web/components/views/mentor-page/ContentCarousel.tsx
+++ apps/web/components/views/mentor-page/ContentCarousel.tsx
@@ -1,38 +1,50 @@
-import { Stack } from "@mui/material"
+import { Stack, SxProps, Typography, TypographyProps } from "@mui/material"
 import useIsDesktop from "components/custom-hook/useIsDesktop"
-import useEmblaCarousel from "embla-carousel-react"
+import useEmblaCarousel, { UseEmblaCarouselType } from "embla-carousel-react"
 import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures"
-import React, { ReactNode } from "react"
-import { sxStyles } from "types/commonTypes"
+import React, { ReactNode, useCallback, useRef } from "react"
+import { combineStyles, sxStyles } from "types/commonTypes"
 import {
    GenericCarousel,
    GenericCarouselProps,
 } from "../common/carousels/GenericCarousel"
 
 const styles = sxStyles({
-   viewport: {
-      // hack to ensure shadows are not cut off
-      padding: 2,
-      marginLeft: -2,
-   },
    container: {
       position: "relative",
       flexDirection: "row",
-      alignItems: "flex-start",
       gap: 2,
+      paddingRight: "30px",
+   },
+   title: {
+      fontSize: 24,
+      fontWeight: 600,
+      lineHeight: 36,
+      color: "neutral.900",
    },
 })
 
 type ContentCarouselProps = Pick<GenericCarouselProps, "children"> & {
    slideWidth: number
-   headerTitle: ReactNode
+   headerTitle: ReactNode | string
+   viewportSx?: SxProps
+   containerSx?: SxProps
+   emblaProps?: {
+      emblaRef: UseEmblaCarouselType[0]
+      emblaApi: UseEmblaCarouselType[1]
+   }
 }
 
 export const ContentCarousel = ({
    children,
    slideWidth,
    headerTitle,
+   viewportSx,
+   containerSx,
+   emblaProps,
 }: ContentCarouselProps) => {
+   const carouselContainerRef = useRef<HTMLDivElement>(null)
+
    const [emblaRef, emblaApi] = useEmblaCarousel(
       {
          loop: false,
@@ -43,23 +55,40 @@ export const ContentCarousel = ({
 
    const isDesktop = useIsDesktop()
 
+   const shouldShowArrows = useCallback(() => {
+      if (!carouselContainerRef.current) return false
+
+      return (
+         isDesktop &&
+         carouselContainerRef.current?.clientWidth <
+            carouselContainerRef.current?.scrollWidth
+      )
+   }, [isDesktop, carouselContainerRef])
+
    return (
-      <Stack>
+      <Stack gap="16px">
          <Stack
             direction="row"
             justifyContent="space-between"
             alignItems="center"
          >
-            {headerTitle}
-            {Boolean(isDesktop && emblaApi?.slidesNotInView) && (
-               <GenericCarousel.Arrows emblaApi={emblaApi} />
+            {typeof headerTitle === "string" ? (
+               <HeaderTitle>{headerTitle}</HeaderTitle>
+            ) : (
+               headerTitle
+            )}
+            {shouldShowArrows() && (
+               <GenericCarousel.Arrows
+                  emblaApi={emblaProps?.emblaApi || emblaApi}
+               />
             )}
          </Stack>
          <GenericCarousel
-            emblaApi={emblaApi}
-            emblaRef={emblaRef}
-            sx={styles.viewport}
-            containerSx={styles.container}
+            emblaApi={emblaProps?.emblaApi || emblaApi}
+            emblaRef={emblaProps?.emblaRef || emblaRef}
+            containerSx={combineStyles(styles.container, containerSx)}
+            sx={viewportSx}
+            containerRef={carouselContainerRef}
          >
             {React.Children.map(children, (child) => (
                <GenericCarousel.Slide
@@ -73,3 +102,17 @@ export const ContentCarousel = ({
       </Stack>
    )
 }
+
+const HeaderTitle = ({ children, sx, ...props }: TypographyProps) => {
+   return (
+      <Typography
+         variant="brandedH3"
+         sx={combineStyles(styles.title, sx)}
+         {...props}
+      >
+         {children}
+      </Typography>
+   )
+}
+
+ContentCarousel.HeaderTitle = HeaderTitle
