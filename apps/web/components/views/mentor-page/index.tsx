d71de054d564383163f2626a40eea1e8fc0d225a
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-07-11 15:26:06 +0100

    feat(mentor-page): adds live stream carousel

--- apps/web/components/views/mentor-page/index.tsx
+++ apps/web/components/views/mentor-page/index.tsx
@@ -2,24 +2,27 @@ import { PublicCreator } from "@careerfairy/shared-lib/groups/creators"
 import { LivestreamEvent } from "@careerfairy/shared-lib/livestreams"
 import { Spark } from "@careerfairy/shared-lib/sparks/sparks"
 import { sxStyles } from "@careerfairy/shared-ui"
-import { Box, Stack, Typography } from "@mui/material"
-import SparksCarousel from "../admin/sparks/general-sparks-view/SparksCarousel"
-import EventsPreviewCarousel, {
-   EventsTypes,
-} from "../portal/events-preview/EventsPreviewCarousel"
+import { Box, Stack } from "@mui/material"
 import { BackButton } from "./BackButton"
+import { LivestreamsCarousel } from "./LivestreamsCarousel"
 import { MentorDetail } from "./MentorDetail"
+import { SparksCarousel } from "./SparksCarousel"
+
+const SLIDE_WIDTH = 368
 
 const styles = sxStyles({
    root: {
+      position: "relative",
+      display: "flex",
       flexDirection: "row",
+      width: "100%",
       gap: "16px",
-      marginLeft: "32px",
+      paddingLeft: "32px",
    },
    mentorDetailsContainer: {
       position: "relative",
       display: "flex",
-      width: "368px",
+      width: `${SLIDE_WIDTH}px`,
       padding: "54px 26px 28px 26px",
       flexDirection: "column",
       alignItems: "center",
@@ -30,8 +33,12 @@ const styles = sxStyles({
       background: "#FFF",
    },
    mentorContentContainer: {
+      position: "relative",
+      display: "flex",
       flexDirection: "column",
       gap: "24px",
+      width: "100%",
+      overflow: "hidden",
    },
 })
 
@@ -55,13 +62,8 @@ export const MentorDetailPage = ({
             <MentorDetail mentor={mentor} companyName={companyName} />
          </Box>
          <Stack sx={styles.mentorContentContainer}>
-            <Typography variant="h6">My Sparks</Typography>
             <SparksCarousel sparks={sparks} />
-            <EventsPreviewCarousel
-               title="My live streams"
-               events={livestreams}
-               type={EventsTypes.RECOMMENDED}
-            />
+            <LivestreamsCarousel livestreams={livestreams} />
          </Stack>
       </Stack>
    )
