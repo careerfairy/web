fb181a96765a709a496f36d0c8bd1efe9605fc4a
goncaloarnauth <goncaloarnauth@outlook.com>
2024-07-15 19:03:15 +0100

    add linked content steps

--- apps/web/components/views/group/admin/jobs/dialog/index.tsx
+++ apps/web/components/views/group/admin/jobs/dialog/index.tsx
@@ -69,6 +69,10 @@ export const JobDialogStep = {
       position: 4,
       key: "form-additional-details",
    },
+   NO_CONTENT_AVAILABLE: {
+      position: 4,
+      key: "no-content-available",
+   },
    FORM_LINKED_LIVE_STREAMS: {
       position: 5,
       key: "form-linked-live-streams",
@@ -101,6 +105,10 @@ const JobAdditionalDetails = dynamic(
    {
       ssr: false,
    }
+)
+
+const JobLinkLiveStreams = dynamic(
+   () => import("./createJob/JobLinkLiveStreams")
 )
 
 type ViewsProps = {
