5a4a36c85cfb9a22a894735d81dbbbbca9a43eca
goncaloarnauth <goncaloarnauth@outlook.com>
2024-07-17 13:58:28 +0100

    update live stream step to only show linked and upcoming live streams

--- apps/web/components/views/group/admin/jobs/dialog/createJob/JobLinkLiveStreams.tsx
+++ apps/web/components/views/group/admin/jobs/dialog/createJob/JobLinkLiveStreams.tsx
@@ -1,4 +1,5 @@
 import { Box, Grid } from "@mui/material"
+import useCustomJobLinkedLivestreams from "components/custom-hook/custom-job/useCustomJobLinkedLivestreams"
 import useGroupFromState from "components/custom-hook/useGroupFromState"
 import useIsMobile from "components/custom-hook/useIsMobile"
 import useListenToStreams from "components/custom-hook/useListenToStreams"
@@ -48,7 +49,7 @@ const styles = sxStyles({
 
 const FIELD_NAME = "livestreamIds"
 
-const JobLinkLiveStreams = () => {
+const JobLinkLiveStreams = ({ job }) => {
    const { moveToPrev, moveToNext, goToStep } = useStepper()
    const { group } = useGroupFromState()
    const isMobile = useIsMobile()
