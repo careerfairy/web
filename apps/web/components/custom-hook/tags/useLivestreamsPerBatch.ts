d9059c78959a740d8d0fca46369e7d766f664189
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-07-26 15:30:47 +0100

    improved drawer hiding

--- apps/web/components/custom-hook/tags/useLivestreamsPerBatch.ts
+++ apps/web/components/custom-hook/tags/useLivestreamsPerBatch.ts
@@ -5,17 +5,23 @@ const useLivestreamsPerBatch = (): number => {
 
    const extraSmall = useMediaQuery(theme.breakpoints.only("xs"))
    const small = useMediaQuery(theme.breakpoints.only("sm"))
+   const smallUpperLimit = useMediaQuery(theme.breakpoints.down(989))
+   const smallLowerLimit = useMediaQuery(theme.breakpoints.up(900))
+
+   const smallIsh = smallLowerLimit && smallUpperLimit
+
    const medium = useMediaQuery(theme.breakpoints.only("md"))
    const large = useMediaQuery(theme.breakpoints.only("lg"))
-   const wide = useMediaQuery(theme.breakpoints.only("xl"))
+   const wide = useMediaQuery(theme.breakpoints.up("lg"))
 
    const sizes = [
       { active: extraSmall, itemsPerBatch: 1 },
       { active: small, itemsPerBatch: 2 },
+      { active: smallIsh, itemsPerBatch: 2 },
       { active: medium || large, itemsPerBatch: 3 },
       { active: wide, itemsPerBatch: 4 },
    ]
-   return sizes.find((sizeData) => sizeData.active)?.itemsPerBatch || 2
+   return sizes.find((sizeData) => sizeData.active)?.itemsPerBatch || 4
 }
 
 export default useLivestreamsPerBatch
