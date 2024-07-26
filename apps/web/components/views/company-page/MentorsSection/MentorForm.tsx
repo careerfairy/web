df857c4f12b9406b5cbd5ea50f32a1f7ca045933
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-07-26 18:59:19 +0100

    feat(MentorForm): adapts to new form layout

--- apps/web/components/views/company-page/MentorsSection/MentorForm.tsx
+++ apps/web/components/views/company-page/MentorsSection/MentorForm.tsx
@@ -1,64 +1,14 @@
 import { Creator, PublicCreator } from "@careerfairy/shared-lib/groups/creators"
 import { CreateCreatorSchemaType } from "@careerfairy/shared-lib/groups/schemas"
-import { sxStyles } from "@careerfairy/shared-ui"
-import { Box } from "@mui/material"
 import useGroupCreator from "components/custom-hook/creator/useGroupCreator"
+import useIsDesktop from "components/custom-hook/useIsDesktop"
 import {
    CreatorFormFields,
    CreatorFormProvider,
 } from "components/views/creator/CreatorForm"
 import useCreatorFormSubmit from "components/views/sparks/forms/hooks/useCreatorFormSubmit"
-import SteppedDialog from "components/views/stepped-dialog/SteppedDialog"
 import { useFormContext } from "react-hook-form"
-
-const styles = sxStyles({
-   container: {
-      height: "100%",
-      display: "flex",
-      flexDirection: "column",
-      overflow: "hidden",
-   },
-   content: {
-      width: "100%",
-      position: "relative",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      px: 2,
-      overflowX: "hidden",
-      overflowY: "scroll",
-      paddingTop: {
-         xs: 13,
-         md: 0,
-      },
-   },
-   form: {
-      marginTop: "25px",
-      marginBottom: "40px",
-      paddingBottom: "48px",
-   },
-   title: {
-      fontSize: { xs: "28px", md: "32px" },
-   },
-   subtitle: {
-      maxWidth: "unset",
-      fontSize: { xs: "16px", md: "16px" },
-   },
-   actions: {
-      display: "flex",
-      backgroundColor: "#FFFFFF !important",
-      height: "90px",
-      position: {
-         xs: "fixed !important",
-         md: "absolute !important",
-      },
-   },
-   avatarGrid: {
-      display: "flex",
-      justifyContent: "center",
-      marginBottom: 1,
-   },
-})
+import { CreatorFormLayout } from "./CreatorFormLayout"
 
 type MentorFormProps = {
    mentor: PublicCreator
@@ -66,6 +16,8 @@ type MentorFormProps = {
 }
 
 export const MentorForm = ({ mentor, handleClose }: MentorFormProps) => {
+   const isDesktop = useIsDesktop()
+
    const { data: creator } = useGroupCreator(
       mentor?.groupId || "",
       mentor?.id || "",
@@ -77,28 +29,27 @@ export const MentorForm = ({ mentor, handleClose }: MentorFormProps) => {
 
    return (
       <CreatorFormProvider creator={creator}>
-         <SteppedDialog.Container
-            sx={styles.content}
-            handleCloseIconClick={handleClose}
-         >
-            <SteppedDialog.Content sx={styles.container}>
-               <SteppedDialog.Title sx={styles.title}>
-                  Edit your{" "}
-                  <Box component="span" color="secondary.main">
-                     contributor
-                  </Box>
-               </SteppedDialog.Title>
-               <SteppedDialog.Subtitle sx={styles.subtitle}>
-                  Check and change your contributor details
-               </SteppedDialog.Subtitle>
-               <Box sx={styles.form} component="form">
+         <CreatorFormLayout handleClose={handleClose}>
+            <CreatorFormLayout.Container>
+               <CreatorFormLayout.Header>
+                  <CreatorFormLayout.Title>
+                     Edit {Boolean(isDesktop) && "your"}{" "}
+                     <CreatorFormLayout.HighlightedTitleText>
+                        contributor
+                     </CreatorFormLayout.HighlightedTitleText>
+                  </CreatorFormLayout.Title>
+                  <CreatorFormLayout.Subtitle>
+                     Check and change your contributor details
+                  </CreatorFormLayout.Subtitle>
+               </CreatorFormLayout.Header>
+               <CreatorFormLayout.Fields>
                   <CreatorFormFields />
-               </Box>
-            </SteppedDialog.Content>
-            <SteppedDialog.Actions sx={styles.actions}>
+               </CreatorFormLayout.Fields>
+            </CreatorFormLayout.Container>
+            <CreatorFormLayout.Actions>
                <Actions creator={creator} handleClose={handleClose} />
-            </SteppedDialog.Actions>
-         </SteppedDialog.Container>
+            </CreatorFormLayout.Actions>
+         </CreatorFormLayout>
       </CreatorFormProvider>
    )
 }
@@ -116,23 +67,25 @@ const Actions = ({ creator, handleClose }: ActionsProps) => {
    const {
       handleSubmit,
       formState: { isSubmitting, isValid, isDirty },
+      setError,
    } = useFormContext<CreateCreatorSchemaType>()
 
    const onSubmit = async (values: CreateCreatorSchemaType) => {
-      await handleCreatorSubmit(values, undefined)
+      await handleCreatorSubmit(values, setError as any)
+      handleClose()
    }
 
    return (
       <>
-         <SteppedDialog.Button
+         <CreatorFormLayout.Button
             variant="outlined"
             color="grey"
             onClick={handleClose}
          >
             Cancel
-         </SteppedDialog.Button>
+         </CreatorFormLayout.Button>
 
-         <SteppedDialog.Button
+         <CreatorFormLayout.Button
             variant="contained"
             color={"secondary"}
             disabled={isSubmitting || !isValid || !isDirty}
@@ -141,7 +94,7 @@ const Actions = ({ creator, handleClose }: ActionsProps) => {
             loading={isSubmitting}
          >
             Save
-         </SteppedDialog.Button>
+         </CreatorFormLayout.Button>
       </>
    )
 }
