0026d4e88de2c80cea6a69a99ca54ecc5f56b866
habibkadiri <kadirit@hotmail.com>
2024-07-31 11:53:50 +0200

    minor: improve error logging for fetching registered streams

--- apps/web/components/custom-hook/useRegisteredStreams.ts
+++ apps/web/components/custom-hook/useRegisteredStreams.ts
@@ -2,15 +2,24 @@ import { livestreamRepo } from "data/RepositoryInstances"
 import { useAuth } from "HOCs/AuthProvider"
 import { useCallback } from "react"
 import useSWR, { useSWRConfig } from "swr"
+import { errorLogAndNotify } from "util/CommonUtil"
 
 const useRegisteredStreams = () => {
    const { authenticatedUser } = useAuth()
 
    return useSWR(
-      authenticatedUser.uid
+      authenticatedUser.email
          ? ["user-registered-streams", authenticatedUser.email]
          : null,
-      async ([, email]) => livestreamRepo.getRegisteredEvents(email)
+      async ([, email]) => livestreamRepo.getRegisteredEvents(email),
+      {
+         onError: (error) => {
+            errorLogAndNotify(error, {
+               message: "Failed to fetch registered streams",
+               userId: authenticatedUser.uid,
+            })
+         },
+      }
    )
 }
 
