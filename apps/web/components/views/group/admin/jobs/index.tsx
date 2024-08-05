a2dfd049b28bf779fe541f476f55895c3febdfe4
goncaloarnauth <goncaloarnauth@outlook.com>
2024-08-05 12:15:47 +0100

    add additionalJobListCard to be reused if needed on the platform

--- apps/web/components/views/group/admin/jobs/index.tsx
+++ apps/web/components/views/group/admin/jobs/index.tsx
@@ -7,7 +7,7 @@ import { useCallback, useMemo } from "react"
 import useGroupCustomJobsStats from "../../../../custom-hook/custom-job/useGroupCustomJobsStats"
 import useGroupFromState from "../../../../custom-hook/useGroupFromState"
 import EmptyJobsView from "../../../admin/jobs/empty-jobs-view/EmptyJobsView"
-import JobList from "./JobList"
+import JobList from "./jobList"
 
 const JobsContent = () => {
    const { group } = useGroupFromState()
