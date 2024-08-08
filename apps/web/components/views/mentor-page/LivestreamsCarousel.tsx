332f40962cc814f5cd0c23fd545d7414bb5444b4
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-08-08 14:06:12 +0100

    style(LivestreamsCarousel): fix to ensure cards are visible and ignore parent padding

--- apps/web/components/views/mentor-page/LivestreamsCarousel.tsx
+++ apps/web/components/views/mentor-page/LivestreamsCarousel.tsx
@@ -4,7 +4,6 @@ import {
 } from "@careerfairy/shared-lib/livestreams"
 import { sxStyles } from "@careerfairy/shared-ui"
 import { Box } from "@mui/material"
-import useIsMobile from "components/custom-hook/useIsMobile"
 import EventPreviewCard from "../common/stream-cards/EventPreviewCard"
 import { ContentCarousel } from "./ContentCarousel"
 
@@ -16,17 +15,11 @@ const styles = sxStyles({
       // hack to ensure shadows are not cut off
       padding: "16px",
       margin: "-16px",
-      width: "calc(100% + 16px)",
-   },
-   mobileCarouselContainer: {
-      gap: "32px !important",
+      width: "calc(100% + 32px)",
    },
    cardWrapper: {
       width: CARD_WIDTH,
       minHeight: CARD_HEIGHT,
-   },
-   lastCard: {
-      paddingRight: "16px",
    },
 })
 
@@ -37,23 +30,14 @@ type LivestreamsCarouselProps = {
 export const LivestreamsCarousel = ({
    livestreams,
 }: LivestreamsCarouselProps) => {
-   const isMobile = useIsMobile()
-
    return (
       <ContentCarousel
          headerTitle="Live streams"
          slideWidth={CARD_WIDTH}
          viewportSx={styles.viewport}
-         containerSx={isMobile ? styles.mobileCarouselContainer : null}
       >
          {livestreams.map((livestream, index) => (
-            <Box
-               key={livestream.id}
-               sx={[
-                  styles.cardWrapper,
-                  index === livestreams.length - 1 ? styles.lastCard : {},
-               ]}
-            >
+            <Box key={livestream.id} sx={styles.cardWrapper}>
                <EventPreviewCard
                   key={livestream.id}
                   index={index}
