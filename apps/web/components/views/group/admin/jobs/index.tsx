676c93d1b8b50797c05e90a44f972f74841cfe64
goncaloarnauth <goncaloarnauth@outlook.com>
2024-08-08 15:14:18 +0100

    add sorted logic

--- apps/web/components/views/group/admin/jobs/index.tsx
+++ apps/web/components/views/group/admin/jobs/index.tsx
@@ -1,6 +1,6 @@
 import {
    CustomJob,
-   CustomJobStats,
+   sortCustomJobs,
 } from "@careerfairy/shared-lib/customJobs/customJobs"
 import { useRouter } from "next/router"
 import { useCallback, useMemo } from "react"
@@ -12,13 +12,11 @@ import JobList from "./JobList"
 const JobsContent = () => {
    const { group } = useGroupFromState()
    const allJobsWithStats = useGroupCustomJobsStats(group.groupId)
-   // const { jobHubV1 } = useFeatureFlags()
-   const { jobHubV1 } = { jobHubV1: true }
    const { push } = useRouter()
 
    const sortedJobs = useMemo(
-      () => (jobHubV1 ? sortJobs(allJobsWithStats) : allJobsWithStats),
-      [allJobsWithStats, jobHubV1]
+      () => sortCustomJobs(allJobsWithStats),
+      [allJobsWithStats]
    )
 
    const handleJobClick = useCallback(
@@ -35,42 +33,3 @@ const JobsContent = () => {
    )
 }
 export default JobsContent
-
-/**
- *
- * This function sorts an array of job statistics.
- * It prioritizes jobs based on their publication status and deadline.
- *
- * @param jobs {CustomJobStats[]}
- * @returns jobs {CustomJobStats[]}
- */
-const sortJobs = (jobs: CustomJobStats[]): CustomJobStats[] => {
-   const now = new Date()
-
-   // Create a new array to avoid mutating the original 'jobs' array
-   const sortedJobs = [...jobs]
-
-   return sortedJobs.sort(({ job: jobA }, { job: jobB }) => {
-      // Sort by 'published' flag
-      if (jobA.published && !jobB.published) {
-         return -1
-      }
-      if (!jobA.published && jobB.published) {
-         return 1
-      }
-
-      // Both have the same 'published' status, so sort by 'deadline'
-      const aDeadlineValid = jobA.deadline.toDate() > now
-      const bDeadlineValid = jobB.deadline.toDate() > now
-
-      if (aDeadlineValid && !bDeadlineValid) {
-         return -1
-      }
-      if (!aDeadlineValid && bDeadlineValid) {
-         return 1
-      }
-
-      // If both jobs have the same 'published' status and 'deadline' validity, maintain the current order
-      return 0
-   })
-}
