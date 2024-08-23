21c79eec015d6ac5fbad11d8c92ecd44ded605a3
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-08-23 06:25:35 +0100

    feat(useAnalyticsLocking): adds tab-based group plan validation

--- apps/web/components/custom-hook/spark/analytics/useAnalyticsLocking.tsx
+++ apps/web/components/custom-hook/spark/analytics/useAnalyticsLocking.tsx
@@ -3,15 +3,32 @@ import useGroupPlanIsValid from "components/custom-hook/group/useGroupPlanIsVali
 import { useAuth } from "HOCs/AuthProvider"
 import { useGroup } from "layouts/GroupDashboardLayout"
 
-export const useAnalyticsLocking = () => {
+export const useAnalyticsLocking = (
+   tabToValidate: "overview" | "audience" | "competitor"
+) => {
+   let groupPlansToValidate = []
+
+   switch (tabToValidate) {
+      case "overview":
+         groupPlansToValidate = [
+            GroupPlanTypes.Tier1,
+            GroupPlanTypes.Tier2,
+            GroupPlanTypes.Tier3,
+         ]
+         break
+      case "audience":
+         groupPlansToValidate = [GroupPlanTypes.Tier2, GroupPlanTypes.Tier3]
+         break
+      case "competitor":
+         groupPlansToValidate = [GroupPlanTypes.Tier3]
+         break
+      default:
+         throw new Error(`Invalid tab to validate: ${tabToValidate}`)
+   }
+
    const { group } = useGroup()
    const { userData } = useAuth()
-
-   const planStatus = useGroupPlanIsValid(group.groupId, [
-      GroupPlanTypes.Tier1,
-      GroupPlanTypes.Tier2,
-      GroupPlanTypes.Tier3,
-   ])
+   const planStatus = useGroupPlanIsValid(group.groupId, groupPlansToValidate)
 
    const shouldLockAnalytics = !userData.isAdmin && !planStatus.valid
 
