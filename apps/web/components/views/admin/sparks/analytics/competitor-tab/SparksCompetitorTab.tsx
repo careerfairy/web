c3b29d07bb6bc3aac64339e5bd84874e9e729047
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-08-23 06:26:48 +0100

    refactor(SparksCompetitorTab): makes use of locking custom hook

--- apps/web/components/views/admin/sparks/analytics/competitor-tab/SparksCompetitorTab.tsx
+++ apps/web/components/views/admin/sparks/analytics/competitor-tab/SparksCompetitorTab.tsx
@@ -1,6 +1,8 @@
 import { TimePeriodParams } from "@careerfairy/shared-lib/sparks/analytics"
 import { Stack } from "@mui/material"
+import { useAnalyticsLocking } from "components/custom-hook/spark/analytics/useAnalyticsLocking"
 import { SuspenseWithBoundary } from "components/ErrorBoundary"
+import { LockedSparksCompetitorTab } from "../components/locking/LockedSparksCompetitorTab"
 import { MostSomethingSkeleton } from "../overview-tab/SparksAnalyticsOverviewTabSkeletons"
 import { TopSparksByAudience } from "./TopSparksByAudience"
 import { TopSparksByIndustry } from "./TopSparksByIndustry"
@@ -12,6 +14,12 @@ type SparksCompetitorTabProps = {
 export const SparksCompetitorTab = ({
    timeFilter,
 }: SparksCompetitorTabProps) => {
+   const { isLocked } = useAnalyticsLocking("competitor")
+
+   if (isLocked) {
+      return <LockedSparksCompetitorTab />
+   }
+
    return (
       <Stack spacing={5} marginBottom={10}>
          <SuspenseWithBoundary fallback={<MostSomethingSkeleton />}>
