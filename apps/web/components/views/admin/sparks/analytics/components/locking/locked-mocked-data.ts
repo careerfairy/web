69363fb7e7f5054f49669bd479479d76f37dc90a
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-08-26 06:29:22 +0100

    feat(locked-mocked-data): adds mocking data for competitor tab

---
+++ apps/web/components/views/admin/sparks/analytics/components/locking/locked-mocked-data.ts
@@ -0,0 +1,161 @@
+export const MockedData = {
+   industry: [
+      {
+         spark: {
+            id: "Wtj4amZs2qfrXNsDujY0",
+            creatorAvatarUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/groups%2FjPLufTLSTz8tPsrVrxxa%2Fcreator-avatars%2F9ed01649-af86-41df-813c-bd5a6d0c406b.jpg?alt=media&token=a7e1860e-b589-4456-8b49-0a131284ae4b",
+            creatorName: "Celine Mano",
+            universityName: "European Bank for Reconstruction and Development",
+            categoryId: "role",
+            question:
+               "What are the kinds of projects you could end up working on?",
+            thumbnailUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fthumbnails%2F197b8eaa-02f6-40a4-9a4a-4606ff2b6f77.jpeg?alt=media&token=ab0b5178-6a56-4759-84ac-18bced97ebe6",
+         },
+         stats: {
+            plays: 1079,
+            likes: 6,
+            shareCTA: 1,
+            numberOfCareerPageClicks: 8,
+         },
+      },
+      {
+         spark: {
+            id: "PEOClAaQSdAt3D1H1ynM",
+            creatorAvatarUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/groups%2FjPLufTLSTz8tPsrVrxxa%2Fcreator-avatars%2Fb7e1e801-4382-4ac7-911e-fa09d515bc0e.jpeg?alt=media&token=97564dd2-45a9-44ce-894e-b16f19f06116",
+            creatorName: "Euan Bose Rosling",
+            universityName: "European Bank for Reconstruction and Development",
+            categoryId: "company-culture",
+            question: "What’s the workplace environment like at the EBRD?",
+            thumbnailUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fthumbnails%2F0f0611f3-f947-4467-8582-511860ab2be3.jpeg?alt=media&token=c4483aa9-e892-4e85-bf4f-96fe7c667e1e",
+         },
+         stats: {
+            plays: 629,
+            likes: 6,
+            shareCTA: 3,
+            numberOfCareerPageClicks: 14,
+         },
+      },
+      {
+         spark: {
+            id: "OVT6t5Suhv53r0DfifG9",
+            creatorAvatarUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/groups%2F1oT3gaoRZz2AkudvUeM1%2Fcreator-avatars%2Fbbb97cc1-9e9d-408b-a18c-26b622acd7fd.jpg?alt=media&token=8ed0b3a1-ddd9-4362-99a4-2dec0aa4e5bb",
+            creatorName: "Frédéric Pellet",
+            universityName: "Wüest Partner",
+            categoryId: "day-in-the-life",
+            question:
+               "Wie sieht der Arbeitsalltag eines Consultants im Bereich Data, Analytics & Technology aus?",
+            thumbnailUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fthumbnails%2F629a973e-972d-4f07-84c5-69354f4de1f7.jpeg?alt=media&token=65c71439-fed4-4403-a8cd-7e77c6ca19bb",
+         },
+         stats: {
+            plays: 848,
+            likes: 0,
+            shareCTA: 0,
+            numberOfCareerPageClicks: 5,
+         },
+      },
+      {
+         spark: {
+            id: "E65JwOHa1kC85hGhiXaT",
+            creatorAvatarUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/groups%2FHfao9tHcOqdv69rMvn7a%2Fcreator-avatars%2F811b45ac-bbe0-44af-9eca-062ce065cce1.jpeg?alt=media&token=57430909-2f9f-40c7-8045-46c01151d54d",
+            creatorName: "Nathalie Owsianowski",
+            universityName: "TRUMPF SE + Co. KG",
+            categoryId: "jobs",
+            question: "Wie sieht das Traineeprogramm aus?",
+            thumbnailUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fthumbnails%2Fb3e60861-3cd8-4f93-89b0-67479308911e.jpeg?alt=media&token=4da76d88-902c-44c4-a225-f044e63ca492",
+         },
+         stats: {
+            plays: 1544,
+            likes: 4,
+            shareCTA: 5,
+            numberOfCareerPageClicks: 0,
+         },
+      },
+   ],
+   audience: [
+      {
+         spark: {
+            id: "PEOClAaQSdAt3D1H1ynM",
+            creatorAvatarUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/groups%2FjPLufTLSTz8tPsrVrxxa%2Fcreator-avatars%2Fb7e1e801-4382-4ac7-911e-fa09d515bc0e.jpeg?alt=media&token=97564dd2-45a9-44ce-894e-b16f19f06116",
+            creatorName: "Euan Bose Rosling",
+            universityName: "European Bank for Reconstruction and Development",
+            categoryId: "company-culture",
+            question: "What’s the workplace environment like at the EBRD?",
+            thumbnailUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fthumbnails%2F0f0611f3-f947-4467-8582-511860ab2be3.jpeg?alt=media&token=c4483aa9-e892-4e85-bf4f-96fe7c667e1e",
+         },
+         stats: {
+            plays: 629,
+            likes: 6,
+            shareCTA: 3,
+            numberOfCareerPageClicks: 14,
+         },
+      },
+      {
+         spark: {
+            id: "WlyUMvhLjXHqSSGgjmsf",
+            creatorAvatarUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/groups%2FJnLDznZA3cU21ngF4Qfc%2Fcreator-avatars%2Fe50dca7e-59f6-4ae0-9ddf-48f5a03bce5d.jpg?alt=media&token=42095a77-7149-4704-8f83-7e1334dd9ecb",
+            creatorName: "Nayana Chakkalakkal",
+            universityName: "PostFinance",
+            categoryId: "day-in-the-life",
+            question:
+               "Wie sieht der Arbeitsalltag einer Werkstudentin im HR aus?",
+            thumbnailUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fthumbnails%2Fa5e7c060-1092-4c63-8269-1cf2d771055f.jpeg?alt=media&token=443fa50c-4d6d-4de6-9d66-1f61deaca525",
+         },
+         stats: {
+            plays: 1596,
+            likes: 6,
+            shareCTA: 4,
+            numberOfCareerPageClicks: 17,
+         },
+      },
+      {
+         spark: {
+            id: "sSpgry8o9vZZ0fqy6XoD",
+            creatorAvatarUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/groups%2FTFlkQFadcncfETQCY5Or%2Fcreator-avatars%2Fd5d9c509-a42b-4dc7-bdd2-6c0a95a733a8.jpg?alt=media&token=6d70326c-0624-41a1-a002-06a7994f86dc",
+            creatorName: "Noémie W.",
+            universityName: "McKinsey & Company",
+            categoryId: "application",
+            question: "How does the application process at McKinsey look like?",
+            thumbnailUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fthumbnails%2Fca9b8047-03cf-4b4c-a8fe-8b9b6d130a80.jpeg?alt=media&token=87d3171c-61e6-439f-bd78-5ee9683dd812",
+         },
+         stats: {
+            plays: 449,
+            likes: 8,
+            shareCTA: 2,
+            numberOfCareerPageClicks: 9,
+         },
+      },
+      {
+         spark: {
+            id: "DLqbPzUv7jHv6xEiztrh",
+            creatorAvatarUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/groups%2F1oT3gaoRZz2AkudvUeM1%2Fcreator-avatars%2Fd46e924e-e059-4a9f-8372-741a04ff1809.jpg?alt=media&token=649fd281-9584-4d79-b555-31100965e440",
+            creatorName: "Sara Ristovska",
+            universityName: "Wüest Partner",
+            categoryId: "company-culture",
+            question: "Lerne unser Team Data, Analytics & Technology kennen",
+            thumbnailUrl:
+               "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/sparks%2Fthumbnails%2F77fc084b-1ef4-4407-87d8-76b27c9b4d56.jpeg?alt=media&token=e4a7062d-d229-445c-b846-21746e0630d1",
+         },
+         stats: {
+            plays: 363,
+            likes: 2,
+            shareCTA: 1,
+            numberOfCareerPageClicks: 3,
+         },
+      },
+   ],
+}
