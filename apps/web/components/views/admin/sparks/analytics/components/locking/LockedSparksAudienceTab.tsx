d4a234e949244a6754f689e0096f41c0d63b05fd
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-08-26 06:33:04 +0100

    refactor(LockedSparksAudienceTab): replaces usage of LockedComponents with LockedComponentsModal

--- apps/web/components/views/admin/sparks/analytics/components/locking/LockedSparksAudienceTab.tsx
+++ apps/web/components/views/admin/sparks/analytics/components/locking/LockedSparksAudienceTab.tsx
@@ -1,28 +1,13 @@
-import useIsMobile from "components/custom-hook/useIsMobile"
-import LockedComponents from "./LockedComponents"
-
-const lockedSparkAnalyticsDesktopImage =
-   "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/misc%2Fsparks-locked-background.png?alt=media&token=f724cb9e-1a8d-4891-a56c-432039935835"
-const lockedSparkAnalyticsMobileImage =
-   "https://firebasestorage.googleapis.com/v0/b/careerfairy-e1fd9.appspot.com/o/misc%2Fsparks-locked-background-mobile.png?alt=media&token=22923a7d-9b9c-4eeb-90df-43e5624bac0e"
+import { LockedComponentsModal } from "./LockedComponentsModal"
 
 const metrics = ["Top countries", "Top universities", "Top field of studies"]
 
 export const LockedSparksAudienceTab = () => {
-   const isMobile = useIsMobile()
-
-   const src = isMobile
-      ? lockedSparkAnalyticsMobileImage
-      : lockedSparkAnalyticsDesktopImage
-
    return (
-      <LockedComponents>
-         <LockedComponents.PlaceholderImage src={src} />
-         <LockedComponents.Info
-            title="Unlock audience"
-            text="Unlock in-depth audience insights"
-            metrics={metrics}
-         />
-      </LockedComponents>
+      <LockedComponentsModal
+         title="Unlock audience"
+         text="Unlock in-depth audience insights"
+         metrics={metrics}
+      />
    )
 }
