4b81ab550c48456fcb032a633ee783a2c8a19e89
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-08-28 08:48:52 +0100

    refactor(SparksAudienceTab): adds skeletons for when the data is loading

--- apps/web/components/views/admin/sparks/analytics/audience-tab/SparksAudienceTab.tsx
+++ apps/web/components/views/admin/sparks/analytics/audience-tab/SparksAudienceTab.tsx
@@ -1,62 +1,20 @@
 import { TimePeriodParams } from "@careerfairy/shared-lib/sparks/analytics"
 import { Grid } from "@mui/material"
 import { useAnalyticsLocking } from "components/custom-hook/spark/analytics/useAnalyticsLocking"
-import useSparksAnalytics from "components/custom-hook/spark/analytics/useSparksAnalytics"
-import { useGroup } from "layouts/GroupDashboardLayout"
-import { FC } from "react"
-import BulletChart from "../components/charts/BulletChart"
-import CFPieChart from "../components/charts/CFPieChart"
-import { GroupSparkAnalyticsCardContainer } from "../components/GroupSparkAnalyticsCardContainer"
-import { GroupSparkAnalyticsCardContainerTitle } from "../components/GroupSparkAnalyticsCardTitle"
+import { SuspenseWithBoundary } from "components/ErrorBoundary"
 import { LockedSparksAudienceTab } from "../components/locking/LockedSparksAudienceTab"
-import {
-   EmptyDataCheckerForBulletChart,
-   EmptyDataCheckerForPieChart,
-} from "./EmptyDataCheckers"
-
-const updateRelativePercentage = (data, maxValue) => {
-   return data.map((item) => ({
-      ...item,
-      relativePercentage: (item.value / maxValue) * 100,
-   }))
-}
+import { LevelsOfStudy } from "./LevelsOfStudy"
+import { PieChartsSkeleton, TopBulletChartsSkeleton } from "./Skeletons"
+import { TopCountries } from "./TopCountries"
+import { TopFieldsOfStudy } from "./TopFieldsOfStudy"
+import { TopUniversities } from "./TopUniversities"
 
 type SparksAudienceTabProps = {
    timeFilter: TimePeriodParams
 }
 
-const SparksAudienceTab: FC<SparksAudienceTabProps> = ({ timeFilter }) => {
+const SparksAudienceTab = ({ timeFilter }: SparksAudienceTabProps) => {
    const { isLocked } = useAnalyticsLocking("audience")
-   const { group } = useGroup()
-   const { topCountries, topUniversities, topFieldsOfStudy, levelsOfStudy } =
-      useSparksAnalytics(group.id)[timeFilter]
-
-   /*
-    * The calculations below scale the bars' values relative to the maximum
-    * absolute value of the dataset. This creates a better user experience
-    * by giving the impression that the bars are larger. Using raw percentages
-    * would make the bars appear consistently small.
-    *
-    * @valueIllusionMargin ensures that the first bar (i.e. maximum absolute value)
-    * does not occupy the entire space, preventing it from being 100%.
-    */
-   const valueIllusionMargin = 1.075
-
-   const maxCountryValue =
-      Math.max(...topCountries.map((country) => country.value)) *
-      valueIllusionMargin
-   const maxUniversityValue =
-      Math.max(...topUniversities.map((university) => university.value)) *
-      valueIllusionMargin
-
-   const updatedTopCountries = updateRelativePercentage(
-      topCountries,
-      maxCountryValue
-   )
-   const updatedTopUniversities = updateRelativePercentage(
-      topUniversities,
-      maxUniversityValue
-   )
 
    if (isLocked) {
       return <LockedSparksAudienceTab />
@@ -65,51 +23,24 @@ const SparksAudienceTab: FC<SparksAudienceTabProps> = ({ timeFilter }) => {
    return (
       <Grid container spacing={5} marginBottom={10} alignItems="stretch">
          <Grid item xs={12} md={6}>
-            <GroupSparkAnalyticsCardContainer>
-               <GroupSparkAnalyticsCardContainerTitle>
-                  Top 10 countries
-               </GroupSparkAnalyticsCardContainerTitle>
-               <EmptyDataCheckerForBulletChart>
-                  <BulletChart
-                     data={updatedTopCountries}
-                     valueIndexer="relativePercentage"
-                  />
-               </EmptyDataCheckerForBulletChart>
-            </GroupSparkAnalyticsCardContainer>
+            <SuspenseWithBoundary fallback={<TopBulletChartsSkeleton />}>
+               <TopCountries timeFilter={timeFilter} />
+            </SuspenseWithBoundary>
          </Grid>
          <Grid item xs={12} md={6}>
-            <GroupSparkAnalyticsCardContainer>
-               <GroupSparkAnalyticsCardContainerTitle>
-                  Top 10 universities
-               </GroupSparkAnalyticsCardContainerTitle>
-               <EmptyDataCheckerForBulletChart>
-                  <BulletChart
-                     data={updatedTopUniversities}
-                     valueIndexer="relativePercentage"
-                     showPercentageOnly
-                  />
-               </EmptyDataCheckerForBulletChart>
-            </GroupSparkAnalyticsCardContainer>
+            <SuspenseWithBoundary fallback={<TopBulletChartsSkeleton />}>
+               <TopUniversities timeFilter={timeFilter} />
+            </SuspenseWithBoundary>
          </Grid>
          <Grid item xs={12} md={6}>
-            <GroupSparkAnalyticsCardContainer>
-               <GroupSparkAnalyticsCardContainerTitle sx={{ marginBottom: 0 }}>
-                  Top 10 fields of study
-               </GroupSparkAnalyticsCardContainerTitle>
-               <EmptyDataCheckerForPieChart>
-                  <CFPieChart data={topFieldsOfStudy} />
-               </EmptyDataCheckerForPieChart>
-            </GroupSparkAnalyticsCardContainer>
+            <SuspenseWithBoundary fallback={<PieChartsSkeleton />}>
+               <TopFieldsOfStudy timeFilter={timeFilter} />
+            </SuspenseWithBoundary>
          </Grid>
          <Grid item xs={12} md={6}>
-            <GroupSparkAnalyticsCardContainer>
-               <GroupSparkAnalyticsCardContainerTitle sx={{ marginBottom: 0 }}>
-                  Level of study
-               </GroupSparkAnalyticsCardContainerTitle>
-               <EmptyDataCheckerForPieChart>
-                  <CFPieChart data={levelsOfStudy} />
-               </EmptyDataCheckerForPieChart>
-            </GroupSparkAnalyticsCardContainer>
+            <SuspenseWithBoundary fallback={<PieChartsSkeleton />}>
+               <LevelsOfStudy timeFilter={timeFilter} />
+            </SuspenseWithBoundary>
          </Grid>
       </Grid>
    )
