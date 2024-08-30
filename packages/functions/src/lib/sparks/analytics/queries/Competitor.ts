ed75d1de4054438669c14d8fb5a8336128762422
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-08-30 11:10:38 +0100

    feat(sparks-analytics-competitor): adds plays and avg watch time

--- packages/functions/src/lib/sparks/analytics/queries/Competitor.ts
+++ packages/functions/src/lib/sparks/analytics/queries/Competitor.ts
@@ -6,33 +6,72 @@ export function topSparksByIndustry(timePeriod: string) {
             JSON_EXTRACT_SCALAR(item, '$.id') as industry,
          FROM careerfairy-e1fd9.firestore_export.groups_schema_groups_latest,
          UNNEST(JSON_EXTRACT_ARRAY(companyIndustries)) AS item
+      ),
+
+      AvgWatchedTime AS (
+         SELECT
+            sparkId,
+            ROUND(AVG(videoEventPositionInSeconds), 2) as seconds
+         FROM careerfairy-e1fd9.SparkAnalytics.SparkSecondsWatched
+         GROUP BY sparkId
+      ),
+
+      Views AS (
+         SELECT 
+            sparkId, 
+            COUNT(sparkId) as value
+         FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
+         WHERE actionType = "Played_Spark"
+         GROUP BY sparkId
       )
 
       SELECT 
          SparkEvents.sparkId,
          GroupByIndustry.industry,
+         Views.value as plays,
+         AvgWatchedTime.seconds as avg_watched_time,
          ROUND(SUM(
-            CASE 
-               WHEN SparkEvents.actionType = "Played_Spark" THEN 0.02
-               WHEN SparkEvents.actionType = "Watched_CompleteSpark" THEN 0.05
-               WHEN SparkEvents.actionType = "Like" THEN 0.4
-               WHEN SparkEvents.actionType LIKE "Share_%" THEN 0.5
-               WHEN SparkEvents.actionType LIKE "Click_%" THEN 0.5
-               ELSE 0.0
-            END
+         CASE 
+            WHEN SparkEvents.actionType = "Played_Spark" THEN 0.02
+            WHEN SparkEvents.actionType = "Watched_CompleteSpark" THEN 0.05
+            WHEN SparkEvents.actionType = "Like" THEN 0.4
+            WHEN SparkEvents.actionType LIKE "Share_%" THEN 0.5
+            WHEN SparkEvents.actionType LIKE "Click_%" THEN 0.5
+            ELSE 0.0
+         END
          ), 2) AS engagement
       FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
       INNER JOIN GroupByIndustry
          ON SparkEvents.groupId = GroupByIndustry.groupId
-      WHERE actionType = "Played_Spark"
-         AND timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL ${timePeriod}))
-      GROUP BY SparkEvents.sparkId, GroupByIndustry.industry
-      ORDER BY count(sparkId) DESC
+      LEFT JOIN AvgWatchedTime
+         ON SparkEvents.sparkId = AvgWatchedTime.sparkId
+      LEFT JOIN Views
+         ON SparkEvents.sparkId = Views.sparkId
+      WHERE SparkEvents.timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL ${timePeriod}))
+      GROUP BY SparkEvents.sparkId, GroupByIndustry.industry, Views.value, AvgWatchedTime.seconds
+      ORDER BY count(sparkId) DESC;
   `
 }
 
 export function topSparksByAudience(timePeriod: string) {
    return `
+      WITH AvgWatchedTime AS (
+         SELECT
+            sparkId,
+            ROUND(AVG(videoEventPositionInSeconds), 2) as seconds
+         FROM careerfairy-e1fd9.SparkAnalytics.SparkSecondsWatched
+         GROUP BY sparkId
+      ),
+
+      Views AS (
+         SELECT 
+            sparkId, 
+            COUNT(sparkId) as value
+         FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents
+         WHERE actionType = "Played_Spark"
+         GROUP BY sparkId
+      )
+
       SELECT 
          SparkEvents.sparkId,
          CASE
@@ -53,6 +92,8 @@ export function topSparksByAudience(timePeriod: string) {
                THEN "social-sciences"
             ELSE "other"
          END as audience,
+         Views.value as plays,
+         AvgWatchedTime.seconds as avg_watched_time,
          ROUND(SUM(
             CASE 
                WHEN SparkEvents.actionType = "Played_Spark" THEN 0.02
@@ -66,10 +107,14 @@ export function topSparksByAudience(timePeriod: string) {
       FROM careerfairy-e1fd9.SparkAnalytics.SparkEvents as SparkEvents
       INNER JOIN careerfairy-e1fd9.firestore_export.userData_schema_userData_latest as userData
          ON SparkEvents.userId = userData.authId
+      LEFT JOIN AvgWatchedTime
+         ON SparkEvents.sparkId = AvgWatchedTime.sparkId
+      LEFT JOIN Views
+         ON SparkEvents.sparkId = Views.sparkId
       WHERE SparkEvents.timestamp >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL ${timePeriod}))
          AND userData.fieldOfStudy_id IS NOT NULL
-      GROUP BY SparkEvents.sparkId, audience
+      GROUP BY SparkEvents.sparkId, Views.value, AvgWatchedTime.seconds, audience
       HAVING engagement > 0.0
-      ORDER BY engagement DESC
+      ORDER BY engagement DESC;
    `
 }
