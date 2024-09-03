720d317770756031faf98d0f566366863f371c3a
goncaloarnauth <goncaloarnauth@outlook.com>
2024-09-03 09:43:38 +0100

    update partnership area of study mapping

--- packages/shared-lib/src/fieldOfStudy/fieldOfStudy.ts
+++ packages/shared-lib/src/fieldOfStudy/fieldOfStudy.ts
@@ -6,37 +6,85 @@ export type FieldOfStudy = Identifiable & {
 
 export type LevelOfStudy = FieldOfStudy
 
-
-export const mapPartnershipFieldsOfStudy = (partnerFieldOfStudy: string): string[] => {
+export const mapPartnershipFieldsOfStudy = (
+   partnerFieldOfStudy: string
+): string[] => {
    switch (partnerFieldOfStudy) {
-      case "wirtschaftswissenschaften" :
-         return ["business_administration_economics", "finance", "luxury_fashion", "marketing", "public_administration", "systems_science", "transportation"]
-      
-         case "ingenieurwissenschaften" :
-         return ["architecture_and_design", "business_engineering", "chemical_engineering", "civil_engineering", "electrical_engineering", "materials_science_and_engineering", "mechanical_engineering", "space_sciences"]
-      
-         case "sprach_kulturwissenschaften" :
-         return ["journalism_media_studies_and_communication", "linguistics_and_languages"]
-      
-         case "matematik_naturwissenschaften" :
-         return ["astronomy", "biology", "chemistry", "computer_science", "earth_sciences", "life_sciences", "mathematics"]
-      
-         case "medizin_gesundheitswissenschaften_psychologie_sport" :
-         return ["human_physical_performance_and_recreation", "medicine", "psychology"]
-      
-         case "rechts_sozialwissenschaften" :
-         return ["anthropology", "archaeology", "divinity", "history", "law", "military_sciences", "philosophy", "political_science", "religion", "sociology"]
-      
-         case "kunst_musik" :
-         return ["literature_arts"]
-      
-         case "erziehungs_bildungswissenschaften_lehrämter" :
-         return ["education"]
-      
-         case "agar_forst_ernährungswissenschaften" :
-         return ["agriculture", "environmental_studies_and_forestry", "geography"]
+      case "wirtschaftswissenschaften":
+         return [
+            "business_administration_economics",
+            "finance",
+            "luxury_fashion",
+            "marketing",
+            "public_administration",
+            "systems_science",
+            "transportation",
+         ]
 
-         default:
-            return []
+      case "ingenieurwissenschaften":
+         return [
+            "architecture_and_design",
+            "business_engineering",
+            "chemical_engineering",
+            "civil_engineering",
+            "electrical_engineering",
+            "materials_science_and_engineering",
+            "mechanical_engineering",
+            "space_sciences",
+         ]
+
+      case "sprach_kulturwissenschaften":
+         return [
+            "journalism_media_studies_and_communication",
+            "linguistics_and_languages",
+         ]
+
+      case "mathematik_naturwissenschaften":
+         return [
+            "astronomy",
+            "biology",
+            "chemistry",
+            "computer_science",
+            "earth_sciences",
+            "life_sciences",
+            "mathematics",
+         ]
+
+      case "medizin_gesundheitswissenschaften_psychologie_sport":
+         return [
+            "human_physical_performance_and_recreation",
+            "medicine",
+            "psychology",
+         ]
+
+      case "rechts_sozialwissenschaften":
+         return [
+            "anthropology",
+            "archaeology",
+            "divinity",
+            "history",
+            "law",
+            "military_sciences",
+            "philosophy",
+            "political_science",
+            "religion",
+            "sociology",
+         ]
+
+      case "kunst_musik":
+         return ["literature_arts"]
+
+      case "erziehungs_bildungswissenschaften_lehrämter":
+         return ["education"]
+
+      case "agrar_forst_ernährungswissenschaften":
+         return [
+            "agriculture",
+            "environmental_studies_and_forestry",
+            "geography",
+         ]
+
+      default:
+         return []
    }
 }
