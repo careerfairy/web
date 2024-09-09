4a56d52e80ff94155c98c2a81f5e40d792dbc000
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-09-09 13:24:22 +0100

    refactor(SparksOverviewTab): renames file and makes use of skeleton when loading data

---
+++ apps/web/components/views/admin/sparks/analytics/overview-tab/index.tsx
@@ -0,0 +1,32 @@
+import { useAnalyticsLocking } from "components/custom-hook/spark/analytics/useAnalyticsLocking"
+import { SuspenseWithBoundary } from "components/ErrorBoundary"
+import { LockedSparksAnalytics } from "../components/locking/LockedSparksAnalytics"
+import { SectionsWrapper } from "../components/SectionsWrapper"
+import { useSparksAnalytics } from "../SparksAnalyticsContext"
+import { EngagementAnalyticsContainer } from "./EngagementAnalyticsContainer"
+import { MostSomethingAnalyticsContainer } from "./MostSomethingAnalyticsContainer"
+import { ReachAnalyticsContainer } from "./ReachAnalyticsContainer"
+import { SparksOverviewTabSkeleton } from "./SparksOverviewTabSkeleton"
+
+export const SparksOverviewTab = () => {
+   const { isLocked } = useAnalyticsLocking("overview")
+   const { isLoading } = useSparksAnalytics()
+
+   if (isLocked) {
+      return <LockedSparksAnalytics />
+   }
+
+   if (isLoading) {
+      return <SparksOverviewTabSkeleton />
+   }
+
+   return (
+      <SuspenseWithBoundary fallback={<SparksOverviewTabSkeleton />}>
+         <SectionsWrapper>
+            <ReachAnalyticsContainer />
+            <EngagementAnalyticsContainer />
+            <MostSomethingAnalyticsContainer />
+         </SectionsWrapper>
+      </SuspenseWithBoundary>
+   )
+}
