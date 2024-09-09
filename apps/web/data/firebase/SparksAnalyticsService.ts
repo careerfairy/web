af46526b0538208f57e63e90205b43c272092aac
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-09-09 13:13:07 +0100

    feat(SparksAnalyticsService): adds service for sparks analytics

---
+++ apps/web/data/firebase/SparksAnalyticsService.ts
@@ -0,0 +1,26 @@
+import { SparksAnalyticsDTO } from "@careerfairy/shared-lib/sparks/analytics"
+import { Functions, httpsCallable } from "firebase/functions"
+import { FunctionsInstance } from "./FirebaseInstance"
+
+export class SparksAnalyticsService {
+   constructor(private readonly functions: Functions) {}
+
+   async fetchSparksAnalytics(groupId: string, forceUpdate: boolean) {
+      const { data: analytics } = await httpsCallable<
+         { groupId: string; forceUpdate: boolean },
+         SparksAnalyticsDTO
+      >(
+         this.functions,
+         "getSparksAnalytics_v4"
+      )({ groupId, forceUpdate })
+
+      return analytics
+   }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const sparksAnalyticsService = new SparksAnalyticsService(
+   FunctionsInstance as any
+)
+
+export default SparksAnalyticsService
