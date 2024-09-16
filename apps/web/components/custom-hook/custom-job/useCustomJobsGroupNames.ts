e16bf325ee11ffbd73fee379fb44b3b3e7ab9faa
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-09-16 18:57:13 +0200

    improved options in useCustomJobsGroupNames

--- apps/web/components/custom-hook/custom-job/useCustomJobsGroupNames.ts
+++ apps/web/components/custom-hook/custom-job/useCustomJobsGroupNames.ts
@@ -1,4 +1,5 @@
 import { CustomJob } from "@careerfairy/shared-lib/customJobs/customJobs"
+import { useMemo } from "react"
 import useSWR from "swr"
 import useFunctionsSWR, {
    reducedRemoteCallsOptions,
@@ -8,13 +9,19 @@ type Result = {
    [jobId: string]: string
 }
 const useCustomJobsGroupNames = (customJobs: CustomJob[]) => {
-   const options: { [jobId: string]: { groupId: string } } = {}
-
-   customJobs.forEach((job) => {
-      options[job.id] = {
-         groupId: job.groupId,
-      }
-   })
+   const options: { [jobId: string]: { groupId: string } } = useMemo(() => {
+      return customJobs
+         .map((job) => {
+            return {
+               [job.id]: {
+                  groupId: job.groupId,
+               },
+            }
+         })
+         .reduce((acc, curr) => {
+            return { ...acc, ...curr } // Merge the current object into the accumulator
+         }, {})
+   }, [customJobs])
 
    const fetcher = useFunctionsSWR<Result>()
 
