7cf5241cd75995a56542b2db6135d8e5f2c9c2c3
goncaloarnauth <goncaloarnauth@outlook.com>
2024-09-17 14:12:38 +0100

    update fetchRequiredData default values
    (cherry picked from commit 4dae096f75a0401e705ff7be29960fd5dff39619)

--- packages/functions/src/lib/newsletter/services/NewsletterService.ts
+++ packages/functions/src/lib/newsletter/services/NewsletterService.ts
@@ -103,23 +103,27 @@ export class NewsletterService {
     * @returns UserData[] - Filtered users according to onboarding step and tolerance for the last notification
     */
    private async filterUsers(users: UserData[]): Promise<UserData[]> {
-      const promises = users.map(async (user) => {
-         const notifications =
-            await this.notificationsRepo.getUserReceivedNotifications(
-               user.userEmail
-            )
-         return {
-            user: user,
-            notifications: notifications,
-         }
-      })
-      const usersDataItems = (await Promise.all(promises)) ?? []
-      const filteredData = usersDataItems.filter((userData) =>
-         this.isSent(userData.notifications, [
-            "livestream1stRegistrationDiscovery",
-         ])
-      )
-      return filteredData.map((userData) => userData.user)
+      if (users?.length) {
+         const promises = users.map(async (user) => {
+            const notifications =
+               await this.notificationsRepo.getUserReceivedNotifications(
+                  user.userEmail
+               )
+            return {
+               user: user,
+               notifications: notifications,
+            }
+         })
+         const usersDataItems = (await Promise.all(promises)) ?? []
+         const filteredData = usersDataItems.filter((userData) =>
+            this.isSent(userData.notifications, [
+               "livestream1stRegistrationDiscovery",
+            ])
+         )
+         return filteredData.map((userData) => userData.user)
+      }
+
+      return []
    }
 
    /**
@@ -136,9 +140,16 @@ export class NewsletterService {
       const [subscribedUsers, futureLivestreams, pastLivestreams] =
          await Promise.all(promises)
 
-      const filteredUsers = await this.filterUsers(
-         subscribedUsers?.length ? subscribedUsers : ([] as UserData[])
+      const filteredUsers =
+         (await this.filterUsers(
+            subscribedUsers?.length ? subscribedUsers : ([] as UserData[])
+         )) ?? []
+
+      console.log(
+         "🚀 ~ NewsletterService ~ fetchRequiredData ~ filteredUsers:",
+         filteredUsers
       )
+
       this.subscribedUsers = convertDocArrayToDict(filteredUsers)
       this.logger.info(
          "NewsletterService ~ fetchRequiredData ~ subscribedUsers:",
