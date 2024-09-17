56a3d3812af5ee3bbe3132dc889855ab501539fb
goncaloarnauth <goncaloarnauth@outlook.com>
2024-09-17 11:44:56 +0100

    set memory space on manualNewsletter function; Add default values on NewsletterService;
    (cherry picked from commit be7bb596a8396664d9ac1a675b6d6b51eaf992ac)

--- packages/functions/src/lib/newsletter/services/NewsletterService.ts
+++ packages/functions/src/lib/newsletter/services/NewsletterService.ts
@@ -113,7 +113,7 @@ export class NewsletterService {
             notifications: notifications,
          }
       })
-      const usersDataItems = await Promise.all(promises)
+      const usersDataItems = (await Promise.all(promises)) ?? []
       const filteredData = usersDataItems.filter((userData) =>
          this.isSent(userData.notifications, [
             "livestream1stRegistrationDiscovery",
@@ -137,7 +137,7 @@ export class NewsletterService {
          await Promise.all(promises)
 
       const filteredUsers = await this.filterUsers(
-         subscribedUsers as UserData[]
+         subscribedUsers?.length ? subscribedUsers : ([] as UserData[])
       )
       this.subscribedUsers = convertDocArrayToDict(filteredUsers)
       this.logger.info(
@@ -147,8 +147,8 @@ export class NewsletterService {
          )
       )
 
-      this.futureLivestreams = futureLivestreams
-      this.pastLivestreams = pastLivestreams
+      this.futureLivestreams = futureLivestreams ?? []
+      this.pastLivestreams = pastLivestreams ?? []
 
       this.futureLivestreams = this.futureLivestreams.filter((l) => {
          // filter out livestreams before now, the bundle might have events for the same day
@@ -158,13 +158,16 @@ export class NewsletterService {
 
       this.logger.info(
          "Total Users subscribed to the newsletter",
-         subscribedUsers.length
+         subscribedUsers?.length ?? 0
       )
       this.logger.info(
          "Total Future Livestreams fetched",
-         futureLivestreams.length
+         futureLivestreams?.length ?? 0
       )
-      this.logger.info("Total Past Livestreams fetched", pastLivestreams.length)
+      this.logger.info(
+         "Total Past Livestreams fetched",
+         pastLivestreams?.length ?? 0
+      )
 
       return this
    }
@@ -263,25 +266,27 @@ export class NewsletterService {
       for (const userEmail of emails) {
          const user = this.users[userEmail]
 
-         if (user.recommendedLivestreams.length < 3) {
-            // we need at least 3 recommended livestreams to send the newsletter
-            usersWithoutMinimumRecommendedLivestreams.push(userEmail)
-            continue
+         if (user) {
+            if (user.recommendedLivestreams.length < 3) {
+               // we need at least 3 recommended livestreams to send the newsletter
+               usersWithoutMinimumRecommendedLivestreams.push(userEmail)
+               continue
+            }
+
+            const name = this.subscribedUsers[userEmail]?.firstName ?? ""
+
+            const followingLivestreams = Object.values(user.followingCompanies)
+               .map((g) => g.livestreams)
+               .flat()
+               .sort(sortLivestreamsDesc)
+
+            this.emailBuilder.addRecipient(
+               userEmail,
+               name,
+               followingLivestreams,
+               user.recommendedLivestreams
+            )
          }
-
-         const name = this.subscribedUsers[userEmail]?.firstName ?? ""
-
-         const followingLivestreams = Object.values(user.followingCompanies)
-            .map((g) => g.livestreams)
-            .flat()
-            .sort(sortLivestreamsDesc)
-
-         this.emailBuilder.addRecipient(
-            userEmail,
-            name,
-            followingLivestreams,
-            user.recommendedLivestreams
-         )
       }
 
       this.logger.info(
