8955c575a393f7957f7766d905e0c75b29fdc8ae
Walter Gonçalves <152179257+wgoncalvescf@users.noreply.github.com>
2024-10-03 17:43:33 +0200

    Portal Job banner (#1309)
    ### Motivation
    > From [issue](https://linear.app/careerfairy/issue/CF-1083/add-job-banner-to-portal-page): To announce the job hub launch we will add a new banner in the carousel at the top of portal page. The same logic of the Sparks one can be applied:
    > - Banner always to be shown for logged out users
    > - Banner to be shown on 5 different days for logged in users which have not applied to a job yet
    
    <!-- A little context about this change. -->
    
    ### Changes
    
    -  Created new component for Jobs CTA banner
    - Updated user data type for containing new property `jobsBannerCTADates`
    
    <!-- You can remove anything that doesn't make sense for this change. -->
    
    
    
    ### Browser Testing
    
    -  [x] Chrome
    -  [ ] Firefox
    -  [ ] Safari
    
    ### Different Resolutions Testing
    
    -  [x] Large Screens
    -  [ ] Medium Screens
    -  [x] Small Screens

--- apps/web/components/views/portal/content-carousel/ContentCarousel.tsx
+++ apps/web/components/views/portal/content-carousel/ContentCarousel.tsx
@@ -20,6 +20,7 @@ import {
    CarouselContent,
 } from "./CarouselContentService"
 import ContentCarouselPagination from "./ContentCarouselPagination"
+import { DiscoverJobsCTAContent } from "./DiscoverJobsCTAContent"
 import LivestreamContent from "./LivestreamContent"
 import WatchSparksCTAContent from "./WatchSparksCTAContent"
 
@@ -198,6 +199,9 @@ const getCTASlide = (contentItem: CTASlide) => {
       }
       case CTASlideTopics.Sparks: {
          return <WatchSparksCTAContent cta={contentItem} />
+      }
+      case CTASlideTopics.Jobs: {
+         return <DiscoverJobsCTAContent cta={contentItem} />
       }
       default: {
          return null
