38b547a0c9b9b61e5ead5738f42c1e6caf21d2b5
Habib Kadiri <habib@careerfairy.io>
2024-10-10 11:18:43 +0200

    Hotfix: Add Applying To Custom Jobs in Stream (#1328)
    * refactor(common/jobs): introduce context provider for job details management
    
    Refactor the CustomJobDetailsDialog by utilizing a context provider to manage state and logic related to custom job details. This improves modularity and simplifies the component by encapsulating related hooks and logic within a provider, leading to cleaner and more maintainable code. Also, added conditional rendering controls for linked livestreams and sparks, enhancing flexibility in job details presentation.
    
    * refactor(common/jobs): move hideLinked properties to Content component
    
    Transferred `hideLinkedLivestreams` and `hideLinkedSparks` properties from `CustomJobDetailsProvider` to `Content` component to simplify context management. This reduces unnecessary context passing, aligning property usage closer to where they are needed.

--- apps/web/components/views/streaming-page/components/end-of-stream/Jobs.tsx
+++ apps/web/components/views/streaming-page/components/end-of-stream/Jobs.tsx
@@ -1,14 +1,17 @@
 import { Job } from "@careerfairy/shared-lib/ats/Job"
-import { CustomJob } from "@careerfairy/shared-lib/customJobs/customJobs"
+import {
+   CustomJob,
+   CustomJobApplicationSourceTypes,
+} from "@careerfairy/shared-lib/customJobs/customJobs"
 import { Skeleton, Stack } from "@mui/material"
 import { SuspenseWithBoundary } from "components/ErrorBoundary"
+import useGroupCustomJobs from "components/custom-hook/custom-job/useGroupCustomJobs"
 import useLivestreamCompanyHostSWR from "components/custom-hook/live-stream/useLivestreamCompanyHostSWR"
-import { useCombinedJobs } from "components/custom-hook/streaming/useCombinedJobs"
+import CustomJobDetailsDialog from "components/views/common/jobs/CustomJobDetailsDialog"
 import JobCard from "components/views/common/jobs/JobCard"
 import { useCallback, useState } from "react"
 import { dataLayerEvent } from "util/analyticsUtils"
 import { useStreamingContext } from "../../context"
-import JobDialog from "../jobs/JobDialog"
 import { JobCardSkeleton } from "../jobs/JobListSkeleton"
 import { EndOfStreamContainer } from "./Container"
 import { Heading } from "./Heading"
@@ -35,20 +38,20 @@ type ContentProps = {
 }
 export const Content = ({ groupId }: ContentProps) => {
    const { livestreamId } = useStreamingContext()
-   const jobsToShow = useCombinedJobs(livestreamId, groupId)
-   const [selectedJob, setSelectedJob] = useState<Job | CustomJob | null>(null)
+   const jobsToShow = useGroupCustomJobs(groupId, {
+      livestreamId: livestreamId,
+   })
+   const [selectedJob, setSelectedJob] = useState<CustomJob | null>(null)
 
    const onCloseDialog = () => {
       setSelectedJob(null)
    }
 
-   const handleClick = useCallback((job: Job | CustomJob) => {
-      const jobName = (job as Job)?.name ?? (job as CustomJob)?.title
-
+   const handleClick = useCallback((job: CustomJob) => {
       setSelectedJob(job)
       dataLayerEvent("livestream_job_open", {
          jobId: job.id,
-         jobName: jobName,
+         jobName: job.title,
       })
    }, [])
 
@@ -70,11 +73,21 @@ export const Content = ({ groupId }: ContentProps) => {
             ))}
          </Stack>
          {selectedJob ? (
-            <JobDialog
-               livestreamId={livestreamId}
-               job={selectedJob}
-               handleDialogClose={onCloseDialog}
-               open={Boolean(selectedJob)}
+            <CustomJobDetailsDialog
+               customJobId={selectedJob.id}
+               isOpen={Boolean(selectedJob)}
+               onClose={onCloseDialog}
+               source={{
+                  source: CustomJobApplicationSourceTypes.Livestream,
+                  id: livestreamId,
+               }}
+               heroContent={
+                  <CustomJobDetailsDialog.CloseButton onClose={onCloseDialog} />
+               }
+               heroSx={{
+                  pb: "8px !important",
+               }}
+               hideLinkedLivestreams
             />
          ) : null}
       </EndOfStreamContainer>
