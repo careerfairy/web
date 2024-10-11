df9e1053486f035ac5b259e04d905ffde7e4beb4
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-10-11 15:30:32 +0100

    feat(useMediaPermissions): adds custom hook to detect camera and mic permission state

---
+++ apps/web/components/views/streaming-page/components/StreamSetupWidget/useMediaPermissions.tsx
@@ -0,0 +1,36 @@
+import { useLocalTracks } from "../../context"
+
+enum PermissionType {
+   Accepted = "accepted",
+   Denied = "denied",
+   Prompting = "prompting",
+   Unknown = "unknown",
+}
+
+function getPermission(activeId: string | undefined, error: Error | undefined) {
+   if (activeId && !error) {
+      return PermissionType.Accepted
+   } else if (!activeId && error) {
+      return PermissionType.Denied
+   } else if (!activeId && !error) {
+      return PermissionType.Prompting
+   }
+
+   return PermissionType.Unknown
+}
+
+export const useMediaPermissions = () => {
+   const {
+      activeMicrophoneId,
+      fetchMicsError,
+      activeCameraId,
+      fetchCamerasError,
+   } = useLocalTracks()
+
+   const permissions = {
+      microphone: getPermission(activeMicrophoneId, fetchMicsError),
+      camera: getPermission(activeCameraId, fetchCamerasError),
+   }
+
+   return permissions
+}
