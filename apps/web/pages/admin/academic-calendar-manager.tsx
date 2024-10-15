4e0da82e0b38eaed51f336611f6a3eeb0a784f42
Sead Puzic <puzic.sead@gmail.com>
2024-10-15 15:58:16 +0200

    Implement Titles for the CF Admin dashboard

--- apps/web/pages/admin/academic-calendar-manager.tsx
+++ apps/web/pages/admin/academic-calendar-manager.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 import AdminDashboardLayout from "../../layouts/AdminDashboardLayout"
 import CalendarManager from "components/views/calendar/management/CalendarManager"
+import Head from "next/head"
 
 const AcademicCalendarManager = () => {
    return (
-      <AdminDashboardLayout>
-         <CalendarManager />
-      </AdminDashboardLayout>
+      <>
+         <Head>
+            <title>CareerFairy | Academic Calendar Management</title>
+         </Head>
+         <AdminDashboardLayout>
+            <CalendarManager />
+         </AdminDashboardLayout>
+      </>
    )
 }
 
