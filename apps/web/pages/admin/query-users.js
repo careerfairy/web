a3e2eae8801cbdc668cee11c179fd82cbc93591b
Sead Puzic <puzic.sead@gmail.com>
2024-10-16 16:38:47 +0200

    Revert of admin pages
    Revert of admin pages

--- apps/web/pages/admin/query-users.js
+++ apps/web/pages/admin/query-users.js
@@ -1,18 +1,12 @@
 import React from "react"
 import AdminDashboardLayout from "../../layouts/AdminDashboardLayout"
 import QueryUsersOverview from "../../components/views/admin/query-users"
-import Head from "next/head"
 
 const QueryDataPage = () => {
    return (
-      <>
-         <Head>
-            <title>CareerFairy | User Query Tool</title>
-         </Head>
-         <AdminDashboardLayout>
-            <QueryUsersOverview />
-         </AdminDashboardLayout>
-      </>
+      <AdminDashboardLayout>
+         <QueryUsersOverview />
+      </AdminDashboardLayout>
    )
 }
 
