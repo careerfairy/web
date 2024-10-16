dae219cfbf0eb275a0bc4f656466d6abdb82dc91
Sead Puzic <puzic.sead@gmail.com>
2024-10-16 16:38:47 +0200

    Revert of admin pages
    Revert of admin pages

--- apps/web/pages/admin/past-livestreams.js
+++ apps/web/pages/admin/past-livestreams.js
@@ -2,18 +2,12 @@ import React from "react"
 import AdminDashboardLayout from "../../layouts/AdminDashboardLayout"
 import AdminStreams from "../../components/views/admin/streams"
 import { withFirebase } from "../../context/firebase/FirebaseServiceContext"
-import Head from "next/head"
 
 const PastStreamsPage = () => {
    return (
-      <>
-         <Head>
-            <title>CareerFairy | Past Live Streams Archive</title>
-         </Head>
-         <AdminDashboardLayout>
-            <AdminStreams typeOfStream="past" />
-         </AdminDashboardLayout>
-      </>
+      <AdminDashboardLayout>
+         <AdminStreams typeOfStream="past" />
+      </AdminDashboardLayout>
    )
 }
 
--- apps/web/pages/admin/upcoming-livestreams.js
+++ apps/web/pages/admin/upcoming-livestreams.js
@@ -2,18 +2,12 @@ import React from "react"
 import AdminDashboardLayout from "../../layouts/AdminDashboardLayout"
 import AdminStreams from "../../components/views/admin/streams"
 import { withFirebase } from "../../context/firebase/FirebaseServiceContext"
-import Head from "next/head"
 
 const UpcomingStreamsPage = () => {
    return (
-      <>
-         <Head>
-            <title>CareerFairy | Upcoming Live Streams Management</title>
-         </Head>
-         <AdminDashboardLayout>
-            <AdminStreams typeOfStream="upcoming" />
-         </AdminDashboardLayout>
-      </>
+      <AdminDashboardLayout>
+         <AdminStreams typeOfStream="upcoming" />
+      </AdminDashboardLayout>
    )
 }
 
