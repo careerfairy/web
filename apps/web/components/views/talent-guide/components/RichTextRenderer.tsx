27f8266d035c4ef081796a067bbdb5b56e02bd8c
habibkadiri <kadirit@hotmail.com>
2024-10-17 16:34:30 +0200

    feat(renderer): optimize rich text rendering and improve mobile support
    Refactor rich text rendering to leverage default component creation, enhancing performance with `useMemo` and better support for mobile styling. Move heading styles and embedding components into a separate module for enhanced modularity. Add custom Image Loader for `@graphcms` asset handling and update next.js config to whitelist new image source domains.

--- apps/web/components/views/talent-guide/components/RichTextRenderer.tsx
+++ apps/web/components/views/talent-guide/components/RichTextRenderer.tsx
@@ -1,7 +1,5 @@
-import { RichText } from "@graphcms/rich-text-react-renderer"
+import { NodeRendererType, RichText } from "@graphcms/rich-text-react-renderer"
 import { EmbedProps } from "@graphcms/rich-text-types"
-import { Typography } from "@mui/material"
-import { GraphCMSImageLoader } from "components/cms/util"
 import useIsMobile from "components/custom-hook/useIsMobile"
 import {
    ArticleBlockType,
@@ -15,7 +13,7 @@ import {
    QuizBlockType,
    SparksCarouselBlockType,
 } from "data/hygraph/types"
-import Image from "next/image"
+import { useMemo } from "react"
 import {
    ArticleBlock,
    FollowCompaniesBlock,
@@ -27,6 +25,7 @@ import {
    QuizBlock,
    SparksCarouselBlock,
 } from "../blocks"
+import { createDefaultRichTextComponents } from "./default-rich-text-components"
 
 type RichTextRendererProps = Pick<ModuleStepType, "content">
 
@@ -35,94 +34,47 @@ export const ModuleStepContentRenderer = ({
 }: RichTextRendererProps) => {
    const isMobile = useIsMobile()
 
-   console.log("🚀 ~ file: RichTextRenderer.tsx:36 ~ content:", content)
+   const renderers = useMemo<NodeRendererType>(
+      () => ({
+         ...createDefaultRichTextComponents(isMobile),
+         embed: {
+            ArticleBlock: (props: EmbedProps<ArticleBlockType>) => (
+               <ArticleBlock {...props} />
+            ),
+            MentorsCarouselBlock: (
+               props: EmbedProps<MentorsCarouselBlockType>
+            ) => <MentorsCarouselBlock {...props} />,
+            LivestreamsCarouselBlock: (
+               props: EmbedProps<LivestreamsCarouselBlockType>
+            ) => <LivestreamsCarouselBlock {...props} />,
+            HighlightsBlock: (props: EmbedProps<HighlightsBlockType>) => (
+               <HighlightsBlock {...props} />
+            ),
+            FollowCompaniesBlock: (
+               props: EmbedProps<FollowCompaniesBlockType>
+            ) => <FollowCompaniesBlock {...props} />,
+            HeaderBlock: (props: EmbedProps<HeaderBlockType>) => (
+               <HeaderBlock {...props} />
+            ),
+            JobsBlock: (props: EmbedProps<JobsBlockType>) => (
+               <JobsBlock {...props} />
+            ),
+            SparksCarouselBlock: (
+               props: EmbedProps<SparksCarouselBlockType>
+            ) => <SparksCarouselBlock {...props} />,
+            QuizBlock: (props: EmbedProps<QuizBlockType>) => (
+               <QuizBlock {...props} />
+            ),
+         },
+      }),
+      [isMobile]
+   )
 
    return (
       <RichText
          content={content.raw}
          references={content.references}
-         renderers={{
-            embed: {
-               ArticleBlock: (props: EmbedProps<ArticleBlockType>) => (
-                  <ArticleBlock {...props} />
-               ),
-               MentorsCarouselBlock: (
-                  props: EmbedProps<MentorsCarouselBlockType>
-               ) => <MentorsCarouselBlock {...props} />,
-               LivestreamsCarouselBlock: (
-                  props: EmbedProps<LivestreamsCarouselBlockType>
-               ) => <LivestreamsCarouselBlock {...props} />,
-               HighlightsBlock: (props: EmbedProps<HighlightsBlockType>) => (
-                  <HighlightsBlock {...props} />
-               ),
-               FollowCompaniesBlock: (
-                  props: EmbedProps<FollowCompaniesBlockType>
-               ) => <FollowCompaniesBlock {...props} />,
-               HeaderBlock: (props: EmbedProps<HeaderBlockType>) => (
-                  <HeaderBlock {...props} />
-               ),
-               JobsBlock: (props: EmbedProps<JobsBlockType>) => (
-                  <JobsBlock {...props} />
-               ),
-               SparksCarouselBlock: (
-                  props: EmbedProps<SparksCarouselBlockType>
-               ) => <SparksCarouselBlock {...props} />,
-               QuizBlock: (props: EmbedProps<QuizBlockType>) => (
-                  <QuizBlock {...props} />
-               ),
-            },
-            h1: ({ children }) => (
-               <Typography
-                  variant={isMobile ? "desktopBrandedH1" : "mobileBrandedH1"}
-                  component="h1"
-               >
-                  {children}
-               </Typography>
-            ),
-            h2: ({ children }) => (
-               <Typography
-                  variant={isMobile ? "desktopBrandedH2" : "mobileBrandedH2"}
-                  component="h2"
-               >
-                  {children}
-               </Typography>
-            ),
-            h3: ({ children }) => (
-               <Typography
-                  variant={isMobile ? "desktopBrandedH3" : "mobileBrandedH3"}
-                  component="h3"
-               >
-                  {children}
-               </Typography>
-            ),
-            h4: ({ children }) => (
-               <Typography
-                  variant={isMobile ? "desktopBrandedH4" : "mobileBrandedH4"}
-                  component="h4"
-               >
-                  {children}
-               </Typography>
-            ),
-            h5: ({ children }) => (
-               <Typography variant={"brandedH5"} component="h5">
-                  {children}
-               </Typography>
-            ),
-            h6: ({ children }) => (
-               <Typography variant="h6" component="h6">
-                  {children}
-               </Typography>
-            ),
-            img: ({ title, altText, height, width, src }) => (
-               <Image
-                  alt={altText || title}
-                  height={height}
-                  width={width}
-                  loader={GraphCMSImageLoader}
-                  src={src}
-               />
-            ),
-         }}
+         renderers={renderers}
       />
    )
 }
