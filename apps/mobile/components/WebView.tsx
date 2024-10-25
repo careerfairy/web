87c0c3dc2aafbab33d37bad1f05dfe9dcf74a655
Sead Puzic <puzic.sead@gmail.com>
2024-10-25 12:26:02 +0200

    Updated comments
    Updated comments

--- apps/mobile/components/WebView.tsx
+++ apps/mobile/components/WebView.tsx
@@ -11,16 +11,14 @@ import * as Notifications from "expo-notifications"
 import * as SecureStore from "expo-secure-store"
 import { BASE_URL, SEARCH_CRITERIA } from "@env"
 import {
-   USER_AUTH,
-   USER_DATA,
-} from "@careerfairy/webapp/scripts/mobile_communication"
-import {
-   HAPTIC,
    MESSAGING_TYPE,
-   NativeEvent,
-   NativeEventStringified,
+   USER_AUTH,
+   HAPTIC,
    PERMISSIONS,
-} from "../constants/constants"
+   NativeEventStringified,
+   NativeEvent,
+} from "@careerfairy/shared-lib/src/messaging"
+import { UserData } from "@careerfairy/shared-lib/src/users"
 
 Notifications.setNotificationHandler({
    handleNotification: async () => ({
@@ -29,9 +27,10 @@ Notifications.setNotificationHandler({
       shouldSetBadge: false,
    }),
 })
+
 interface WebViewScreenProps {
-   onTokenInjected: () => void // Callback prop
-   onPermissionsNeeded: (permissions: string[]) => void // Callback prop
+   onTokenInjected: () => void
+   onPermissionsNeeded: (permissions: string[]) => void
    onLogout: (userId: string) => void
 }
 
@@ -81,13 +80,10 @@ const WebViewComponent: React.FC<WebViewScreenProps> = ({
 
    const handleMessage = (event: NativeEventStringified) => {
       try {
-         // Parse the received message
          const receivedData = JSON.parse(event.nativeEvent.data)
 
-         // Access the `type` and `data` fields
          const { type, data } = receivedData as NativeEvent
 
-         // Handling operations according to defined types
          switch (type) {
             case MESSAGING_TYPE.USER_AUTH:
                return handleUserAuth(data as USER_AUTH)
@@ -127,7 +123,7 @@ const WebViewComponent: React.FC<WebViewScreenProps> = ({
       try {
          const userData = await SecureStore.getItemAsync("userData")
          if (userData) {
-            const user: USER_DATA = JSON.parse(userData)
+            const user: UserData = JSON.parse(userData)
             onLogout(user.id)
          }
          await SecureStore.deleteItemAsync("authToken")
@@ -152,7 +148,6 @@ const WebViewComponent: React.FC<WebViewScreenProps> = ({
       }
    }, [])
 
-   // Handling of back button press inside the webview for natural native behavior
    const handleBackButtonPress = () => {
       if (webViewRef.current) {
          webViewRef.current.goBack()
@@ -167,7 +162,6 @@ const WebViewComponent: React.FC<WebViewScreenProps> = ({
       return regex.test(url)
    }
 
-   // Handling opening of external links in default mobile browser
    const handleNavigation = (request: any) => {
       if (!request.url.includes(SEARCH_CRITERIA)) {
          if (isValidUrl(request.url)) {
--- apps/mobile/env.d.ts
+++ apps/mobile/env.d.ts
@@ -2,4 +2,11 @@ declare module "@env" {
    export const BASE_URL: string
    export const SEARCH_CRITERIA: string
    export const PROJECT_ID: string
+   export const API_KEY: string
+   export const AUTH_DOMAIN: string
+   export const DATABASE_URL: string
+   export const FB_PROJECT_ID: string
+   export const STORAGE_BUCKET: string
+   export const MESSAGING_SENDER_ID: string
+   export const APPS_ID: string
 }
