e4f147348692cf5f127d06de68180099864bb4cd
Sead Puzic <puzic.sead@gmail.com>
2024-10-29 10:36:33 +0100

    Update of profile dropdown
    Update of profile dropdown

--- apps/web/layouts/GenericDashboardLayout/TopBar/ProfileMenu.tsx
+++ apps/web/layouts/GenericDashboardLayout/TopBar/ProfileMenu.tsx
@@ -169,14 +169,6 @@ const ProfileMenu = () => {
                      My career skills
                   </Typography>
                </MenuItem>
-               <MenuItem
-                  onClick={() => push(`/streaming/host/RjrqWmsgzq79CaDGDwrs`)}
-               >
-                  <ListItemIcon>
-                     <PersonOutlineOutlinedIcon fontSize="small" />
-                  </ListItemIcon>
-                  Livestream
-               </MenuItem>
                <MenuItem onClick={() => push("/profile/referrals")}>
                   <ListItemIcon>
                      <PeopleOutlinedIcon fontSize="small" />
