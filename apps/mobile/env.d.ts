5353c5a603d8a344061ac0a510b54f0a580e685b
Sead Puzic <puzic.sead@gmail.com>
2024-11-05 00:25:43 +0100

    Fixed comments
    Fixed comments

--- apps/mobile/env.d.ts
+++ apps/mobile/env.d.ts
@@ -3,11 +3,11 @@ declare module "@env" {
    export const SEARCH_CRITERIA: string
    export const INCLUDES_PERMISSIONS: string
    export const PROJECT_ID: string
-   export const API_KEY: string
-   export const AUTH_DOMAIN: string
-   export const DATABASE_URL: string
-   export const FB_PROJECT_ID: string
-   export const STORAGE_BUCKET: string
-   export const MESSAGING_SENDER_ID: string
-   export const APPS_ID: string
+   export const FIREBASE_API_KEY: string
+   export const FIREBASE_AUTH_DOMAIN: string
+   export const FIREBASE_DATABASE_URL: string
+   export const FIREBASE_PROJECT_ID: string
+   export const FIREBASE_STORAGE_BUCKET: string
+   export const FIREBASE_MESSAGING_SENDER_ID: string
+   export const FIREBASE_APPS_ID: string
 }
