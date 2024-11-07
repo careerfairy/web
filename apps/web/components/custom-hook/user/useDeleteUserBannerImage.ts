8f00a3b8a8ad742b9e0c52c2df42b5de4b746049
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-11-07 18:08:44 +0100

    delete image from folders and minor styles improvements

--- apps/web/components/custom-hook/user/useDeleteUserBannerImage.ts
+++ apps/web/components/custom-hook/user/useDeleteUserBannerImage.ts
@@ -1,6 +1,8 @@
+import { useAuth } from "HOCs/AuthProvider"
 import { userRepo } from "data/RepositoryInstances"
 import useSWRMutation from "swr/mutation"
 import useSnackbarNotifications from "../useSnackbarNotifications"
+import useFirebaseDelete from "../utils/useFirebaseDelete"
 
 const getKey = (userId: string) => {
    if (!userId) {
@@ -15,11 +17,16 @@ const getKey = (userId: string) => {
  * @returns An object containing the mutation function to delete a banner image for the user and its related SWR mutation state.
  */
 export const useDeleteUserBannerImage = (userId: string) => {
+   const { userData } = useAuth()
    const { errorNotification } = useSnackbarNotifications()
 
-   const fetcher = () =>
-      userRepo.updateUserData(userId, { bannerImageUrl: null })
+   const [deleteImages] = useFirebaseDelete()
 
+   const fetcher = () => {
+      deleteImages([userData.bannerImageUrl]).then(() => {
+         userRepo.updateUserData(userId, { bannerImageUrl: null })
+      })
+   }
    return useSWRMutation(getKey(userId), fetcher, {
       onError: (error, key) => {
          errorNotification(error, "Failed to delete user banner image", {
