7094d4fd47a7396f581b702fe8f89a42f867ca17
habibkadiri <kadirit@hotmail.com>
2024-11-07 14:58:08 +0100

    refactor(quiz-block): update prop type to QuizModelType
    Switched prop type from QuizBlockType to QuizModelType for better alignment with updated data model definitions. This enhances type consistency across the application.

--- apps/web/components/views/talent-guide/blocks/QuizBlock.tsx
+++ apps/web/components/views/talent-guide/blocks/QuizBlock.tsx
@@ -1,10 +1,10 @@
 import { Box } from "@mui/material"
-import { QuizBlockType } from "data/hygraph/types"
+import { QuizModelType } from "data/hygraph/types"
 import { sxStyles } from "types/commonTypes"
 
 const styles = sxStyles({ root: {} })
 
-type Props = QuizBlockType
+type Props = QuizModelType
 
 export const QuizBlock = ({ __typename }: Props) => {
    return <Box sx={styles.root}>{__typename}</Box>
