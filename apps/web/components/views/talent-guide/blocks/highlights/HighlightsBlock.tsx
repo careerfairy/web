a4afac64f66d0b883c2ce6784a3fe9f6ce94c60d
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-11-08 17:53:34 +0000

    feat(HighlightsBlock): adds component

---
+++ apps/web/components/views/talent-guide/blocks/highlights/HighlightsBlock.tsx
@@ -0,0 +1,66 @@
+import { Box } from "@mui/material"
+
+import {
+   HighlightComponentType,
+   HighlightsBlockType,
+   SparkComponentType,
+} from "data/hygraph/types"
+import { useCallback, useEffect, useState } from "react"
+import { sxStyles } from "types/commonTypes"
+import { HighlightCard } from "./HighlightCard"
+import { SparkCard } from "./SparkCard"
+
+const styles = sxStyles({
+   root: {
+      display: "grid",
+      gridTemplateColumns: "repeat(2, 168px)",
+      gap: 1,
+   },
+})
+
+type Props = HighlightsBlockType
+
+export const HighlightsBlock = ({ highlights }: Props) => {
+   const [currentPlayingIndex, setCurrentPlayingIndex] = useState<number>(0)
+
+   const handleEndedPlaying = useCallback(() => {
+      setCurrentPlayingIndex((prevIndex) => {
+         return (prevIndex + 1) % highlights.length
+      })
+   }, [highlights.length])
+
+   // Leaving this just for testing purposes so its easier for you to see
+   useEffect(() => {
+      const interval = setInterval(() => {
+         setCurrentPlayingIndex((prevIndex) => {
+            return (prevIndex + 1) % highlights.length
+         })
+      }, 1000)
+
+      return () => clearInterval(interval)
+   }, [highlights.length])
+
+   return (
+      <Box sx={styles.root}>
+         {highlights.map((highlight, index) => {
+            const isSpark = highlight.__typename === "Spark"
+            return (
+               <Box key={index}>
+                  {isSpark ? (
+                     <SparkCard
+                        spark={highlight as SparkComponentType}
+                        isPlaying={currentPlayingIndex === index}
+                     />
+                  ) : (
+                     <HighlightCard
+                        highlight={highlight as HighlightComponentType}
+                        isPlaying={currentPlayingIndex === index}
+                        onEnded={handleEndedPlaying}
+                     />
+                  )}
+               </Box>
+            )
+         })}
+      </Box>
+   )
+}
