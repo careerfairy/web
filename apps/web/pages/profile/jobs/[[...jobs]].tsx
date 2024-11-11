1640b478376183cb5a602e20141f041662fc56a5
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-11-11 19:34:54 +0100

    remove server side props

--- apps/web/pages/profile/jobs/[[...jobs]].tsx
+++ apps/web/pages/profile/jobs/[[...jobs]].tsx
@@ -1,25 +1,20 @@
 import { CustomJobApplicationSourceTypes } from "@careerfairy/shared-lib/customJobs/customJobs"
 import { useTheme } from "@mui/styles"
 import { CustomJobDialogLayout } from "components/views/jobs/components/custom-jobs/CustomJobDialogLayout"
-import { getCustomJobDialogData } from "components/views/jobs/components/custom-jobs/utils"
 import { TalentProfileView } from "layouts/UserLayout/TalentProfile/TalentProfileView"
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
 import { useRouter } from "next/router"
 import SEO from "../../../components/util/SEO"
 import UserLayout from "../../../layouts/UserLayout"
 
 export const DIALOG_SOURCE = "jobs"
 
-const JobsPage = ({
-   customJobDialogData,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const JobsPage = () => {
    const theme = useTheme()
    const { pathname } = useRouter()
 
    return (
       <UserLayout backgroundColor={theme.brand.white[300]}>
          <CustomJobDialogLayout
-            customJobDialogData={customJobDialogData}
             source={{
                source: CustomJobApplicationSourceTypes.Profile,
                id: CustomJobApplicationSourceTypes.Profile,
@@ -37,13 +32,4 @@ const JobsPage = ({
    )
 }
 
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-   const customJobDialogData = await getCustomJobDialogData(ctx, DIALOG_SOURCE)
-
-   return {
-      props: {
-         customJobDialogData,
-      },
-   }
-}
 export default JobsPage
