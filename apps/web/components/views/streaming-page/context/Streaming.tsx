8576a1dda237b34c21472b04505d343918dbacf3
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-11-12 16:21:15 +0000

    fix(Streaming):always dual stream for the client

--- apps/web/components/views/streaming-page/context/Streaming.tsx
+++ apps/web/components/views/streaming-page/context/Streaming.tsx
@@ -104,7 +104,7 @@ export const StreamingProvider: FC<StreamProviderProps> = ({
 
    const config = useClientConfig(client, {
       hostCondition: shouldStream && isReady && (isHost || viewerCanJoinPanel),
-      enableDualStream: isHost,
+      enableDualStream: true,
    })
 
    const shouldJoinChannel =
