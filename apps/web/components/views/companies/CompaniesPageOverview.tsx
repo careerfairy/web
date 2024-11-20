36b134078773bb3b4b2762f86ad67a1513198092
matilderamosCF <matilde.ramos@careerfairy.io>
2024-11-20 14:52:17 +0000

    Mobile: Algolia for company search

--- apps/web/components/views/companies/CompaniesPageOverview.tsx
+++ apps/web/components/views/companies/CompaniesPageOverview.tsx
@@ -1,29 +1,14 @@
-import { FC, useState } from "react"
 import { CircularProgress, Container, Grid } from "@mui/material"
-import Companies from "./Companies"
-import { Group } from "@careerfairy/shared-lib/groups"
-import CompanySearch from "./CompanySearch"
 import { SuspenseWithBoundary } from "components/ErrorBoundary"
+import CompanySearch from "./CompanySearch"
 
-type Props = {
-   serverSideCompanies: Group[]
-}
-
-const CompaniesPageOverview: FC<Props> = ({ serverSideCompanies }) => {
-   const [resultCount, setResultCount] = useState<number>(null)
-
+const CompaniesPageOverview = () => {
    return (
       <Container maxWidth="xl">
          <SuspenseWithBoundary fallback={<CircularProgress />} hide={true}>
             <Grid container spacing={2}>
                <Grid item xs={12}>
-                  <CompanySearch filterResults={resultCount} />
-               </Grid>
-               <Grid item xs={12}>
-                  <Companies
-                     initialData={serverSideCompanies}
-                     setResults={setResultCount}
-                  />
+                  <CompanySearch />
                </Grid>
             </Grid>
          </SuspenseWithBoundary>
