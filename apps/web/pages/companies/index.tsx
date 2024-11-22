32f58d927ed28ba27b53de03ec0961eabb1c0202
matilderamosCF <155964282+matilderamosCF@users.noreply.github.com>
2024-11-22 18:45:38 +0000

    Mobile: Algolia for company search (#1392)
    * Mobile: Algolia for company search
    
    * Enhances type safety with Algolia types
    
    Updates components to use Algolia-specific types for improved
    type safety. Replaces generic group types with more specific
    types like `CompanyAlgoliaHit` and `CompanySearchResult`.
    
    Improves API interaction by retrieving the latest group data
    before follow/unfollow operations, ensuring data consistency.
    
    * Updates type import for company components
    
    Replaces `CompanyAlgoliaHit` with `CompanySearchResult` in
    components to align with updated data model for search results.
    
    Improves type consistency and reflects changes in the underlying
    data structure.
    
    ---------
    
    Co-authored-by: habibkadiri <kadirit@hotmail.com>

--- apps/web/pages/companies/index.tsx
+++ apps/web/pages/companies/index.tsx
@@ -1,19 +1,9 @@
-import ScrollToTop from "../../components/views/common/ScrollToTop"
-import React from "react"
 import SEO from "../../components/util/SEO"
-import GenericDashboardLayout from "../../layouts/GenericDashboardLayout"
+import ScrollToTop from "../../components/views/common/ScrollToTop"
 import CompaniesPageOverview from "../../components/views/companies/CompaniesPageOverview"
-import { InferGetServerSidePropsType, NextPage } from "next"
-import { FilterCompanyOptions } from "@careerfairy/shared-lib/groups"
-import { companyService } from "data/firebase/CompanyService"
-import { CompanyIndustryValues } from "@careerfairy/shared-lib/constants/forms"
-import {
-   queryParamToArr,
-   queryParamToBool,
-} from "@careerfairy/shared-lib/utils"
+import GenericDashboardLayout from "../../layouts/GenericDashboardLayout"
 
-type Props = InferGetServerSidePropsType<typeof getServerSideProps>
-const CompaniesPage: NextPage<Props> = ({ serverSideCompanies }) => {
+const CompaniesPage = () => {
    return (
       <>
          <SEO
@@ -22,35 +12,11 @@ const CompaniesPage: NextPage<Props> = ({ serverSideCompanies }) => {
             title={"CareerFairy | Companies"}
          />
          <GenericDashboardLayout pageDisplayName={"Companies"}>
-            <CompaniesPageOverview serverSideCompanies={serverSideCompanies} />
+            <CompaniesPageOverview />
          </GenericDashboardLayout>
          <ScrollToTop hasBottomNavBar />
       </>
    )
-}
-/**
- *
- * @param query Query string object, all aplied filters are passed by query string parameters.
- * @returns @type FilterCompanyOptions mapped from the query string object
- */
-const getQueryVariables = (query): FilterCompanyOptions => {
-   return {
-      companyCountries: queryParamToArr(query.companyCountries),
-      companyIndustries: queryParamToArr(query.companyIndustries),
-      publicSparks: queryParamToBool(query.companySparks as string),
-      companySize: queryParamToArr(query.companySizes),
-      allCompanyIndustries: CompanyIndustryValues,
-   }
-}
-
-export const getServerSideProps = async (ctx) => {
-   const query = getQueryVariables(ctx.query)
-   const companies = await companyService.fetchCompanies(query)
-   return {
-      props: {
-         serverSideCompanies: companies,
-      },
-   }
 }
 
 export default CompaniesPage
