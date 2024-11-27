c4f55a444cd14a91a79948cb968ce9e1ee25ebf6
habibkadiri <kadirit@hotmail.com>
2024-11-27 09:00:52 +0100

    Fixes error with undefined group IDs in hooks
    Adds default value for empty group IDs array to prevent
    TypeError.
    
    Provides safeguards for queries with no IDs and
    ensures a fallback to an empty state, enhancing error
    handling and stability in client-side application.
    
    Fixes client-side error in test live stream rooms that have no group Ids

--- apps/web/components/custom-hook/useGroupsByIds.ts
+++ apps/web/components/custom-hook/useGroupsByIds.ts
@@ -1,20 +1,25 @@
-import { useFirestoreCollection } from "./utils/useFirestoreCollection"
 import { Group } from "@careerfairy/shared-lib/groups"
 import { collection, documentId, query, where } from "firebase/firestore"
-import { FirestoreInstance } from "../../data/firebase/FirebaseInstance"
 import { MAX_QUERY_ARRAY_IN_LENGTH } from "../../constants/firebase"
+import { FirestoreInstance } from "../../data/firebase/FirebaseInstance"
+import { useFirestoreCollection } from "./utils/useFirestoreCollection"
 
+const empty = ["empty"]
 /**
  * Returns a collection of group based on their IDs.
  * @param ids Array of group IDs to be fetched (max 10).
  * @returns Firestore collection containing interests.
  * @remarks If more than 10 IDs are passed, only the first 10 will be fetched.
  */
-const useGroupsByIds = (ids: string[]) => {
+const useGroupsByIds = (ids: string[] = empty) => {
+   const groupIds = ids?.length
+      ? ids.slice(0, MAX_QUERY_ARRAY_IN_LENGTH)
+      : empty
+
    return useFirestoreCollection<Group>(
       query(
          collection(FirestoreInstance, "careerCenterData"),
-         where(documentId(), "in", ids.slice(0, MAX_QUERY_ARRAY_IN_LENGTH))
+         where(documentId(), "in", groupIds)
       )
    )
 }
