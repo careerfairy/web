d33a2b742181f46090cf639757ea782dcfa92534
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-12-02 17:36:58 +0100

    switched from dictionary to array for notices

--- apps/web/components/custom-hook/streaming/hand-raise/useUserHasNoticedHandRaise.ts
+++ apps/web/components/custom-hook/streaming/hand-raise/useUserHasNoticedHandRaise.ts
@@ -1,4 +1,3 @@
-import { DateTime } from "luxon"
 import { useCallback, useState } from "react"
 import { useLocalStorage } from "react-use"
 
@@ -6,26 +5,29 @@ const USER_HAND_RAISE_NOTICE_MAP = "handRaiseAck"
 
 const MAX_ALLOWED_LENGTH = 15
 
+/**
+ * Custom hook which checks if the current user has seen the hand raise notice (red icon on the menu item) and allows adding data.
+ * The data is stored in localStorage, with key @constant USER_HAND_RAISE_NOTICE_MAP and stores a map of live stream ids
+ * numerical value which is the date the notice was acknowledge in epoch seconds, allowing at maximum of @constant MAX_ALLOWED_LENGTH keys.
+ * @param eventId Id of the live stream to be checked and store if noticed.
+ */
 export const useUserHasNoticedHandRaise = (eventId: string) => {
-   const [notices, setNotices] = useLocalStorage<Record<string, number>>(
+   const [notices, setNotices] = useLocalStorage<string[]>(
       USER_HAND_RAISE_NOTICE_MAP,
-      {}
+      []
    )
-   const [hasNoticed, setHasNoticed] = useState(Boolean(notices[eventId]))
+   const [hasNoticed, setHasNoticed] = useState(notices.includes(eventId))
 
    const setNoticed = useCallback(() => {
-      const noticeKeys = Object.keys(notices)
-      const newNotices = { ...notices }
-      if (noticeKeys.length > MAX_ALLOWED_LENGTH) {
-         noticeKeys
-            .sort((keyA, keyB) => notices[keyB] - notices[keyA])
-            .filter((_, idx) => idx >= MAX_ALLOWED_LENGTH)
-            .forEach((eventKey) => {
-               delete newNotices[eventKey]
-            })
+      let updatedNotices = [...notices]
+
+      if (notices.length > MAX_ALLOWED_LENGTH - 1) {
+         updatedNotices = updatedNotices.slice(0, MAX_ALLOWED_LENGTH - 1)
       }
-      newNotices[eventId] = DateTime.now().toUnixInteger()
-      setNotices(newNotices)
+
+      updatedNotices.push(eventId)
+
+      setNotices(updatedNotices)
       setHasNoticed(true)
    }, [notices, eventId, setNotices])
 
