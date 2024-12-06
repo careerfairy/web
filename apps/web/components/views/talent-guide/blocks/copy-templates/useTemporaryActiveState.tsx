9b15df97935b6b0feeae9d2006549604ff0ff123
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-12-06 11:54:33 +0000

    feat(useTemporaryActiveState): adds custom hook to help with temporary animations

---
+++ apps/web/components/views/talent-guide/blocks/copy-templates/useTemporaryActiveState.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+
+type UseTemporaryStateOptions = {
+   duration?: number
+   onReset?: () => void
+}
+
+export const useTemporaryState = (options: UseTemporaryStateOptions = {}) => {
+   const { duration = 1300, onReset } = options
+   const [isActive, setIsActive] = useState(false)
+
+   useEffect(() => {
+      if (isActive) {
+         const timer = setTimeout(() => {
+            setIsActive(false)
+            onReset?.()
+         }, duration)
+         return () => clearTimeout(timer)
+      }
+   }, [isActive, duration, onReset])
+
+   return {
+      isActive,
+      setIsActive,
+   }
+}
