ce7f715652f2aa77355b0866396009f8f836bd28
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-12-09 16:35:23 +0000

    fix(HighlightExpanded): ensures card data is only rendered once the video is ready, so everything renders "at the same time"

--- apps/web/components/views/talent-guide/blocks/highlights/HighlightExpanded.tsx
+++ apps/web/components/views/talent-guide/blocks/highlights/HighlightExpanded.tsx
@@ -2,7 +2,7 @@ import { Group } from "@careerfairy/shared-lib/groups"
 import { Box } from "@mui/material"
 import useIsMobile from "components/custom-hook/useIsMobile"
 import { HighlightComponentType } from "data/hygraph/types"
-import { SyntheticEvent } from "react"
+import { SyntheticEvent, useState } from "react"
 import ReactPlayer from "react-player"
 import { sxStyles } from "types/commonTypes"
 import { ExpandedCard } from "./ExpandedCard"
@@ -23,45 +23,42 @@ type Props = {
    onClose: (event: SyntheticEvent) => void
 }
 
-const Mobile = ({ isPlaying, highlight, group, onEnded }: Props) => (
-   <>
-      <ExpandedHeader highlight={highlight} group={group} />
-      <ReactPlayer
-         className="react-player"
-         width="100%"
-         height="100%"
-         url={highlight.videoClip.url}
-         playing={isPlaying}
-         onEnded={onEnded}
-         playsinline
-      />
-      <HighlightVideoOverlay />
-   </>
-)
-
-const Desktop = ({ isPlaying, highlight, group, onEnded }: Props) => (
-   <>
-      <ReactPlayer
-         url={highlight.videoClip.url}
-         className="react-player"
-         width="100%"
-         height="100%"
-         playing={isPlaying}
-         onEnded={onEnded}
-         playsinline
-      />
-      <Box sx={styles.desktopHeaderContainer}>
-         <ExpandedHeader highlight={highlight} group={group} />
-         <HighlightVideoOverlay />
-      </Box>
-   </>
-)
-
-export const HighlightExpanded = (props: Props) => {
+export const HighlightExpanded = ({
+   isPlaying,
+   highlight,
+   group,
+   onEnded,
+   onClose,
+}: Props) => {
    const isMobile = useIsMobile()
+   const [isVideoReady, setIsVideoReady] = useState<boolean>(false)
+
    return (
-      <ExpandedCard onClose={props.onClose}>
-         {isMobile ? <Mobile {...props} /> : <Desktop {...props} />}
+      <ExpandedCard onClose={onClose}>
+         <ReactPlayer
+            url={highlight.videoClip.url}
+            className="react-player"
+            width="100%"
+            height="100%"
+            playing={isPlaying}
+            onEnded={onEnded}
+            playsinline
+            onReady={() => setIsVideoReady(true)}
+         />
+         {Boolean(isVideoReady) &&
+            (isMobile ? (
+               <>
+                  <ExpandedHeader highlight={highlight} group={group} />
+                  <HighlightVideoOverlay />
+               </>
+            ) : (
+               <>
+                  <Box sx={styles.desktopHeaderContainer}>
+                     <ExpandedHeader highlight={highlight} group={group} />
+                     <HighlightVideoOverlay />
+                  </Box>
+               </>
+            ))}
       </ExpandedCard>
    )
 }
