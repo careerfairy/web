4c02a60a366d9c02d4c99d0f0f381046a0b36e18
Sead Puzic <puzic.sead@gmail.com>
2024-12-10 14:07:49 +0100

    Bug/cf 1207 not possible to join ls on ios (#1424)
    * Update of webview to make it work on ios
    
    * Small changes
    
    * Small changes
    
    * Resolve comment

--- apps/mobile/components/WebView.tsx
+++ apps/mobile/components/WebView.tsx
@@ -278,6 +278,10 @@ const WebViewComponent: React.FC<WebViewScreenProps> = ({
             domStorageEnabled={true}
             startInLoadingState={true}
             allowsInlineMediaPlayback={true}
+            userAgent="Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"
+            sharedCookiesEnabled={true}
+            thirdPartyCookiesEnabled={true}
+            useWebKit={true}
             originWhitelist={[
                "https://*",
                "http://*",
