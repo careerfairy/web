41bae60e992ae59238909036efd018554f31eac3
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-12-11 13:08:06 +0100

    refactor

--- apps/web/layouts/UserLayout/TalentProfile/Details/Profile/forms/selectors/StudyDomainSelector.tsx
+++ apps/web/layouts/UserLayout/TalentProfile/Details/Profile/forms/selectors/StudyDomainSelector.tsx
@@ -73,7 +73,7 @@ export const StudyDomainSelector = ({
             disabled={isSubmitting}
             fullWidth
             value={value}
-            requiredText="(required)"
+            requiredText={requiredText}
          >
             {options.map((option) => (
                <MenuItem
