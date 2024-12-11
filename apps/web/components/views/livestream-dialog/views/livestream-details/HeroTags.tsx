3917af66b991a0a9f39758f769cf93800ff18532
matilderamosCF <155964282+matilderamosCF@users.noreply.github.com>
2024-12-11 17:06:07 +0000

    Mobile: Fix UI in livestream dialog (#1430)
    ### Motivation
    Fixes two things:
    - the back button on mobile not showing when a live stream is live
    - the register button behind the nav bar when scrolling
    
    
    ### Changes
    - changed positioning of register button to sticky to allow for zIndex usage as fixed positioning does not work well with it.
    - made room for the back button
    
    
    ### Demo
    
    ![Screenshot 2024-12-11 at 16.22.59.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Qb035qW3HLJok1JZLYqK/1a444d64-8ed4-4b1d-aeaf-ce4652e622e5.png)
    
    
    ### Browser Testing
    
    -  [x] Chrome
    -  [ ] Firefox
    -  [ ] Safari
    
    ### Different Resolutions Testing
    
    -  [ ] Large Screens
    -  [ ] Medium Screens
    -  [x] Small Screens

--- apps/web/components/views/livestream-dialog/views/livestream-details/HeroTags.tsx
+++ apps/web/components/views/livestream-dialog/views/livestream-details/HeroTags.tsx
@@ -1,16 +1,19 @@
-import { FC } from "react"
 import LiveIcon from "@mui/icons-material/RadioButtonChecked"
 import { Chip, Stack } from "@mui/material"
-import { sxStyles } from "types/commonTypes"
 import { useLiveStreamDialog } from "components/views/livestream-dialog/LivestreamDialog"
+import { FC } from "react"
+import { sxStyles } from "types/commonTypes"
 
 const styles = sxStyles({
    root: {
       position: "absolute",
       top: 0,
-      left: 0,
+      left: { xs: "35px", md: 0 },
       padding: 1,
       width: "calc(100% - 100px)", // to not overlap with the share/close button
+   },
+   chip: {
+      margin: "4px !important",
    },
 })
 
@@ -20,7 +23,12 @@ const HeroTags: FC = () => {
    return (
       <Stack sx={styles.root} spacing={1} direction="row">
          {livestreamPresenter.isLive() ? (
-            <Chip icon={<LiveIcon />} color="error" label={"LIVE"} />
+            <Chip
+               icon={<LiveIcon />}
+               color="error"
+               label={"LIVE"}
+               sx={styles.chip}
+            />
          ) : null}
       </Stack>
    )
