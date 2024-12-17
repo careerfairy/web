899faaae48eb989b83cdedcb33ee02f04d4b817e
Sead Puzic <puzic.sead@gmail.com>
2024-12-17 14:22:17 +0100

    Comment update
    Comment update

--- apps/web/pages/install-mobile-application.tsx
+++ apps/web/pages/install-mobile-application.tsx
@@ -1,9 +1,12 @@
 import { GetServerSideProps } from "next"
+import { useRouter } from "next/router"
 import React, { useEffect } from "react"
 import UAParser from "ua-parser-js"
 
-const APP_STORE_LINK = 'https://play.google.com/store/apps/details?id=com.admin_product_team.careerfairymobileapp';
-const GOOGLE_STORE_LINK = 'https://play.google.com/store/apps/details?id=com.admin_product_team.careerfairymobileapp'
+const APP_STORE_LINK =
+   "https://play.google.com/store/apps/details?id=com.admin_product_team.careerfairymobileapp"
+const GOOGLE_STORE_LINK =
+   "https://play.google.com/store/apps/details?id=com.admin_product_team.careerfairymobileapp"
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
    const userAgent = context.req.headers["user-agent"] || ""
@@ -14,9 +17,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
    // Redirection logic based on OS
    let destination: string | null = null
    if (os?.includes("iOS") || os?.includes("macOS")) {
-      destination = APP_STORE_LINK;
+      destination = APP_STORE_LINK
    } else {
-      destination = GOOGLE_STORE_LINK;
+      destination = GOOGLE_STORE_LINK
    }
 
    if (destination) {
@@ -29,26 +32,16 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
    }
 
    return {
-      props: { os }, // Pass the OS to the client as a fallback
+      props: {},
    }
 }
 
-const DetectOS: React.FC<{ os: string }> = ({ os }) => {
-   useEffect(() => {
-      if (!os) {
-         const userAgent = navigator.userAgent
-         // @ts-ignore
-         const parser = new UAParser(userAgent)
-         const detectedOS = parser.getOS().name || ""
+const DetectOS: React.FC = () => {
+   const router = useRouter()
 
-         // Redirect logic on the client
-         if (detectedOS.includes("iOS") || detectedOS.includes("macOS")) {
-            window.location.href = APP_STORE_LINK;
-         } else {
-            window.location.href = GOOGLE_STORE_LINK;
-         }
-      }
-   }, [os])
+   useEffect(() => {
+      router.push("/")
+   })
 
    return null
 }
