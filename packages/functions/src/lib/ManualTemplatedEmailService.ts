7cdd91f2dd3967c932012f99ca730314b23799e4
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-12-18 16:38:40 +0100

    Vacation email #1 - Testimonial

--- packages/functions/src/lib/ManualTemplatedEmailService.ts
+++ packages/functions/src/lib/ManualTemplatedEmailService.ts
@@ -3,36 +3,6 @@ import { Logger } from "@careerfairy/shared-lib/utils/types"
 import { ManualTemplatedEmailBuilder } from "./ManualTemplatedEmailBuilder"
 import { IUserFunctionsRepository } from "./UserFunctionsRepository"
 
-const FIELD_OF_STUDIES = [
-   "business_administration_economics",
-   "finance",
-   "law",
-   "luxury_fashion",
-   "marketing",
-   "military_sciences",
-   "public_administration",
-   "transportation",
-   "business_engineering",
-   "chemical_engineering",
-   "civil_engineering",
-   "electrical_engineering",
-   "materials_science_and_engineering",
-   "mechanical_engineering",
-   "space_sciences",
-   "computer_science",
-   "mathematics",
-   "astronomy",
-   "biology",
-   "chemistry",
-   "earth_sciences",
-   "environmental_studies_and_forestry",
-   "geography",
-   "life_sciences",
-   "medicine",
-   "physics",
-   "systems_science",
-]
-
 /**
  * Gathers all the required data to build the release email
  */
@@ -54,21 +24,15 @@ export class ManualTemplatedEmailService {
     * Fetches the required data for generating the email
     */
    async fetchRequiredData(overrideUsers: string[]) {
-      const users = await this.userRepo.getSubscribedUsersByCountryCodes(
-         ["DE", "CH"],
+      const users = await this.userRepo.getSubscribedUsersLastActive(
+         new Date("2023-06-01"),
          overrideUsers
       )
 
-      const filteredUsers = users.filter(
-         (user) =>
-            user.fieldOfStudy?.id &&
-            FIELD_OF_STUDIES.includes(user.fieldOfStudy.id)
-      )
-
-      this.subscribedUsers = convertDocArrayToDict(filteredUsers)
+      this.subscribedUsers = convertDocArrayToDict(users)
 
       this.logger.info(
-         "Total Users for X-mas newsletter - ",
+         "Total Users for X-mas testimonial - ",
          Object.keys(this.subscribedUsers || {}).length
       )
 
