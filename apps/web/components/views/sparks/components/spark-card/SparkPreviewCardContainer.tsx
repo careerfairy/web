b7815e29c41a58e93c461451847b22716d78ae1b
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-12-19 18:17:01 +0000

    fix(SparkPreviewCardContainer): adds backdropFilter grayscale animation to ensure UI elements rendering gets triggered always

--- apps/web/components/views/sparks/components/spark-card/SparkPreviewCardContainer.tsx
+++ apps/web/components/views/sparks/components/spark-card/SparkPreviewCardContainer.tsx
@@ -17,6 +17,10 @@ const styles = sxStyles({
       flexDirection: "column",
       overflow: "hidden",
       aspectRatio: "9/16",
+      // CSS hack for Safari to fix https://linear.app/careerfairy/issue/CF-1251/sparks-overlay-breaks
+      "*:not(.MuiChip-root, .MuiChip-root *)": {
+         backdropFilter: "grayscale(0)",
+      },
    },
    cardContent: {
       "&::after": {
