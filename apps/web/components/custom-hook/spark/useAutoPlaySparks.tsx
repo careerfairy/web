5512f464c2a331408f8ca505c8be36e7a5d45059
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-12-20 11:06:00 +0000

    feat(useAutoPlaySparks): handles resuming auto play after a user scroll

--- apps/web/components/custom-hook/spark/useAutoPlaySparks.tsx
+++ apps/web/components/custom-hook/spark/useAutoPlaySparks.tsx
@@ -1,5 +1,5 @@
 import { EmblaCarouselType } from "embla-carousel-react"
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { useAreSlidesInView } from "../embla-carousel/useAreSlidesInView"
 import useIsMobile from "../useIsMobile"
 
@@ -17,8 +17,37 @@ export const useAutoPlaySparks = (
 
    const { areSlidesInView } = useAreSlidesInView(emblaApi)
 
+   const [isUserScrolling, setIsUserScrolling] = useState(false)
+
+   const getMiddleSlideIndex = useCallback(() => {
+      if (!isMobile || !emblaApi?.slideNodes()) return null
+
+      const viewportBoundingRect = emblaApi.rootNode().getBoundingClientRect()
+      const viewportMiddlePoint =
+         viewportBoundingRect.left + viewportBoundingRect.width / 2
+
+      const slideNodes = emblaApi.slideNodes()
+
+      for (let i = 0; i < slideNodes.length; i++) {
+         const slideBoundingRect = slideNodes[i].getBoundingClientRect()
+         const slideMiddlePoint =
+            slideBoundingRect.left + slideBoundingRect.width / 2
+
+         if (Math.abs(slideMiddlePoint - viewportMiddlePoint) < 100) {
+            return i
+         }
+      }
+
+      return null
+   }, [isMobile, emblaApi])
+
    const moveToNextSlide = useCallback(() => {
-      if (!areSlidesInView || numberOfElementsToPlay === null) return
+      if (
+         !areSlidesInView ||
+         isUserScrolling ||
+         numberOfElementsToPlay === null
+      )
+         return
 
       setAutoPlayingIndex((prevIndex) => {
          const nextIndex = (prevIndex + 1) % numberOfElementsToPlay
@@ -29,16 +58,46 @@ export const useAutoPlaySparks = (
 
          return nextIndex
       })
-   }, [emblaApi, areSlidesInView, numberOfElementsToPlay])
+   }, [emblaApi, areSlidesInView, numberOfElementsToPlay, isUserScrolling])
 
    const shouldDisableAutoPlay = useCallback(
       (index: number) => {
-         if (!areSlidesInView) return true
+         if (!areSlidesInView || isUserScrolling) return true
 
          return isMobile && areSlidesInView && autoPlayingIndex !== index
       },
-      [isMobile, areSlidesInView, autoPlayingIndex]
+      [isMobile, areSlidesInView, autoPlayingIndex, isUserScrolling]
    )
+
+   useEffect(() => {
+      if (!isMobile || !emblaApi) return
+
+      const handleUserScrollStart = () => {
+         setIsUserScrolling(true)
+      }
+
+      const handleSettle = () => {
+         if (!isUserScrolling) return
+
+         setIsUserScrolling(false)
+         setAutoPlayingIndex(getMiddleSlideIndex())
+      }
+
+      emblaApi.on("pointerDown", handleUserScrollStart)
+      emblaApi.on("settle", handleSettle)
+
+      return () => {
+         emblaApi.off("pointerDown", handleUserScrollStart)
+         emblaApi.off("settle", handleSettle)
+      }
+   }, [emblaApi, getMiddleSlideIndex, isMobile, isUserScrolling])
+
+   useEffect(() => {
+      return () => {
+         setAutoPlayingIndex(0)
+         setIsUserScrolling(false)
+      }
+   }, [])
 
    return {
       shouldDisableAutoPlay,
