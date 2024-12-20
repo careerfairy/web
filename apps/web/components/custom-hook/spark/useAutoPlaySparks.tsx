511e22ea55bf1b69ea67ae8b9166d599cff55c66
Carlos Rijo <carlos.rijo@careerfairy.io>
2024-12-20 18:40:17 +0000

    fix(useAutoPlaySparks): handle edge cases on "next slide to play after scroll" calculation

--- apps/web/components/custom-hook/spark/useAutoPlaySparks.tsx
+++ apps/web/components/custom-hook/spark/useAutoPlaySparks.tsx
@@ -14,32 +14,49 @@ export const useAutoPlaySparks = (
 ): ReturnType => {
    const isMobile = useIsMobile()
    const [autoPlayingIndex, setAutoPlayingIndex] = useState<number>(0)
-   const [hasPlayed, setHasPlayed] = useState(false)
 
    const { areSlidesInView } = useAreSlidesInView(emblaApi)
 
    const [isUserScrolling, setIsUserScrolling] = useState(false)
 
-   const getMiddleSlideIndex = useCallback(() => {
+   const getSlideToPlayAfterScroll = useCallback(() => {
       if (!isMobile || !emblaApi?.slideNodes()) return null
 
       const viewportBoundingRect = emblaApi.rootNode().getBoundingClientRect()
-      const viewportMiddlePoint =
-         viewportBoundingRect.left + viewportBoundingRect.width / 2
+      const viewportLeft = viewportBoundingRect.left
+      const viewportRight = viewportBoundingRect.right
 
-      const slideNodes = emblaApi.slideNodes()
+      // Embla appends an extra node to the end of the carousel
+      // to ensure the last slide is fully visible (I think...)
+      // so we need to filter out the extra node
+      const slideNodes = emblaApi.slideNodes().slice(0, -1)
+
+      let firstVisibleIndex = null
+      let lastVisibleIndex = null
 
       for (let i = 0; i < slideNodes.length; i++) {
          const slideBoundingRect = slideNodes[i].getBoundingClientRect()
-         const slideMiddlePoint =
-            slideBoundingRect.left + slideBoundingRect.width / 2
 
-         if (Math.abs(slideMiddlePoint - viewportMiddlePoint) < 100) {
-            return i
+         const isFullyVisible =
+            slideBoundingRect.left >= viewportLeft &&
+            slideBoundingRect.right <= viewportRight
+
+         if (isFullyVisible) {
+            if (firstVisibleIndex === null) {
+               firstVisibleIndex = i
+            }
+            lastVisibleIndex = i
          }
       }
 
-      return null
+      // In case the user scroll to the end of the carousel,
+      // we want to play the last slide
+      if (lastVisibleIndex === slideNodes.length - 1) {
+         return lastVisibleIndex
+      }
+
+      // Otherwise, we want to play the first slide that is fully visible
+      return firstVisibleIndex
    }, [isMobile, emblaApi])
 
    const moveToNextSlide = useCallback(() => {
@@ -71,29 +88,20 @@ export const useAutoPlaySparks = (
    )
 
    useEffect(() => {
-      if (autoPlayingIndex !== 0) {
-         setHasPlayed(true)
-      }
-
-      return () => {
-         setHasPlayed(false)
-      }
-   }, [autoPlayingIndex])
-
-   useEffect(() => {
       if (!isMobile || !emblaApi) return
 
       const handleUserScrollStart = () => {
-         if (!hasPlayed) return
+         if (!areSlidesInView) return
 
          setIsUserScrolling(true)
       }
 
       const handleSettle = () => {
          if (!isUserScrolling) return
+         const indexToPlay = getSlideToPlayAfterScroll()
 
          setIsUserScrolling(false)
-         setAutoPlayingIndex(getMiddleSlideIndex())
+         setAutoPlayingIndex(indexToPlay)
       }
 
       emblaApi.on("pointerDown", handleUserScrollStart)
@@ -105,9 +113,9 @@ export const useAutoPlaySparks = (
       }
    }, [
       areSlidesInView,
+      autoPlayingIndex,
       emblaApi,
-      getMiddleSlideIndex,
-      hasPlayed,
+      getSlideToPlayAfterScroll,
       isMobile,
       isUserScrolling,
    ])
