032b1d0af820a76222c57a339cc2a0ba68bb8376
wgoncalvescf <walter.goncalves@careerfairy.io>
2024-12-23 18:54:12 +0100

    Settings #5 - Password

--- apps/web/layouts/UserLayout/TalentProfile/Header/Settings/Password.tsx
+++ apps/web/layouts/UserLayout/TalentProfile/Header/Settings/Password.tsx
@@ -1,9 +1,79 @@
-import { Stack, Typography } from "@mui/material"
+import { Button, Stack } from "@mui/material"
+import useSnackbarNotifications from "components/custom-hook/useSnackbarNotifications"
+import { auth } from "data/firebase/FirebaseInstance"
+import {
+   EmailAuthProvider,
+   reauthenticateWithCredential,
+   updatePassword,
+} from "firebase/auth"
+import { useFormContext } from "react-hook-form"
+import { PasswordFormFields, PasswordFormProvider } from "./forms/PasswordForm"
+import { PasswordFormValues, PasswordSchemaType } from "./forms/schemas"
 
 export const Password = () => {
    return (
-      <Stack>
-         <Typography>Password</Typography>
+      <PasswordFormProvider>
+         <PasswordForm />
+      </PasswordFormProvider>
+   )
+}
+
+const PasswordForm = () => {
+   const { errorNotification, successNotification } = useSnackbarNotifications()
+   const {
+      formState: { isDirty, isValid },
+      reset,
+      handleSubmit,
+   } = useFormContext<PasswordSchemaType>()
+
+   const onSubmit = async (data: PasswordFormValues) => {
+      try {
+         const user = auth.currentUser
+
+         if (!user) {
+            throw new Error("User is not authenticated.")
+         }
+
+         // Create credentials using the current password
+         const credential = EmailAuthProvider.credential(
+            user.email,
+            data.currentPassword
+         )
+
+         try {
+            // Reauthenticate the user
+            await reauthenticateWithCredential(user, credential)
+         } catch (error) {
+            errorNotification("Authentication failed. Please try again.")
+            return
+         }
+
+         // Now you can proceed with password update
+         await updatePassword(user, data.newPassword)
+
+         reset()
+         successNotification("Password updated successfully")
+      } catch (error) {
+         errorNotification(
+            error,
+            "We encountered a problem while updating your password. Rest assured, we're on it!"
+         )
+      }
+   }
+
+   const handleSave = async () => handleSubmit(onSubmit)()
+
+   return (
+      <Stack spacing={1.5}>
+         <PasswordFormFields />
+         <Button
+            onClick={handleSave}
+            disabled={!isDirty || !isValid}
+            variant="contained"
+            color="primary"
+         >
+            Update password
+         </Button>
       </Stack>
    )
 }
