a202ea9d00d3a5c17c4af976e50d6a542ea3d71d
Walter Gonçalves <152179257+wgoncalvescf@users.noreply.github.com>
2024-12-27 10:49:48 +0100

    Vacation email #2 (#1457)
    ### Motivation
    From [issue](https://linear.app/careerfairy/issue/CF-1258/vacation-email-2-recap): Over the holidays period we want to send some communication at our talent community. The second email is a CF unwrapped edition
    
    For each user we would need to calculate:
    - number of LS watched 
    - number of Sparks watched
    
    <!-- A little context about this change. -->
    
    ### Changes
    
    -  Updated manual templated email logic for filtering out european users and calculate their seen sparks and participated events.
    
    <!-- You can remove anything that doesn't make sense for this change. -->
    
    ### Cloud functions to deploy
    - [ ] manualTemplatedEmail

--- packages/functions/src/lib/ManualTemplatedEmailService.ts
+++ packages/functions/src/lib/ManualTemplatedEmailService.ts
@@ -1,7 +1,44 @@
 import { convertDocArrayToDict } from "@careerfairy/shared-lib/BaseFirebaseRepository"
 import { Logger } from "@careerfairy/shared-lib/utils/types"
+import { processInBatches } from "../util"
+import { ILivestreamFunctionsRepository } from "./LivestreamFunctionsRepository"
 import { ManualTemplatedEmailBuilder } from "./ManualTemplatedEmailBuilder"
 import { IUserFunctionsRepository } from "./UserFunctionsRepository"
+
+const EUROPEAN_COUNTRY_CODES = [
+   "AT",
+   "AD",
+   "BE",
+   "BG",
+   "CH",
+   "CZ",
+   "DE",
+   "DK",
+   "EE",
+   "ES",
+   "FI",
+   "FR",
+   "GB",
+   "GR",
+   "HR",
+   "HU",
+   "IE",
+   "IT",
+   "LI",
+   "LU",
+   "MC",
+   "MT",
+   "NL",
+   "NO",
+   "PL",
+   "PT",
+   "RO",
+   "RS",
+   "SE",
+   "SI",
+   "SK",
+   "SM",
+]
 
 /**
  * Gathers all the required data to build the release email
@@ -13,9 +50,14 @@ export class ManualTemplatedEmailService {
    // Leaving this commented as the most common usage will be with the full UserData
    // private subscribedUsers: Record<string, UserData>
    private subscribedUsers: Record<string, { id: string; firstName: string }>
+   private usersStats: Record<
+      string,
+      { seenSparks: number; watchedEvents: number }
+   > = {}
 
    constructor(
       private readonly userRepo: IUserFunctionsRepository,
+      private readonly livestreamRepo: ILivestreamFunctionsRepository,
       private readonly emailBuilder: ManualTemplatedEmailBuilder,
       private readonly logger: Logger
    ) {}
@@ -29,10 +71,35 @@ export class ManualTemplatedEmailService {
          overrideUsers
       )
 
-      this.subscribedUsers = convertDocArrayToDict(users)
+      const europeanUsers = users.filter((user) =>
+         EUROPEAN_COUNTRY_CODES.includes(user.universityCountryCode)
+      )
+
+      const batchSize = 200
+
+      await processInBatches(
+         europeanUsers.map((user) => user.userEmail),
+         batchSize,
+         async (userEmail) => {
+            const [seenSparks, watchedEvents] = await Promise.all([
+               this.userRepo
+                  .getUserSeenSparks(userEmail)
+                  .then((seenSparks) => seenSparks?.length || 0),
+               this.livestreamRepo
+                  .getParticipatedEvents(userEmail)
+                  .then((watchedEvents) => watchedEvents?.length || 0),
+            ])
+
+            this.usersStats[userEmail] = { seenSparks, watchedEvents }
+         },
+         this.logger,
+         300
+      )
+
+      this.subscribedUsers = convertDocArrayToDict(europeanUsers)
 
       this.logger.info(
-         "Total Users for X-mas testimonial - ",
+         "Total Users for X-mas highlight - ",
          Object.keys(this.subscribedUsers || {}).length
       )
 
@@ -51,7 +118,8 @@ export class ManualTemplatedEmailService {
       for (const userEmail of emails) {
          this.emailBuilder.addRecipient(
             userEmail,
-            this.subscribedUsers[userEmail].firstName
+            this.subscribedUsers[userEmail].firstName,
+            this.usersStats[userEmail]
          )
       }
 
