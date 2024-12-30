70e82b8be2fee6770c129dda50ce59394bebe29c
habibkadiri <kadirit@hotmail.com>
2024-12-30 20:22:20 +0100

    feat(talent-guide): Enhance module navigation and UI
    Adds animation with AnimatePresence for smoother transitions between modules.
    Refactors NextModuleSection to improve module interactivity and styling.
    Refines the logic for determining the next module and updating user progress.
    Updates styles and layouts for better consistency across devices.
    Handles errors in fetching next module with logging and notification.

--- apps/web/components/views/talent-guide/components/end-of-module/TalentGuideEndLayout.tsx
+++ apps/web/components/views/talent-guide/components/end-of-module/TalentGuideEndLayout.tsx
@@ -2,7 +2,9 @@ import { Box, useMediaQuery } from "@mui/material"
 import { AnimatePresence } from "framer-motion"
 import { useAuth } from "HOCs/AuthProvider"
 import { useNextTalentGuideModule } from "hooks/useNextTalentGuideModule"
+import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+import { errorLogAndNotify } from "util/CommonUtil"
 import { TalentGuideLayout } from "../TalentGuideLayout"
 import { CongratsSection } from "./CongratsSection"
 import { FeedbackSection } from "./FeedbackSection"
@@ -13,12 +15,33 @@ const SHOW_CONGRATS_TIME = 1000
 
 export const TalentGuideEndLayout = () => {
    const { authenticatedUser } = useAuth()
+   const { push } = useRouter()
    const [ratingClicked, setRatingClicked] = useState(false)
    const [someTimeHasPassed, setSomeTimeHasPassed] = useState(false)
    const [feedbackSubmitted, setFeedbackSubmitted] = useState(false)
 
    const isShortScreen = useMediaQuery("(max-height: 800px)")
    const isShorterScreen = useMediaQuery("(max-height: 530px)")
+
+   const { data: nextModule } = useNextTalentGuideModule(
+      authenticatedUser?.uid,
+      "de",
+      {
+         onSuccess: (nextModule) => {
+            if (!nextModule) {
+               push("/talent-guide")
+            }
+         },
+         onError: (error, key) => {
+            errorLogAndNotify(error, {
+               message: "Error fetching next talent guide module",
+               key,
+            })
+            push("/talent-guide")
+         },
+         suspense: false,
+      }
+   )
 
    useEffect(() => {
       const timer = setTimeout(() => {
@@ -28,15 +51,10 @@ export const TalentGuideEndLayout = () => {
       return () => clearTimeout(timer)
    }, [])
 
-   const { data: nextModule } = useNextTalentGuideModule(
-      authenticatedUser?.uid,
-      "de"
-   )
-
    const showCongrats =
       !ratingClicked && !(isShorterScreen && someTimeHasPassed)
    const showFeedback = someTimeHasPassed && !feedbackSubmitted
-   const showNextModule = Boolean(nextModule) && feedbackSubmitted
+   const showNextModule = feedbackSubmitted
 
    return (
       <TalentGuideLayout sx={{ px: { xs: 3.25, md: 0 }, maxWidth: 600 }}>
