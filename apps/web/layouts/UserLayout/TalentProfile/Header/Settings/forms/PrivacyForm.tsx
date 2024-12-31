72691b25eab4bb8094ae2e835c144ded8b4b02b8
Walter Gonçalves <152179257+wgoncalvescf@users.noreply.github.com>
2024-12-31 14:50:38 +0100

    Settings #4 - Privacy (#1454)
    ### Motivation
    > From [issue](https://linear.app/careerfairy/issue/CF-789/settings-privacy): In this block a talent will be able to manage their privacy options: currently we only have the "Newsletter subscription", but moving forward we might want to add others. So we need to ensure that the infrastructure we build to manage the privacy settings is scalable.
    <!-- A little context about this change. -->
    
    ### Changes
    
    -  Updated `Privacy.tsx` for newsletter switch
    - Created base schema (for future fields support)
    
    <!-- You can remove anything that doesn't make sense for this change. -->
    
    ### Demo
    
    <div class='graphite__hidden'>
              <div>🎥 Video uploaded on Graphite:</div>
                <a href="https://app.graphite.dev/media/video/S0WylOadBGaro8NXGBwS/24902e50-1bae-42e9-b68e-95e29a1d521a.mov">
                  <img src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/S0WylOadBGaro8NXGBwS/24902e50-1bae-42e9-b68e-95e29a1d521a.mov">
                </a>
              </div>
    <video src="https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/S0WylOadBGaro8NXGBwS/24902e50-1bae-42e9-b68e-95e29a1d521a.mov">Gravação do ecrã 2024-12-23, às 13.27.24.mov</video>
    
    <!-- Consider including a short demo video. -->
    
    ### Browser Testing
    
    -  [x] Chrome
    -  [ ] Firefox
    -  [ ] Safari
    
    ### Different Resolutions Testing
    
    -  [x] Large Screens
    -  [ ] Medium Screens
    -  [x] Small Screens

---
+++ apps/web/layouts/UserLayout/TalentProfile/Header/Settings/forms/PrivacyForm.tsx
@@ -0,0 +1,105 @@
+import { UserData } from "@careerfairy/shared-lib/users"
+import { Stack, Typography } from "@mui/material"
+import { useAuth } from "HOCs/AuthProvider"
+import { useYupForm } from "components/custom-hook/form/useYupForm"
+import BrandedSwitch from "components/views/common/inputs/BrandedSwitch"
+import { userRepo } from "data/RepositoryInstances"
+import { ReactNode, useCallback, useEffect } from "react"
+import { FormProvider, UseFormReturn } from "react-hook-form"
+import { sxStyles } from "types/commonTypes"
+import {
+   PrivacySchema,
+   PrivacySchemaType,
+   getInitialPrivacyValues,
+} from "./schemas"
+
+const styles = sxStyles({
+   formRoot: {
+      mx: {
+         md: 0,
+         sm: 1,
+         xs: 1,
+      },
+      mt: 2,
+   },
+   switch: {
+      "& .MuiSwitch-switchBase": {
+         "&.Mui-checked": {
+            "& + .MuiSwitch-track": {
+               backgroundColor: (theme) => theme.palette.primary.main,
+            },
+         },
+      },
+   },
+})
+
+type PrivacyFormProviderProps = {
+   userData?: UserData
+   children:
+      | ((methods: UseFormReturn<PrivacySchemaType>) => ReactNode)
+      | ReactNode
+}
+
+export const PrivacyFormProvider = ({
+   children,
+   userData,
+}: PrivacyFormProviderProps) => {
+   const defaultValues = getInitialPrivacyValues(userData)
+
+   const methods = useYupForm({
+      schema: PrivacySchema,
+      defaultValues: defaultValues,
+      mode: "onChange",
+      reValidateMode: "onChange",
+   })
+
+   // Explicitly reset form values after initialization if they change
+   useEffect(() => {
+      methods.reset(defaultValues)
+      return () => {
+         methods.reset({})
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [userData])
+
+   return (
+      <FormProvider {...methods}>
+         {typeof children === "function" ? children(methods) : children}
+      </FormProvider>
+   )
+}
+
+export const PrivacyFormFields = () => {
+   const { userData } = useAuth()
+
+   const handleToggleNewsletter = useCallback(async () => {
+      await userRepo.updateUserData(userData.id, {
+         unsubscribed: !userData.unsubscribed,
+      })
+   }, [userData])
+
+   return (
+      <Stack spacing={2} sx={styles.formRoot}>
+         <Stack direction={"row"} alignItems={"center"} spacing={2.5}>
+            <Stack spacing={"10px"}>
+               <Typography
+                  variant="brandedBody"
+                  fontWeight={400}
+                  color="neutral.800"
+               >
+                  Newsletter
+               </Typography>
+               <Typography variant="small" fontWeight={400} color="neutral.600">
+                  I want to join 80´000+ students who receive personalised
+                  invitations to career events and job openings
+               </Typography>
+            </Stack>
+            <BrandedSwitch
+               checked={!userData?.unsubscribed}
+               onChange={handleToggleNewsletter}
+               sx={styles.switch}
+            />
+         </Stack>
+      </Stack>
+   )
+}
