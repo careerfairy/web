71b4b70d71ebf24f1167bb0d30b4bb651fea8005
habibkadiri <kadirit@hotmail.com>
2025-01-07 10:12:47 +0100

    feat(icons): Add outlined style option for LevelsIcon
    Enhances LevelsIcon component with an optional outlined style,
    improving visual customization flexibility. Updates to the
    GenericDashboardLayout to factor in drawer state for responsive
    design adjustments, impacting mobile view thresholds.
    
    Improves user experience by providing detailed metadata in
    CourseOverview component, enhancing content clarity on different
    screen sizes. Utilizes useGenericDashboard hook to adjust
    layout dynamically based on drawer state.

--- apps/web/components/views/levels/components/course-overview/CourseOverview.tsx
+++ apps/web/components/views/levels/components/course-overview/CourseOverview.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Stack, Typography } from "@mui/material"
-import useIsMobile from "components/custom-hook/useIsMobile"
+import { Box, Button, Stack, Typography, useTheme } from "@mui/material"
+import { LevelsIcon } from "components/views/common/icons/LevelsIcon"
 import { Page, TalentGuideModule } from "data/hygraph/types"
+import { Clock } from "react-feather"
 import { sxStyles } from "types/commonTypes"
 import { CourseIllustration } from "./CourseIllustration"
 import { ProgressWithPercentage } from "./ProgressWithPercentage"
@@ -8,26 +9,33 @@ import { ProgressWithPercentage } from "./ProgressWithPercentage"
 const styles = sxStyles({
    root: {
       width: "100%",
-      maxWidth: {
-         xs: "100%",
-         sm: 360,
-      },
+      maxWidth: 360,
       position: "relative",
       display: "flex",
       flexDirection: "column",
       alignItems: "start",
    },
-   content: {
+   rootMobile: {
+      maxWidth: "100%",
+   },
+   content: (theme) => ({
       width: "100%",
       position: "sticky",
       top: 64, // offset for sticky header
+      border: `1px solid ${theme.palette.secondary[50]}`,
+      backgroundColor: theme.brand.white[100],
+      borderRadius: "8px",
+   }),
+   contentMobile: {
+      border: "none",
+      backgroundColor: "none",
    },
 
    details: {
       padding: 2,
    },
-   metadata: {
-      color: "secondary.600",
+   metadataIcon: {
+      fontSize: 16,
    },
    description: {
       color: "secondary.700",
@@ -40,6 +48,7 @@ const styles = sxStyles({
 
 type Props = {
    pages: Page<TalentGuideModule>[]
+   isMobile: boolean
 }
 
 const copy = {
@@ -48,22 +57,59 @@ const copy = {
       "Ready to land your dream job? Learn the insider tips and practical strategies to stand out in today's competitive job market. Start building the career you deserve!",
 }
 
-export const CourseOverview = ({ pages }: Props) => {
-   const isMobile = useIsMobile("sm")
+export const CourseOverview = ({ pages, isMobile }: Props) => {
+   const theme = useTheme()
 
    return (
-      <Box sx={styles.root}>
-         <Stack sx={styles.content}>
+      <Box sx={[styles.root, isMobile && styles.rootMobile]}>
+         <Stack sx={[styles.content, isMobile && styles.contentMobile]}>
             <CourseIllustration isMobile={isMobile} />
             <Stack sx={styles.details} spacing={2}>
-               <Stack direction="row" alignItems="center" spacing={1}>
-                  <Box>
-                     <Typography variant="h4">{copy.title}</Typography>
-                     <Typography variant="body2" sx={styles.metadata}>
-                        {pages.length} levels • 2 hours
+               <Stack
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  spacing={1}
+               >
+                  <Stack
+                     color={isMobile ? theme.brand.white[50] : "neutral.600"}
+                     spacing={0.5}
+                  >
+                     <Typography
+                        fontWeight={700}
+                        variant="desktopBrandedH4"
+                        component="h4"
+                     >
+                        {copy.title}
                      </Typography>
-                  </Box>
-                  <ProgressWithPercentage percentageComplete={75} />
+                     <Stack direction="row" alignItems="center" spacing={1.5}>
+                        <Stack
+                           direction="row"
+                           alignItems="center"
+                           spacing={0.5}
+                        >
+                           <LevelsIcon sx={styles.metadataIcon} isOutlined />
+                           <Typography
+                              variant={isMobile ? "xsmall" : "small"}
+                              component="p"
+                           >
+                              {pages.length} levels
+                           </Typography>
+                        </Stack>
+                        <Stack
+                           direction="row"
+                           alignItems="center"
+                           spacing={0.5}
+                        >
+                           <Clock size={16} />
+                           <Typography variant="body2">2 hours</Typography>
+                        </Stack>
+                     </Stack>
+                  </Stack>
+                  <ProgressWithPercentage
+                     isOverlay={isMobile}
+                     percentageComplete={75}
+                  />
                </Stack>
                <Typography variant="body1" sx={styles.description}>
                   {copy.description}
