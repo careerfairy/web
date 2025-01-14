220ee60de8f5f661dedccc0357c9df8d65995480
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-01-14 18:51:49 +0100

    feat: fuzzy search for countries and cities

--- packages/functions/src/countries.ts
+++ packages/functions/src/countries.ts
@@ -7,6 +7,7 @@ import {
 
 import { City, Country, State } from "country-state-city"
 import * as functions from "firebase-functions"
+import Fuse from "fuse.js"
 import { InferType, object, string } from "yup"
 import config from "./config"
 
@@ -14,20 +15,39 @@ const CountryCitiesOptionsSchema = {
    countryCode: string().required(),
 }
 
-const CountryCitiesSchema = object().shape(CountryCitiesOptionsSchema)
-
 const CountryCityDataOptionsSchema = {
    countryIsoCode: string().optional(),
    generatedCityId: string().optional(),
+}
+
+const SearchCountryOptionsSchema = {
+   searchValue: string().required().min(2),
+}
+
+const SearchCityOptionsSchema = {
+   searchValue: string().required().min(2),
+   countryId: string().required(),
 }
 
 const CityDataOptionsSchema = {
    generatedCityId: string().optional(),
 }
 
+const CountryDataOptionsSchema = {
+   countryIsoCode: string().required(),
+}
+
+const CountryCitiesSchema = object().shape(CountryCitiesOptionsSchema)
+
 const CountryCityDataSchema = object().shape(CountryCityDataOptionsSchema)
 
 const CityDataSchema = object().shape(CityDataOptionsSchema)
+
+const CountryDataSchema = object().shape(CountryDataOptionsSchema)
+
+const SearchCountrySchema = object().shape(SearchCountryOptionsSchema)
+
+const SearchCitySchema = object().shape(SearchCityOptionsSchema)
 
 type CountryCitiesOptions = InferType<typeof CountryCitiesSchema>
 
@@ -35,9 +55,30 @@ type CountryCityDataOptions = InferType<typeof CountryCityDataSchema>
 
 type CityDataOptions = InferType<typeof CityDataSchema>
 
-export const fetchCountriesList = functions
+type CountryDataOptions = InferType<typeof CountryDataSchema>
+
+type SearchCountryOptions = InferType<typeof SearchCountrySchema>
+
+type SearchCityOptions = InferType<typeof SearchCitySchema>
+
+const performFuzzySearch = <T>(items: T[], searchValue: string): T[] => {
+   // Configure Fuse options
+   const fuseOptions = {
+      keys: ["name"],
+      threshold: 0.5, // Adjust this value to control fuzzy matching sensitivity
+   }
+
+   const fuse = new Fuse(items, fuseOptions)
+   const searchResults = fuse.search(searchValue)
+
+   return searchResults.map((result) => result.item)
+}
+
+export const searchCountries = functions
    .region(config.region)
-   .https.onCall(() => {
+   .https.onCall((data: SearchCountryOptions) => {
+      const { searchValue } = data
+
       const countries: CountryOption[] = Country.getAllCountries()
          .map((country) => ({
             name: country.name,
@@ -47,15 +88,23 @@ export const fetchCountriesList = functions
             countryA.name.localeCompare(countryB.name)
          )
 
-      const countriesMap: Record<string, CountryOption> = countries.reduce(
-         (acc, country) => ({
-            ...acc,
-            [country.id]: country,
-         }),
-         {} as Record<string, CountryOption>
-      )
+      return performFuzzySearch(countries, searchValue)
+   })
 
-      return countriesMap
+export const searchCities = functions
+   .region(config.region)
+   .https.onCall((data: SearchCityOptions) => {
+      const { searchValue, countryId } = data
+
+      const cities: CityOption[] = City.getCitiesOfCountry(countryId)
+         .map((city) => ({
+            name: city.name,
+            id: generateCityId(city),
+            stateIsoCode: city.stateCode,
+         }))
+         .sort((cityA, cityB) => cityA.name.localeCompare(cityB.name))
+
+      return performFuzzySearch(cities, searchValue)
    })
 
 export const fetchCountryCitiesList = functions
@@ -147,4 +196,23 @@ export const fetchCityData = functions
       }
 
       return cityResult
+   })
+
+export const fetchCountryData = functions
+   .region(config.region)
+   .https.onCall((data: CountryDataOptions) => {
+      const { countryIsoCode } = data
+
+      let countryResult: CountryOption | null = null
+
+      if (countryIsoCode) {
+         const country = Country.getCountryByCode(countryIsoCode)
+
+         countryResult = {
+            name: country.name,
+            id: country.isoCode,
+         }
+      }
+
+      return countryResult
    })
