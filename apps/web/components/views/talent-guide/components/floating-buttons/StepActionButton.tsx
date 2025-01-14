638fd29037b0a916638a41a563a0d210dce01a73
habibkadiri <kadirit@hotmail.com>
2025-01-14 17:53:02 +0100

    refactor(talent-guide): Update useModuleProgress hook and related components
    Changes the useModuleProgress hook to return loading status alongside module progress data, enhancing the handling of loading states in components. Updates NextModuleSection to utilize useTraceUpdate for better performance tracking. Modifies StepActionButton to destructure moduleProgress from useModuleProgress, and comments out the restart button logic for future implementation. Adjusts Status component to handle loading states with skeleton placeholders, improving user experience during data fetching.
    
    These changes collectively enhance the responsiveness and clarity of module progress handling across the talent guide components.

--- apps/web/components/views/talent-guide/components/floating-buttons/StepActionButton.tsx
+++ apps/web/components/views/talent-guide/components/floating-buttons/StepActionButton.tsx
@@ -1,16 +1,13 @@
 import { QUIZ_STATE } from "@careerfairy/shared-lib/talent-guide/types"
-import { useModuleProgress } from "components/custom-hook/talent-guide/useModuleProgress"
 import {
    useCurrentQuiz,
    useCurrentStep,
    useIsLastStep,
-   useModuleId,
    useQuizState,
 } from "store/selectors/talentGuideSelectors"
 import { FinishModuleButton } from "./FinishModuleButton"
 import { NextButton } from "./NextButton"
 import { QuizButton } from "./QuizButton"
-import { RestartModuleButton } from "./RestartModuleButton"
 
 export const completedQuizIds = ["abc", "def", "ghi"] // TODO: store and get from firestore
 
@@ -19,16 +16,17 @@ export const StepActionButton = () => {
    const currentQuiz = useCurrentQuiz()
    const quizState = useQuizState(currentQuiz?.id)
    const isLastStep = useIsLastStep()
-   const moduleId = useModuleId()
-   const moduleProgress = useModuleProgress(moduleId)
-   const isModuleCompleted = Boolean(moduleProgress?.completedAt)
+   // const moduleId = useModuleId()
+   // const { moduleProgress } = useModuleProgress(moduleId)
+   // const isModuleCompleted = Boolean(moduleProgress?.completedAt)
 
    if (!currentStep) return null
 
    // Show restart button if module is completed
-   if (isModuleCompleted) {
-      return <RestartModuleButton />
-   }
+   // if (isModuleCompleted) {
+   //    return <RestartModuleButton />
+   // }
+   // TODO: uncomment this before merge
 
    if (currentQuiz) {
       const quizIsAttempted = quizState.state !== QUIZ_STATE.NOT_ATTEMPTED
