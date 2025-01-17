b4dc72da374da035c1f39d04d01764952ea3c4e8
Walter Gonçalves <152179257+wgoncalvescf@users.noreply.github.com>
2025-01-17 13:08:24 +0100

    Talent Profile: UI improvements (#1486)
    ### Motivation
    From [issue](https://linear.app/careerfairy/issue/CF-1283/ui-improvements-on-talent-profile): After further review of the talent profile these UI improvements are needed before going live
    <!-- A little context about this change. -->
    
    ### Changes
    
    -  Minor styling updates in components
    - Update snack bar options, allowing specification of notification position.
    - Fixed back navigation on settings page and other minor bugs
    
    <!-- You can remove anything that doesn't make sense for this change. -->
    
    ### Demo
    
    <div class='graphite__hidden'>
              <div>🎥 Video uploaded on Graphite:</div>
                <a href="https://app.graphite.dev/media/video/S0WylOadBGaro8NXGBwS/4013375c-631a-4b58-bd4e-6a52c146766f.mov">
                  <img src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/S0WylOadBGaro8NXGBwS/4013375c-631a-4b58-bd4e-6a52c146766f.mov">
                </a>
              </div>
    <video src="https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/S0WylOadBGaro8NXGBwS/4013375c-631a-4b58-bd4e-6a52c146766f.mov">Gravação do ecrã 2025-01-15, às 10.19.28.mov</video>
    
    <!-- Consider including a short demo video. -->
    
    ### Browser Testing
    
    -  [x] Chrome
    -  [ ] Firefox
    -  [ ] Safari
    
    ### Different Resolutions Testing
    
    -  [x] Large Screens
    -  [ ] Medium Screens
    -  [x] Small Screens

--- apps/web/components/views/common/ConditionalWrapper.tsx
+++ apps/web/components/views/common/ConditionalWrapper.tsx
@@ -5,9 +5,10 @@ type ConditionalWrapperProps = {
    condition: boolean
    wrapper: (children: React.ReactElement) => JSX.Element
 }
-const ConditionalWrapper: React.FC<ConditionalWrapperProps> = ({
+export const ConditionalWrapper: React.FC<ConditionalWrapperProps> = ({
    condition,
    wrapper,
    children,
 }) => (condition ? wrapper(children) : children)
-export default ConditionalWrapper
+
+// export default ConditionalWrapper
