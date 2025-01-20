0d4b361db97981a0532f7cb6139aa4c504bff165
Habib Kadiri <habib@careerfairy.io>
2025-01-20 11:31:36 +0100

    refactor: Enhance error handling across login and signup forms (#1503)
    - Updated error logging to use `errorLogAndNotify` function for better error tracking and user feedback.
    - Added specific error messages for login, signup, and location updates to improve clarity.

--- apps/web/components/views/signup/steps/SocialInformation.tsx
+++ apps/web/components/views/signup/steps/SocialInformation.tsx
@@ -1,13 +1,14 @@
-import { Grid, Typography } from "@mui/material"
-import { sxStyles } from "../../../../types/commonTypes"
-import { useLocalStorage } from "react-use"
-import { localStorageReferralCode } from "../../../../constants/localStorageKeys"
-import React, { useCallback, useEffect, useState } from "react"
-import { useAuth } from "../../../../HOCs/AuthProvider"
-import { userRepo } from "../../../../data/RepositoryInstances"
-import ReferralCodeInput from "../../common/inputs/ReferralCodeInput"
-import LinkedInInput from "../../common/inputs/LinkedInInput"
 import { UserData } from "@careerfairy/shared-lib/dist/users"
+import { Grid, Typography } from "@mui/material"
+import { useCallback, useEffect, useState } from "react"
+import { useLocalStorage } from "react-use"
+import { errorLogAndNotify } from "util/CommonUtil"
+import { useAuth } from "../../../../HOCs/AuthProvider"
+import { localStorageReferralCode } from "../../../../constants/localStorageKeys"
+import { userRepo } from "../../../../data/RepositoryInstances"
+import { sxStyles } from "../../../../types/commonTypes"
+import LinkedInInput from "../../common/inputs/LinkedInInput"
+import ReferralCodeInput from "../../common/inputs/ReferralCodeInput"
 
 const styles = sxStyles({
    inputLabel: {
@@ -42,7 +43,9 @@ const SocialInformation = () => {
                fieldToUpdate
             )
          } catch (error) {
-            console.log(error)
+            errorLogAndNotify(error, {
+               message: "Error updating social information in SignUp",
+            })
          }
       },
       [currentUser]
@@ -52,11 +55,16 @@ const SocialInformation = () => {
       if (userData) {
          const { linkedinUrl, referredBy } = userData
 
-         if (linkedinUrl && inputValues[LINKEDIN_FIELD_NAME] === "") {
-            setInputValues((prev) => ({
-               ...prev,
-               [LINKEDIN_FIELD_NAME]: linkedinUrl || "",
-            }))
+         if (linkedinUrl) {
+            setInputValues((prev) => {
+               if (prev[LINKEDIN_FIELD_NAME] !== "") {
+                  return prev
+               }
+               return {
+                  ...prev,
+                  [LINKEDIN_FIELD_NAME]: linkedinUrl || "",
+               }
+            })
          }
 
          if (referredBy) {
@@ -74,43 +82,41 @@ const SocialInformation = () => {
    }, [])
 
    return (
-      <>
-         <Grid
-            container
-            spacing={2}
-            justifyContent="center"
-            data-testid="registration-social-information-step"
-         >
-            <Grid item xs={12} sm={8}>
-               <Typography sx={styles.inputLabel} variant="h5">
-                  Do you have a linkedin account?
-               </Typography>
-            </Grid>
-            <Grid item xs={12} sm={8}>
-               <LinkedInInput
-                  name={LINKEDIN_FIELD_NAME}
-                  linkedInValue={inputValues[LINKEDIN_FIELD_NAME]}
-                  onUpdateField={updateFields}
-                  onChange={handleInputChange}
-               />
-            </Grid>
-
-            <Grid item xs={12} sm={8}>
-               <Typography sx={styles.inputLabel} variant="h5">
-                  Do you have a referral code?
-               </Typography>
-            </Grid>
-            <Grid item xs={12} sm={8}>
-               <ReferralCodeInput
-                  name={REFERRAL_CODE_FIELD_NAME}
-                  referralCodeValue={inputValues[REFERRAL_CODE_FIELD_NAME]}
-                  onChange={handleInputChange}
-                  isValid={isValidReferralCode}
-                  onSetIsValid={setIsValidReferralCode}
-               />
-            </Grid>
+      <Grid
+         container
+         spacing={2}
+         justifyContent="center"
+         data-testid="registration-social-information-step"
+      >
+         <Grid item xs={12} sm={8}>
+            <Typography sx={styles.inputLabel} variant="h5">
+               Do you have a linkedin account?
+            </Typography>
          </Grid>
-      </>
+         <Grid item xs={12} sm={8}>
+            <LinkedInInput
+               name={LINKEDIN_FIELD_NAME}
+               linkedInValue={inputValues[LINKEDIN_FIELD_NAME]}
+               onUpdateField={updateFields}
+               onChange={handleInputChange}
+            />
+         </Grid>
+
+         <Grid item xs={12} sm={8}>
+            <Typography sx={styles.inputLabel} variant="h5">
+               Do you have a referral code?
+            </Typography>
+         </Grid>
+         <Grid item xs={12} sm={8}>
+            <ReferralCodeInput
+               name={REFERRAL_CODE_FIELD_NAME}
+               referralCodeValue={inputValues[REFERRAL_CODE_FIELD_NAME]}
+               onChange={handleInputChange}
+               isValid={isValidReferralCode}
+               onSetIsValid={setIsValidReferralCode}
+            />
+         </Grid>
+      </Grid>
    )
 }
 
