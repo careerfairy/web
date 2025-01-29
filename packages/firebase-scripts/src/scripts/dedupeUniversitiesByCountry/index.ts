33be29146e133ab32b3d26404c1f5e2db04dc765
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-01-29 09:03:26 +0100

    update users

--- packages/firebase-scripts/src/scripts/dedupeUniversitiesByCountry/index.ts
+++ packages/firebase-scripts/src/scripts/dedupeUniversitiesByCountry/index.ts
@@ -2,13 +2,40 @@ import {
    University,
    UniversityCountry,
 } from "@careerfairy/shared-lib/dist/universities"
+import { UserData } from "@careerfairy/shared-lib/dist/users"
 import { findElementsWithDuplicatePropertiesInArray } from "@careerfairy/shared-lib/dist/utils"
 import * as cliProgress from "cli-progress"
 import Counter from "../../lib/Counter"
 import { firestore } from "../../lib/firebase"
-import { universitiesRepo } from "../../repositories"
+import { universitiesRepo, userRepo } from "../../repositories"
 import { logAction } from "../../util/logger"
 import { getCLIBarOptions } from "../../util/misc"
+
+type RemovedUniversitiesData = Record<
+   string,
+   {
+      removedUniversity: University
+      replacedBy: University
+   }
+>
+
+type DeduplicationData = Record<
+   string,
+   {
+      originalDocument: UniversityCountry
+      deduplicatedUniversities: University[]
+      removedUniversities: RemovedUniversitiesData
+   }
+>
+
+type UserUniversitiesData = Record<
+   string,
+   {
+      userIds: string[]
+      countryId: string
+      newUniversity: UserData["university"]
+   }
+>
 
 const counter = new Counter()
 
@@ -20,6 +47,7 @@ const dedupeProgressBar = new cliProgress.SingleBar(
    cliProgress.Presets.shades_classic
 )
 
+const PERFORM_UPDATE = false
 export async function run() {
    console.log("Start deduping universities by country collection")
    const bulkWriter = firestore.bulkWriter()
@@ -39,39 +67,98 @@ export async function run() {
       // console.log("🚀 ~ run ~ china length:", china?.universities?.length)
 
       const duplicationData = getCountriesWithDuplicateUniversities(
-         allUniversitiesByCountry.slice(0, 2)
+         allUniversitiesByCountry.slice(3, 4)
       )
 
       const duplicatedCountries = Object.keys(duplicationData) || []
 
+      // Fetch all users using the removed universities
+      const userUniversities: UserUniversitiesData = {}
+
+      for (const countryId of duplicatedCountries) {
+         const removedUniversities =
+            duplicationData[countryId].removedUniversities
+
+         for (const removedUniversity of Object.keys(removedUniversities)) {
+            const users = await getUsersUsingRemovedUniversities(
+               countryId,
+               removedUniversities[removedUniversity].removedUniversity
+            )
+
+            counter.addToReadCount(users?.length || 0)
+            users?.forEach((user) => {
+               const userIds =
+                  userUniversities[removedUniversity]?.userIds || []
+               userUniversities[removedUniversity] = {
+                  userIds: [...userIds, user.id],
+                  countryId,
+                  newUniversity: {
+                     ...user.university,
+                     // universitiesByCountry collection university has field id but userData university has field code
+                     code: removedUniversities[removedUniversity].replacedBy.id,
+                  },
+               }
+            })
+         }
+      }
+
       // Logging
-      console.log("🚀 ~ duplicate countries list: ", duplicatedCountries.length)
+      console.log("🚀 ~ duplicate countries: ", duplicatedCountries.length)
       duplicatedCountries.forEach((countryId) => {
          console.log(
             `🚀 ~ ~ country: ${countryId} - original universities: ${duplicationData[countryId].originalDocument.universities.length} - deduplicated universities: ${duplicationData[countryId].deduplicatedUniversities.length}`
          )
-         console.log(
-            `🚀 ~ ~ ~ removed universities: ${duplicationData[
-               countryId
-            ].removedUniversities
-               .map((university) => university.name)
-               .join(", ")}`
+         const removedUnis = Object.keys(
+            duplicationData[countryId].removedUniversities
          )
+
+         removedUnis.forEach((removedUniversity) => {
+            console.log(
+               `🚀 ~ ~ ~ removed university: ${removedUniversity} -  ${duplicationData[countryId].removedUniversities[removedUniversity].removedUniversity.name}`
+            )
+
+            console.log(
+               `🚀 ~ ~ ~ ~ ~ replaced by: ${duplicationData[countryId].removedUniversities[removedUniversity].replacedBy.id} - ${duplicationData[countryId].removedUniversities[removedUniversity].replacedBy.name}`
+            )
+
+            console.log(
+               `🚀 ~ ~ ~ ~ ~ ~ ~ users to be updated: ${
+                  userUniversities[removedUniversity]?.userIds ?? "none"
+               }`
+            )
+         })
       })
 
+      console.log("🚀 ~ run ~ userUniversities:", userUniversities)
       // Data update
-      duplicatedCountries.forEach((countryId) => {
-         const docRef = firestore
-            .collection("universitiesByCountry")
-            .doc(countryId)
+      if (PERFORM_UPDATE) {
+         duplicatedCountries.forEach((countryId) => {
+            const docRef = firestore
+               .collection("universitiesByCountry")
+               .doc(countryId)
 
-         bulkWriter.update(docRef, {
-            universities: duplicationData[countryId].deduplicatedUniversities,
+            bulkWriter.update(docRef, {
+               universities:
+                  duplicationData[countryId].deduplicatedUniversities,
+            })
+
+            counter.writeIncrement()
+            dedupeProgressBar.increment()
          })
 
-         counter.writeIncrement()
-         dedupeProgressBar.increment()
-      })
+         Object.keys(userUniversities)?.forEach((universityId) => {
+            userUniversities[universityId].userIds.forEach((userId) => {
+               const docRef = firestore.collection("users").doc(userId)
+
+               bulkWriter.update(docRef, {
+                  university: userUniversities[universityId].newUniversity,
+               })
+
+               counter.writeIncrement()
+               dedupeProgressBar.increment()
+            })
+         })
+      }
 
       dedupeProgressBar.stop()
       await logAction(() => bulkWriter.close(), "Closing BulkWriter")
@@ -82,17 +169,17 @@ export async function run() {
    }
 }
 
+const getUsersUsingRemovedUniversities = async (
+   countryId: string,
+   removedUniversity: University
+) => {
+   return await userRepo.getUsersByUniversity(countryId, removedUniversity.id)
+}
+
 const getCountriesWithDuplicateUniversities = (
    universitiesByCountry: UniversityCountry[]
 ) => {
-   const countriesWithDuplicates: Record<
-      string,
-      {
-         originalDocument: UniversityCountry
-         deduplicatedUniversities: University[]
-         removedUniversities: University[]
-      }
-   > = {}
+   const countriesWithDuplicates: DeduplicationData = {}
 
    universitiesByCountry?.forEach((universityCountry) => {
       const universities = universityCountry.universities
@@ -113,10 +200,22 @@ const getCountriesWithDuplicateUniversities = (
             (university) => !deduplicatedUniversities.includes(university)
          )
 
+         const removedUniversitiesData: RemovedUniversitiesData = {}
+
+         removedUniversities.forEach((university) => {
+            removedUniversitiesData[university.id] = {
+               removedUniversity: university,
+               replacedBy: deduplicatedUniversities.find(
+                  (deduplicatedUniversity) =>
+                     deduplicatedUniversity.name === university.name
+               ),
+            }
+         })
+
          countriesWithDuplicates[universityCountry.countryId] = {
             originalDocument: universityCountry,
             deduplicatedUniversities: deduplicatedUniversities,
-            removedUniversities: removedUniversities,
+            removedUniversities: removedUniversitiesData,
          }
       }
    })
