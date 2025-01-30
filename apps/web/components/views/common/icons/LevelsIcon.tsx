19446b8b2ffff39295cd854957e6e434f518f51b
matilderamosCF <155964282+matilderamosCF@users.noreply.github.com>
2025-01-30 19:18:12 +0000

    Levels: Menu UI (#1522)
    ### Motivation
    
    Adds Levels to the menu and updates the menu ui. the levels option only shows to students who have indicated they are from/studied in CH or DE, or through their IP if they are signed out.
    
    ### Changes
    - Several styling updated
    - Implemented tabs-based navigation for mobile views
    - Added country-specific targeting for Levels feature (DE/CH only)
    - Added support for icon fill states to indicate active/inactive states
    
    ### Demo
    
    ![Screenshot 2025-01-30 at 11.20.15.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Qb035qW3HLJok1JZLYqK/25fdc873-6e0d-4b5d-865c-76e67e62e1a0.png)
    
    ![Screenshot 2025-01-30 at 11.21.30.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Qb035qW3HLJok1JZLYqK/7e236bf4-329b-4b32-83c7-2c407bcd94f7.png)
    
    ![Screenshot 2025-01-30 at 11.23.29.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Qb035qW3HLJok1JZLYqK/a52806ee-9f5d-486c-93fc-16ab79d96a9a.png)
    
    ### Different Resolutions Testing
    
    - [ ] Large Screens
    - [ ] Medium Screens
    - [ ] Small Screens
    
    ### Browser Testing
    
    - [ ] Chrome
    - [ ] Firefox
    - [ ] Safari

--- apps/web/components/views/common/icons/LevelsIcon.tsx
+++ apps/web/components/views/common/icons/LevelsIcon.tsx
@@ -1,53 +1,6 @@
 import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon"
 
-type LevelsIconProps = SvgIconProps & {
-   isOutlined?: boolean
-}
-
-export const LevelsIcon = ({ isOutlined, ...props }: LevelsIconProps) => {
-   if (isOutlined) {
-      return (
-         <SvgIcon viewBox="0 0 16 16" {...props}>
-            <svg
-               xmlns="http://www.w3.org/2000/svg"
-               width="16"
-               height="16"
-               viewBox="0 0 16 16"
-               fill="none"
-            >
-               <g>
-                  <path
-                     d="M8.00065 1.33301L1.33398 4.66634L8.00065 7.99967L14.6673 4.66634L8.00065 1.33301Z"
-                     stroke="currentColor"
-                     strokeWidth="1.5"
-                     strokeLinecap="round"
-                     strokeLinejoin="round"
-                  />
-                  <path
-                     d="M1.33398 11.333L8.00065 14.6663L14.6673 11.333"
-                     stroke="currentColor"
-                     strokeWidth="1.5"
-                     strokeLinecap="round"
-                     strokeLinejoin="round"
-                  />
-                  <path
-                     d="M1.33398 8L8.00065 11.3333L14.6673 8"
-                     stroke="currentColor"
-                     strokeWidth="1.5"
-                     strokeLinecap="round"
-                     strokeLinejoin="round"
-                  />
-               </g>
-               <defs>
-                  <clipPath id="clip0_16149_138847">
-                     <rect width="16" height="16" fill="white" />
-                  </clipPath>
-               </defs>
-            </svg>
-         </SvgIcon>
-      )
-   }
-
+export const LevelsIcon = ({ fill = "none", ...props }: SvgIconProps) => {
    return (
       <SvgIcon viewBox="0 0 24 24" {...props}>
          <svg
@@ -55,29 +8,30 @@ export const LevelsIcon = ({ isOutlined, ...props }: LevelsIconProps) => {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            fill="none"
          >
             <path
                d="M12 2L2 7L12 12L22 7L12 2Z"
-               fill="currentColor"
                stroke="currentColor"
-               strokeWidth="2"
+               strokeWidth={fill == "none" ? "1.5" : 2}
                strokeLinecap="round"
                strokeLinejoin="round"
+               fill={fill}
             />
             <path
                d="M2 17L12 22L22 17"
                stroke="currentColor"
-               strokeWidth="2"
+               strokeWidth={fill == "none" ? "1.5" : 2}
                strokeLinecap="round"
                strokeLinejoin="round"
+               fill="none"
             />
             <path
                d="M2 12L12 17L22 12"
                stroke="currentColor"
-               strokeWidth="2"
+               strokeWidth={fill == "none" ? "1.5" : 2}
                strokeLinecap="round"
                strokeLinejoin="round"
+               fill="none"
             />
          </svg>
       </SvgIcon>
