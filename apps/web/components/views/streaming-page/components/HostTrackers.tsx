cd22bcd47215b0d8b0bdcea87286feb5339983df
habibkadiri <kadirit@hotmail.com>
2025-02-05 13:24:31 +0100

    fix(streaming): add error tracking for Agora RTC client and improve error handling in talent guide pages

--- apps/web/components/views/streaming-page/components/HostTrackers.tsx
+++ apps/web/components/views/streaming-page/components/HostTrackers.tsx
@@ -1,6 +1,8 @@
+import { useClientEvent, useRTCClient } from "agora-rtc-react"
 import { useHandRaiseNotificationTracker } from "components/custom-hook/streaming/hand-raise/useHandRaiseNotificationTracker"
 import { ActiveViews } from "store/reducers/streamingAppReducer"
 import { useSidePanel } from "store/selectors/streamingAppSelectors"
+import { errorLogAndNotify } from "util/CommonUtil"
 import { useStreamingContext } from "../context"
 
 /**
@@ -9,11 +11,20 @@ import { useStreamingContext } from "../context"
 export const HostTrackers = () => {
    const { livestreamId } = useStreamingContext()
    const { isOpen, activeView } = useSidePanel()
+   const rtcClient = useRTCClient()
 
    useHandRaiseNotificationTracker(
       livestreamId,
       isOpen && activeView === ActiveViews.HAND_RAISE
    )
+
+   useClientEvent(rtcClient, "exception", ({ code, msg, uid }) => {
+      errorLogAndNotify(
+         new Error(
+            `Agora exception: Message: ${msg}, Code: ${code}, UID: ${uid}`
+         )
+      )
+   })
 
    return null
 }
