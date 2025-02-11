f86359987bbb59db2e9fcf449d418ecc32ec5086
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-02-11 16:43:56 +0100

    update

--- apps/web/components/views/portal/companies/featured/FeaturedCompaniesHeader.tsx
+++ apps/web/components/views/portal/companies/featured/FeaturedCompaniesHeader.tsx
@@ -1,59 +1,9 @@
 import { FieldOfStudyCategory } from "@careerfairy/shared-lib/fieldOfStudy"
-import { sxStyles } from "@careerfairy/shared-ui"
 import { Box, Stack, Typography } from "@mui/material"
-import { useAuth } from "HOCs/AuthProvider"
-import useFeaturedCompanies from "components/custom-hook/group/useFeaturedCompanies"
-import { useFieldOfStudyById } from "components/custom-hook/useCollection"
 import useIsMobile from "components/custom-hook/useIsMobile"
 import ConditionalWrapper from "components/util/ConditionalWrapper"
 import Link from "next/link"
 import { ChevronLeft, ChevronRight } from "react-feather"
-
-const styles = sxStyles({
-   root: {
-      mx: 2,
-   },
-})
-
-export const FeaturedCompanies = () => {
-   const { userData } = useAuth()
-
-   // Possibly return null if user also does not have a field of study
-   // Checking field of study, as the copy of the header is based on the field of study
-   if (!userData || !userData.fieldOfStudy?.id) return null
-
-   return (
-      <FeaturedCompaniesComponent
-         fieldOfStudyId={userData.fieldOfStudy.id}
-         suspense={false}
-      />
-   )
-}
-
-type Props = {
-   fieldOfStudyId: string
-   suspense?: boolean
-}
-
-const FeaturedCompaniesComponent = ({
-   fieldOfStudyId,
-   suspense = true,
-}: Props) => {
-   const { data: featuredCompanies } = useFeaturedCompanies(suspense)
-   const { data: fieldOfStudy } = useFieldOfStudyById(fieldOfStudyId, suspense)
-
-   if (!featuredCompanies) return null
-
-   return (
-      <Box sx={styles.root}>
-         <FeaturedCompaniesHeader
-            category={fieldOfStudy?.category}
-            onPreviousClick={() => {}}
-            onNextClick={() => {}}
-         />
-      </Box>
-   )
-}
 
 type FeaturedCompaniesHeader = {
    category: FieldOfStudyCategory
@@ -61,7 +11,7 @@ type FeaturedCompaniesHeader = {
    onNextClick: () => void
 }
 
-const FeaturedCompaniesHeader = ({
+export const FeaturedCompaniesHeader = ({
    category,
    onPreviousClick,
    onNextClick,
