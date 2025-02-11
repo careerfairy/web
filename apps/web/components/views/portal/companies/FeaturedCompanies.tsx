e878fc7991ce614cb92690f3634d8a9aaebfa8a4
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-02-11 13:38:06 +0100

    Featured companies #2: portal ui

--- apps/web/components/views/portal/companies/FeaturedCompanies.tsx
+++ apps/web/components/views/portal/companies/FeaturedCompanies.tsx
@@ -1,7 +1,13 @@
+import { FieldOfStudyCategory } from "@careerfairy/shared-lib/fieldOfStudy"
 import { sxStyles } from "@careerfairy/shared-ui"
-import { Box, Typography } from "@mui/material"
+import { Box, Stack, Typography } from "@mui/material"
 import { useAuth } from "HOCs/AuthProvider"
 import useFeaturedCompanies from "components/custom-hook/group/useFeaturedCompanies"
+import { useFieldOfStudyById } from "components/custom-hook/useCollection"
+import useIsMobile from "components/custom-hook/useIsMobile"
+import ConditionalWrapper from "components/util/ConditionalWrapper"
+import Link from "next/link"
+import { ChevronLeft, ChevronRight } from "react-feather"
 
 const styles = sxStyles({
    root: {
@@ -13,24 +19,77 @@ export const FeaturedCompanies = () => {
    const { userData } = useAuth()
 
    // Possibly return null if user also does not have a field of study
-   if (!userData) return null
+   // Checking field of study, as the copy of the header is based on the field of study
+   if (!userData || !userData.fieldOfStudy?.id) return null
 
-   return <FeaturedCompaniesComponent suspense={false} />
+   return (
+      <FeaturedCompaniesComponent
+         fieldOfStudyId={userData.fieldOfStudy.id}
+         suspense={false}
+      />
+   )
 }
 
 type Props = {
+   fieldOfStudyId: string
    suspense?: boolean
 }
 
-const FeaturedCompaniesComponent = ({ suspense = true }: Props) => {
+const FeaturedCompaniesComponent = ({
+   fieldOfStudyId,
+   suspense = true,
+}: Props) => {
    const { data: featuredCompanies } = useFeaturedCompanies(suspense)
+   const { data: fieldOfStudy } = useFieldOfStudyById(fieldOfStudyId, suspense)
 
    if (!featuredCompanies) return null
+
    return (
       <Box sx={styles.root}>
-         <Typography variant="h6">
-            Featured Companies: {featuredCompanies?.length}
-         </Typography>
+         <FeaturedCompaniesHeader
+            category={fieldOfStudy?.category}
+            onPreviousClick={() => {}}
+            onNextClick={() => {}}
+         />
       </Box>
+   )
+}
+
+type FeaturedCompaniesHeader = {
+   category: FieldOfStudyCategory
+   onPreviousClick: () => void
+   onNextClick: () => void
+}
+
+const FeaturedCompaniesHeader = ({
+   category,
+   onPreviousClick,
+   onNextClick,
+}: FeaturedCompaniesHeader) => {
+   const isMobile = useIsMobile()
+
+   const MobileHeader = (
+      <Typography variant="h6">Featured Companies: {category}</Typography>
+   )
+
+   return (
+      <ConditionalWrapper condition={!isMobile} fallback={MobileHeader}>
+         <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+         >
+            <Typography variant="h6">
+               Featured Companies v2: {category}
+            </Typography>
+            <Stack direction="row" alignItems="center" gap={1}>
+               <Link href={`/portal/companies/featured/${category}`}>
+                  <Typography>Sell all companies</Typography>
+               </Link>
+               <Box component={ChevronLeft} onClick={onPreviousClick} />
+               <Box component={ChevronRight} onClick={onNextClick} />
+            </Stack>
+         </Stack>
+      </ConditionalWrapper>
    )
 }
