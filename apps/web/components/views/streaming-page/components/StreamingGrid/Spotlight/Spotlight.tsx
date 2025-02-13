85aa436e89da77f02616458dbc940ffee8f0cfd7
matilderamosCF <155964282+matilderamosCF@users.noreply.github.com>
2025-02-13 09:58:16 +0000

    LS room: full screen support (#1536)
    ### Motivation
    Enhances the streaming experience by adding fullscreen capabilities, improved mute controls, and a first-time user presentation prompt.
    
    ### Changes
    - Added fullscreen toggle functionality for mobile devices
    - Implemented a new overlay control system for mute and fullscreen actions
    - Created a first-time user presentation prompt with animation
    
    ### Demo
    
    [Screen Recording 2025-02-07 at 09.37.19.mov <span class="graphite__hidden">(uploaded via Graphite)</span> <img class="graphite__hidden" src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/Qb035qW3HLJok1JZLYqK/0d7ecc07-8973-4fbd-8083-b6fc1eec9121.mov" />](https://app.graphite.dev/media/video/Qb035qW3HLJok1JZLYqK/0d7ecc07-8973-4fbd-8083-b6fc1eec9121.mov)
    
    ### Mobile Builds
    Android
    ![Screenshot 2025-02-07 at 10.11.47.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Qb035qW3HLJok1JZLYqK/0a4701aa-f877-4f29-95af-8be4daec3bb9.png)
    
    iOS
    
    ![Screenshot 2025-02-07 at 10.15.55.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Qb035qW3HLJok1JZLYqK/f9626025-72f8-4bbd-a1d5-125d391ce1d1.png)
    
    ### Browser Testing
    - [x] Chrome
    - [ ] Firefox
    - [ ] Safari
    
    ### Different Resolutions Testing
    - [ ] Large Screens
    - [ ] Medium Screens
    - [x] Small Screens

--- apps/web/components/views/streaming-page/components/StreamingGrid/Spotlight/Spotlight.tsx
+++ apps/web/components/views/streaming-page/components/StreamingGrid/Spotlight/Spotlight.tsx
@@ -1,16 +1,44 @@
 import { LivestreamModes } from "@careerfairy/shared-lib/livestreams"
-import { Box, Slide } from "@mui/material"
-import { useStreamIsLandscape } from "components/custom-hook/streaming"
+import MuteIcon from "@mui/icons-material/VolumeOff"
+import UnmuteIcon from "@mui/icons-material/VolumeUp"
+import {
+   Box,
+   Button,
+   IconButton,
+   Slide,
+   Stack,
+   Typography,
+} from "@mui/material"
+import {
+   useStreamIsLandscape,
+   useStreamIsMobile,
+} from "components/custom-hook/streaming"
+import FramerBox from "components/views/common/FramerBox"
+import BrandedSwipeableDrawer from "components/views/common/inputs/BrandedSwipeableDrawer"
+import { useStreamingContext } from "components/views/streaming-page/context"
+import { AnimatePresence } from "framer-motion"
+import dynamic from "next/dynamic"
+import { useState } from "react"
+import { Maximize, Minimize } from "react-feather"
+import { useLocalStorage } from "react-use"
 import {
    useIsSpotlightMode,
    useLivestreamMode,
 } from "store/selectors/streamingAppSelectors"
 import { sxStyles } from "types/commonTypes"
 import { UserStream } from "../../../types"
-import { SpotlightProvider } from "./SpotlightProvider"
+import { SpotlightProvider, useSpotlight } from "./SpotlightProvider"
 import { SpotlightPDF } from "./pdf/SpotlightPDF"
 import { SpotlightStream } from "./stream/SpotlightStream"
 import { SpotlightVideo } from "./video/SpotlightVideo"
+
+const DotLottiePlayer = dynamic(
+   () =>
+      import("@lottiefiles/dotlottie-react").then((mod) => mod.DotLottieReact),
+   { ssr: false }
+)
+
+const ICON_SIZE = 90
 
 const styles = sxStyles({
    root: {
@@ -20,6 +48,62 @@ const styles = sxStyles({
       borderRadius: "10px",
       justifyContent: "center",
       alignItems: "center",
+   },
+   framerBox: {
+      position: "relative",
+   },
+   framerBoxDefault: {
+      width: "100%",
+      height: "100%",
+   },
+   fullScreenStyles: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100vw",
+      height: "100vh",
+      zIndex: 9999,
+      overflow: "hidden",
+      borderRadius: 0,
+   },
+   fullScreenPortraitStyles: {
+      position: "fixed",
+      top: "50%",
+      left: "50%",
+      width: "100dvh",
+      height: "100dvw",
+      transform: "translate(-50%, -50%) rotate(90deg)",
+   },
+   overlay: {
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: "100%",
+      display: "flex",
+      alignItems: "flex-end",
+      justifyContent: "flex-end",
+      borderRadius: "10px",
+      visibility: "hidden",
+      background:
+         "linear-gradient(rgba(0, 0, 0, 0.00) 66.67%, rgba(0, 0, 0, 0.50) 100%)",
+   },
+   overlayVisible: {
+      visibility: "visible",
+   },
+   overlayFullScreen: {
+      borderRadius: 0,
+      background:
+         "linear-gradient(0deg, rgba(0, 0, 0, 0.50) 1.76%, rgba(0, 0, 0, 0.00) 18.19%)",
+   },
+   controlsButton: {
+      color: "white",
+   },
+   drawerContent: {
+      m: 2.5,
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
    },
 })
 
@@ -35,20 +119,148 @@ type Props = {
 
 export const Spotlight = ({ stream }: Props) => {
    const isLandscape = useStreamIsLandscape()
+   const isMobile = useStreamIsMobile()
    const isSpotlightMode = useIsSpotlightMode()
    const livestreamMode = useLivestreamMode()
+   const { isHost } = useStreamingContext()
+
+   const [isFullScreen, setIsFullScreen] = useState(false)
+   const [showOverlay, setShowOverlay] = useState(false)
+   const [hasSeenFullscreenPrompt, setHasSeenFullscreenPrompt] =
+      useLocalStorage("hasSeenFullscreenPrompt", false)
+
+   const toggleShowControls = () => {
+      setShowOverlay((isOverlay) => !isOverlay)
+   }
+
+   const handleFullscreenClick = (e: React.MouseEvent) => {
+      e.stopPropagation() // Prevent overlay toggle
+      setIsFullScreen(!isFullScreen)
+   }
+
+   if (!Content[livestreamMode]) return null
 
    return (
-      <Slide
-         in={isSpotlightMode}
-         direction={isLandscape ? "right" : "up"}
-         unmountOnExit
+      <>
+         <AnimatePresence>
+            <Slide
+               in={isSpotlightMode}
+               direction={isLandscape ? "right" : "up"}
+               unmountOnExit
+            >
+               <Box style={styles.root}>
+                  <FramerBox
+                     sx={styles.framerBox}
+                     onClick={toggleShowControls}
+                     initial={false}
+                     animate={{
+                        ...(!isFullScreen && styles.framerBoxDefault), // prevent from using fixed values
+                        ...(isFullScreen && styles.fullScreenStyles),
+                        ...(isFullScreen &&
+                           !isLandscape &&
+                           styles.fullScreenPortraitStyles),
+                     }}
+                     layout
+                  >
+                     <SpotlightProvider stream={stream}>
+                        {Content[livestreamMode]}
+                        {!isHost && (
+                           <Overlay
+                              isFullScreen={isFullScreen}
+                              handleFullscreenClick={handleFullscreenClick}
+                              showOverlay={showOverlay}
+                           />
+                        )}
+                     </SpotlightProvider>
+                  </FramerBox>
+               </Box>
+            </Slide>
+         </AnimatePresence>
+         <PresentationPrompt
+            open={Boolean(isMobile) && !isHost && !hasSeenFullscreenPrompt}
+            handleClose={() => setHasSeenFullscreenPrompt(true)}
+         />
+      </>
+   )
+}
+
+const Overlay = ({ isFullScreen, handleFullscreenClick, showOverlay }) => {
+   const { isMuted, handleToggleMute, showMute } = useSpotlight()
+   const isMobile = useStreamIsMobile()
+
+   if (!isMobile && !showMute) return null
+
+   return (
+      <FramerBox
+         sx={{
+            ...styles.overlay,
+            ...(isFullScreen && styles.overlayFullScreen),
+            ...(showOverlay && styles.overlayVisible),
+         }}
+         animate={{
+            opacity: showOverlay ? 1 : 0,
+         }}
       >
-         <Box sx={styles.root}>
-            <SpotlightProvider stream={stream}>
-               {Content[livestreamMode] || null}
-            </SpotlightProvider>
-         </Box>
-      </Slide>
+         <Stack spacing={1} direction={"row"}>
+            {Boolean(showMute) && (
+               <IconButton
+                  onClick={(e) => {
+                     e.stopPropagation()
+                     handleToggleMute()
+                  }}
+                  sx={styles.controlsButton}
+               >
+                  {isMuted ? (
+                     <MuteIcon sx={{ height: "24px", width: "24px" }} />
+                  ) : (
+                     <UnmuteIcon sx={{ height: "24px", width: "24px" }} />
+                  )}
+               </IconButton>
+            )}
+            {Boolean(isMobile) && (
+               <IconButton
+                  onClick={handleFullscreenClick}
+                  sx={styles.controlsButton}
+               >
+                  {isFullScreen ? <Minimize /> : <Maximize />}
+               </IconButton>
+            )}
+         </Stack>
+      </FramerBox>
+   )
+}
+
+const PresentationPrompt = ({ open, handleClose }) => {
+   return (
+      <BrandedSwipeableDrawer
+         open={open}
+         anchor="bottom"
+         onOpen={() => {}}
+         onClose={handleClose}
+         disableEnforceFocus
+      >
+         <Stack sx={styles.drawerContent}>
+            <DotLottiePlayer
+               src="https://lottie.host/34421851-937b-48a7-84bd-3c4e99350217/rCvwWuT7YN.lottie"
+               autoplay
+               loop
+               style={{
+                  width: ICON_SIZE,
+                  height: ICON_SIZE,
+                  marginBottom: 2,
+               }}
+            />
+            <Typography variant="brandedH3" fontWeight={600} mb={1.5}>
+               The streamer is now presenting
+            </Typography>
+            <Typography variant="brandedBody" mb={3}>
+               Click on the presentation to have the option to make it full
+               screen!
+            </Typography>
+            <Button fullWidth variant="contained" onClick={handleClose}>
+               Got it!
+            </Button>
+         </Stack>
+      </BrandedSwipeableDrawer>
    )
 }
