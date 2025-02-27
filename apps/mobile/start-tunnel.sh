206ff80a1d721178ce9fe1248af40a7e4c6f5780
Habib Kadiri <kadirit@hotmail.com>
2025-02-27 14:16:19 +0100

    Add documentation for start-tunnel.sh

--- apps/mobile/start-tunnel.sh
+++ apps/mobile/start-tunnel.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# On Android, the remote URL for the Expo development server defaults to our package name
+# (com.admin_product_team.careerfairymobileapp) as the domain for the Ngrok URL. However,
+# the underscore in our package name causes a crash. This script fixes the issue by
+# generating a remote domain without underscores and ensuring unique URLs to prevent
+# collisions when multiple developers share Expo builds.
 export EXPO_TUNNEL_SUBDOMAIN="careerfairy-$(openssl rand -hex 4)"
+
 expo start -c --tunnel 
