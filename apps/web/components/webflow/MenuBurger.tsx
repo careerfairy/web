46c258e5fc32a5547190e624e76fbcd655984bca
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-03-17 16:35:34 +0100

    fix: :lipstick: b2c drawer

--- apps/web/components/webflow/MenuBurger.tsx
+++ apps/web/components/webflow/MenuBurger.tsx
@@ -53,9 +53,37 @@ const MenuBurger: FC<Props> = ({ id }) => {
 
    const isB2C = id === "burger-menu-b2c"
 
+   const DrawerContent = () => {
+      if (isB2C) {
+         return <B2CDrawer onCloseDrawer={() => setOpen(false)} />
+      }
+      return (
+         <Box>
+            <Link href={"/employers"}>
+               <Box sx={styles.link}>
+                  <Typography variant="brandedBody">For employers</Typography>
+               </Box>
+            </Link>
+            <Link href={"/levels"}>
+               <Box sx={styles.link}>
+                  <Typography variant="brandedBody">Career guide</Typography>
+               </Box>
+            </Link>
+            <Link href={"/whos-hiring"}>
+               <Box sx={styles.link}>
+                  <Typography variant="brandedBody">
+                     Who&apos;s hiring
+                  </Typography>
+               </Box>
+            </Link>
+         </Box>
+      )
+   }
+
    if (!isMobile) {
       return null
    }
+
    return (
       <Box sx={[styles.wrapper]}>
          <Button onClick={() => setOpen(true)}>
@@ -63,30 +91,7 @@ const MenuBurger: FC<Props> = ({ id }) => {
          </Button>
          <Drawer open={open} anchor="right" onClose={() => setOpen(false)}>
             <Box sx={styles.drawer}>
-               {isB2C ? (
-                  <B2CDrawer onCloseDrawer={() => setOpen(false)} />
-               ) : (
-                  <Link href={"/employers"}>
-                     <Box sx={styles.link}>
-                        <Typography variant="brandedBody">
-                           For employers
-                        </Typography>
-                     </Box>
-                  </Link>
-               )}
-               <Link href={"/levels"}>
-                  <Box sx={styles.link}>
-                     <Typography variant="brandedBody">Career guide</Typography>
-                  </Box>
-               </Link>
-               <Link href={"/whos-hiring"}>
-                  <Box sx={styles.link}>
-                     <Typography variant="brandedBody">
-                        Who&apos;s hiring
-                     </Typography>
-                  </Box>
-               </Link>
-
+               <DrawerContent />
                {authenticatedUser.isLoaded && authenticatedUser.isEmpty ? (
                   <Stack spacing={1.5} sx={styles.actions}>
                      <Button
