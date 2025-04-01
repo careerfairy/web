689971f0aa9aa90a9b1a44e81db87893b044adb6
habibkadiri <kadirit@hotmail.com>
2025-04-01 13:13:30 +0200

    init

--- packages/functions/src/fetchUserCountryCode.ts
+++ packages/functions/src/fetchUserCountryCode.ts
@@ -1,9 +1,6 @@
 import * as functions from "firebase-functions"
-import config from "./config"
 import { getCountryCode } from "./util"
 
-export const fetchUserCountryCode = functions
-   .region(config.region)
-   .https.onCall((_, context) => {
-      return getCountryCode(context)
-   })
+export const fetchUserCountryCode = functions.https.onCall((_, context) => {
+   return getCountryCode(context)
+})
