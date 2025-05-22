5668acca0b44c09a1fab4aa635c389900d70a5f3
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-05-22 10:52:56 +0200

    feat: :sparkles: implemented search results

--- apps/web/components/views/jobs-page/overview/search/SearchResultsCount.tsx
+++ apps/web/components/views/jobs-page/overview/search/SearchResultsCount.tsx
@@ -1,31 +1,67 @@
-import { Stack } from "@mui/material"
+import { Stack, useTheme } from "@mui/material"
 
 import { Typography } from "@mui/material"
 import { Frown } from "react-feather"
+import { sxStyles } from "types/commonTypes"
 import { useJobsOverviewContext } from "../../JobsOverviewContext"
+
+const styles = sxStyles({
+   resultsCount: {
+      px: {
+         xs: 2,
+         sm: 2,
+         md: 4,
+      },
+   },
+   notFoundRoot: {
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "8px",
+      border: (theme) => `1px solid ${theme.palette.neutral[50]}`,
+      background: (theme) => theme.brand.white[100],
+      p: "52px 12px",
+   },
+})
 
 export const SearchResultsCount = () => {
    const { searchResultsCount, hasFilters } = useJobsOverviewContext()
 
    if (!hasFilters) return null
 
-   return <Typography>{searchResultsCount} results</Typography>
+   return (
+      <Typography
+         variant="brandedBody"
+         color="neutral.700"
+         sx={styles.resultsCount}
+      >
+         {searchResultsCount} result{searchResultsCount > 1 ? "s" : ""}
+      </Typography>
+   )
 }
 
 export const NoResultsFound = () => {
+   const theme = useTheme()
    const { searchResultsCount, hasFilters, searchTerm } =
       useJobsOverviewContext()
 
    if (!hasFilters || searchResultsCount) return null
 
    return (
-      <Stack alignItems="center" justifyContent="center">
-         <Frown size={40} />
-         <Typography>
-            No jobs found{" "}
+      <Stack sx={styles.notFoundRoot} spacing={1}>
+         <Frown size={40} color={theme.palette.neutral[600]} />
+         <Typography
+            variant="brandedBody"
+            color="neutral.700"
+            textAlign="center"
+         >
+            No jobs found for{" "}
             {searchTerm ? (
-               <Typography component="span">
-                  for &quot;{searchTerm}&quot;
+               <Typography
+                  variant="brandedBody"
+                  color="neutral.700"
+                  fontWeight={600}
+               >
+                  &quot;{searchTerm}&quot;
                </Typography>
             ) : (
                ""
