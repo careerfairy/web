e650815edf0ebda35c48ccb27c9a8716e86b5d66
matilderamosCF <matilde.ramos@careerfairy.io>
2025-06-18 11:19:46 +0100

    Sparks: Add language to recommendation service

--- packages/functions/src/lib/UserFunctionsRepository.ts
+++ packages/functions/src/lib/UserFunctionsRepository.ts
@@ -4,6 +4,7 @@ import {
 } from "@careerfairy/shared-lib/BaseFirebaseRepository"
 import {
    CompanyFollowed,
+   ProfileLanguage,
    RegisteredLivestreams,
    StudyBackground,
    UserData,
@@ -487,6 +488,19 @@ export class UserFunctionsRepository
       return querySnapshot.empty
          ? []
          : querySnapshot.docs.map((doc) => doc.data() as StudyBackground)
+   }
+
+   async getUserLanguages(userId: string): Promise<ProfileLanguage[]> {
+      const querySnapshot = await this.firestore
+         .collection("userData")
+         .doc(userId)
+         .collection("languages")
+         .withConverter(createCompatGenericConverter<ProfileLanguage>())
+         .get()
+
+      return querySnapshot.empty
+         ? []
+         : querySnapshot.docs.map((doc) => doc.data())
    }
 
    async sendWelcomeEmail(
