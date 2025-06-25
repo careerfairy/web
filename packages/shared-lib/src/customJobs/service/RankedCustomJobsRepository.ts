74ff5d8bca818bd45cda3bcc590e4348436bf84d
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-06-25 16:28:49 +0200

    feat: :chart_with_upwards_trend: additional ranking rules & results shuffling
    Added new ranking criteria and points, as well as shuffling mechanism for results

--- packages/shared-lib/src/customJobs/service/RankedCustomJobsRepository.ts
+++ packages/shared-lib/src/customJobs/service/RankedCustomJobsRepository.ts
@@ -12,25 +12,27 @@ export class RankedCustomJobsRepository {
    // from userData
    public readonly USER_BUSINESS_FUNCTIONS_POINTS = 2
    public readonly USER_APPLIED_JOBS_BUSINESS_FUNCTIONS_POINTS = 1.5
-   public readonly USER_JOB_LOCATION_POINTS = 4
+   public readonly USER_JOB_LOCATION_POINTS = 5
    public readonly USER_COUNTRIES_OF_INTEREST_POINTS = 1
-   public readonly USER_FOLLOWING_COMPANIES_POINTS = 1
-   public readonly USER_LAST_VIEWED_JOBS_LOCATIONS_POINTS = 1.5
-   public readonly USER_LAST_VIEWED_JOBS_INDUSTRIES_POINTS = 1
+   public readonly USER_FOLLOWING_COMPANIES_POINTS = 3
+   public readonly USER_LAST_VIEWED_JOBS_LOCATIONS_POINTS = 2.5
+   public readonly USER_LAST_VIEWED_JOBS_INDUSTRIES_POINTS = 1.5
    public readonly USER_LAST_REGISTERED_LIVESTREAMS_INDUSTRIES_POINTS = 0.5
    public readonly USER_LAST_VIEWED_JOBS_BUSINESS_FUNCTIONS_POINTS = 0.5
    public readonly USER_SAVED_JOBS_POINTS = 2
 
    // from referenceJob
-   public readonly REFERENCE_JOB_TYPE_POINTS = 1
+   public readonly REFERENCE_JOB_TYPE_POINTS = 0.5
    public readonly REFERENCE_JOB_BUSINESS_FUNCTIONS_POINTS = 1
-   public readonly REFERENCE_JOB_LOCATION_POINTS = 1.5
+   public readonly REFERENCE_JOB_LOCATION_POINTS = 1
+   public readonly REFERENCE_JOB_INDUSTRY_POINTS = 1.5
 
    // from jobsInfo
-   public readonly JOB_LINKED_UPCOMING_EVENTS_COUNT_POINTS = 2
+   public readonly JOB_LINKED_UPCOMING_EVENTS_COUNT_POINTS = 4
    public readonly JOB_DEADLINE_ONE_WEEK_POINTS = 8
    public readonly JOB_DEADLINE_TWO_WEEKS_POINTS = 2
    public readonly JOB_DEADLINE_ONE_MONTH_POINTS = 0.5
+   public readonly JOB_PUBLISHING_DATE_TWO_WEEKS_POINTS = 4
 
    private readonly customJobs: RankedCustomJob[]
 
