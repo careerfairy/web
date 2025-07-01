ffca209e5778c913e30501fee477cb335b93b0d0
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-07-01 11:43:45 +0200

    test: :white_check_mark: add location filtering tests
    add tests for location based filtering, which takes into consideration hierarchies when generating location filtering data

--- packages/functions/src/__tests__/locations.test.ts
+++ packages/functions/src/__tests__/locations.test.ts
@@ -1,4 +1,8 @@
-import { inLocation } from "@careerfairy/shared-lib/countries/types"
+import {
+   inLocation,
+   normalizeLocationIds,
+   normalizeLocationIdsForFiltering,
+} from "@careerfairy/shared-lib/countries/types"
 
 describe("inLocation", () => {
    it("returns all ids in the same country hierarchy", () => {
@@ -55,5 +59,130 @@ describe("inLocation", () => {
       expect(
          inLocation("CH-FR-Villars", ["CH-FR-Villars", "CH-FR", "CH"])
       ).toEqual(["CH-FR-Villars"])
+   })
+})
+
+describe("normalizeLocationIds", () => {
+   it("returns all normalized ids for countries", () => {
+      expect(normalizeLocationIds(["US"]).sort()).toEqual(["Country_US"])
+      expect(normalizeLocationIds(["CH", "PT"]).sort()).toEqual([
+         "Country_CH",
+         "Country_PT",
+      ])
+   })
+
+   it("returns all normalized ids for states", () => {
+      expect(normalizeLocationIds(["US-CA"]).sort()).toEqual([
+         "Country_US",
+         "Country_US_State_CA",
+      ])
+      expect(normalizeLocationIds(["CH-FR", "PT-LI"]).sort()).toEqual([
+         "Country_CH",
+         "Country_CH_State_FR",
+         "Country_PT",
+         "Country_PT_State_LI",
+      ])
+   })
+
+   it("returns all normalized ids for cities", () => {
+      expect(normalizeLocationIds(["US-CA-San Francisco"]).sort()).toEqual([
+         "Country_US",
+         "Country_US_State_CA",
+         "Country_US_State_CA_City_San Francisco",
+      ])
+      expect(
+         normalizeLocationIds(["CH-FR-Fribourg", "PT-LI-Lisbon"]).sort()
+      ).toEqual([
+         "Country_CH",
+         "Country_CH_State_FR",
+         "Country_CH_State_FR_City_Fribourg",
+         "Country_PT",
+         "Country_PT_State_LI",
+         "Country_PT_State_LI_City_Lisbon",
+      ])
+   })
+
+   it("removes duplicates", () => {
+      expect(
+         normalizeLocationIds(["US-CA-San Francisco", "US-CA", "US"]).sort()
+      ).toEqual([
+         "Country_US",
+         "Country_US_State_CA",
+         "Country_US_State_CA_City_San Francisco",
+      ])
+   })
+
+   it("handles empty input", () => {
+      expect(normalizeLocationIds([])).toEqual([])
+   })
+})
+
+describe("normalizeLocationIdsForFiltering", () => {
+   it("returns highest precision id for countries", () => {
+      expect(normalizeLocationIdsForFiltering(["US"]).sort()).toEqual([
+         "Country_US",
+      ])
+      expect(normalizeLocationIdsForFiltering(["CH", "PT"]).sort()).toEqual([
+         "Country_CH",
+         "Country_PT",
+      ])
+   })
+
+   it("returns highest precision id for states", () => {
+      expect(normalizeLocationIdsForFiltering(["US-CA"]).sort()).toEqual([
+         "Country_US_State_CA",
+      ])
+      expect(
+         normalizeLocationIdsForFiltering(["CH-FR", "PT-LI"]).sort()
+      ).toEqual(["Country_CH_State_FR", "Country_PT_State_LI"])
+   })
+
+   it("returns highest precision id for cities", () => {
+      expect(
+         normalizeLocationIdsForFiltering(["US-CA-San Francisco"]).sort()
+      ).toEqual(["Country_US_State_CA_City_San Francisco"])
+      expect(
+         normalizeLocationIdsForFiltering([
+            "CH-FR-Fribourg",
+            "PT-LI-Lisbon",
+         ]).sort()
+      ).toEqual([
+         "Country_CH_State_FR_City_Fribourg",
+         "Country_PT_State_LI_City_Lisbon",
+      ])
+   })
+
+   it("multiple locations", () => {
+      expect(
+         normalizeLocationIdsForFiltering([
+            "US-CA-San Francisco",
+            "US-CA",
+            "US",
+         ]).sort()
+      ).toEqual([
+         "Country_US",
+         "Country_US_State_CA",
+         "Country_US_State_CA_City_San Francisco",
+      ])
+   })
+
+   it("removes duplicates", () => {
+      expect(
+         normalizeLocationIdsForFiltering([
+            "US-CA-San Francisco",
+            "US-CA",
+            "US",
+            "US",
+            "US-CA",
+         ]).sort()
+      ).toEqual([
+         "Country_US",
+         "Country_US_State_CA",
+         "Country_US_State_CA_City_San Francisco",
+      ])
+   })
+
+   it("handles empty input", () => {
+      expect(normalizeLocationIdsForFiltering([])).toEqual([])
    })
 })
