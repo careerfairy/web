324ebee538f0e0f6e05fc8c7b2656628bc74c35a
habibkadiri <kadirit@hotmail.com>
2025-07-04 13:10:21 +0200

    Refactor: Simplify Livestream Analytics Navigation Tabs
    ### Changes
    - Updated `LivestreamAnalyticsNavigationTabs.tsx` to remove unnecessary `catchAllPath` properties from tab definitions, streamlining the tab structure.
    - Introduced a new `Tab` type for better type safety and clarity.
    - Enhanced the `getActiveTab` function to directly match the current pathname without checking for catch-all paths.
    
    This refactor improves code readability and maintainability in the navigation component.

--- apps/web/components/views/group/admin/analytics-new/live-stream/LivestreamAnalyticsNavigationTabs.tsx
+++ apps/web/components/views/group/admin/analytics-new/live-stream/LivestreamAnalyticsNavigationTabs.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material"
 import { BrandedTabs } from "components/views/common/BrandedTabs"
 import { useRouter } from "next/router"
-import { SyntheticEvent } from "react"
+import { ReactNode, SyntheticEvent } from "react"
 import { AtSign, Hexagon, MessageCircle, Users } from "react-feather"
 import { sxStyles } from "types/commonTypes"
 
@@ -11,49 +11,47 @@ const styles = sxStyles({
    },
 })
 
+type Tab = {
+   label: string
+   pathname: string
+   icon: ReactNode
+}
+
+const tabs = [
+   {
+      label: "Overview",
+      pathname: "/group/[groupId]/admin/analytics/live-streams/overview",
+      icon: <Hexagon />,
+   },
+   {
+      label: "Live stream analytics",
+      pathname:
+         "/group/[groupId]/admin/analytics/live-streams/[[...livestreamId]]",
+      icon: <AtSign />,
+   },
+   {
+      label: "Registration Source",
+      pathname:
+         "/group/[groupId]/admin/analytics/live-streams/registration-sources",
+      icon: <Users />,
+   },
+   {
+      label: "Feedback",
+      pathname:
+         "/group/[groupId]/admin/analytics/live-streams/feedback/[[...feedback]]",
+      icon: <MessageCircle />,
+   },
+] as const satisfies Tab[]
+
 export const LivestreamAnalyticsNavigationTabs = () => {
    const { push, query, pathname } = useRouter()
    const groupId = query.groupId as string
-
-   const tabs = [
-      {
-         label: "Overview",
-         pathname: "/group/[groupId]/admin/analytics/live-streams/overview",
-         catchAllPath: undefined,
-         icon: <Hexagon />,
-      },
-      {
-         label: "Live stream analytics",
-         pathname: "/group/[groupId]/admin/analytics/live-streams",
-         catchAllPath:
-            "/group/[groupId]/admin/analytics/live-streams/[[...livestreamId]]",
-         icon: <AtSign />,
-      },
-      {
-         label: "Registration Source",
-         pathname:
-            "/group/[groupId]/admin/analytics/live-streams/registration-sources",
-         catchAllPath: undefined,
-         icon: <Users />,
-      },
-      {
-         label: "Feedback",
-         pathname:
-            "/group/[groupId]/admin/analytics/live-streams/feedback/[[...feedback]]",
-         catchAllPath: undefined,
-         icon: <MessageCircle />,
-      },
-   ] as const
 
    type TabValue = (typeof tabs)[number]["pathname"]
 
    // Find active tab by matching the current pathname with tab values
    const getActiveTab = (): TabValue => {
-      return tabs.find(
-         (tab) =>
-            tab.pathname === pathname ||
-            (tab.catchAllPath && tab.catchAllPath === pathname)
-      )?.pathname
+      return tabs.find((tab) => tab.pathname === pathname)?.pathname
    }
 
    const activeTab = getActiveTab()
