c58120b53f6bdc8d0b099f10d587d5f68a9e667c
habibkadiri <kadirit@hotmail.com>
2025-07-09 16:16:20 +0200

    Refactor promotional page components and introduce new styled elements
    ### Changes
    - Created new styled components for `BenefitsSection` and `FinalCTASection` to enhance modularity and maintainability.
    - Moved styles from the deleted `styles.ts` file to dedicated `hero-section-styles.ts`, improving organization.
    - Updated `HeroSection` to reference the new styles file, ensuring consistency across components.
    - Enhanced layout and responsiveness of the promotional page with new styling adjustments.
    
    These updates improve the structure, maintainability, and visual appeal of the promotional page components.

--- apps/web/components/views/admin/sparks/components/promotional-page/FinalCTASection.tsx
+++ apps/web/components/views/admin/sparks/components/promotional-page/FinalCTASection.tsx
@@ -1,5 +1,30 @@
-import { Button } from "@mui/material"
-import { StyledCTATitle, StyledFinalCTA } from "./styles"
+import { Button, styled, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+
+export const StyledCTATitle = styled(Typography)(({ theme }) => ({
+   fontSize: 40,
+   fontWeight: 800,
+   color: theme.palette.neutral[800],
+   letterSpacing: "-2px",
+   textAlign: "center",
+   [theme.breakpoints.down("md")]: {
+      fontSize: 28,
+      letterSpacing: "-1.4px",
+      lineHeight: 1.09,
+   },
+}))
+
+export const StyledFinalCTA = styled(Box)(({ theme }) => ({
+   display: "flex",
+   flexDirection: "column",
+   alignItems: "center",
+   gap: "12px",
+   width: "100%",
+   maxWidth: "904px",
+   [theme.breakpoints.down("md")]: {
+      maxWidth: "none",
+   },
+}))
 
 export const FinalCTASection = () => {
    return (
