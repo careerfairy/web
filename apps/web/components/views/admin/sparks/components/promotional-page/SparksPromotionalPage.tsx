edde347f46327735b8c3a9a36bae840801c68266
habibkadiri <kadirit@hotmail.com>
2025-07-09 16:16:20 +0200

    Refactor promotional page components and introduce new styled elements
    ### Changes
    - Created new styled components for `BenefitsSection` and `FinalCTASection` to enhance modularity and maintainability.
    - Moved styles from the deleted `styles.ts` file to dedicated `hero-section-styles.ts`, improving organization.
    - Updated `HeroSection` to reference the new styles file, ensuring consistency across components.
    - Enhanced layout and responsiveness of the promotional page with new styling adjustments.
    
    These updates improve the structure, maintainability, and visual appeal of the promotional page components.

--- apps/web/components/views/admin/sparks/components/promotional-page/SparksPromotionalPage.tsx
+++ apps/web/components/views/admin/sparks/components/promotional-page/SparksPromotionalPage.tsx
@@ -1,7 +1,37 @@
+import { Box, Container, styled } from "@mui/material"
 import { BenefitsSection } from "./BenefitsSection"
 import { FinalCTASection } from "./FinalCTASection"
 import { HeroSection } from "./HeroSection"
-import { StyledContainer, StyledPageBackground } from "./styles"
+
+export const StyledContainer = styled(Container)(({ theme }) => ({
+   display: "flex",
+   flexDirection: "column",
+   alignItems: "center",
+   gap: "24px",
+   padding: "24px 32px",
+   [theme.breakpoints.down("md")]: {
+      padding: "12px 0px 56px 0px",
+   },
+}))
+
+export const StyledPageBackground = styled(Box)(({ theme }) => ({
+   background: `linear-gradient(180deg, rgba(147, 215, 208, 0.15) 6.95%, rgba(34, 117, 216, 0.15) 69.54%), #FCFCFE`,
+   borderRadius: "16px 16px 0px 0px",
+   border: `1px solid ${theme.brand.white[400]}`,
+   padding: "32px 20px",
+   display: "flex",
+   flexDirection: "column",
+   alignItems: "center",
+   gap: "32px",
+   width: "100%",
+   maxWidth: "1200px",
+   [theme.breakpoints.down("md")]: {
+      borderRadius: "16px",
+      gap: "48px",
+      padding: "32px 16px",
+      border: "none",
+   },
+}))
 
 export const SparksPromotionalPage = () => {
    return (
