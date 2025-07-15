67e247f55f6e5b5b2c7ac484e8ab56842d4bccb6
habibkadiri <kadirit@hotmail.com>
2025-07-15 17:00:42 +0200

    Implement Profile Menu and enhance Profile Avatar functionality
    ### Changes
    - Introduced `ProfileMenu` component to manage user profile options, including navigation to profile, switching companies, and logging out.
    - Updated `ProfileAvatar` to handle avatar clicks more efficiently, integrating mobile and desktop interactions.
    - Enhanced the layout by wrapping the `AvatarButton` and `ProfileMenu` in a `Fragment` for better structure.
    
    These updates improve user interaction with profile features and streamline the overall user experience in the Group Dashboard layout.

--- apps/web/layouts/GroupDashboardLayout/TopBar/ProfileAvatar.tsx
+++ apps/web/layouts/GroupDashboardLayout/TopBar/ProfileAvatar.tsx
@@ -1,11 +1,14 @@
 import { ButtonBase } from "@mui/material"
 import { styled } from "@mui/material/styles"
+import { Fragment } from "react"
 
 // project imports
 import useIsMobile from "components/custom-hook/useIsMobile"
+import useMenuState from "components/custom-hook/useMenuState"
 import { useAuth } from "../../../HOCs/AuthProvider"
 import ColorizedAvatar from "../../../components/views/common/ColorizedAvatar"
 import { useGroupDashboard } from "../GroupDashboardLayoutProvider"
+import { ProfileMenu } from "./ProfileMenu"
 
 const AvatarButton = styled(ButtonBase)(({ theme }) => ({
    borderRadius: "50%",
@@ -30,25 +33,28 @@ export const ProfileAvatar = () => {
    const { userData } = useAuth()
    const { toggleMobileProfileDrawer } = useGroupDashboard()
    const isMobile = useIsMobile()
+   const { anchorEl, handleClick, handleClose, open } = useMenuState()
+
+   const handleAvatarClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (isMobile) toggleMobileProfileDrawer()
+      else handleClick(event)
+   }
 
    return (
-      <AvatarButton
-         onClick={() => {
-            if (isMobile) {
-               toggleMobileProfileDrawer()
-            } else {
-               /**
-                * TODO: Open drop down menu
-                */
-            }
-         }}
-         aria-label="Open profile menu"
-      >
-         <Avatar
-            imageUrl={userData?.avatar}
-            lastName={userData?.lastName}
-            firstName={userData?.firstName}
-         />
-      </AvatarButton>
+      <Fragment>
+         <AvatarButton
+            onClick={handleAvatarClick}
+            aria-label="Open profile menu"
+         >
+            <Avatar
+               imageUrl={userData?.avatar}
+               lastName={userData?.lastName}
+               firstName={userData?.firstName}
+            />
+         </AvatarButton>
+
+         {/* Desktop Profile Menu */}
+         <ProfileMenu anchorEl={anchorEl} open={open} onClose={handleClose} />
+      </Fragment>
    )
 }
