91bd1e8f30c299706939c70b7883a9e8e0907083
matilderamosCF <155964282+matilderamosCF@users.noreply.github.com>
2025-08-11 23:14:31 +0100

    Search #3: Livestreams and Recordings tabs (#1699)
    ### Motivation
    
    Enhanced the search functionality to provide better filtering capabilities for livestreams and recordings.
    
    ### Changes
    
    - Implemented LivestreamsTab with search results and filtering
    - Implemented RecordingsTab with search results and filtering
    - Added filter components for livestreams and recordings (Language, Field of Study, Industry, Location, Company Size)
    - Implemented URL-based filter state management
    - Added livestream dialog support to search page
    
    ### Browser Testing
    
    - [ ] Chrome
    - [ ] Firefox
    - [ ] Safari
    
    ### Different Resolutions Testing
    
    - [ ] Large Screens
    - [ ] Medium Screens
    - [ ] Small Screens

---
+++ apps/web/components/views/search/results/filter/FilterFieldOfStudy.tsx
@@ -0,0 +1,44 @@
+import { useFieldsOfStudy } from "components/custom-hook/useCollection"
+import useIsMobile from "components/custom-hook/useIsMobile"
+import { ChipDropdown } from "components/views/common/ChipDropdown/ChipDropdown"
+import { useMemo } from "react"
+import { useSearchContext } from "../../SearchContext"
+
+const FilterFieldOfStudy = () => {
+   const { handleFilterSelect, getFilterValues } = useSearchContext()
+   const isMobile = useIsMobile()
+   const { data: fieldsOfStudy, isLoading } = useFieldsOfStudy()
+
+   const fieldOfStudyDropdownOptions = useMemo(() => {
+      if (isLoading) {
+         return []
+      }
+      return fieldsOfStudy.map((field) => ({
+         id: field.id,
+         value: field.name,
+      }))
+   }, [fieldsOfStudy, isLoading])
+
+   const selectedFieldsOfStudy = getFilterValues("fieldsOfStudy")
+
+   return (
+      <ChipDropdown
+         label="Field of Study"
+         options={fieldOfStudyDropdownOptions}
+         selection={{
+            selectedOptions: selectedFieldsOfStudy,
+            onChange: (fieldsOfStudy: string[]) =>
+               handleFilterSelect("fieldsOfStudy", fieldsOfStudy),
+            showApply: isMobile,
+         }}
+         ui={{
+            isDialog: isMobile,
+            popperSx: {
+               zIndex: 2,
+            },
+         }}
+      />
+   )
+}
+
+export default FilterFieldOfStudy
