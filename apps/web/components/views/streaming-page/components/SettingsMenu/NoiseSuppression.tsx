74195a4fd6179323e5b755dd5d02d952ad2aa172
habibkadiri <kadirit@hotmail.com>
2025-09-22 15:35:55 +0200

    Comment out Noise Suppression feature in SettingsMenu component
    ### Changes
    - Commented out the entire NoiseSuppression component, including its state management and UI elements.
    
    This update removes the noise suppression functionality from the SettingsMenu, simplifying the audio settings interface.

--- apps/web/components/views/streaming-page/components/SettingsMenu/NoiseSuppression.tsx
+++ apps/web/components/views/streaming-page/components/SettingsMenu/NoiseSuppression.tsx
@@ -1,36 +1,33 @@
-import { Stack, Switch, Typography } from "@mui/material"
-import { useLocalTracks } from "../../context/LocalTracks"
+// import { Stack, Switch, Typography } from "@mui/material"
 
 export const NoiseSuppression = () => {
-   const {
-      noiseSuppressionEnabled,
-      toggleNoiseSuppression,
-      isNoiseSuppressionSupported,
-   } = useLocalTracks()
-
-   if (!isNoiseSuppressionSupported) return null
-
-   return (
-      <Stack
-         direction="row"
-         alignItems="center"
-         pt={1}
-         justifyContent="space-between"
-      >
-         <span>
-            <Typography variant="small" color="neutral.800">
-               Noise suppression
-            </Typography>
-            <Typography variant="xsmall" component="p" color="neutral.400">
-               Reduces background noise to improve audio clarity during
-               streaming
-            </Typography>
-         </span>
-         <Switch
-            checked={noiseSuppressionEnabled}
-            onChange={toggleNoiseSuppression}
-            color="primary"
-         />
-      </Stack>
-   )
+   // const {
+   //    noiseSuppressionEnabled,
+   //    toggleNoiseSuppression,
+   //    isNoiseSuppressionSupported,
+   // } = useLocalTracks()
+   // if (!isNoiseSuppressionSupported) return null
+   // return (
+   //    <Stack
+   //       direction="row"
+   //       alignItems="center"
+   //       pt={1}
+   //       justifyContent="space-between"
+   //    >
+   //       <span>
+   //          <Typography variant="small" color="neutral.800">
+   //             Noise suppression
+   //          </Typography>
+   //          <Typography variant="xsmall" component="p" color="neutral.400">
+   //             Reduces background noise to improve audio clarity during
+   //             streaming
+   //          </Typography>
+   //       </span>
+   //       <Switch
+   //          checked={noiseSuppressionEnabled}
+   //          onChange={toggleNoiseSuppression}
+   //          color="primary"
+   //       />
+   //    </Stack>
+   // )
 }
