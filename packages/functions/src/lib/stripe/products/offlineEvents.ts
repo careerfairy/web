b0eeecf0bec163f18c218a989a28c2ddf6e83562
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-10-16 13:58:49 +0200

    notify increase failed

--- packages/functions/src/lib/stripe/products/offlineEvents.ts
+++ packages/functions/src/lib/stripe/products/offlineEvents.ts
@@ -1,3 +1,4 @@
+import { Group } from "@careerfairy/shared-lib/groups"
 import {
    OfflineEventFetchStripeCustomerSession,
    OfflineEventSessionMetadata,
@@ -5,8 +6,12 @@ import {
 } from "@careerfairy/shared-lib/stripe/types"
 import { Stripe } from "stripe"
 import { groupRepo, stripeRepo } from "../../../api/repositories"
-import { notifyOfflineEventPurchased } from "../../../api/slack"
+import {
+   notifyOfflineEventIncreaseFailed,
+   notifyOfflineEventPurchased,
+} from "../../../api/slack"
 import config from "../../../config"
+import { logAndThrow } from "../../../lib/validations"
 import functions = require("firebase-functions")
 
 export async function handleOfflineEventSession(
@@ -33,50 +38,92 @@ export async function handleOfflineEventSession(
 export async function handleOfflineEventCheckoutSessionCompleted(
    event: Stripe.CheckoutSessionCompletedEvent
 ): Promise<void> {
-   try {
-      const sessionId = event.data.object.id
+   // Being defensive about the event object (but should never happen), but if it does
+   // it will crash in the try/catch block and notifity Slack of the error.
+   const sessionId = event?.data?.object?.id
+   // Being defensive about the metadata object (but should never happen) as this is called
+   // after checking the metadata type and that it also contains the groupId
+   const metadata = event?.data?.object
+      ?.metadata as unknown as OfflineEventSessionMetadata
 
-      // For a session completed event, there must always be a quantity, at least 1.
-      const totalQuantityBought =
+   let totalQuantityBought: number
+   let group: Group
+
+   try {
+      /**
+       * If any of these operations fail, a Slack notification must be, as
+       * the group will not have the correct number of available offline events when any of
+       * these operations fail.
+       */
+
+      totalQuantityBought =
          await stripeRepo.getTotalQuantityFromCheckoutSessionById(sessionId)
 
-      const metadata = event.data.object
-         .metadata as unknown as OfflineEventSessionMetadata
+      group = await groupRepo.getGroupById(metadata.groupId)
 
-      await Promise.all([
-         groupRepo.increaseAvailableOfflineEvents(
-            metadata.groupId,
-            totalQuantityBought
-         ),
-         // Get the group information for the Slack notification
-         groupRepo.getGroupById(metadata.groupId).then((group) => {
-            return notifyOfflineEventPurchased(
-               config.slackWebhooks.offlineEventPurchased,
-               {
-                  groupName: group.universityName ?? metadata.groupId,
-                  groupLogoUrl: group.logoUrl,
-                  groupId: metadata.groupId,
-                  quantityPurchased: totalQuantityBought,
-                  customerEmail: metadata.userEmail,
-                  customerName: metadata.userName,
-               }
-            ).catch((slackError) => {
-               functions.logger.error(
-                  "❌ Error sending Slack notification for offline event purchase:",
-                  slackError
-               )
-            })
-         }),
-      ])
+      await groupRepo.increaseAvailableOfflineEvents(
+         metadata.groupId,
+         totalQuantityBought
+      )
 
       functions.logger.info(
          `📦 Added ${totalQuantityBought} (bought) offline events to group ${metadata.groupId}`
       )
+
+      await notifyOfflineEventPurchased(
+         config.slackWebhooks.offlineEventPurchased,
+         {
+            groupName: group.universityName,
+            groupLogoUrl: group.logoUrl,
+            groupId: metadata.groupId,
+            quantityPurchased: totalQuantityBought,
+            previousCredits: group.availableOfflineEvents,
+            updatedCredits:
+               (group.availableOfflineEvents || 0) + totalQuantityBought,
+            customerEmail: metadata.userEmail,
+            customerName: metadata.userName,
+         }
+      )
+
+      functions.logger.info(
+         `✅ Successfully processed offline event checkout session completed event for Customer: ${metadata.groupId}, Quantity: ${totalQuantityBought}`
+      )
    } catch (error) {
-      functions.logger.error(
-         "❌ Error handling Stripe Offline Event checkout 'checkout.session.completed' event:",
+      await notifyOfflineEventIncreaseFailed(
+         config.slackWebhooks.offlineEventIncreaseFailed,
+         {
+            groupName: group?.universityName || "Could not determine group",
+            groupLogoUrl: group?.logoUrl || "unknown-group-logo-url",
+            groupId: metadata?.groupId || "unknown-group-id",
+            currentCredits: `${
+               group?.availableOfflineEvents !== undefined
+                  ? group.availableOfflineEvents
+                  : "Could not determine current credits"
+            }`,
+            expectedCredits: `${
+               group?.availableOfflineEvents ||
+               "Could not determine current credits"
+            } + ${
+               totalQuantityBought !== undefined
+                  ? totalQuantityBought
+                  : "Could not determine quantity bought"
+            }`,
+            quantityToIncrease: `${
+               totalQuantityBought !== undefined
+                  ? totalQuantityBought
+                  : "Could not determine quantity bought"
+            }`,
+            customerEmail:
+               metadata?.userEmail ||
+               "Could not determine customer email in metadata",
+            customerName:
+               metadata?.userName ||
+               "Could not determine customer name in metadata",
+         }
+      )
+      logAndThrow(
+         "Error handling Stripe Offline Event checkout 'checkout.session.completed' event",
          error
       )
-      throw error
    }
 }
