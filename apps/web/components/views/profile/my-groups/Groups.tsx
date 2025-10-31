c7ebd0bb96cdcca9ab000c3a3b869ac1698a0f99
habibkadiri <kadirit@hotmail.com>
2025-10-31 10:43:52 +0100

    feat: add `autoFocus` property to the input field for improved user experience.

--- apps/web/components/views/profile/my-groups/Groups.tsx
+++ apps/web/components/views/profile/my-groups/Groups.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useMemo, useState } from "react"
+import { Group } from "@careerfairy/shared-lib/dist/groups"
+import { UserAdminGroup } from "@careerfairy/shared-lib/dist/users"
 import {
    Box,
    Button,
@@ -11,22 +12,21 @@ import {
    TextField,
    Typography,
 } from "@mui/material"
-import { useRouter } from "next/router"
-import CurrentGroup from "components/views/profile/CurrentGroup"
-import makeStyles from "@mui/styles/makeStyles"
-import useInfiniteScrollClientWithHandlers from "../../../custom-hook/useInfiniteScrollClientWithHandlers"
-import ContentCard from "../../../../layouts/UserLayout/ContentCard"
-import { useAuth } from "../../../../HOCs/AuthProvider"
-import Link from "../../common/Link"
-import ContentCardTitle from "../../../../layouts/UserLayout/ContentCardTitle"
-import { groupRepo } from "../../../../data/RepositoryInstances"
 import Autocomplete from "@mui/material/Autocomplete"
+import makeStyles from "@mui/styles/makeStyles"
 import match from "autosuggest-highlight/match"
 import parse from "autosuggest-highlight/parse"
-import { Group } from "@careerfairy/shared-lib/dist/groups"
-import useSnackbarNotifications from "../../../custom-hook/useSnackbarNotifications"
-import { UserAdminGroup } from "@careerfairy/shared-lib/dist/users"
+import CurrentGroup from "components/views/profile/CurrentGroup"
+import { useRouter } from "next/router"
+import React, { useEffect, useMemo, useState } from "react"
 import { Search as FindIcon } from "react-feather"
+import { groupRepo } from "../../../../data/RepositoryInstances"
+import { useAuth } from "../../../../HOCs/AuthProvider"
+import ContentCard from "../../../../layouts/UserLayout/ContentCard"
+import ContentCardTitle from "../../../../layouts/UserLayout/ContentCardTitle"
+import useInfiniteScrollClientWithHandlers from "../../../custom-hook/useInfiniteScrollClientWithHandlers"
+import useSnackbarNotifications from "../../../custom-hook/useSnackbarNotifications"
+import Link from "../../common/Link"
 
 const useStyles = makeStyles((theme) => ({
    header: {
@@ -153,6 +153,7 @@ const Groups = ({ isOnDialog = false, containerRef }: Props) => {
 
    useEffect(() => {
       resetInput()
+      // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [value])
 
    useEffect(() => {
@@ -297,6 +298,7 @@ export const Highlights = ({
                   margin="normal"
                   InputProps={{
                      ...params.InputProps,
+                     autoFocus: true,
                      startAdornment: (
                         <InputAdornment position="start">
                            <FindIcon color={"black"} />
