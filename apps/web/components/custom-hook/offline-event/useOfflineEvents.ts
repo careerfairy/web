86500f595ab3c1b7486d0654e6de1732e1308d4f
habibkadiri <kadirit@hotmail.com>
2025-11-04 13:26:11 +0100

    refactor(useOfflineEvents): update to use OfflineEventWithDistance for enhanced event data
    - Changed the type from OfflineEvent[] to OfflineEventWithDistance[] in the useOfflineEvents hook to incorporate distance information for offline events.
    - This update aligns with recent enhancements to provide users with proximity data for offline events.

--- apps/web/components/custom-hook/offline-event/useOfflineEvents.ts
+++ apps/web/components/custom-hook/offline-event/useOfflineEvents.ts
@@ -1,10 +1,10 @@
-import { OfflineEvent } from "@careerfairy/shared-lib/offline-events/offline-events"
+import { OfflineEventWithDistance } from "@careerfairy/shared-lib/offline-events/offline-events"
 import { offlineEventService } from "data/firebase/OfflineEventService"
 import useSWR from "swr"
 import { reducedRemoteCallsOptions } from "../utils/useFunctionsSWRFetcher"
 
 export const useOfflineEvents = () => {
-   return useSWR<OfflineEvent[]>(
+   return useSWR<OfflineEventWithDistance[]>(
       ["offline-events"],
       async () => {
          return offlineEventService.getOfflineEvents()
