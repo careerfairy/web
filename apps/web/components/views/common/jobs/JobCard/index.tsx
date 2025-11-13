b7ef5f50fd05dd42e6c4c08bacbcccb66f512f29
habibkadiri <kadirit@hotmail.com>
2025-11-13 16:09:08 +0100

    Update JobCard styles to adjust width for improved layout consistency

--- apps/web/components/views/common/jobs/JobCard/index.tsx
+++ apps/web/components/views/common/jobs/JobCard/index.tsx
@@ -27,7 +27,7 @@ const styles = sxStyles({
    jobState: {
       display: "flex",
       borderRadius: "8px 0 0 8px",
-      width: "8px",
+      width: "4px",
    },
    listItemContainer: {
       display: "flex",
