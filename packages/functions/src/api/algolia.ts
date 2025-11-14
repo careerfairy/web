bce9e9a5cb75b55f199819501b990c5912479ba6
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-11-14 12:10:09 +0100

    update ci settings

--- packages/functions/src/api/algolia.ts
+++ packages/functions/src/api/algolia.ts
@@ -1,18 +1,18 @@
 import algoliasearch, { SearchClient } from "algoliasearch"
 import { logger } from "firebase-functions/v2"
-import { isLocalEnvironment } from "../util"
+import { isLocalEnvironment, isTestEnvironment } from "../util"
 
 export const getAlgoliaClient = (): SearchClient => {
    let algoliaAppId = process.env.ALGOLIA_APP_ID
    let algoliaApiKey = process.env.ALGOLIA_API_KEY
 
-   if (isLocalEnvironment()) {
+   if (isLocalEnvironment() && !isTestEnvironment()) {
       algoliaAppId = process.env.DEV_ALGOLIA_APP_ID || algoliaAppId
       algoliaApiKey = process.env.DEV_ALGOLIA_API_KEY || algoliaApiKey
       logger.info(
          `Using Algolia in DEV mode (${process.env.DEV_NAME}), please ensure you have the correct credentials for your OWN Algolia application`
       )
-   } else {
+   } else if (isTestEnvironment()) {
       logger.info(
          `Using Algolia in PROD mode, last api key ends in: ${algoliaApiKey?.slice(
             -10
