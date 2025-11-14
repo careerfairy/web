5a5c93d22cd32e541da678dde961ca46f0956af2
wgoncalvescf <walter.goncalves@careerfairy.io>
2025-11-14 11:09:45 +0100

    update ci settings

--- packages/functions/src/api/algolia.ts
+++ packages/functions/src/api/algolia.ts
@@ -1,18 +1,22 @@
 import algoliasearch, { SearchClient } from "algoliasearch"
 import { logger } from "firebase-functions/v2"
-import { isLocalEnvironment, isTestEnvironment } from "../util"
+import { isLocalEnvironment } from "../util"
 
 export const getAlgoliaClient = (): SearchClient => {
-   let algoliaAppId = process.env.ALGOLIA_APP_ID || "WBAREE5TWQ"
-   let algoliaApiKey =
-      process.env.ALGOLIA_API_KEY || "993b607c53d0965f6d93f10daea25a68"
+   let algoliaAppId = process.env.ALGOLIA_APP_ID
+   let algoliaApiKey = process.env.ALGOLIA_API_KEY
 
-   if (isLocalEnvironment() && !isTestEnvironment()) {
-      algoliaAppId = process.env.DEV_ALGOLIA_APP_ID
-      algoliaApiKey = process.env.DEV_ALGOLIA_API_KEY
-
+   if (isLocalEnvironment()) {
+      algoliaAppId = process.env.DEV_ALGOLIA_APP_ID || algoliaAppId
+      algoliaApiKey = process.env.DEV_ALGOLIA_API_KEY || algoliaApiKey
       logger.info(
          `Using Algolia in DEV mode (${process.env.DEV_NAME}), please ensure you have the correct credentials for your OWN Algolia application`
+      )
+   } else {
+      logger.info(
+         `Using Algolia in PROD mode, last api key ends in: ${algoliaApiKey?.slice(
+            -10
+         )}`
       )
    }
 
